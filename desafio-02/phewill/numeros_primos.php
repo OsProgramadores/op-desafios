a73ded1031146a228fe590e2bdec686dea1ba02f
phewill <phewill22@hotmail.com>
2017-07-12 02:05:17 -0300

    Add coments

--- desafio-02/phewill/numeros_primos.php
+++ desafio-02/phewill/numeros_primos.php
@@ -1,11 +1,9 @@
 <?php
 //Autor - William Garcia
 //Executável - https://repl.it/JUZS/6
-
 $limite = 1000;
 $numDePrimos=0;
 $cont = 0;
-
 echo "---------NÚMEROS PRIMOS------------\n";
 for($n = 2;$n <=$limite;$n++){
   for($div = 1; $div <=$n; $div++){
