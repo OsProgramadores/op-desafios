6a0458dc88cfaff2994224513abd84337680a9ec
William da rosa garcia <phewill22@hotmail.com>
2017-07-12 02:12:17 -0300

    Adicionadas soluçoes dos desafios 1-3 (phewill)

---
+++ desafio-03/phewill/palindromos.php
@@ -0,0 +1,22 @@
+<?php
+//Autor - William Garcia 
+//Executável - https://repl.it/JVaf/latest
+$contP = 0;
+$min = 3500;
+$max = 3749;
+
+echo "------Números Palindromos------\n";
+
+for($min; $min <=$max;$min++){
+  $inversao =strrev($min);
+  if($inversao == $min){
+    echo "{$min} é Palindromo\n";
+    $contP++;
+  }
+}
+echo "-------------------------------\n";
+echo "Existem {$contP} números Palindromos entre {$min} e {$max}.\n";
+?>
+
+
+
