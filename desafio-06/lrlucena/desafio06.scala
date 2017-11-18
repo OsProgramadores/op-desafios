4a03b8026cdd7c948a8f7ae67b631b586fff9cca
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-11-18 19:06:10 -0200

    Simplificando a solução do Desafio 06
    A solução fica mais simples usando o método diff

--- desafio-06/lrlucena/desafio06.scala
+++ desafio-06/lrlucena/desafio06.scala
@@ -1,37 +1,21 @@
 object Desafio6 extends App {
 
-  implicit class Texto(s: String) {
-    def clean: String = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
-
-    def bag: Map[Char, Int] =
-      s.groupBy(c => c).map { case (a, b) => (a, b.length) } withDefaultValue 0
-
-    def subtr(b: String): Map[Char, Int] = {
-      val (sb, bb) = (s.bag, b.bag)
-      sb ++ (bb map { case (c, i) => c -> (sb(c) - i) })
-    }
-
-    def holds(b: String): Boolean = s.subtr(b).values.forall(_ >= 0)
-
-    def -(b: String): String = if (s holds b)
-      s.subtr(b).map { case (c, i) => c.toString * i }.mkString
-    else ""
-  }
+  def clean(s: String) = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
 
   def anagrams(input: String, words: List[String], list: List[String] = Nil): List[String] = {
     var wrds = words
     if (input.nonEmpty)
       words.flatMap { word =>
-        wrds = wrds.dropWhile(_ <= word)
-        val w = input - word
-        anagrams(w, wrds.filter(w.holds), word :: list)
+        wrds = wrds.tail
+        val w = input diff word
+        anagrams(w, wrds.filter(_.diff(w)==""), word :: list)
       }
     else List(list.reverse.mkString(" "))
   }
 
-  val input = args.headOption.getOrElse("oi gente").clean
+  val input = clean(args.headOption.getOrElse("oi gente"))
 
-  val words = io.Source.fromFile("words.txt").getLines.toList.filter(input.holds).sorted
+  val words = io.Source.fromFile("words.txt").getLines.toList.filter(_.diff(input) == "").sorted
   //.fromURL("https://osprogramadores.com/desafios/d06/words.txt")
 
   val a = anagrams(input, words).mkString("\n")
