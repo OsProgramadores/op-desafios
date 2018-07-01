4266d9708fa212da5d8eee45f3f0d31423df26e5
Marco Antonio Rocha <marco.rocha@matera.com>
2018-07-01 13:30:45 -0300

    Classe inicial com parametrização e readme

---
+++ desafio-03/mrcrch/PalindromeNumbers.java
@@ -0,0 +1,53 @@
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
+
+public class PalindromeNumbers {
+
+    public static void main(final String[] args) {
+
+        long min = 0;
+        long max = 1000;
+
+        if (args.length > 0) {
+
+            if (args.length != 2) {
+                throw new IllegalArgumentException(
+                    "Quantidade de parâmetros inválida. São esperados 2 parâmetro (min/max)");
+            }
+
+            final String paramMin = args[0];
+            final String paramMax = args[1];
+
+            try {
+                min = Long.valueOf(paramMin);
+            } catch (final NumberFormatException e) {
+                throw new IllegalArgumentException("Parâmetro mínimo não é um número válido: " + paramMin);
+            }
+
+            try {
+                max = Long.valueOf(paramMax);
+            } catch (final NumberFormatException e) {
+                throw new IllegalArgumentException("Parâmetro máximo não é um número válido: " + paramMax);
+            }
+
+        }
+
+        if (min < 0) {
+            throw new IllegalArgumentException("Valor mínimo não pode ser negativo: " + min);
+        }
+
+        if (max < 0) {
+            throw new IllegalArgumentException("Valor máximo não pode ser negativo: " + max);
+        }
+
+        if (max <= min) {
+            throw new IllegalArgumentException("Valor máximo deve ser maior que o valor mínimo: " + min + "/" + max);
+        }
+
+        // TODO: Run
+
+    }
+
+}
---
+++ desafio-03/mrcrch/README.md
@@ -0,0 +1,21 @@
+# Requisitos
+
+- Java 8 ou superior
+
+# Compilação
+
+```
+javac PalindromeNumbers.java
+```
+
+# Execução
+
+- Com intervalo padrão (0 a 1000)
+```
+java PalindromeNumbers
+```
+
+- Com intervalo definido (15 a 70)
+```
+java PalindromeNumbers 15 70
+```
