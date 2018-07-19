f10f552cf4370e91630f769e856b8d7c1113ec21
Leo Silva Souza <leovano@protonmail.com>
2018-07-18 22:34:03 -0300

    Desafio 2 (leovano) (#40)

---
+++ desafio-02/leovano/main.rs
@@ -0,0 +1,22 @@
+// Autor: Leo Silva Souza
+// Requer Nightly.
+
+#![feature(iterator_step_by)]
+
+const MAX: usize = 10_000;
+
+fn main() {
+    let mut list = [false; MAX + 1];
+
+    print!("2");
+    for idx in (3..=MAX).step_by(2) {
+        if !list[idx] {
+            print!(", {}", idx);
+            (idx..=MAX)
+                .step_by(idx)
+                .skip(1)
+                .for_each(|i| list[i] = true);
+        }
+    }
+    println!(".");
+}
