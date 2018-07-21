e47e76cc8e50999992a5ee95ee27d78107c0d9cc
Choco02 <wesleyassis455@gmail.com>
2018-07-21 16:07:16 -0300

    Desafio 02

---
+++ desafio-02/choco02/Prime-numbers.py
@@ -0,0 +1,13 @@
+from time import time
+
+for a in range(1, 10000):
+  if a == 1:
+    continue
+  elif (a == 2) or (a == 3) or (a == 5) or (a == 7):
+    print(a)
+  else:
+    if (a % 2 == 0) or (a % 3 == 0) or (a % 5 == 0) or (a % 7 == 0):
+      continue
+    else:
+      print(a)
+#by /Choco02
