b5c55101b3486c81d670744987464428bb976a47
Choco02 <wesleyassis455@gmail.com>
2018-07-24 18:27:51 -0300

    finished

--- desafio-02/choco02/Prime-numbers.py
+++ desafio-02/choco02/Prime-numbers.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
-from time import time
+test = []
+prime = []
 
-for a in range(1, 10000):
-  if a == 1:
-    continue
-  elif (a == 2) or (a == 3) or (a == 5) or (a == 7):
-    print(a)
-  else:
-    if (a % 2 == 0) or (a % 3 == 0) or (a % 5 == 0) or (a % 7 == 0):
-      continue
-    else:
-      print(a)
-#by /Choco02
+for a in range(2, 10000):
+      if (a == 2) or (a == 3) or (a == 5) or (a == 7):
+            prime.append(a)
+      else:
+            if (a % 2 == 0) or (a % 3 == 0) or (a % 5 == 0) or (a % 7 == 0):
+                  continue
+            else:
+                  prime.append(a)
+                  if (a <= 100):
+                        test.append(a)
+                        #print(a)
+''' print(test)
+print(prime) '''
+
+for z in test:
+      for x in prime:
+            if (x % z == 0):
+                  prime.remove(x)
+print(prime)
+#by Choco02
