b82754d9180d57794201608b1dd860e520b54a15
Choco02 <wesleyassis455@gmail.com>
2018-07-25 10:19:19 -0300

    Adicionado comentarios

--- desafio-02/choco02/Prime-numbers.py
+++ desafio-02/choco02/Prime-numbers.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-test = []
+test = [2, 3, 5, 7]
 prime = []
 
 for a in range(2, 10000):
@@ -9,16 +9,19 @@ for a in range(2, 10000):
             if (a % 2 == 0) or (a % 3 == 0) or (a % 5 == 0) or (a % 7 == 0):
                   continue
             else:
-                  prime.append(a)
-                  if (a <= 100):
+                  prime.append(a) #Numeros nao divisiveis por 2, 3, 5 e 7 serao add a um array prime para uma verificacao final
+                  if (a <= 100): #Numeros primos anteriores a raiz do numero alvo serao add a um array de test para encontrar primos restantes
                         test.append(a)
-                        #print(a)
-''' print(test)
-print(prime) '''
+print("Numeros adicionados em test \n")
+print(test)
+print("################################################")
+print("\n")
 
 for z in test:
       for x in prime:
             if (x % z == 0):
                   prime.remove(x)
-print(prime)
-#by Choco02
+#Nesse bloco todos os números nao divisiveis por 2, 3, 5 e 7 serao divididos por cada elemento no array test, se divisivel sera removido do array prime
+print(test + prime)
+	#by Choco02
+	#Utilizado o Crivo de Eratostenes
