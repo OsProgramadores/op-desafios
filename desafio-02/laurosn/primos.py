2c020f6af788e0f839903349b079763bfff9f18a
Lauro Silveira <lauro.neto@camara.gov.br>
2018-08-15 12:39:02 -0300

    Proposta de solução para o desafio 2 - Participante Lauro Silveira Neto

---
+++ desafio-02/laurosn/primos.py
@@ -0,0 +1,17 @@
+import time
+def erastotenes(n):
+    # cria uma lista onde ficarao os multiplos do numero primo
+    multiples = []
+    # iteracao a partir do 2 (primeiro primo) ate o limite definido como parametro
+    for i in range(2, n+1):
+        # se o numero nao esta na lista de multiplos do numero primo, ele e primo
+        if i not in multiples:
+            # imprime os numeros primos
+            print (i)
+            # adiciona os multiplos na
+            for j in range(i*i, n+1, i):
+                multiples.append(j)
+start_time = time.time()
+erastotenes(10000)
+print(" Tempo de execucao --- %s segundos ---" % (time.time() - start_time))
+
