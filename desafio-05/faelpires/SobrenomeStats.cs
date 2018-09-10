0508b8343240d24800510f8874701d80338bad7f
Rafael Pires de Freitas <faelpires@gmail.com>
2018-09-10 02:30:24 -0300

    Soluçao desafio 5 - Rafael Pires de Freitas

---
+++ desafio-05/faelpires/SobrenomeStats.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Desafio5
+{
+    public class SobrenomeStats
+    {
+        public byte[] Sobrenome { get; set; }
+        public double MaxSalario { get; set; }
+        public int TotalFuncionarios { get; set; }
+        public List<byte[]> Nomes { get; set; }
+    }
+}
