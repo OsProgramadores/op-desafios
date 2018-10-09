74ad81c49fec385fa23ac629082d901352b7d3b8
Rafael De Souza Santos <RafaelDSS199916@gmail.com>
2018-10-09 19:02:38 -0300

    Corrigido o desafio5 com saída incorreta (#74)

--- desafio-05/rafaeldss/desafio5.py
+++ desafio-05/rafaeldss/desafio5.py
@@ -1,11 +1,13 @@
 """Solução para o desafio 05 - RafaelDSS."""
 
-import json
 import sys
+import json
 
 FILE_NAME = sys.argv[1]
 
+
 def processar(file_name):
+
     """Processa arquivo JSON e gera saida em formato padronizado."""
 
     with open(file_name, 'r', encoding="utf8") as file:
@@ -19,8 +21,8 @@ def processar(file_name):
     global_avg = {0: 0, 1: 0}
 
     # questão 2
-    area_max = []
-    area_min = []
+    area_max = {}
+    area_min = {}
 
     # questão 3
     least_employees = {0: menor, 1: []}
@@ -30,7 +32,7 @@ def processar(file_name):
     last_name_max = {}
     last_name = {}
 
-    areas = {area['codigo']: [area['nome'], 9999999, 0, 0, 0] for area in dados['areas']}
+    areas = {area['codigo']: [area['nome'], menor, 0, 0, 0] for area in dados['areas']}
 
     for funcionario in dados['funcionarios']:
         salario = funcionario['salario']
@@ -61,18 +63,18 @@ def processar(file_name):
         # Menores salários - áreas
         if salario < areas[codigo_area][1]:
             areas[codigo_area][1] = salario
-            area_min = [[areas[codigo_area][0], nome, sobrenome, salario]]
+            area_min[areas[codigo_area][0]] = [[nome, sobrenome, salario]]
 
         elif salario == areas[codigo_area][1]:
-            area_min += [[areas[codigo_area][0], nome, sobrenome, salario]]
+            area_min[areas[codigo_area][0]] += [[nome, sobrenome, salario]]
 
         # Maiores salários - áreas
         if salario > areas[codigo_area][2]:
             areas[codigo_area][2] = salario
-            area_max = [[areas[codigo_area][0], nome, sobrenome, salario]]
+            area_max[areas[codigo_area][0]] = [[nome, sobrenome, salario]]
 
         elif salario == areas[codigo_area][2]:
-            area_max += [[areas[codigo_area][0], nome, sobrenome, salario]]
+            area_max[areas[codigo_area][0]] += [[nome, sobrenome, salario]]
 
         # Cada area vai receber seus respectivos salários
         areas[codigo_area][3] += salario
@@ -82,24 +84,23 @@ def processar(file_name):
 
         # Maiores salários para funcionários com o mesmo sobrenome
         if sobrenome in last_name_max:
-            if salario > last_name_max[sobrenome][1]:
-                qt_sn = last_name_max[sobrenome][1]
-                last_name_max[sobrenome] = [salario, qt_sn + 1]
+            if salario > last_name_max[sobrenome]:
+                last_name_max[sobrenome] = salario
                 last_name[sobrenome] = [[nome, salario]]
 
-            elif salario == last_name_max[sobrenome][1]:
-                qt_sn = last_name_max[sobrenome][1]
-                last_name_max[sobrenome] = [salario, qt_sn + 1]
+            elif salario == last_name_max[sobrenome]:
+                last_name_max[sobrenome] = salario
                 last_name[sobrenome] += [[nome, salario]]
 
         else:
-            last_name_max[sobrenome] = [salario, 1]
+            last_name_max[sobrenome] = salario
+            last_name[sobrenome] = [[nome, salario]]
 
     # Número de funcionários
     for area in areas.values():
 
         # Menor número
-        if area[4] < least_employees[0]:
+        if area[4] < least_employees[0] and area[4] != 0:
             least_employees[0] = area[4]
             least_employees[1] = [[area[0], area[4]]]
 
@@ -116,43 +117,47 @@ def processar(file_name):
 
     saida = ''
 
-    # Questao 1
+    # Questão 1
+
+    saida += f'global_avg|{global_avg[0] / global_avg[1]:.2f}\n'
+
     for func in global_max[1]:
-        saida += '\nglobal_max|{} {}|{:.2f}'.format(*func)
+        saida += 'global_max|{} {}|{:.2f}\n'.format(*func)
 
     for func in global_min[1]:
-        saida += '\nglobal_min|{} {}|{:.2f}'.format(*func)
+        saida += 'global_min|{} {}|{:.2f}\n'.format(*func)
 
-    saida += f'\nglobal_avg|{global_avg[0] / global_avg[1]:.2f}'
+    # Questão 2
 
-    # Questao 2
+    for key_max in area_max:
+        for func_max in area_max[key_max]:
+            if func_max[2] > 0:
+                saida += f'area_max|{key_max}|{func_max[0]} {func_max[1]}|{func_max[2]:.2f}\n'
 
-    for area_m in area_max:
-        saida += '\narea_max|{}|{} {}|{:.2f}'.format(*area_m)
-
-    for area_m in area_min:
-        saida += '\narea_min|{}|{} {}|{:.2f}'.format(*area_m)
+    for key_min in area_min:
+        for func_min in area_min[key_min]:
+            if func_min[2] > 0:
+                saida += f'area_min|{key_min}|{func_min[0]} {func_min[1]}|{func_min[2]:.2f}\n'
 
     for area in areas.values():
         if area[4] > 1:
-            saida += f'\narea_avg|{area[0]}|{area[3] / area[4]:.2f}'
+            saida += f'area_avg|{area[0]}|{area[3] / area[4]:.2f}\n'
 
-    # Questao 3
+    # Questão 3
 
     for most in most_employees[1]:
-        saida += '\nmost_employees|{}|{}'.format(*most)
+        saida += 'most_employees|{}|{}\n'.format(*most)
 
     for least in least_employees[1]:
-        saida += '\nleast_employees|{}|{}'.format(*least)
+        saida += 'least_employees|{}|{}\n'.format(*least)
 
-    # Questao 4
+    # Questão 4
 
-    for valor, key in zip(last_name_max.values(), last_name_max.keys()):
-        if valor[1] > 1:
-            for name in last_name[key]:
-                saida += f'\nlast_name_max|{key}|{name[0]} {key}|{name[1]}'
+    for key in last_name_max:
+        for name in last_name[key]:
+            saida += f'last_name_max|{key}|{name[0]} {key}|{name[1]:.2f}\n'
 
-    print(saida)
+    print(saida, end='')
 
 
 processar(FILE_NAME)
