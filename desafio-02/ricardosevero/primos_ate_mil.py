614554146b4599d95e366a276e6cdbd241da97d9
Ricardo Severo <ricardoseverodev@gmail.com>
2018-11-02 16:52:30 -0300

    Solução e correções do desafio 2

--- desafio-02/ricardosevero/primos_ate_mil.py
+++ desafio-02/ricardosevero/primos_ate_mil.py
@@ -1,17 +1,32 @@
-def is_prime(n):
+"""
+Escreva um programa para listar todos os números primos entre 1 e 10000.
+"""
+def is_prime(num):
+    """
+    Verifica se o parâmetro é um número primo ou não.
+    :param num: Recebe um número
+    :return:  Retorna o Booleano da função
+    """
     div = 2
-    if n == 2:
+    if num == 2:
         return True
-    
-    while n % div != 0 and div <= n/2:
+    while num % div != 0 and div <= num/2:
         div += 1
-    if n % div == 0:
+    if num % div == 0:
         return False
     return True
-    
-start = 2
-stop = 1000
-while start < stop:
-    if is_prime(start):
-        print(start, end=', ')
-    start += 1
+
+def main():
+    """
+    Função principal
+    :return: Main
+    """
+    start = 2
+    stop = 1000
+    while start < stop:
+        if is_prime(start):
+            print(start, end=', ')
+        start += 1
+
+if __name__ == "__main__":
+    main()
