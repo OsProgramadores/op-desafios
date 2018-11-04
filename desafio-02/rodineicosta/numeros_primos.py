c9bd51dcc9ec25a02175b08962a251b254f08f33
Rodinei Costa <rodineicosta@physistec.com.br>
2018-11-04 09:52:50 -0300

    Correção de exercício para Desafio #2

--- desafio-02/rodineicosta/numeros_primos.py
+++ desafio-02/rodineicosta/numeros_primos.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
+'''
+Escreva um programa para listar todos os números primos entre 1 e 10000.
+'''
+
 from math import sqrt
 
 limite, primos = 10000, []
 
 def ehPrimo(numero):
+    '''
+    Verificação de primalidade
+    '''
     if  numero < 2:
-        return False 
+        return False
     for i in range(2, int(sqrt(numero)) + 1):
         if numero % i == 0:
             return False
