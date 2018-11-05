1a2bb4fdc123ffc1f2780aa5219bc1cf38340947
Rodinei Costa <rodineicosta@physistec.com.br>
2018-11-05 12:24:30 -0300

    Correção de solução para o Desafio #2

--- desafio-02/rodineicosta/numeros_primos.py
+++ desafio-02/rodineicosta/numeros_primos.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 '''
-Escreva um programa para listar todos os números np_primos entre 1 e 10000.
+Programa para listar todos os números primos entre 1 e 10000.
 '''
 
 from math import sqrt
 
-num_primos = []
+def main():
+    '''
+    Array de números primos
+    '''
+    num_primos = []
 
-def np_primo(numero):
-    '''
-    Verificação de primalidade
-    '''
-    if  numero < 2:
-        return False
-    for i in range(2, int(sqrt(numero)) + 1):
-        if numero % i == 0:
+    def np_primo(numero):
+        '''
+        Verificação de primalidade
+        '''
+        if  numero < 2:
             return False
-    return True
+        for i in range(2, int(sqrt(numero)) + 1):
+            if numero % i == 0:
+                return False
+        return True
 
-for num in range(2, 10000 + 1):
-    if np_primo(num):
-        num_primos.append(num)
+    for num in range(2, 10000 + 1):
+        if np_primo(num):
+            num_primos.append(num)
 
-print(num_primos)
+    print(num_primos)
+
+if __name__ == "__main__":
+    main()
