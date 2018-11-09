94ea29392102389df7dd0cc475b30027f4eb7099
Anthony Louis <anthony.physis@gmail.com>
2018-11-09 02:26:43 -0200

    aprimorando o programa

--- desafio-07/anthonyfisicabsb/Desafio7.java
+++ desafio-07/anthonyfisicabsb/Desafio7.java
@@ -1,5 +1,12 @@
 import java.io.RandomAccessFile;
 import java.io.IOException;
+import java.util.LinkedList;
+
+/*
+    Programa que resolve o desafio do site osprogramadores.com.
+    O desafio consiste em implementar a função nativa tac do linux,
+    que lê os arquivos e os imprime em ordem reversa.
+*/
 
 public class Desafio7
 {
@@ -11,44 +18,64 @@ public class Desafio7
         }
 
         RandomAccessFile ptr = null;
+
+        final int sizeArray = 1000000; // tamanho em bytes do array
+
         long posPtr = 0;
         long posAtual = 0;
-        String impressao = "";
-        byte[] caracteres = null;
+        byte[] charChunk = null;
+        LinkedList<Byte> listaImpressao = new LinkedList<>();
         char test = 'a';
 
         try {
             ptr = new RandomAccessFile(args[0], "r");
 
-            posPtr = ptr.length() -1;
-            posAtual = posPtr - 1;
+            posPtr = ptr.length();
+            posAtual = (posPtr - sizeArray) < 0 ? 0: (posPtr - sizeArray);
 
             do{
-                posAtual--;
+                ptr.seek(posAtual);
 
-                    ptr.seek(posAtual);
-                    if((test = (char)ptr.read()) == '\n'){
-                        ptr.seek(posAtual + 1);
+                charChunk = new byte[(int)posPtr - (int)posAtual];
+                ptr.readFully(charChunk);
 
-                        caracteres = new byte[(int)posPtr - (int)posAtual];
-                        ptr.readFully(caracteres);
-                        impressao = new String(caracteres, "UTF-8");
-                        posPtr = posAtual;
+                readArray(charChunk, listaImpressao);
 
-                        System.out.print(impressao);
-                    }
-            }while(posAtual > 0);
+                posPtr = posAtual;
+                posAtual = (posPtr - sizeArray) < 0 ? 0: (posPtr - sizeArray);
+
+            }while(posPtr > 0);
+
+            printString(listaImpressao);
 
         }catch(Exception e) {
-        }finally {
-            ptr.seek(0);
-
-            caracteres = new byte[(int)posPtr + 1];
-            ptr.readFully(caracteres);
-
-            impressao = new String(caracteres, "UTF-8");
-
-            System.out.print(impressao);
+            throw new RuntimeException("Ocorreu algum erro", e);
         }
+    }
+
+    public static void readArray(byte charChunk[], LinkedList<Byte> impressao) throws Exception
+    {
+        for(int i = charChunk.length - 1; i >= 0; i--){
+            if((char)charChunk[i] == '\n'){
+                printString(impressao);
+                impressao.clear();
+
+                impressao.addFirst(charChunk[i]);
+            }else{
+                impressao.addFirst(charChunk[i]);
+            }
+        }
+    }
+
+    public static void printString(LinkedList<Byte> lista) throws Exception 
+    {
+        byte bytes[] = new byte[lista.size()];
+
+        for(int i=0; i < lista.size(); i++){
+            bytes[i] = lista.get(i);
+        }
+
+        String impressao = new String(bytes, "UTF-8");
+        System.out.print(impressao);
     }
 }
--- desafio-07/anthonyfisicabsb/README.md
+++ desafio-07/anthonyfisicabsb/README.md
@@ -1,2 +1,2 @@
 **Compilação do Programa**: javac Desafio7.java  
-**Execução do Programa**: java Desafio7 <arquivo-texto>  
+**Execução do Programa**: java Desafio7 *arquivo-texto*  
