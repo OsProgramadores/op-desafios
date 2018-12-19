6970401582da388e3b0f001aca0703c7e2f2de70
Jackson Osvaldo <jacksonosvaldo@live.com>
2018-12-19 13:21:37 -0300

    Update desafio_02_primos.py

--- desafio-02/JacksonOsvaldo/desafio_02_primos.py
+++ desafio-02/JacksonOsvaldo/desafio_02_primos.py
@@ -9,24 +9,19 @@ E-mail: jacksonosvaldo@live.com
 
 
 def primo(numero):
-	a = 0
+	num = 0
 	for i in range(numero):
-		if i == 0:
-			continue
-		else:
-			resto = numero%i
-			if resto == 0:
-				a = a + 1
-			if a > 2:
-				break
-	if a == 2:
+		resto = numero%(i+1)
+		if resto == 0:
+			num = num + 1
+		if num > 2:
+			break
+	if num == 2:
 		return True
 	else:
 		return False
 
-for i in range(10001):
+for i in range(10000):
 	testeprimo = primo(i)
-	if testeprimo == True or i == 2:
+	if testeprimo == True:
 		print('O número primo é: {}'.format(i))
-	# if i >= 0 and i%2 == 0:
-	# 	print('O número par é: {}'.format(i))
