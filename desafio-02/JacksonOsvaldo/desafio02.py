da17239b821dabe9f586f6f305a870801012ae80
Jackson Osvaldo <jacksonosvaldo@live.com>
2018-12-19 16:11:34 -0300

    Update desafio02.py

--- desafio-02/JacksonOsvaldo/desafio02.py
+++ desafio-02/JacksonOsvaldo/desafio02.py
@@ -10,19 +10,21 @@ E-mail: jacksonosvaldo@live.com
 
 
 def primo(numero):
-	"""Função para testar se é ou não primo retornando valor lógico."""
-	num = 0
-	for i in range(numero):
-		resto = numero % (i + 1)
-		if resto == 0:
-			num = num + 1
-		if num > 2:
-			break
-	if num == 2:
-		return True
+    """Função para testar se é ou não primo retornando valor lógico."""
+    num = 0
+    for i in range(numero):
+        resto = numero % (i + 1)
+        if resto == 0:
+            num = num + 1
+            if num > 2:
+                break
+    if num == 2:
+        return True
 
 
 for numeroContador in range(10000):
-	testeprimo = primo(numeroContador)
-	if testeprimo == True:
-		print('O número primo é: {}'.format(numeroContador))
+    testeprimo = primo(numeroContador)
+    if testeprimo is True:
+        print('O número primo é: {}'.format(numeroContador))
+    else:
+        pass
