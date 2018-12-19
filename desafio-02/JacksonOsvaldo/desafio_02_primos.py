baec1909a0adc67bc8c9752d48093cd3cb43d21d
Jackson Osvaldo <jacksonosvaldo@live.com>
2018-12-19 13:26:51 -0300

    Update desafio_02_primos.py

--- desafio-02/JacksonOsvaldo/desafio_02_primos.py
+++ desafio-02/JacksonOsvaldo/desafio_02_primos.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-#-*- ecoding: utf-8 -*-
-
-"""
-Resposta de Jackson Osvaldo da Silva Braga
-GitHub: https://github.com/JacksonOsvaldo
-E-mail: jacksonosvaldo@live.com
-"""
-
-
 def primo(numero):
 	num = 0
 	for i in range(numero):
