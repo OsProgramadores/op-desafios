2b5b9e606aecfb9d8b60649ceb912b96d05da95f
Harrison <lucas_harrison@live.com>
2018-12-24 00:59:40 -0200

    Solução para o desafio 2 em Python

---
+++ desafio-02/lucasharrison/python/desafio02.py
@@ -0,0 +1,15 @@
+numeros = range(10000)
+for numero in numeros:
+    div = 1
+    count = 0
+    while div <= numero:
+        if numero%div == 0:
+            count += 1
+        if div >= numero:
+            break
+        div += 1
+    if count >= 2:
+        if count >= 3:
+            pass
+        else:
+            print numero
