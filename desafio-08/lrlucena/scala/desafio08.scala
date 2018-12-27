ac0445bac2d963e1f4ccbd21183e40345e659cf7
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2018-12-26 22:00:14 -0200

    Simplificação da solução do desafio 08

--- desafio-08/lrlucena/scala/desafio08.scala
+++ desafio-08/lrlucena/scala/desafio08.scala
@@ -1,29 +1,22 @@
-import scala.io.StdIn._
-
 def mdc(a: Int, b: Int): Int = (a, b) match {
-  case (0, b)          => b
-  case (a, b) if a > b => mdc(a % b, b)
-  case (a, b) if a < b => mdc(b, a)
+  case (0, b) => b
+  case (a, b) => mdc(b % a, a)
 }
 
 def simplificar(a: Int, b: Int): String = {
-  val m = mdc(a % b, b)
-  if (m == b) {
-    s"${a / m}"
-  } else if (a > b) {
-    val c = a / b
-    s"${c} ${(a % b) / m}/${b / m}"
-  } else {
-    s"${a / m}/${b / m}"
-  }
+  val (c, d) = (a / b, a % b)
+  val m = mdc(d, b)
+  (if (c == 0) "" else s"${c} ") + (if (d == 0) "" else s"${d / m}/${b / m}")
 }
 
-var entrada = readLine()
+var entrada = scala.io.StdIn.readLine()
 while (entrada != null) {
-  entrada.split("/").map(_.toInt).toList match {
-    case List(a)    => println(a)
-    case List(a, 0) => println("ERR")
-    case List(a, b) => println(simplificar(a, b))
+  println {
+    entrada.split("/").map(_.toInt).toList match {
+      case List(a)    => a
+      case List(a, 0) => "ERR"
+      case List(a, b) => simplificar(a, b)
+    }
   }
-  entrada = readLine()
+  entrada = scala.io.StdIn.readLine()
 }
