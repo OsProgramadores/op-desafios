45bffceb1eb93fbab114ff82834cb48a5b757d33
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-12-26 22:19:34 -0200

    Update desafio08.scala

--- desafio-08/lrlucena/scala/desafio08.scala
+++ desafio-08/lrlucena/scala/desafio08.scala
@@ -1,9 +1,6 @@
-def mdc(a: Int, b: Int): Int = (a, b) match {
-  case (0, b) => b
-  case (a, b) => mdc(b % a, a)
-}
+def mdc(a: Int, b: Int): Int = if (a==0) b else mdc(b % a, a)
 
-def simplificar(a: Int, b: Int): String = {
+def simplificar(a: Int, b: Int = 1): String = {
   val (c, d) = (a / b, a % b)
   val m = mdc(d, b)
   (if (c == 0) "" else s"${c} ") + (if (d == 0) "" else s"${d / m}/${b / m}")
@@ -11,12 +8,10 @@ def simplificar(a: Int, b: Int): String = {
 
 var entrada = scala.io.StdIn.readLine()
 while (entrada != null) {
-  println {
-    entrada.split("/").map(_.toInt).toList match {
-      case List(a)    => a
-      case List(a, 0) => "ERR"
-      case List(a, b) => simplificar(a, b)
-    }
+  entrada.split("/").map(_.toInt).toList match {
+    case List(a)    => println(a)
+    case List(a, 0) => println("ERR")
+    case List(a, b) => println(simplificar(a, b))
   }
   entrada = scala.io.StdIn.readLine()
 }
