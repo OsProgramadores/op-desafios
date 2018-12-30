58c472ce00d97eb2b5dc12dd0aaf03982aa12133
Karinne Cristina <karinnecristinapereira@gmail.com>
2018-12-30 01:37:45 -0200

    Solução do desafio-02

--- desafio-02/karinnecristina/primos.py
+++ desafio-02/karinnecristina/primos.py
@@ -3,9 +3,9 @@ Autor(a): karinnecristina
 Linguagem: Python'''
 
 for numero in range(1, 10001):
-    if numero > 1:
-     for divisor in range(2, numero):
-      if numero % divisor == 0:
-       break
-     else:
-      print(numero)
+ if numero > 1:
+  for divisor in range(2, numero):
+   if numero % divisor == 0:
+    break
+  else:
+   print(numero)
