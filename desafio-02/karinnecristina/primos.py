c4a3584abb2a94abd8e94a9025bc563870f83695
Karinne Cristina <karinnecristinapereira@gmail.com>
2018-12-30 02:12:55 -0200

    Solução do desafio-02

--- desafio-02/karinnecristina/primos.py
+++ desafio-02/karinnecristina/primos.py
@@ -1,3 +1,7 @@
+'''Descrição: Solução do desafio-02 - osprogramadores.com
+Autor(a): karinnecristina
+Linguagem: Python'''
+
 for numero in range(1, 10001):
     if numero > 1:
         for divisor in range(2, numero):
