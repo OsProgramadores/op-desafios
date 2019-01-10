7c1030888cd153cce4786b9b3ffcd08d1f132ad9
Augusto da Silva Costa <contato@guto.pro.br>
2019-01-10 10:21:10 -0200

    Criado função main, alterado variaveis para caixa baixa

--- desafio-02/gutocosta/python/listar_numeros_primos.py
+++ desafio-02/gutocosta/python/listar_numeros_primos.py
@@ -2,16 +2,23 @@
     Programa para listar todos os números primos entre 1 e 10000
 '''
 
-VALOR_LIMITE = 10000
-LISTA_NUM_INT = [True for i in range(VALOR_LIMITE+1)]
-P = 2
 
-while P * P <= VALOR_LIMITE:
-    if LISTA_NUM_INT[P]:
-        for i in range(P * 2, VALOR_LIMITE+1, P):
-            LISTA_NUM_INT[i] = False
-    P += 1
+def main():
+    '''
+        função verifica os numeros primos de 1 a 10000
+    '''
+    valor_limite = 10000
+    lista_num_int = [True for i in range(valor_limite+1)]
+    primo = 2
+    while primo * primo <= valor_limite:
+        if lista_num_int[primo]:
+            for i in range(primo * 2, valor_limite+1, primo):
+                lista_num_int[i] = False
+        primo += 1
+    for primo in range(2, valor_limite):
+        if lista_num_int[primo]:
+            print(primo)
 
-for P in range(2, VALOR_LIMITE):
-    if LISTA_NUM_INT[P]:
-        print(P)
+
+if __name__ == "__main__":
+    main()
