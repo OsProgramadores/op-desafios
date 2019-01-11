47c1d7445d1363f7a47c25ea3ba287b5f1761ef5
Marco Paganini <paganini@paganini.net>
2019-01-11 15:03:33 -0800

    Better travis/travisbuddy configuration. (#154)
    Better travis/travisbuddy configuration.

--- travis-ci/codecheck.sh
+++ travis-ci/codecheck.sh
@@ -2,9 +2,6 @@
 # Test code formatting compliance.
 # So far only python3 supported.
 
-TMPFILE=$(mktemp)
-trap "rm -f $TMPFILE" EXIT
-
 # TRAVIS sets TRAVIS_COMMIT_RANGE to the range of commits for the current commit.
 if [[ -z "$TRAVIS_COMMIT_RANGE" ]]; then
   echo >&2 "Note: TRAVIS_COMMIT_RANGE environment variable not set. Defaulting to HEAD HEAD^"
@@ -22,13 +19,6 @@ git diff --diff-filter=AM --name-only $TRAVIS_COMMIT_RANGE | while read fname; d
   # May be possible to use TRAVIS_CI_BUILD instead of .. below.
   ext="${fname##*.}"
   if [[ "$ext" == "py" ]]; then
-    pylint --rcfile=pylint3.rc ../"$fname"
+    pylint --rcfile="travis-ci/pylint3.rc" "$fname"
   fi
 done
-
-# Error out if any files need formatting.
-if grep -q . "$TMPFILE"; then
-  echo "ERROR: The following files need formatting with gofmt -s:"
-  cat $TMPFILE
-  exit 1
-fi
