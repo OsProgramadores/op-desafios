c5104890f0963a246c304b6ced5accbd32a66f83
WhoisBsa <mbscbsjbs@gmail.com>
2019-01-12 13:04:58 -0300

    revisando pylint desafio-03

--- desafio-03/WhoisBsa/python/main.py
+++ desafio-03/WhoisBsa/python/main.py
@@ -4,14 +4,14 @@
 
 """ Declaração do intervalo """
 while True:
-    inicio = int(input('Valor inicial: '))
-    fim = int(input('Valor final: '))
-    if inicio >= fim:
+    INICIO = int(input('Valor inicial: '))
+    FIM = int(input('Valor final: '))
+    if INICIO >= FIM:
         print('Valor inicial maior que o final, tente novamente')
     else:
         break
 
 """ Verificação de palíndromo """
-for numero in range(inicio, fim + 1):
+for numero in range(INICIO, FIM + 1):
     if numero == int(str(numero)[::-1]):
         print(numero)
