620c66495f8e9630ff230ba8c7bd9d0f6c0b55e9
WhoisBsa <mbscbsjbs@gmail.com>
2019-01-15 21:55:44 -0300

    desafio-04

--- desafio-04/WhoisBsa/python/main.py
+++ desafio-04/WhoisBsa/python/main.py
@@ -1,50 +1,54 @@
 
 """ Tabuleiro de Xadrez """
 
+def main():
+    """ Contas as peças do tabuleiro """
+    pecas = {1: [0, 'Peão'],
+             2: [0, 'Bispo'],
+             3: [0, 'Cavalo'],
+             4: [0, 'Torre'],
+             5: [0, 'Rainha'],
+             6: [0, 'Rei']}
 
-pecas = {1: [0, 'Peão'],
-         2: [0, 'Bispo'],
-         3: [0, 'Cavalo'],
-         4: [0, 'Torre'],
-         5: [0, 'Rainha'],
-         6: [0, 'Rei']}
+    f1 = open('ex1.txt', 'r')
+    f2 = open('ex2.txt', 'r')
+    ex1 = f1.read()
+    ex2 = f2.read()
+    print('Exemplo 1:')
+    print(ex1)
+    print('Exemplo 2:')
+    print(ex2)
 
-f1 = open('ex1.txt', 'r')
-f2 = open('ex2.txt', 'r')
-ex1 = f1.read()
-ex2 = f2.read()
-print('Exemplo 1:')
-print(ex1)
-print('Exemplo 2:')
-print(ex2)
+    while True:
+        op = str(input('Qual tabuleiro você deseja usar? [1/2] '))
+        if op not in '12':
+            print('Há apenas 2 opções, tente novamente\n')
+        else:
+            print('-=' * 20)
+            break
 
-while True:
-    op = str(input('Qual tabuleiro você deseja usar? [1/2] '))
-    if op not in '12':
-        print('Há apenas 2 opções, tente novamente\n')
+    print()
+
+    if op == '1':
+        tabuleiro = ex1
     else:
-        print('-=' * 20)
-        break
+        tabuleiro = ex2
 
-print()
+    print(tabuleiro)
 
-if op == '1':
-    tabuleiro = ex1
-else:
-    tabuleiro = ex2
+    pecas[1][0] = tabuleiro.count('1')
+    pecas[2][0] = tabuleiro.count('2')
+    pecas[3][0] = tabuleiro.count('3')
+    pecas[4][0] = tabuleiro.count('4')
+    pecas[5][0] = tabuleiro.count('5')
+    pecas[6][0] = tabuleiro.count('6')
 
-print(tabuleiro)
+    print()
+    for i in pecas.values():
+        print(f'{i[1]}: {i[0]} peças')
 
-pecas[1][0] = tabuleiro.count('1')
-pecas[2][0] = tabuleiro.count('2')
-pecas[3][0] = tabuleiro.count('3')
-pecas[4][0] = tabuleiro.count('4')
-pecas[5][0] = tabuleiro.count('5')
-pecas[6][0] = tabuleiro.count('6')
+    f1.close()
+    f2.close()
 
-print()
-for i in pecas.values():
-    print(f'{i[1]}: {i[0]} peças')
-
-f1.close()
-f2.close()
+if __name__ == "__main__":
+    main()
