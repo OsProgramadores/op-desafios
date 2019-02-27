f5a47725f8154301199ec67896d5042d73eb0066
Karinne Cristina <karinnecristinapereira@gmail.com>
2019-02-26 22:15:48 -0300

    Solucao Desafio-05

--- desafio-05/karinnecristina/python/empresa.py
+++ desafio-05/karinnecristina/python/empresa.py
@@ -5,62 +5,72 @@ Linguagem: Python'''
 import json
 import pandas as pd
 from pandas.io.json import json_normalize
-with open('funcionarios.json') as file:
-    DATA = json.load(file)
 
-FUNCIONARIO = json_normalize(DATA['funcionarios'])
-AREA = json_normalize(DATA['areas'])
-DADOS = pd.merge(FUNCIONARIO, AREA, left_on="area", right_on="codigo")
+def company(filename):
 
-# 1. Quem mais recebe e quem menos recebe na empresa e a média salarial da empresa.
-MAXIMUMVALUE = DADOS.loc[DADOS['salario'].idxmax()]
-HIGHERWAGES = DADOS[DADOS['salario'] == MAXIMUMVALUE['salario']]
-for i in HIGHERWAGES.values:
-    print('global_max|' + i[2] + ' ' + i[4] + '|' + '{:.2f}'.format(float(i[3])))
+    '''Processa arquivo JSON e une os dados em uma tabela.'''
 
-MINIMUMVALUE = DADOS.loc[DADOS['salario'].idxmin()]
-LOWERWAGES = DADOS[DADOS['salario'] == MINIMUMVALUE['salario']]
-for i in LOWERWAGES.values:
-    print('global_min|' + i[2] + ' ' + i[4] + '|' + '{:.2f}'.format(float(i[3])))
+    with open(filename) as file:
+        data = json.load(file)
 
-AVERAGEWAGES = DADOS['salario'].mean()
-print("global_avg|" + '{:.2f}'.format(float(AVERAGEWAGES)))
+    funcionario = json_normalize(data['funcionarios'])
+    area = json_normalize(data['areas'])
+    dados = pd.merge(funcionario, area, left_on="area", right_on="codigo")
 
-# 2. Quem mais recebe e quem menos recebe em cada área e a média salarial em cada área.
+    def main():
+        # 1. Quem mais recebe e quem menos recebe na empresa e a média salarial da empresa.
+        maximum_value = dados.loc[dados['salario'].idxmax()]
+        value = dados[dados['salario'] == maximum_value['salario']]
+        for i in value.values:
+            print('global_max|' + i[2] + ' ' + i[4] + '|' + '{:.2f}'.format(float(i[3])))
 
-ARE = DADOS['maximo_salario'] = DADOS.groupby(["area"])[["salario"]].transform('max')
-ARE = DADOS[(DADOS['salario'] == DADOS['maximo_salario'])]
-for i in ARE.values:
-    print('area_max|' + i[6] + '|' + i[2] + ' ' + i[4]+ '|' + '{:.2f}'.format(float(i[3])))
+        maximum_value = dados.loc[dados['salario'].idxmin()]
+        value = dados[dados['salario'] == maximum_value['salario']]
+        for i in value.values:
+            print('global_min|' + i[2] + ' ' + i[4] + '|' + '{:.2f}'.format(float(i[3])))
 
-ARE = DADOS['minimo_salario'] = DADOS.groupby(["area"])[["salario"]].transform('min')
-ARE = DADOS[(DADOS['salario'] == DADOS['minimo_salario'])]
-for i in ARE.values:
-    print('area_min|' + i[6] + '|' + i[2] + ' ' + i[4]+ '|' + '{:.2f}'.format(float(i[3])))
+        value = dados['salario'].mean()
+        print("global_avg|" + '{:.2f}'.format(float(value)))
 
-AVERAGE = DADOS['salario'].groupby(DADOS['area']).mean()
-for i, item in enumerate(AVERAGE):
-    print('area_avg|' + AREA.loc[AREA['codigo'] == AVERAGE.index.values[i]].values[0][1]
-          + "|" + '{:.2f}'.format(item))
+        # 2. Quem mais recebe e quem menos recebe em cada área e a média salarial em cada área.
 
-# 3. Área(s) com o maior e menor número de funcionários.
-MAXIMUMAREA = DADOS['nome_y'].value_counts()
-DATAARE = dict(MAXIMUMAREA)
-MAXIMUM = max(DATAARE, key=lambda x: DATAARE[x])
-VALUE = DATAARE[MAXIMUM]
-print('most_employees|'  + MAXIMUM, '|' + str(VALUE))
+        areas = dados['maximo_salario'] = dados.groupby(["area"])[["salario"]].transform('max')
+        areas = dados[(dados['salario'] == dados['maximo_salario'])]
+        for i in areas.values:
+            print('area_max|' + i[6] + '|' + i[2] + ' ' + i[4]+ '|' + '{:.2f}'.format(float(i[3])))
 
-MAXIMUMAREA = DADOS['nome_y'].value_counts()
-DATAARE = dict(MAXIMUMAREA)
-MINIMUM = min(DATAARE, key=lambda x: DATAARE[x])
-VALUE = DATAARE[MINIMUM]
-print('least_employees|'  + MINIMUM, '|' + str(VALUE))
+        areas = dados['minimo_salario'] = dados.groupby(["area"])[["salario"]].transform('min')
+        areas = dados[(dados['salario'] == dados['minimo_salario'])]
+        for i in areas.values:
+            print('area_min|' + i[6] + '|' + i[2] + ' ' + i[4]+ '|' + '{:.2f}'.format(float(i[3])))
 
-# 4. Maiores salários para funcionários com o mesmo sobrenome
-SALARYLASTNAME = DADOS.groupby('sobrenome').filter(lambda group: len(group) > 1).copy()
-SALARYLASTNAME['funcionario'] = SALARYLASTNAME.groupby(["sobrenome"])[["salario"]]. \
-    transform('max')
-SALARYLASTNAME = SALARYLASTNAME[(SALARYLASTNAME["salario"] == SALARYLASTNAME["funcionario"])]
-for item in SALARYLASTNAME.values:
-    print("last_name_max|" + item[4] + "|" + item[2] + " " + item[4]+ "|" + '{:.2f}'. \
-          format(float(item[7])))
+        value = dados['salario'].groupby(dados['area']).mean()
+        for i, item in enumerate(value):
+            print('area_avg|' + area.loc[area['codigo'] == value.index.values[i]].values[0][1]
+                  + "|" + '{:.2f}'.format(item))
+
+        # 3. Área(s) com o maior e menor número de funcionários.
+        maximum_area = dados['nome_y'].value_counts()
+        maximum_area = dict(maximum_area)
+        maximum = max(maximum_area, key=lambda x: maximum_area[x])
+        value = maximum_area[maximum]
+        print('most_employees|'  + maximum, '|' + str(value))
+
+        maximum_area = dados['nome_y'].value_counts()
+        maximum_area = dict(maximum_area)
+        minimum = min(maximum_area, key=lambda x: maximum_area[x])
+        value = maximum_area[minimum]
+        print('least_employees|'  + minimum, '|' + str(value))
+
+        # 4. Maiores salários para funcionários com o mesmo sobrenome
+        salary = dados.groupby('sobrenome').filter(lambda group: len(group) > 1).copy()
+        salary['funcionario'] = salary.groupby(["sobrenome"])[["salario"]]. \
+            transform('max')
+        salary = salary[(salary['salario'] == \
+                                           salary['funcionario'])]
+        for item in salary.values:
+            print('last_name_max|' + item[4] + '|' + item[2] + ' ' + item[4]+ '|' + '{:.2f}'. \
+                  format(float(item[7])))
+
+    if __name__ == '__main__':
+        main()
