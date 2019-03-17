716f26863663904abc117355374c768799ef63d8
GabrielF9 <gabriel.ferreira@dcomp.ufs.br>
2019-03-16 23:38:09 -0300

    Conserto de indentacao.

--- desafio-02/gabrielf9/python/desafio-02.py
+++ desafio-02/gabrielf9/python/desafio-02.py
@@ -3,16 +3,16 @@
 primes = []
 
 for i in range(2, 10001):
-  if i % 2 == 0 and i != 2:
-    continue
+    if i % 2 == 0 and i != 2:
+        continue
 
-  _isPrime = True
-  for j in range(2, int(i**1/2)+1):
-    if i % j == 0 and i != j:
-      _isPrime = False
-  
-  if _isPrime:
-    primes.append(i)
-    print(f'{i} é primo')
+    _isPrime = True
+    for j in range(2, int(i**1/2)+1):
+        if i % j == 0 and i != j:
+            _isPrime = False
+
+    if _isPrime:
+        primes.append(i)
+        print(f'{i} é primo')
 
 print(f'Um total de {len(primes)} primos.')
