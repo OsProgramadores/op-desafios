42569d4707436af2825711501509e3292c7f13e7
ClaudioBoaventura <42983580+ClaudioBoaventura@users.noreply.github.com>
2019-06-16 01:27:27 -0300

    Update desafio03_final_v2.c

--- desafio-03/ClaudioBoaventura/C/desafio03_final_v2.c
+++ desafio-03/ClaudioBoaventura/C/desafio03_final_v2.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
  
 int main(){
- int num,cont,inv_num,resto;
- for (num = 0; num < 10000; num++){
- scanf("%d",&num);
- cont = num;
- inv_num = 0;
- while(cont != 0){
-    	resto = cont % 10;
-    	inv_num = (inv_num * 10) + resto;
-    	cont = cont / 10;
+	int num,cont,inv_num,resto;
+ 		for (num = 0; num < 10000; num++){
+ 		scanf("%d",&num);
+ 		cont = num;
+ 		inv_num = 0;
+	
+		while(cont != 0){
+    			resto = cont % 10;
+    			inv_num = (inv_num * 10) + resto;
+    			cont = cont / 10;
  	}
  		if(num == inv_num)
-    	printf("%d\n",num);
+    			printf("%d\n",num);
 	}
 }
