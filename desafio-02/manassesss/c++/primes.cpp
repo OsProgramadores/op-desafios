ed24c02a9c583742a30c4207e1557e88f622cb28
manassess <manasses3mm@gmail.com>
2019-10-01 18:34:55 -0300

    manassesss desafio 2

---
+++ desafio-02/manassesss/c++/primes.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define N 1000
+
+using namespace std;
+
+
+void isPrime(int k)
+{
+	int c = 1;
+	for ( int i = 2 ; i <= k - 1; i++)
+	{
+		if(k % 2 != 0 || i == 2 )
+		{
+			if (k % i == 0)
+			{
+				c++;
+			}
+			if(c > 2)
+				break;
+		}
+	}
+	if (c + 1 == 2)
+		cout << k << " ";
+}
+
+int main ()
+{
+	for (int i = 2 ; i <= N ; i++)
+	{
+		isPrime(i);
+	}
+	return 0;
+}
