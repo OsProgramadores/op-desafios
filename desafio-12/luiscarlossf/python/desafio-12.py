5a0e138acf646b4a3aa408a8de5d8274b162622a
Luis Carlos <luiscarlos.sf2017@gmail.com>
2019-10-01 22:52:28 -0300

    Update desafio-12.py

--- desafio-12/luiscarlossf/python/desafio-12.py
+++ desafio-12/luiscarlossf/python/desafio-12.py
@@ -1,11 +1,20 @@
-arq = open("d12.txt", 'r')
-for n in arq.readlines():
-    number = int(n)
-    cont = 0
-    while (2 ** cont) < number:
-        cont += 1
-    
-    if number == (2 ** cont):
-        print(number, " true ", cont)
-    else:
-        print(number, " false ")
+"""
+Desafio 12 - Luis Carlos da Silva Filho
+"""
+def main():
+    """
+    Main Function
+    """
+    arq = open("d12.txt", 'r')
+    for n in arq.readlines():
+        number = int(n)
+        cont = 0
+        while (2 ** cont) < number:
+            cont += 1
+        if number == (2 ** cont):
+            print("{0} true {1}".format(number, cont))
+        else:
+            print("{0} false".format(number))
+
+if __name__=="__main__":
+    main()
