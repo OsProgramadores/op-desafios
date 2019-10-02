59e19acfe81222a5d4856de691966173450621d2
Luis Carlos <luiscarlos.sf2017@gmail.com>
2019-10-01 22:08:10 -0300

    Create d12.py

---
+++ desafio-12/luiscarlossf/python/d12.py
@@ -0,0 +1,11 @@
+arq = open("d12.txt", 'r')
+for n in arq.readlines():
+    number = int(n)
+    cont = 0
+    while (2 ** cont) < number:
+        cont += 1
+    
+    if number == (2 ** cont):
+        print(number, " true ", cont)
+    else:
+        print(number, " false ")
