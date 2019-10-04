359280bda013f6dd49510e153ba86ba6bf8e5015
manassess <manasses3mm@gmail.com>
2019-10-04 14:26:20 -0300

    did I fix it?

--- desafio-02/manassesss/python/primes.py
+++ desafio-02/manassesss/python/primes.py
@@ -1,13 +1,13 @@
 def isPrime(k):
 	c = 1
 	for i in range(2, k-1):
-		if k % 2 != 0 or i == 2:
-			if k % i == 0 :
+		if k%2 != 0 or i == 2:
+			if k%i == 0 :
 				c = c+1
 			if c > 2:
 				break
-	if c + 1 == 2 :
+	if c+1 == 2 :
 		print(k)
 
-for i in range(2, 10000):
+for i in range(2, 10):
     isPrime(i)
