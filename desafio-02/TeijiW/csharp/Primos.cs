34cf7b15da07eeef32cf51a75a2b416c03ab9892
TeijiW <teijiwatanabe01@gmail.com>
2020-01-13 22:09:44 -0400

    Solução para o Desafio 02 feito em CSharp

---
+++ desafio-02/TeijiW/csharp/Primos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace desafio2 {
+    class Primos {
+        static void Main () {
+            int counter = 0;
+            for (int i = 0; i <= 100; i++) {
+                counter = 0;
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) counter++;
+                    if (counter > 2) break;
+                }
+                if (counter == 2) {
+                    Console.WriteLine (i);
+                }
+
+            }
+        }
+    }
+}
