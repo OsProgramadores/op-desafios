1d3c58a21cf0ff3cf81d6d54226773f85955959e
satuctkode <gleydsonjosewarface@hotmail.com>
2020-02-10 03:02:52 -0300

    Adicionando solução do desafio 6 por @satuctkode

---
+++ desafio-06/satuctkode/php/desafio6-anagramas.php
@@ -0,0 +1,48 @@
+<?php
+class Anagramas {
+  public function mostrarAnagramas($arquivoRecebido, $expressaoRecebida) {
+    define("EXPRESSAO_UPPER_NOSPACE", strtoupper(preg_replace("/\s+/", "", $expressaoRecebida)));
+
+    if(preg_match("/^[a-z]{1,16}$/i", EXPRESSAO_UPPER_NOSPACE, $match)) {
+      $grupoTodasExpressoes = [];
+      $grupoExpressoesEscolhidas = [];
+      $arquivoDados = fopen($arquivoRecebido, 'r') or die("Erro ao abrir arquivo!");
+      while(!feof($arquivoDados)) {
+        array_push($grupoTodasExpressoes, fgets($arquivoDados));
+      }
+      fclose($arquivoDados);
+
+      foreach($grupoTodasExpressoes as $expressaoOriginal) {
+        if(count(str_split(preg_replace("/\s+/", "", $expressaoOriginal))) === count(str_split(EXPRESSAO_UPPER_NOSPACE))) {
+          array_push($grupoExpressoesEscolhidas, $expressaoOriginal);
+        }
+      }
+
+      define("ARRAY_EXPRESSAO_UPPER_NOSPACE_LET_SEP", str_split(preg_replace("/\s+/", "", EXPRESSAO_UPPER_NOSPACE)));
+
+      foreach($grupoExpressoesEscolhidas as $expressaoArray) {
+        $arrayExpUpNSLetSepTemporario = ARRAY_EXPRESSAO_UPPER_NOSPACE_LET_SEP;
+        $arrayExpressaoArrayLetSep = str_split(preg_replace("/\s+/", "", $expressaoArray));
+
+        foreach($arrayExpressaoArrayLetSep as $letraExpArray) {
+          foreach($arrayExpUpNSLetSepTemporario as $indexExpUpNSTemp => $letraExpUpNSTemp) {
+            if($letraExpArray === $letraExpUpNSTemp) {
+              unset($arrayExpUpNSLetSepTemporario[$indexExpUpNSTemp]);
+              break;
+            }
+          }
+        }
+  
+        if(empty($arrayExpUpNSLetSepTemporario)) {
+          echo $expressaoArray."<br>";
+        }
+      }
+
+    } else {
+      die("Error, a expressão precisa ter no máximo 16 caracteres e não pode ter caracteres especiais, exceto espaços!");
+    }
+  }
+}
+
+$anagramas = new Anagramas;
+$anagramas->mostrarAnagramas("word.txt", "ogd");
