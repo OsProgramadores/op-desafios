6c8e91ba6db89444f9c74b347cba2e5fa2b12663
Rubens Abraão da Silva Sousa <abrahaosilva.ifce@gmail.com>
2020-04-06 23:38:25 -0300

    Desafio em Python

---
+++ desafio-02/RubensSousa/desafio02.py
@@ -0,0 +1,8 @@
+for i in range(1, 10000):
+        primo = 1
+        for x in range(2, i-1):
+            if i % x == 0:
+                primo = 0
+                break
+        if primo:
+            print(i)
