982eaeab8aa902bd98b56680aedcd3bb985260fa
hiagoleres <hiagoleres2@gmail.com>
2020-04-12 01:34:57 -0300

    solução para o desafio 2

---
+++ desafio-02/hiagoleresdev/python/solucao.py
@@ -0,0 +1,10 @@
+primes = [1]
+dividers = 0
+for i in range(2,10000):
+    dividers = 0
+    for n in range(2,10000):
+        if i % n == 0:
+            dividers +=1
+    if dividers == 1:
+        primes.append(i)
+print(primes)
