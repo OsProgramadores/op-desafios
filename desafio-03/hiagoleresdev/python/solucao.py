10bd3b942e56a512441f0e9a8ce45b8b150f50b6
hiagoleres <hiagoleres2@gmail.com>
2020-04-21 01:33:06 -0300

    Solução para o desafio 3

---
+++ desafio-03/hiagoleresdev/python/solucao.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun April 19 22:30:12 2020
+Desafio 03 - Listar todos os numeros palindromos
+@author: Hiago Leres
+"""
+palindromes = []
+firstNumber = int(input('Type a first number to see all palindromes: '))
+finalNumber = int(input('Type a final number to see all palindromes: '))
+numberInverted = 0
+
+def isPalindrome(numero):    
+    numberInverted = str(numero)[::-1]
+        
+    if int(numero) == int(numberInverted):
+        palindromes.append(i)
+
+for i in range(firstNumber, finalNumber):
+    isPalindrome(i)
+
+print(palindromes)
+
