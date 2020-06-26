c47f8f2d561510ccf7db3b775bf59bde8247c831
Lucas Cegielkowski Guimaraes <cegielkowski@MacBook-Pro.local>
2020-06-26 20:07:05 -0300

    feat(routines): added go routines and channels

--- desafio-02/cegielkowski/go/prime.go
+++ desafio-02/cegielkowski/go/prime.go
@@ -2,23 +2,31 @@ package main
 
 import "fmt"
 
-func sieveOfEratosthenes(N int) (primes []int) {
+var primes = make(chan int)
+
+func sieveOfEratosthenes(primes chan<- int, N int) {
 	b := make([]bool, N)
 	for i := 2; i < N; i++ {
 		if b[i] == true {
 			continue
 		}
-		primes = append(primes, i)
+		primes <- i
 		for k := i * i; k < N; k += i {
 			b[k] = true
 		}
 	}
+	close(primes)
 	return
 }
 
 func main() {
-	primes := sieveOfEratosthenes(10000)
-	for _, p := range primes {
-		fmt.Println(p)
+	go sieveOfEratosthenes(primes, 10000)
+	for {
+		v := <-primes
+		if v != 0 {
+			fmt.Println("this is prime:", v)
+		} else {
+			return
+		}
 	}
 }
