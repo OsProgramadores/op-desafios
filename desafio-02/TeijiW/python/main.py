847094b75868b72ff15e130df8406aa20154d079
Teijiw <teijiwatanabe01@gmail.com>
2020-07-16 01:43:49 -0400

    Resolução do desafio 2 em python

---
+++ desafio-02/TeijiW/python/main.py
@@ -0,0 +1,12 @@
+numbers_to_print = ""
+print("Please, wait, I'm calculating =)")
+for n1 in range(1, 10000):
+    matches = 0
+    for n2 in range(1, n1):
+        if n1 % n2 == 0:
+            matches += 1
+    if matches <= 2:
+        string_concat = "{} \n"
+        numbers_to_print += string_concat.format(n1)
+
+print(numbers_to_print)
