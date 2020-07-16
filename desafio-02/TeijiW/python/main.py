fd7d8a703275463f5295d7a3b447c4c34ecd23d6
Teijiw <teijiwatanabe01@gmail.com>
2020-07-16 01:52:31 -0400

    Adding main function

--- desafio-02/TeijiW/python/main.py
+++ desafio-02/TeijiW/python/main.py
@@ -1,11 +1,17 @@
-numbers_to_print = ""
-for n1 in range(1, 10000):
-    matches = 0
-    for n2 in range(1, n1):
-        if n1 % n2 == 0:
-            matches += 1
-    if matches <= 2:
-        string_concat = "{} \n"
-        numbers_to_print += string_concat.format(n1)
 
-print(numbers_to_print)
+def main():
+    numbers_to_print = ""
+    for n1 in range(1, 10000):
+        matches = 0
+        for n2 in range(1, n1):
+            if n1 % n2 == 0:
+                matches += 1
+        if matches <= 2:
+            string_concat = "{} \n"
+            numbers_to_print += string_concat.format(n1)
+
+    print(numbers_to_print)
+
+
+if __name__ == "__main__":
+    main()
