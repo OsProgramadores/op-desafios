f14c6b320fb75c291154d4293b0ed2e5f3ef3022
G4BR13LR0CH4 <gabriel.gabrielroch@gmail.com>
2020-07-17 07:16:29 -0300

    Resolução desafio 04 por Gabriel Rocha

--- desafio-04/g4br13lr0ch4/python/xadrez.py
+++ desafio-04/g4br13lr0ch4/python/xadrez.py
@@ -9,16 +9,15 @@ peca = {"Peao" : 1, "Bispo" : 2, "Cavalo" : 3, "Torre": 4, "Rainha" : 5, "Rei" :
 
 def criacao():
     """Posiciona as pecas do tabuleiro"""
-    print("Por favor preencha o Tabuleiro, lembre-se que voce esta preenchendo da esquerda para direita da parte inferior")
-    print("obs: caso nao queira colocar nenhum peca naquela coordenada adicionar valor 0")
-
     for x in range(64):
-        aux = int(input("Digite o id da peca: "))
-        tabuleiro.append(aux)
+        print("Casa: " + str(x))
+        arm_peca = int(input("Digite o id da peca: "))
+        tabuleiro.append(arm_peca)
 
-def cont(aux, clone):
-    while aux in clone:
-        clone.remove(aux)
+def cont(flag, clone):
+    """Faz a contagem das pecas"""
+    while flag in clone:
+        clone.remove(flag)
     return len(clone)
 
 criacao()
@@ -26,9 +25,5 @@ for key in peca:
     aux = cont(peca[key], tabuleiro[:])
     peca[key] = 64 - int(aux)
 
-print("\nPeão: " + str(peca['Peao']) + " peça(s)")
-print("Bispo: " + str(peca['Bispo']) + " peça(s)")
-print("Cavalo: " + str(peca['Cavalo']) + " peça(s)")
-print("Torre: " + str(peca['Torre']) + " peça(s)")
-print("Rainha: " + str(peca['Rainha']) + " peça(s)")
-print("Rei: " + str(peca['Rei']) + " peça(s)")
+for key in peca:
+    print(key + ": " + str(peca[key]) + " peças")
