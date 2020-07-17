809379386e36eb2b7ac42e4c666b10f907984a29
Teijiw <teijiwatanabe01@gmail.com>
2020-07-17 00:04:35 -0400

    Resolução do desafio 3 em python

---
+++ desafio-03/TeijiW/python/desafio03.py
@@ -0,0 +1,28 @@
+"""
+Author: Teiji Watanabe
+Palindrome Verification
+"""
+
+
+def reverse(text):
+    """Return the inverse text"""
+    return str(text)[::-1]
+
+
+def isPalindrome(text):
+    """Verify if text is palindrome return true or false"""
+    return str(text) == reverse(str(text))
+
+
+def main():
+    numbers_list = []
+    start = input("Start of period: ")
+    end = input("End of period: ")
+    for number in range(int(start), int(end)):
+        if(isPalindrome(number)):
+            numbers_list.append(number)
+    print(*numbers_list, sep="\n")
+
+
+if __name__ == "__main__":
+    main()
