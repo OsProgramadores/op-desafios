d25323ce64a9626a6fe1e3a6217556a324872c9f
kortkamp <marcelusmedius@gmail.com>
2020-07-23 22:53:09 +0000

    tirando perror de dentro de funcoes

--- desafio-07/kortkamp/c/tac.c
+++ desafio-07/kortkamp/c/tac.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdio.h>
-#define LINE_BUFFER_SIZE 1000
-#define BUFFER_SIZE 10000 
+#define LINE_BUFFER_SIZE	1000
+#define BUFFER_SIZE		10000 
+#define ERROR			-1
+
 
 /* TODO LIST	
  * (DONE!)		-need to deal with lines bigger then LINE_BUFFER_SIZE(yes, im noob)
@@ -16,60 +18,42 @@
 // Precisa preservar o SEEK_CUR no arquivo 
 int print_line(FILE *fptr,long start, long end){
 	long cur_pos = ftell(fptr);
-
 	// o +1 facilita as contas já que posso encher 
 	// o buffer com LINE_BUFFER_SIZE bytes e o último
 	// elemento será deixado em '\0' facilitando o printf
 	char linebuffer[LINE_BUFFER_SIZE+1];
 
 	fseek(fptr,start,SEEK_SET);
-	if(ferror(fptr)){
-	     perror("Seek error");
-	     return(-1);
-	}
-	linebuffer[LINE_BUFFER_SIZE] = '\0';
+	if(ferror(fptr)) return(ERROR);
+	
 	while(end-start > LINE_BUFFER_SIZE){
-		start += fread(&linebuffer, sizeof(char), LINE_BUFFER_SIZE, fptr);
+		start += fread(&linebuffer, 1, LINE_BUFFER_SIZE, fptr);
+		linebuffer[LINE_BUFFER_SIZE] = '\0';
+		if(ferror(fptr)) return(ERROR);
 		printf("%s",linebuffer);
-
 		fseek(fptr,start,SEEK_SET);
-		if(ferror(fptr)){
-		     perror("Seek error");
-		     return(-1);
-		}
-
+		if(ferror(fptr)) return(ERROR);
 	}
 //	if((end - start) <= LINE_BUFFER_SIZE){
-
-		fread(&linebuffer, sizeof(char),end - start, fptr);
-		if(ferror(fptr)){
-			perror("Read error");
-			return(-1);
-		}
+		fread(&linebuffer, 1,end - start, fptr);
+		if(ferror(fptr)) return(ERROR);
 		linebuffer[end - start] = '\0';
 		printf("%s\n",linebuffer);
 //	}
 
 	// precisa retornar pra onde o ponteiro do arquivo estava antes
 	fseek(fptr,cur_pos,SEEK_SET);
-	if(ferror(fptr)){
-	     perror("Seek error");
-	     return(-1);
-	}
+	if(ferror(fptr)) return(ERROR);
 	return(0);
 }
 
 int main(int argc , char* argv[]){
-	
-	
 	FILE *fptr;
 	char buffer[BUFFER_SIZE];
-	
-	
 	long pos = LINE_BUFFER_SIZE-1 ;
 	long read_ret;
 	long seek_ret;
-	int last_bytes = BUFFER_SIZE; //last bytes to read when fseek reaches SEEK_SET
+	int last_bytes = BUFFER_SIZE; // last bytes to read when fseek reaches SEEK_SET
 	long fsize = 0;
 	long last_cr = 0;
 	if(argc <= 1){
@@ -79,60 +63,65 @@ int main(int argc , char* argv[]){
 	fptr = fopen(argv[1],"rb+");
 	if(fptr == NULL){
 		perror("Error opening file");
-		return(-1);
+		return(ERROR);
 	}
-	//dispensa o último char do arquivo porque costuma ser convertido em \n
+	// dispensa o último char do arquivo porque costuma ser convertido em \n
 	fseek(fptr,-1,SEEK_END);	
 	if(ferror(fptr)){
-	     perror("Seek error");
-	     return(-1);
+		perror("Seek error");
+		return(ERROR);
 	}
 	fsize = ftell(fptr);
 		
 	long count = 1;
-	long end = fsize; //end of line intended to print
-	long cr_pos = -1;  // position os CR found, start of line intended to print
+	long end = fsize; // end of line intended to print
+	long cr_pos = -1; // position os CR found, start of line intended to print
 	long bytes_to_read = BUFFER_SIZE;	
 	// this while must start with fseek to end 
 	while(seek_ret == 0){ // pega buffers de todo o arquivo
+
 		seek_ret = fseek(fptr,-BUFFER_SIZE,SEEK_CUR);
 		if(ferror(fptr)){
 			perror("Seek error");
-			return(-1);
+			return(ERROR);
 		}
 		if(seek_ret != 0){
-			bytes_to_read = end;
+			if(count == 1) bytes_to_read = fsize;	// se for a primeira leitura
+			else bytes_to_read = cr_pos;		
 		       	fseek(fptr,0,SEEK_SET);
 			if(ferror(fptr)){
 				perror("Seek error");
-				return(-1);
+				return(ERROR);
 			}
 		}
 		cr_pos = ftell(fptr);
-//		clear_buffer();
-		read_ret = fread(&buffer, sizeof(char),bytes_to_read, fptr);
+//		printf("bytes_to_read: %d ,  cr_pos: %d \n",bytes_to_read, cr_pos);
+		read_ret = fread(&buffer, 1,bytes_to_read, fptr);
 		if(ferror(fptr)){
 			perror("Read error");
-			return(-1);
+			return(ERROR);
 		}
 		for(long i = 1; i <= read_ret ; i++){
 			if(buffer[read_ret - i] == '\n'){
 				cr_pos = ftell(fptr) - i  ;
-				print_line(fptr,cr_pos+1,end);
-
+				if(print_line(fptr,cr_pos+1,end) == ERROR){
+					perror("Erro ao imprimir linha do arquivo");
+					return(ERROR);
+				}
 				end = cr_pos;
 			}
-
 		}
 		fseek(fptr,cr_pos,SEEK_SET);
 		if(ferror(fptr)){
 			perror("Seek error");
-			return(-1);
+			return(ERROR);
 		}
 		count++;
-		
 	}
-	print_line(fptr,0,end);
+	if(print_line(fptr,0,end) == ERROR){
+		perror("Erro ao imprimir linha do arquivo");
+		return(ERROR);
+	}
 	fclose(fptr);
 	return(0);
 }
