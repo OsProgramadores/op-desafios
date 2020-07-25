be9280fde2de046cb18a1d1b07c69ae82a8cf74d
kortkamp <marcelusmedius@gmail.com>
2020-07-25 13:39:09 +0000

    solucao inicial do desafio anagramas

---
+++ desafio-06/kortkamp/c/anagramas.c
@@ -0,0 +1,150 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define INPUT_LIMIT	100		// max input lenght
+#define WORDS_FILE	"words.txt"	// name of the file
+#define NUM_WORDS	24853		// num of words in file 
+#define MAX_LEN		23		// lenght of the largest word + '\0'
+
+char word_array[NUM_WORDS][MAX_LEN]; // [num_of_words][len of largest word]
+int solution[INPUT_LIMIT] = {0}; // store the indexes os word_array for current solution
+
+//push a new char into a buffer and return the new lenght
+int push(char *buffer, char c){
+	int pos = 0; //position in buffer
+	while( buffer[pos]!='\0') pos++;
+	buffer[pos] = c;
+	buffer[pos + 1] = '\0';
+	return(pos+1);
+}	
+// return 0 if a buffer is completely filled by spaces
+int blank(char *buffer){	
+	for(int i = 0; i < strlen(buffer);i++) 
+		if(buffer[i] != ' ') return(-1);
+	
+	return(0);
+}
+void print_solution(int max_index){
+	printf("");
+	for(int i = 0; i <= max_index; i++)
+		printf("%s ", word_array[solution[i]]);
+	printf("\n");
+}
+// Test if a word of word_index was already used in solution array
+// return 0 if not used and -1 if already used 
+int used(int word_index, int solution_max_index){
+	for(int i = 0; i <= solution_max_index; i++)
+		if(word_index == solution[i]) return(-1);
+	return(0);
+}
+
+// Return 0 if sub is a subset of set
+// in another words, all chars in string sub 
+// are too in the string set
+// a important note: set is partialy destroyed if successful
+// a chars of set that are part of sub are substituted for spaces in set.
+int comb(char *set, char *sub){
+	char *index;
+	char temp[INPUT_LIMIT];
+	strcpy(temp,set);
+	for(int j = 0 ; j < strlen(sub) ; j++){ // para cada letra da string sub
+		if((index = strchr(set,sub[j])) != NULL){
+			index[0] = ' ';
+		}else{
+			strcpy(set,temp);
+			return(-1);
+		}
+	}
+	return(0);
+}
+/* A função de busca deve criar um buffer e testar cada palavra da words_list
+ * se a palavra coincidir e não for maior que o work_buffer(input_buffer), a gente
+ * exclui as letras encontradas e chama a mesma função passando como work_buffer 
+ * as letras restantes.*
+ */
+int search(char *input, int index){
+	char test_word[MAX_LEN];
+	char test_input[INPUT_LIMIT];
+	strcpy(test_input,input);
+	
+//	printf("(%s) ",test_input);
+
+	for(int i = 0; i < NUM_WORDS; i++){// testa todas as palavras
+		if(used(i,index) == 0)//dicard used words		
+			if(comb(test_input,word_array[i]) == 0) {
+				
+			//	printf("%d %s ",index,word_array[i]);
+				solution[index] = i; // current word
+				if(blank(test_input) == 0){ //the input is blank 
+					print_solution(index);
+					//'printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+					return(0); // sucess
+				}
+				if(search(test_input,index+1) == 0) ;
+				strcpy(test_input,input);
+			}
+		}
+	solution[index] = 0;
+//	printf("<\n");	
+	return(-1);
+}
+
+int main(int argc,char* argv[]){
+	FILE *words_fp;
+	char input[INPUT_LIMIT] = {0}; // stores the input from user, without spaces and all uppercase
+	char temp[50];
+	//trata os erros de entrada
+	if(argc != 2){
+	       	fprintf(stderr,"usage: %s \"sentenca de entrada\"\n",argv[0]);
+		return(-1);
+	}
+	if(strlen(argv[1])>INPUT_LIMIT){
+		fprintf(stderr,"Erro: entrada maxima %d\n",INPUT_LIMIT);
+		return(-1);
+	}
+	
+	//trata a entrada
+	for(int i = 0 ; i < strlen(argv[1]);i++){
+		if((argv[1][i]>='A')&&(argv[1][i]<='Z')) push(input,argv[1][i]);
+		else if((argv[1][i]>='a')&&(argv[1][i]<='z')) push(input,argv[1][i] + 'A' - 'a');
+		else if(argv[1][i] == ' ');
+		else{
+			fprintf(stderr,"Caracteres permitidos: A-Z a-z\n");
+			return(-1);
+		}
+	}	
+
+	//abre o arquivo , carrega o array e fecha
+	words_fp = fopen(WORDS_FILE,"rb+");
+	if(words_fp == NULL){
+		perror("Erro ao abrir o arquivo " WORDS_FILE );
+		return(-1);
+	}
+	
+	int count = 0;
+	while(!feof(words_fp)){
+		fscanf(words_fp,"%s",word_array[count++]); 
+		if(ferror(words_fp)){
+			perror("Erro lendo o arquivo " WORDS_FILE);
+			return(-1);
+		}
+	//	printf("%d\n",count);
+	}
+	fclose(words_fp);
+
+	//aqui a brincadeira começa
+
+//	printf("largest word: %d \n", largest(words_buffer));	
+//	printf("%d ",pull_word(temp,words_buffer,24852));
+//	pull_word(temp,words_buffer,3);
+	//printf(">>%s", word_array[1]);
+//	search(input);
+
+	char test[] = "Marcelo";
+	int ret = comb(test,"aero");
+	//printf("ret: %d, test[]:%s blank:%d",ret,test,blank("      "));
+		
+
+	return(search(input,0));
+}
