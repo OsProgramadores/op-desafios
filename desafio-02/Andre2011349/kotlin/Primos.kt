554b01d19ca5d7d15fbdd424110233f8028177c5
Andre2011349 <2011349@aluno.univesp.br>
2020-07-25 10:52:04 -0300

    Alterado lógica para verificar o numero testado no loop somente por fatores primos: @Andre2011349.

--- desafio-02/Andre2011349/kotlin/Primos.kt
+++ desafio-02/Andre2011349/kotlin/Primos.kt
@@ -7,41 +7,36 @@ versão 1.0
 Descrição:
 Lógica desenvolvida para o desafio 02 do grupo OsProgramadores com o objetivo de identificar os numeros
 primos entre 1 e 10000
- */
+O número primo 2 foi previamente inserido na lista para que a mesma não seja inicializada com valor Null */
 fun main() {
 
     // declaração das variáveis
-    var Lista_Primos = mutableListOf<String>()
-    var Num_Testado : Int = 2
+    var Lista_Primos = mutableListOf<Int>()
+    var Num_Testado : Int = 3
     var Qtd_Divisores : Int = 0
     var Qtd_Primos : Int = 0
-    var Num_Max : Int = 10000
+    var Num_Max : Long = 10000
 
-         Lista_Primos.clear() // limpa a lista
 
-    for (Indice_1:Int in 1..Num_Max){ // Quantidade de testes a serem feitos.
-        Qtd_Divisores=0 // Zera a váriavel para iniciar a verificação.
+    Lista_Primos.clear() // limpa a lista
+    Lista_Primos.add(2) // adiciona o 2 na lista
+    for (Indice_1: Long in 2..Num_Max ) { // Quantidade de testes a serem feitos.
 
-        for (Indice_2:Int in 1..Num_Testado){ // Loop para testar se o número é primo
+        for (Indice_2: Int in 0..(Qtd_Primos)) { // Loop para testar se o número é primo
 
-            if (Num_Testado % Indice_2 == 0){  // Divide o numero testado pelo indice do loop e verifica se o resto é 0 caso o resto seja 0 incrementa a variavel quantidade de divisores
-
-                Qtd_Divisores +=1 // Quantidade de divisores encontrados para o numero testado
+            if (Num_Testado % Lista_Primos.get(Indice_2) == 0)  {  // Verifica se o numero testado é divisivel por algum numero primo presente na lista
+                break
+            }else  if (Indice_2 == Qtd_Primos) {
+                Lista_Primos.add(Num_Testado) // se nao houve divisores na lista o numero é primo
+                Qtd_Primos += 1 // incrementa a quantidade de numeros primos encontrados
             }
 
         }
-        if (Qtd_Divisores == 2){ // verifica se a quantidade de divisores para o numero testado foram 2
-            Lista_Primos.add(Num_Testado.toString()) // caso tenha sido 2 adiciona o numero testado na lista
-            Qtd_Primos += 1 // incrementa a quantidade de numeros primos encontrados
-
-        }
-
-        Num_Testado +=1 //incrementa o numero testado para reiniciar o teste com o próximo valor
-
+        Num_Testado += 1 //incrementa o numero testado para reiniciar o teste com o próximo valor
     }
 
     // mostra a quantidade de numeros primos encontrados e quais foram estes números
-println("Entre os números 1 e 10000 existem $Qtd_Primos números primos.")
+println("Entre os números 1 e $Num_Max existem ${Qtd_Primos +1} números primos.")
     println("E eles são:")
 print(Lista_Primos)
 
