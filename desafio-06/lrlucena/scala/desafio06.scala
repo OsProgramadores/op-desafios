ac574594ca5c0abe88fb3ffab73b52ab81212c36
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2020-08-04 16:26:11 -0400

    Update desafio06.scala

--- desafio-06/lrlucena/scala/desafio06.scala
+++ desafio-06/lrlucena/scala/desafio06.scala
@@ -1,18 +1,19 @@
-def clean(s: String) = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
-
 implicit class D(r: String) { def -(s: String) = r.toSeq.diff(s).unwrap }
+def clean(s: String) = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
+def contains(letters: String)(word: String) = (word - letters).isEmpty
 
-def anagrams(input: String, words: List[String], list: List[String] = Nil): List[String] =
-  if (input.nonEmpty)
+def anagrams(input: String, words: List[String], list: List[String] = Nil): Iterator[String] =
+  if (input.isEmpty)
+    Iterator(list.reverse.mkString(" "))
+  else 
     words.tails.flatMap {
-      case wrds @ word :: _ =>
-        val unusedLetters = input - word
-        anagrams(unusedLetters, wrds.filter(_ - unusedLetters == ""), word :: list)
+      case word :: wrds =>
+        val unusedInput = input - word
+        anagrams(unusedInput, wrds.filter(contains(unusedInput)), word :: list)
       case _ => Nil
-    }.toList
-  else List(list.reverse.mkString(" "))
+    }
 
 val input = clean(args.headOption.getOrElse("oi gente"))
-val words = io.Source.fromFile("words.txt").getLines().filter(_ - input == "").toList.sorted
+val words = io.Source.fromFile("words.txt").getLines().filter(contains(input)).toList.sorted
 
 println(anagrams(input, words).mkString("\n"))
