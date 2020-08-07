7ddaa701297d99ce3eb03a4729e69a223d210f87
Mekylei-Belchior <mekylei@msn.com>
2020-08-07 12:05:13 -0300

    resolvido a questão 1 através da função (geral)

--- desafio-05/Mekylei-Belchior/python/desafio5.py
+++ desafio-05/Mekylei-Belchior/python/desafio5.py
@@ -14,9 +14,19 @@ import pandas as pd
 
 def geral(dados):
     """
-    Quem mais recebe e quem menos recebe na empresa e a média salarial da empresa. 
+    Quem mais recebe e quem menos recebe na empresa e a média salarial da empresa.
     """
-    pass
+    gmax = dados[dados['salario'] == dados['salario'].max()][['nome', 'sobrenome', 'salario']]
+    gmin = dados[dados['salario'] == dados['salario'].min()][['nome', 'sobrenome', 'salario']]
+    gavg = dados['salario'].mean()
+
+    for linha in gmax.itertuples():
+        print(f'global_max|{linha[1]} {linha[2]}|{linha[3]:.2f}')
+
+    for linha in gmin.itertuples():
+        print(f'global_min|{linha[1]} {linha[2]}|{linha[3]:.2f}')
+
+    print(f'global_avg|{gavg:.2f}')
 
 
 def area():
@@ -45,22 +55,23 @@ def empregados(arquivo):
 
     with open(arquivo, 'r', encoding='utf-8') as arquivo_json:
        dados_json = load(arquivo_json)
-    
+
     funcionarios = dados_json.pop('funcionarios')
     areas = dados_json.pop('areas')
-    
+
     base = pd.DataFrame(funcionarios)
     area = pd.DataFrame(areas)
     area.rename(columns={'codigo': 'area'}, inplace=True)
     empregados = pd.merge(base, area, on='area', suffixes=[None, '_area'])
-    
+
     return empregados
 
 
 def main(json):
     """ Função principal. """
 
-    Emp = empregados(json)   
+    emp = empregados(json)
+    geral(emp)
 
 
 if __name__ == '__main__':
