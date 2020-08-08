1d6df2f8767a8964f1340dcff9ae0baa46c6bfb6
Roberto Arruda <roberto0arruda@hotmail.com>
2020-08-07 23:42:13 -0400

    Atualizando  solução
    Atualizando solução com base na recomendação do @marcopaganini

--- desafio-02/roberto0arruda/python/primos.py
+++ desafio-02/roberto0arruda/python/primos.py
@@ -3,11 +3,8 @@ Autor: Roberto Arruda
 #02 - Primos
 """
 for num in range(1, 10000 + 1):
-    tot = 0
-    for n in range(1, num + 1):
-        if num % n == 0:
-            tot += 1
-
-    if tot == 2:
-        print('\033[32m', end='')
-        print("{} é primo".format(num), end='\n')
+  for n in range(2, num - 1):
+    if (num % n == 0):
+      break
+  else:
+    print("{} é primo".format(num), end='\n')
