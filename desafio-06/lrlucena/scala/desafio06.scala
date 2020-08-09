24401ecf2ed4fc1685279eee422d756381cec305
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2020-08-08 22:25:58 -0400

    Melhoria do desempenho do desafio 06
    ````terminal
    time scala desafio06.jar "maryhadalittleox" > maryhadalittleox.txt
    
    real    0m37.317s
    user    0m33.344s
    sys     0m6.078s
    ````

--- desafio-06/lrlucena/scala/desafio06.scala
+++ desafio-06/lrlucena/scala/desafio06.scala
@@ -1,19 +1,16 @@
+import io.Source.fromFile
+
 implicit class D(r: String) { def -(s: String) = r.toSeq.diff(s).unwrap }
 def clean(s: String) = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
 def contains(letters: String)(word: String) = (word - letters).isEmpty
 
-def anagrams(input: String, words: List[String], list: List[String] = Nil): Iterator[String] =
-  if (input.isEmpty)
-    Iterator(list.reverse.mkString(" "))
-  else 
-    words.tails.flatMap {
-      case word :: wrds =>
-        val unusedInput = input - word
-        anagrams(unusedInput, wrds.filter(contains(unusedInput)), word :: list)
-      case _ => Nil
-    }
+def anagrams(input: String, words: List[String], list: List[String] = Nil): Unit = input match {
+  case "" => println(list.mkString(" "))
+  case _  => for (word :: wrds <- words.tails) {
+               val unusedInput = input - word
+               anagrams(unusedInput, wrds.filter(contains(unusedInput)), word :: list) }}
 
 val input = clean(args.headOption.getOrElse("oi gente"))
-val words = io.Source.fromFile("words.txt").getLines().filter(contains(input)).toList.sorted
+val words = fromFile("words.txt").getLines().filter(contains(input)).toList.sorted.reverse
 
-println(anagrams(input, words).mkString("\n"))
+anagrams(input, words)
