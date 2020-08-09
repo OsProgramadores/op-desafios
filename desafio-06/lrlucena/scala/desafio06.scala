25fbfe46db07c088f30ddd2f4a672ae1d6296056
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2020-08-08 23:34:15 -0400

    Update desafio06.scala
    time scala -save desafio06.scala "maryhadalittleox" > maryhadalittleox.txt
    
    real    0m23.350s
    user    0m19.391s
    sys     0m5.641s

--- desafio-06/lrlucena/scala/desafio06.scala
+++ desafio-06/lrlucena/scala/desafio06.scala
@@ -1,16 +1,20 @@
-import io.Source.fromFile
-
-implicit class D(r: String) { def -(s: String) = r.toSeq.diff(s).unwrap }
 def clean(s: String) = s.toUpperCase.filter(c => c >= 'A' && c <= 'Z')
+
 def contains(letters: String)(word: String) = (word - letters).isEmpty
 
+def readFile(name: String) = io.Source.fromFile("words.txt").getLines()
+
+implicit class D(r: String) {
+  def -(s: String) = r.toSeq.diff(s).unwrap
+}
+
 def anagrams(input: String, words: List[String], list: List[String] = Nil): Unit = input match {
-  case "" => println(list.mkString(" "))
+  case "" => println(list.sorted.mkString(" "))
   case _  => for (word :: wrds <- words.tails) {
                val unusedInput = input - word
                anagrams(unusedInput, wrds.filter(contains(unusedInput)), word :: list) }}
 
 val input = clean(args.headOption.getOrElse("oi gente"))
-val words = fromFile("words.txt").getLines().filter(contains(input)).toList.sorted.reverse
+val words = readFile("words.txt").filter(contains(input)).toList.sortBy(_.length).reverse
 
 anagrams(input, words)
