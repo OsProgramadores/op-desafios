f1cc350a4e11e5d33edc92c10acc5e7e3f5bb351
Nathanbahia <nathanbabahia@gmail.com>
2020-08-18 08:44:46 -0300

    Altera modo de  concatenação com join

--- desafio-03/nathanbahia/python/desafio3.py
+++ desafio-03/nathanbahia/python/desafio3.py
@@ -4,18 +4,14 @@
 def is_palindrome(num):
     """ Converte um número em string, o inverte e verifica
     se ele é igual ao número original """
-
-    split_num = [n for n in str(num)]
-    split_num.reverse()
-    reversed_num = ''
-    for n in split_num:
-        reversed_num += n
+   
+    reversed_num = ''.join(list(str(num)))
     if int(reversed_num) == num:
         print(reversed_num)
 
 
 num_inicial = 0
-num_final = 10
+num_final = 1000
 
 print(f'\nNúmeros palíndromos entre {num_inicial} e {num_final}:\n')
 
