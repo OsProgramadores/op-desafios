d9073f8917dc55ef3f5683e01f95a788dde5302a
nathanbahia <nathanbabahia@gmail.com>
2020-08-20 16:09:33 -0300

    Desafio 12 em Python

---
+++ desafio-12/nathanbahia/python/potenciade2.py
@@ -0,0 +1,24 @@
+""" Solução do desafio 12 em Python """
+
+
+def is_power(num):
+    """ Verifica se um número é petência de 2 e imprime
+    na tela suas informações """
+    c = 0
+    n = 2
+    while True:
+        res = n ** c
+        if res > num:
+            print(num, 'false')
+            return
+        elif res == num:
+            print(num, 'true', c)
+            return
+        c += 1
+
+
+with open('d12.txt', 'r') as txt:
+    for line in txt.readlines():
+        num = int(line)
+        is_power(num)
+
