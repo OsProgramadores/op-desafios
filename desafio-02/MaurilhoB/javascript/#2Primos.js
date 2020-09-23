ccca4ddd2d17d6a88038729b9754e2968e9a6190
MaurilhoB <maurilhobatista450@gmail.com>
2020-09-23 13:28:05 -0300

    Solução desafio 02 javascript

---
+++ desafio-02/MaurilhoB/javascript/#2Primos.js
@@ -0,0 +1,14 @@
+for (var i = 1; i <= 10000; i++) {
+	imprimePrimos(i)
+}
+
+
+function imprimePrimos(input_number){
+	let divisores = 0;
+	for (var i = 1; i <= input_number; i++) {
+	if (input_number % i == 0){
+		divisores++
+	}
+	}
+	if (divisores == 2) {console.log(input_number)}
+}
