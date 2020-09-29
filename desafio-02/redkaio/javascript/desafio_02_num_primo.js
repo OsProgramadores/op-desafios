a465bb9689e47e50239b9cce9f586703ac5f6ace
redkaio <chrno.92@gmail.com>
2020-09-29 20:53:12 -0300

    Solucao do usuario @redkaio para o desafio 02 usando Javascript

--- desafio-02/redkaio/javascript/desafio_02_num_primo.js
+++ desafio-02/redkaio/javascript/desafio_02_num_primo.js
@@ -1,50 +1,41 @@
 function primo() {
     
-    //var i para os números que serão checados se são primos ou não
-    var i = 0;
-    
-    //var x para que haja a divisão entre i e x para averiguar o modal entre elas
-    var x = 0;
-    
-    //var divisor que será a soma de divisores totais entre os número i e número x
-    var divisor = 0;
+  //var i para os números que serão checados se são primos ou não
+  var i = 0;
   
-    for (i = 2; i <= 10000; i++) {
+  //var x para que haja a divisão entre i e x para averiguar o modal entre elas
+  var x = 0;
+  
+  //var divisor que será a soma de divisores totais entre os número i e número x
+  var divisor = 0;
+
+  for (i = 2; i <= 10000; i++) {
+    
+    //toda vez que o for loop começar e se e rodar novamente, devemos resetar a variável divisor
+    //para que ela não se acumule
+    divisor = 0;
+    
+    //a utilização do número x irá checar a quantidade de divisores entre a divisão i e x
+    //note que o for loop aqui irá realizar a divisão de todos os números x que começará do 1
+    //até chegar no número i
+    for (x = 1; x <= i; x++) {
       
-      //toda vez que o for loop começar e se e rodar novamente, devemos resetar a variável divisor
-      //para que ela não se acumule
-      divisor = 0;
+      //aqui checaremos se o modal entre i e x é zero
+      //caso seja zero, o número de divisores irá ser acrescido
       
-      //a utilização do número x irá checar a quantidade de divisores entre a divisão i e x
-      //note que o for loop aqui irá realizar a divisão de todos os números x que começará do 1
-      //até chegar no número i
-      for (x = 1; x <= i; x++) {
-        
-        //aqui checaremos se o modal entre i e x é zero
-        //caso seja zero, o número de divisores irá ser acrescido
-        
-        if (i % x == 0) {
-<<<<<<< HEAD
-          divisor += 1;
-=======
-<<<<<<< HEAD
-          divisor += 1;
-=======
-          divisor =+ 1;
->>>>>>> a2398105acfbe0b6e9e382efe4f02834bdef4695
->>>>>>> 7e49c2d32ecc5690b5b93d91c7a30dcbc1f87e4a
-        }
-      
+      if (i % x == 0) {
+        divisor += 1;
       }
-      //números primos só são divisíveis por 1 e por eles mesmos, portanto
-      //se o número de divisores for no total 2, o número é primo, e o código irá retornar o valor
-      //por fim, o divisor será resetado no início do código, para que seja feita a verificação novamente
-      if (divisor == 2) {
-        console.log(i);
-      }
+    
+    }
+    //números primos só são divisíveis por 1 e por eles mesmos, portanto
+    //se o número de divisores for no total 2, o número é primo, e o código irá retornar o valor
+    //por fim, o divisor será resetado no início do código, para que seja feita a verificação novamente
+    if (divisor == 2) {
+      console.log(i);
     }
   }
-  
-  primo();
-  
-  
+}
+
+primo();
+
