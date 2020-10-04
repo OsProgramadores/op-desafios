52b8cb8857f0c942dee0aa8c3beb189d13c9e60a
Marco Paganini <paganini@paganini.net>
2020-10-03 19:42:47 -0700

    Fix files with invalid utf-8 encoding and others. (#458)
    - Fix files with invalid utf-8 encoding (tip: inside vim, use
      set fileencoding=utf-8 and save.)
    - Removed binary files (notably, a jar file that somehow ended
      inside the repo.)

--- desafio-03/AnaPaulaMazi/java/polindromos.java
+++ desafio-03/AnaPaulaMazi/java/polindromos.java
@@ -6,42 +6,28 @@ import java.util.Scanner;
 
 
 public class polindromos {
-
 	public static void main(String[] args) {
-		
-			
 		Scanner teclado = new Scanner(System.in);
-				
 		System.out.println("Informe 2 numeros inteiros e positivos: ");
-			
-			try {
-				 Long num1 = teclado.nextLong();
-				 Long num2 = teclado.nextLong();
-				 
-				 if(num1 < 0 || num2 < 0) {
-					 System.out.println("Você deve informar 2 números inteiros positivos!");
-					 
-					 } else if (num1 > num2) {
-						 System.out.println("O primeiro número deve ser menor que o segundo número!");
-						 } else {
-						 
-								 System.out.println("Os polindrômicos do intervalo entre "+num1+" e "+num2+ " : ");
-								 
-								 for(Long i = num1; i <= num2; i++) {
-									 
-									 String reverso = new StringBuilder( i + "").reverse().toString();
-									 
-									 	if(reverso.equals(i + "")) {
-									 		System.out.println(i);
-									 	}
-								  }
-						 	}
-				} catch (InputMismatchException e) {
-					System.out.println("Os números devem ser inteiros, com no maximo 64 bits!");
-					
-				}
-			 
-			 teclado.close();
-		}			
+		try {
+			Long num1 = teclado.nextLong();
+			Long num2 = teclado.nextLong();
+			if(num1 < 0 || num2 < 0) {
+			        System.out.println("VocÃª deve informar 2 nÃºmeros inteiros positivos!");
+			} else if (num1 > num2) {
+				 System.out.println("O primeiro nÃºmero deve ser menor que o segundo nÃºmero!");
+			} else {
+			        System.out.println("Os polindrÃ´micos do intervalo entre "+num1+" e "+num2+ " : ");
+				for(Long i = num1; i <= num2; i++) {
+				        String reverso = new StringBuilder( i + "").reverse().toString();
+					if(reverso.equals(i + "")) {
+					        System.out.println(i);
+				        }
+			        }
+			}
+		} catch (InputMismatchException e) {
+		        System.out.println("Os nÃºmeros devem ser inteiros, com no maximo 64 bits!");
+		}
+	        teclado.close();
 	}
-
+}
