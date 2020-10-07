907fad157f8bcc25a93d7c1702dd9ed7b27f734c
@marciofiladelfo <marcio.kabana@gmail.com>
2020-10-07 18:06:26 -0300

    Feito desafio

--- desafio-02/marciofiladelfo/javascript/primo.js
+++ desafio-02/marciofiladelfo/javascript/primo.js
@@ -1,11 +1,11 @@
 function getPrime(valor){
-    let result = 0;  
+    let result = 0;
     for(let counter = 1; counter <= valor; counter++){
         let divisor = 0;
         for(let aux = 1 ; aux <= counter ; aux++){
             if(counter % aux === 0){
                 divisor++;
-            }  
+            }
         }
         if(divisor === 2){
             result += `${counter}, `;
