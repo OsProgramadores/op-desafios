aa11129a484eb095997c17733a5878ef3a264117
ArianeVCarvalho <carvalhobva@gmail.com>
2020-10-13 13:46:57 -0400

    Desafio 02.

--- desafio-02/arianevcarvalho/javascript/primos.js
+++ desafio-02/arianevcarvalho/javascript/primos.js
@@ -1,16 +1,16 @@
 function identifier(){
-    let primeNumbers=[],divisores = 0, l=0
-    for (i=2; i<10000; i++){ 
-        for (n=0; n<=i;n++){
-            if (i%n==0){
-                divisores +=1
-            }
+let primeNumbers=[],divisores = 0, l=0
+for (i=2; i<10000; i++){ 
+    for (n=0; n<=i;n++){
+        if (i%n==0){
+            divisores +=1
         }
-        if (divisores<3){
-            primeNumbers[l]=i
-            l+=1
-        }
-         divisores=0
+    }
+    if (divisores<3){
+         primeNumbers[l]=i
+        l+=1
+    } 
+        divisores=0
     }
    return primeNumbers
 }
