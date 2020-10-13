296703edd36604044ce19eb9fe03940f0a438c88
ArianeVCarvalho <carvalhobva@gmail.com>
2020-10-13 14:00:45 -0400

    DESAFIO02.

--- desafio-02/arianevcarvalho/javascript/primos.js
+++ desafio-02/arianevcarvalho/javascript/primos.js
@@ -1,6 +1,6 @@
 function identifier(){
 let primeNumbers=[],divisores = 0, l=0
-for(let i=2;i<10000;i++){ 
+for(let i=2;i<10000;i++){
     for(n=0;n<=i;n++){
         if(i%n==0){
             divisores+=1
