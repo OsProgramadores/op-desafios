13eca54151f1f6e85114e7a8ce370250ae2e11ec
rafaeldss <rafaeldss199916@gmail.com>
2020-10-17 00:47:41 -0300

    Solução do desafio 03 em C

---
+++ desafio-03/rafaeldss/c/palindromes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int begin = 100;
+    int end = 1000;
+    
+    for (int i = begin; i <= end; i++) {
+        int aux = i;
+        int rev = 0;
+
+        while (aux != 0) {
+            rev = rev * 10 + aux % 10; 
+            aux = aux / 10;
+        }
+        if (rev == i) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
