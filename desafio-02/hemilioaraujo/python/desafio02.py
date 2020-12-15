7711bd136b4ce47d391e015d48b63dd062418703
HLAM <hemilioaraujo@gmail.com>
2020-12-15 08:40:59 -0300

    removido trecho desnecessário
    De acordo com review

--- desafio-02/hemilioaraujo/python/desafio02.py
+++ desafio-02/hemilioaraujo/python/desafio02.py
@@ -8,20 +8,7 @@ fim = 10000
 trigger = True
 primos = []
 
-while inicio < fim:
-    divisor = 1
-    cont = 0
-    # encontra o primeiro número primo
-    while divisor <= inicio and trigger:
-        if inicio % divisor == 0:
-            cont += 1
-        if inicio == divisor and cont == 2:
-            primos.append(inicio)
-            trigger = False
-        divisor += 1
-    inicio += 1
-
-for num in range(primos[0]+1, fim):
+for num in range(2, fim):
     for item in primos:
         if num % item == 0:
             break
