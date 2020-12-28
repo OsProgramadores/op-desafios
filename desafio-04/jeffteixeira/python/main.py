ce586f2585cd4484abbf31362de1b5dc3eaa60da
Jefferson Teixeira <jeffersonads2019@gmail.com>
2020-12-28 00:43:55 -0300

    Added solution of the challenge 4 in Python

---
+++ desafio-04/jeffteixeira/python/main.py
@@ -0,0 +1,38 @@
+"""Counts the pieces of the board"""
+
+
+def main():
+    """Function main of the program"""
+    pieces = {
+        1: { 'name': 'Peão', 'quantity': 0 },
+        2: { 'name': 'Bispo', 'quantity': 0 },
+        3: { 'name': 'Cavalo', 'quantity': 0 },
+        4: { 'name': 'Torre', 'quantity': 0 },
+        5: { 'name': 'Rainha', 'quantity': 0 },
+        6: { 'name': 'Rei', 'quantity': 0 }
+    }
+    board = get_board()
+    count_pieces(pieces, board)
+    show_pieces(pieces)
+
+
+def get_board():
+    """Function that returns a filled matrix that represents the board"""
+    return [list(map(int, input().split())) for i in range(8)]
+
+
+def count_pieces(pieces, board):
+    """Function that counts the occurrences of each piece on the board"""
+    for line in board:
+        for number in pieces.keys():
+            pieces[number]['quantity'] += line.count(number)
+
+
+def show_pieces(pieces):
+    """Function that shows the name and occurrences of each piece on the board"""
+    for piece in pieces.values():
+        print('{}: {} peça(s)'.format(piece['name'], piece['quantity']))
+
+
+if __name__ == "__main__":
+    main()
