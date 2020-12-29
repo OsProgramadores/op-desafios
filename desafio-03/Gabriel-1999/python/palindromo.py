5f38abd0530df1f41f40001e7017372b09209646
Clesh <ekokoclesh07@gmail.com>
2020-12-30 00:12:22 +0200

    Minha solucao do desafio 3 de palindromo

--- desafio-03/Gabriel-1999/python/palindromo.py
+++ desafio-03/Gabriel-1999/python/palindromo.py
@@ -1,11 +1,7 @@
-# list to store the input number
 listNumber = []
-# inicial number
 inicio = int(input("Numero Inicial: "))
-# finally number
 fim = int(input("Numero Final: "))
 print("\n\n")
-# verifying if the start is great than the end number
 if inicio > fim:
     for x in range(fim,inicio+1):
         listNumber.append(x)
