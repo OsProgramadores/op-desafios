92b4c850c2addac572f574f4dc217c73fc9abaab
marcellobatiista <batista.marcelo34@gmail.com>
2020-12-29 17:32:23 -0300

    Solução para listagem dos números primos

--- desafio-02/marcellobatiista/java/NumerosPrimos.java
+++ desafio-02/marcellobatiista/java/NumerosPrimos.java
@@ -1,24 +1,21 @@
 public class NumerosPrimos {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        int quantidade = 10000;
 		
-		int quantidade = 10000;
-		
-		for (int numero = 1; numero <= quantidade; numero++) {
-			int qt_divisores = 0;
-			for (int div = 1; div <= quantidade; div++) {	
-				if (numero % div == 0) {
-					qt_divisores += 1;
+        for (int numero = 1; numero <= quantidade; numero++) {
+            int qt_divisores = 0;
+            for (int div = 1; div <= quantidade; div++) {	
+                if (numero % div == 0) {
+                    qt_divisores += 1;
 					
-				}
-				//if (qt_divisores > 2) {
-				//	break;
-				//}
-			}
-			if (numero == 1 || qt_divisores == 2) {
-				System.out.printf("%d ", numero);
-			}
-		}
-	}
-
+                }
+                //if (qt_divisores > 2) {
+                //    break;
+                //}
+             }
+             if (numero == 1 || qt_divisores == 2) {
+                 System.out.printf("%d ", numero);
+             }
+        }
+    }
 }
