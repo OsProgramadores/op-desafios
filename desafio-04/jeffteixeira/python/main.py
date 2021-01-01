ee08fe5b10d0f43cc20e73428f804350de1ee0bd
Jefferson Teixeira <jeffersonads2019@gmail.com>
2021-01-01 15:20:04 -0300

    Updated implementation

--- desafio-04/jeffteixeira/python/main.py
+++ desafio-04/jeffteixeira/python/main.py
@@ -1,86 +1,59 @@
-"""Counts the pieces of the board"""
+"""Counts the occurrences of each piece"""
 
 
 def main():
     """Function main of the program"""
-    pieces = {
-        1: { 'name': 'Peão', 'quantity': 0 },
-        2: { 'name': 'Bispo', 'quantity': 0 },
-        3: { 'name': 'Cavalo', 'quantity': 0 },
-        4: { 'name': 'Torre', 'quantity': 0 },
-        5: { 'name': 'Rainha', 'quantity': 0 },
-        6: { 'name': 'Rei', 'quantity': 0 }
-    }
-    board = get_valid_board()
-    count_pieces_on_the_board(board, pieces)
+    pieces = get_pieces()
+    count_pieces(pieces)
     show_pieces(pieces)
 
 
-def get_valid_board():
-    """Function that returns a filled matrix that represents the board"""
-    board = []
+def get_pieces():
+    """Function that returns a dictionary that represents the pieces"""
+    return {
+        1: { 'name': 'Peão', 'current_quantity': 0, 'maximum_quantity': 16 },
+        2: { 'name': 'Bispo', 'current_quantity': 0, 'maximum_quantity': 4 },
+        3: { 'name': 'Cavalo', 'current_quantity': 0, 'maximum_quantity': 4 },
+        4: { 'name': 'Torre', 'current_quantity': 0, 'maximum_quantity': 4 },
+        5: { 'name': 'Rainha', 'current_quantity': 0, 'maximum_quantity': 2 },
+        6: { 'name': 'Rei', 'current_quantity': 0, 'maximum_quantity': 2 }
+    }
+
+
+def count_pieces(pieces):
+    """Function that counts the occurrences of each piece"""
+    accepted_codes = [0, 1, 2, 3, 4, 5, 6]
     for i in range(8):
-        line = []
         for j in range(8):
-            message = 'Enter a number to line {} and column {} between 0 and 8: '.format(
-                i + 1, j + 1
-            )
-            number = get_valid_number(message)
-            line.append(number)
-        board.append(line)
-    if is_valid(board):
-        return board
-    print('Invalid board!\n')
-    return get_valid_board()
+            msg = 'Line {} and column {}: '.format(i + 1, j + 1)
+            code = get_valid_code(accepted_codes, msg)
+            if code > 0:
+                piece = pieces[code]
+                piece['current_quantity'] += 1
+                if piece['current_quantity'] == piece['maximum_quantity']:
+                    print('\nThe code {} has reached its limit of {} occurrences!\n'
+                    .format(code, pieces[code]['maximum_quantity']))
+                    accepted_codes.remove(code)
 
 
-def get_valid_number(message):
-    """Function that returns a valid number between 0 and 8"""
+def get_valid_code(accepted_codes, message):
+    """Function that returns a valid code of a piece"""
+    message_with_accepted_codes = ', '.join(list(map(str, accepted_codes)))
     while True:
         try:
-            number = int(input(message))
-            if 0 <= number <= 8:
-                return number
+            code = int(input(message))
+            if code in accepted_codes:
+                return code
+            print('\nAccepted codes: {}\n'.format(message_with_accepted_codes))
         except ValueError:
-            print('Only numbers are acepted!')
-
-
-def is_valid(board):
-    """Function that checks if the board is valid or not"""
-    quantity_per_number = {
-        0: 0,
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-        7: 0,
-        8: 0
-    }
-    for line in board:
-        for number in line:
-            quantity_per_number[number] += 1
-    return quantity_per_number[1] <= 16 and \
-        quantity_per_number[2] <= 4 and \
-            quantity_per_number[3] <= 4 and \
-                quantity_per_number[4] <= 4 and \
-                    quantity_per_number[5] <= 2 and \
-                        quantity_per_number[6] <= 2
-
-
-def count_pieces_on_the_board(board, pieces):
-    """Function that counts the occurrences of each piece on the board"""
-    for line in board:
-        for number in pieces.keys():
-            pieces[number]['quantity'] += line.count(number)
+            print('\nOnly numbers are acepted!\n')
 
 
 def show_pieces(pieces):
-    """Function that shows the name and occurrences of each piece on the board"""
+    """Function that shows the name and occurrences of each piece"""
     print()
     for piece in pieces.values():
-        print('{}: {} peça(s)'.format(piece['name'], piece['quantity']))
+        print('{}: {} peça(s)'.format(piece['name'], piece['current_quantity']))
 
 
 if __name__ == "__main__":
