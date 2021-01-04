0f77b96138973ab6d1a10f2782e400289bed2026
marcellobatiista <batista.marcelo34@gmail.com>
2021-01-03 23:00:37 -0300

    Solução Xadrez

--- desafio-04/marcellobatiista/java/Xadrez.java
+++ desafio-04/marcellobatiista/java/Xadrez.java
@@ -1,21 +1,19 @@
-
 public class Xadrez {
-
     public static void main(String[] args) {
         String tab = 
-                     "4 3 2 5 6 2 3 4" + 
-                     "1 1 1 1 1 1 1 1" + 
-                     "0 0 0 0 0 0 0 0" + 
-                     "0 0 0 0 0 0 0 0" + 
-                     "0 0 0 0 0 0 0 0" + 
-                     "0 0 0 0 0 0 0 0" + 
-                     "1 1 1 1 1 1 1 1" + 
-                     "4 3 2 5 6 2 3 4";
-        
+                    "4 3 2 5 6 2 3 4"+
+                    "1 1 1 1 1 1 1 1"+
+                    "0 0 0 0 0 0 0 0"+
+                    "0 0 0 0 0 0 0 0"+
+                    "0 0 0 0 0 0 0 0"+
+                    "0 0 0 0 0 0 0 0"+
+                    "1 1 1 1 1 1 1 1"+
+                    "4 3 2 5 6 2 3 4";
+
         String tipo[] = {"vazio" ,"Peao", "Bispo", "Cavalo", "Torre", "Rainha", "Rei"};
         // pecas = {vazio, peao, bispo, cavalo, torre, rainha, rei}
         int quant[] = {0, 0, 0, 0, 0, 0, 0};
-        
+
         int i = 0;
         while (i <= tab.length() - 1) {
             for (int peca = 1; peca < quant.length; peca++) {
@@ -26,10 +24,9 @@ public class Xadrez {
             }
             i++;
         }
-        
+
         for (int j = 1; j < quant.length; j++) {
-            System.out.println(tipo[j]+": "+quant[j]+" peças(s)");
+            System.out.println(tipo[j]+": "+quant[j]+" peÃ§as(s)");
         }
     }
-
 }
