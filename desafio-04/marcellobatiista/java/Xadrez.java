255cf7cc1d229e201b7b10ca172c703f77d2a1e3
marcellobatiista <batista.marcelo34@gmail.com>
2021-01-03 22:41:36 -0300

    Solução Xadrez

--- desafio-04/marcellobatiista/java/Xadrez.java
+++ desafio-04/marcellobatiista/java/Xadrez.java
@@ -1,33 +1,33 @@
 public class Xadrez {
 
     public static void main(String[] args) {
-        String tab = 
-                     "4 3 2 5 6 2 3 4\r\n" + 
-                     "1 1 1 1 1 1 1 1\r\n" + 
-                     "0 0 0 0 0 0 0 0\r\n" + 
-                     "0 0 0 0 0 0 0 0\r\n" + 
-                     "0 0 0 0 0 0 0 0\r\n" + 
-                     "0 0 0 0 0 0 0 0\r\n" + 
-                     "1 1 1 1 1 1 1 1\r\n" + 
-                     "4 3 2 5 6 2 3 4";
-        
-        String tipo[] = {"vazio" ,"Peao", "Bispo", "Cavalo", "Torre", "Rainha", "Rei"};
-        //pecas = {vazio, peao, bispo, cavalo, torre, rainha, rei}
-        int quant[] = {0, 0, 0, 0, 0, 0, 0};
-        
-        int i = 0;
-        while (i <= tab.length() - 1) {
-            for (int peca = 1; peca < quant.length; peca++) {
-                while (tab.charAt(i) == Integer.toString(peca).charAt(0)) {
-                    quant[peca]++;
-                    break;
+    String tab = 
+                "4 3 2 5 6 2 3 4\r\n"+
+                "1 1 1 1 1 1 1 1\r\n"+
+                "0 0 0 0 0 0 0 0\r\n"+
+                "0 0 0 0 0 0 0 0\r\n"+
+                "0 0 0 0 0 0 0 0\r\n"+
+                "0 0 0 0 0 0 0 0\r\n"+
+                "1 1 1 1 1 1 1 1\r\n"+
+                "4 3 2 5 6 2 3 4";
+		
+                String tipo[] = {"vazio" ,"Peao", "Bispo", "Cavalo", "Torre", "Rainha", "Rei"};
+                // pecas = {vazio, peao, bispo, cavalo, torre, rainha, rei}
+                int quant[] = {0, 0, 0, 0, 0, 0, 0};
+
+                int i = 0;
+                while (i <= tab.length() - 1) {
+                    for (int peca = 1; peca < quant.length; peca++) {
+                        while (tab.charAt(i) == Integer.toString(peca).charAt(0)) {
+                            quant[peca]++;
+                            break;
+                        }
+                    }
+                    i++;
                 }
-            }
-            i++;
-        }
-        
-        for (int j = 1; j < quant.length; j++) {
-            System.out.println(tipo[j]+": "+quant[j]+" peças(s)");
-        }
+
+                for (int j = 1; j < quant.length; j++) {
+                    System.out.println(tipo[j]+": "+quant[j]+" peças(s)");
+                }
     }
 }
