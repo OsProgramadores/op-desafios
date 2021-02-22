344a977c4036566ca8bc1b96d14d1629f8f6d253
Teiji Watanabe <teijiwatanabe01@gmail.com>
2021-02-22 08:46:44 -0400

    Requested changes

--- desafio-02/TeijiW/elixir/prime_numbers/lib/prime_numbers.ex
+++ desafio-02/TeijiW/elixir/prime_numbers/lib/prime_numbers.ex
@@ -1,22 +1,25 @@
 defmodule PrimeNumbers do
   def main do
     primes(1, 10000)
-  end
-
-  def primes(starting, ending) do
-    Enum.to_list(starting..ending)
-    |> Enum.filter(&is_prime?/1)
     |> IO.inspect(limit: :infinity)
   end
 
-  defp is_prime?(number) when number == 1, do: 1
+  def primes(starting, ending) do
+    result =
+      Enum.to_list(starting..ending)
+      |> Enum.filter(&is_prime?/1)
+
+    {:ok, result}
+  end
+
+  defp is_prime?(number) when number == 2, do: true
 
   defp is_prime?(number) do
     result =
       Enum.to_list(2..(number - 1))
-      |> Enum.reduce_while([], fn elem, acc -> stop_if_over_two(elem, acc, number) end)
+      |> Enum.reduce_while([1, number], fn elem, acc -> stop_if_over_two(elem, acc, number) end)
 
-    length(result ++ [1, number]) <= 2
+    length(result) <= 2
   end
 
   defp stop_if_over_two(_elem, acc, _number) when length(acc) > 2, do: {:halt, acc}
