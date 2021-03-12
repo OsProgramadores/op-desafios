9f1f63da8714c4a16c2284f470d778b9ffdb0cbe
Alcione Ferreira <alcione@3oficioddos.local>
2021-03-12 09:48:32 -0400

    Desafio corrigido espaços

--- desafio-05/ksombrah/php/processar.php
+++ desafio-05/ksombrah/php/processar.php
@@ -23,189 +23,189 @@ set_time_limit(0);
 if ($argc >= 2)
 	{
 	$arq = $argv[1];
-	if (file_exists ( $arq ))
-	{
-	$response = file_get_contents($arq); 
-	$response=str_replace('},
-
-]',"}
-
-]",$response);
-	$data = json_decode($response);
-	/* Global Max e Min - Quem recebe mais */
-	$max = 0.0;
-	$min = 9999999.9;
-	$avg = 0.0;
-	$qtd = count($data->funcionarios);
-	foreach($data->funcionarios as $funcionario)
+	if (file_exists($arq))
 		{
-		if ($funcionario->salario > $max)
+		$response=file_get_contents($arq); 
+		$response=str_replace('},
+
+	]',"}
+
+	]",$response);
+		$data=json_decode($response);
+		/* Global Max e Min - Quem recebe mais */
+		$max=0.0;
+		$min=9999999.9;
+		$avg=0.0;
+		$qtd=count($data->funcionarios);
+		foreach($data->funcionarios as $funcionario)
 			{
-			$max = $funcionario->salario;
-			}
-		if ($funcionario->salario < $min)
-			{
-			$min = $funcionario->salario;
-			}
-		$avg += $funcionario->salario;
-		}
-	$avg = $avg/$qtd;
-	/* Buscando Max e Min */
-	$q_max = "";
-	$q_min = "";
-	$q_last = "";
-	$imax = $imin = false;
-	foreach($data->funcionarios as $funcionario)
-		{
-		if ($funcionario->salario == $max)
-			{
-			if ($imax)
+			if ($funcionario->salario > $max)
 				{
-				$q_max .= "\n";
-				$q_last .= "\n";
+				$max=$funcionario->salario;
 				}
-			$q_max .= "global_max|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
-			$q_last .= "last_name_max|".$funcionario->sobrenome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
-			$imax = true;
-			}
-		if ($funcionario->salario == $min)
-			{
-			if ($imin)
+			if ($funcionario->salario < $min)
 				{
-				$q_min .= "\n";
+				$min=$funcionario->salario;
 				}
-			$q_min .= "global_min|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
-			$imin = true;
+			$avg+=$funcionario->salario;
 			}
-		}
-		echo ('
+		$avg=$avg/$qtd;
+		/* Buscando Max e Min */
+		$q_max="";
+		$q_min="";
+		$q_last="";
+		$imax=$imin=false;
+		foreach($data->funcionarios as $funcionario)
+			{
+			if ($funcionario->salario == $max)
+				{
+				if ($imax)
+					{
+					$q_max.="\n";
+					$q_last.="\n";
+					}
+				$q_max.="global_max|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
+				$q_last.="last_name_max|".$funcionario->sobrenome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
+				$imax=true;
+				}
+			if ($funcionario->salario == $min)
+				{
+				if ($imin)
+					{
+					$q_min.="\n";
+					}
+				$q_min.="global_min|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
+				$imin=true;
+				}
+			}
+			echo ('
 '.$q_max.'
 '.$q_min.'
 global_avg|'.number_format($avg,2,'.',''));
-	/*Varetura por área*/
-	$qtd_z = false;
-	$most = 0;
-	$least = 99999;
-	$ia=0;
-	foreach($data->areas as $area)
-		{
-		$max_a = 0.0;
-		$min_a = 9999999.9;
-		$avg_a = 0.0;
-		$qtd_a = 0;
-		$q_max_a = "";
-		$q_min_a = "";
-		$q_avg_a = "";
-		$imax = $imin = false;
-		foreach($data->funcionarios as $funcionario)
+		/*Varetura por área*/
+		$qtd_z=false;
+		$most=0;
+		$least=99999;
+		$ia=0;
+		foreach($data->areas as $area)
 			{
-			if (strcmp($funcionario->area,$area->codigo) == 0)
-				{
-				if ($funcionario->salario > $max_a)
-					{
-					$max_a = $funcionario->salario;
-					}
-				if ($funcionario->salario < $min_a)
-					{
-					$min_a = $funcionario->salario;
-					}
-				$avg_a += $funcionario->salario;
-				$qtd_a++;
-				}
-			}
-		$areaq[$ia]['nome'] = $area->nome;
-		$areaq[$ia]['qtd'] = $qtd_a;
-		if ($qtd_a > $most)
-			{
-			$most = $qtd_a;
-			$q_most = "most_employees|".$area->nome."|".$qtd_a;
-			}
-		if ($qtd_a < $least)
-			{
-			$least = $qtd_a;
-			$q_least = "least_employees|".$area->nome."|".$qtd_a;
-			}
-		if ($qtd_a == 0)
-			{
-			$avg_a = 0;
-			if ($qtd_z)
-				{
-				$q_least .= "\nleast_employees|".$area->nome."|".$qtd_a;
-				}
-			$qtd_z = true;
-			}
-		else
-			{
-			$avg_a = $avg_a/$qtd_a;
+			$max_a=0.0;
+			$min_a=9999999.9;
+			$avg_a=0.0;
+			$qtd_a=0;
+			$q_max_a="";
+			$q_min_a="";
+			$q_avg_a="";
 			foreach($data->funcionarios as $funcionario)
 				{
 				if (strcmp($funcionario->area,$area->codigo) == 0)
 					{
-					if ($funcionario->salario == $max_a)
+					if ($funcionario->salario > $max_a)
 						{
-						if ($imax)
-							{
-							$q_max_a .= "\n";
-							}
-						$q_max_a .= "area_max|".$area->nome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
-						$imax = true;
+						$max_a=$funcionario->salario;
 						}
-					if ($funcionario->salario == $min_a)
+					if ($funcionario->salario < $min_a)
 						{
-						if ($imin)
+						$min_a=$funcionario->salario;
+						}
+					$avg_a+=$funcionario->salario;
+					$qtd_a++;
+					}
+				}
+			$areaq[$ia]['nome']=$area->nome;
+			$areaq[$ia]['qtd']=$qtd_a;
+			if ($qtd_a > $most)
+				{
+				$most=$qtd_a;
+				$q_most="most_employees|".$area->nome."|".$qtd_a;
+				}
+			if ($qtd_a < $least)
+				{
+				$least=$qtd_a;
+				$q_least="least_employees|".$area->nome."|".$qtd_a;
+				}
+			if ($qtd_a == 0)
+				{
+				$avg_a=0;
+				if ($qtd_z)
+					{
+					$q_least.="\nleast_employees|".$area->nome."|".$qtd_a;
+					}
+				$qtd_z=true;
+				}
+			else
+				{
+				$avg_a=$avg_a/$qtd_a;
+				$imax=$imin=false;
+				foreach($data->funcionarios as $funcionario)
+					{
+					if (strcmp($funcionario->area,$area->codigo) == 0)
+						{
+						if ($funcionario->salario == $max_a)
 							{
-							$q_min_a .= "\n";
+							if ($imax)
+								{
+								$q_max_a.="\n";
+								}
+							$q_max_a.="area_max|".$area->nome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
+							$imax=true;
 							}
-						$q_min_a .= "area_min|".$area->nome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
-						$imin = true;
+						if ($funcionario->salario == $min_a)
+							{
+							if ($imin)
+								{
+								$q_min_a.="\n";
+								}
+							$q_min_a .= "area_min|".$area->nome."|".$funcionario->nome." ".$funcionario->sobrenome."|".number_format($funcionario->salario,2,'.','');
+							$imin=true;
+							}
 						}
 					}
 				}
-			}
-		$q_avg_a = "area_avg|".$area->nome."|".number_format($avg_a,2,'.','');
-		$ia++;
-		echo ('
+			$q_avg_a="area_avg|".$area->nome."|".number_format($avg_a,2,'.','');
+			$ia++;
+			echo ('
 '.$q_max_a.'
 '.$q_min_a.'
 '.$q_avg_a);
-		}
-	if ($most > 0)
-		{
-		$q = 0;
-		foreach($areaq as $am)
+			}
+		if ($most > 0)
 			{
-			if ($am['qtd'] == $most)
+			$q = 0;
+			foreach($areaq as $am)
 				{
-				$q++;
-				}
-			if ($q > 1)
-				{
-				$q_most .= "\nmost_employees|".$am['nome']."|".$am['qtd'];
+				if ($am['qtd'] == $most)
+					{
+					$q++;
+					}
+				if ($q > 1)
+					{
+					$q_most.="\nmost_employees|".$am['nome']."|".$am['qtd'];
+					}
 				}
 			}
-		}
-	if ($least > 0)
-		{
-		$q = 0;
-		foreach($areaq as $am)
+		if ($least > 0)
 			{
-			if ($am['qtd'] == $least)
+			$q = 0;
+			foreach($areaq as $am)
 				{
-				$q++;
-				}
-			if ($q > 1)
-				{
-				$q_leat .= "\nleast_employees|".$am['nome']."|".$am['qtd'];
+				if ($am['qtd'] == $least)
+					{
+					$q++;
+					}
+				if ($q > 1)
+					{
+					$q_leat.="\nleast_employees|".$am['nome']."|".$am['qtd'];
+					}
 				}
 			}
-		}
 
-	echo ('
+		echo ('
 '.$q_most.'
 '.$q_least.'
 '.$q_last.'
-	');
-	}
+		');
+		}
 	}
 else
 	{
