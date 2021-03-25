704af69bfec1f38c91956a2bb56d26602f959639
Gabriel Mendonça <gabriel.mendonca@agenciamop.com.br>
2021-03-25 09:17:12 -0300

    Enviar desafio 03

---
+++ desafio-03/brunoom1/typescript/e-palindromo.ts
@@ -0,0 +1,24 @@
+export const ePalindromo = (n: string | number): boolean => {
+
+  let s:string = '';
+
+  if (typeof(n) === 'number') {
+    s = n.toString();
+  } else if (typeof(n) === 'string') {
+    s = n;
+  }
+  
+  if (s.indexOf('-') !== -1) {
+    s = s.substring(1, s.length);
+  }
+
+  let i = 0;
+  while (i < s.length) {
+    if (s[i] !== s[(s.length - 1) - i]) {
+      return false;
+    }
+    i++;
+  }
+
+  return true;
+}
