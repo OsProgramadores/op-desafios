69d1b1e6c96a708e6801f41f6204b451bebcaa42
Thiago Soares <thiagosantana12@gmail.com>
2021-03-27 14:46:57 -0300

    Desafio 2, realizado por Thiago Soares

---
+++ desafio-02/thiagosoares/desafio2_numerosPrimos.js
@@ -0,0 +1,15 @@
+for(let i = 2; i <= 1000; i++){
+  let nPrimo = true;
+
+    for(let j = 2; j < i; j++){
+       if(i % j === 0){
+         nPrimo = false;
+         break;
+       }
+      
+    }
+    
+  if(nPrimo){
+      console.log(i);
+  }
+}
