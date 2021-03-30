9f0b474fd264b64c4c1aa7c21a3f772a9651270b
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2021-03-30 07:30:30 -0300

    Update #2.js

--- desafio-02/dansenpir/javascript/#2.js
+++ desafio-02/dansenpir/javascript/#2.js
@@ -3,7 +3,7 @@ listNumbers = []
 isPrime = (number) => {
     if (number < 2) { return false }
     if (number % 2 == 0 && number > 2) { return false }
-    
+
     for (let i = 3; i <= Math.sqrt(number); i++) {
         if (number % i === 0) { return false }
     }
