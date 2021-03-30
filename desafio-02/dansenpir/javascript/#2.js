0f0c92a7a4c8405fd725b1323a68e5f347a27576
dansenpir <senegalha@protonmail.com>
2021-03-30 07:16:43 -0300

    Inclusão de participante no desafio 02: @dansenpir

---
+++ desafio-02/dansenpir/javascript/#2.js
@@ -0,0 +1,18 @@
+listNumbers = []
+
+isPrime = (number) => {
+    if (number < 2) { return false }
+    if (number % 2 == 0 && number > 2) { return false }
+    
+    for (let i = 3; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) { return false }
+    }
+
+    return true
+}
+
+for (let i = 0; i < 10000; i++) {
+    listNumbers.push(i)
+}
+
+console.log(listNumbers.filter(isPrime))
