75d7da8c30afae3e13be2ba9686f6754e0bf8fb7
m1thrandirr <pedrolnascimento2004@gmail.com>
2021-04-06 13:13:30 -0300

    Iteração do loop corrigida, espaços removidos.

--- desafio-02/m1thrandirr/python/numprimos.py
+++ desafio-02/m1thrandirr/python/numprimos.py
@@ -1,13 +1,12 @@
 primos_list = []
 
-for i in range(2,1001):
+for i in range(2,10001):
     for j in range(2,i+1):
         if i%j==0:
-            if j==i:
+            if i==j:
                 primos_list.append(i)
             else:
                 break
 
 for k in range(len(primos_list)):
-    print(primos_list[k])
-             
+    print(primos_list[k])  
