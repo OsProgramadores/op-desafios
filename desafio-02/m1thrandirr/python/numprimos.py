fde821c3d5031eb94122bb45c40c813995a07a68
m1thrandirr <pedrolnascimento2004@gmail.com>
2021-04-06 15:16:00 -0300

    Código com docstring.

--- desafio-02/m1thrandirr/python/numprimos.py
+++ desafio-02/m1thrandirr/python/numprimos.py
@@ -1,9 +1,10 @@
+"""Código que imprime todos os números primos entre 1 e 10000"""
 primos_list = []
 
 for i in range(2,10001):
     for j in range(2,i+1):
-        if i%j==0:
-            if i==j:
+        if i%j == 0:
+            if i == j:
                 primos_list.append(i)
             else:
                 break
