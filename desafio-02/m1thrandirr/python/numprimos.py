8cb84835cf45cfd90f7cd7cadf0849d5e8bcf2f1
m1thrandirr <77247842+m1thrandirr@users.noreply.github.com>
2021-04-06 13:07:49 -0300

    Update numprimos.py

--- desafio-02/m1thrandirr/python/numprimos.py
+++ desafio-02/m1thrandirr/python/numprimos.py
@@ -1,5 +1,13 @@
-for i in range(1001):
-    if i in (2,3,5):
-        print(i)
-    if i%2!=0 and i%3!=0 and i%5!=0:
-        print(i)
+primos_list = []
+
+for i in range(2,1001):
+    for j in range(2,i+1):
+        if i%j==0:
+            if j==i:
+                primos_list.append(i)
+            else:
+                break
+
+for k in range(len(primos_list)):
+    print(primos_list[k])
+             
