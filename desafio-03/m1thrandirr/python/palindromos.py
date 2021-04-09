a55f103be667c4833518bdc2053a39479d4a6c4e
m1thrandirr <pedrolnascimento2004@gmail.com>
2021-04-09 00:14:34 -0300

    Linha diminuída, break removido.

--- desafio-03/m1thrandirr/python/palindromos.py
+++ desafio-03/m1thrandirr/python/palindromos.py
@@ -1,22 +1,22 @@
 """ Algoritmo para verificar números palindromicos em um intervalo. """
 
-
-while True:
-    intervalo = input("Digite o intervalo de inteiros a ser analisado: ").split() # variavel que recebe o intervalo.
-    palind_lista = [] # lista que armazena os palindromos dentro do intervalo.
-    palind = "" # variavel que verifica se o número é um palindromo.
+ver = 0
+while ver == 0:
+    intervalo = input("Digite o intervalo de inteiros a ser analisado: ").split()
+    palind_lista = []
+    palind = ""
 
     if len(intervalo) == 2:
-        num_1 = int(intervalo[0]) # variavel que recebe o começo do intervalo.
-        num_2 = int(intervalo[1]) # variavel que recebe o fim do intervalo.
+        num_1 = int(intervalo[0])
+        num_2 = int(intervalo[1])
 
-        for i in range(num_1, num_2+1): # loop que percorre o intervalo.
-            palind = str(i)             # palind recebe o iterador em string.
-            if int(palind[::-1]) == i:  # condição que compara se i é palindromo.
-                palind_lista.append(i)  # adiciona i em palind_lista.
+        for i in range(num_1, num_2+1):
+            palind = str(i)
+            if int(palind[::-1]) == i:
+                palind_lista.append(i)
 
-        print(palind_lista) # imprime todos os palindromos do intervalo.
-        break
+        print(palind_lista)
+        ver+=1
 
     else:
         print("Por favor, insira 2 números inteiros.")
