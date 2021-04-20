9ec70f4b45f88fd41e63a0f426a93a8d3628309f
ThiagoCostaBarbosa <82596085+ThiagoCostaBarbosa@users.noreply.github.com>
2021-04-19 23:26:06 -0300

    Resolução do Desafio 02 em Javascript

---
+++ desafio-02/ThiagoCostaBarbosa/javascript/desafio02.js
@@ -0,0 +1,11 @@
+function primeNumbers(x) {
+  var arr = []
+  for (let n = 2; n <= x; n++) {
+    count = 0
+    for (let i = 2; i < n; i++) n % i ? '' : (count++, count > 0 ? i = n : '')
+    count === 0 ? arr.push(n) : ''
+  }
+  return arr
+}
+
+console.log(primeNumbers(10000))
