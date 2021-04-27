05a3e83b99c3fc7d1309b2919eb48d062ee66838
TH <82596085+ThiagoCostaBarbosa@users.noreply.github.com>
2021-04-27 10:56:56 -0300

    Add Console.log

--- desafio-03/ThiagoCostaBarbosa/javascript/desafio03.js
+++ desafio-03/ThiagoCostaBarbosa/javascript/desafio03.js
@@ -1,18 +1,22 @@
-function palindromos(max) {
+function palindromos(max, min = 1) {
     if (parseInt(max) && max > 0) {
         parseInt(max)
     } else {
         max = 0
     }
     let arr = []
-    for (let num = 1; num <= max; num++) {
-        if (num == parseInt(num.toString().split('').reverse().join(''))) arr.push(num)
+    for (min; min <= max; min++) {
+        if (min == parseInt(min.toString().split('').reverse().join(''))) arr.push(min)
     }
     return arr
 }
 
 // Alternativa um pouco mais funcional
-function palindromos2(max, arr = [], num = 1) {
+function palindromos2(max, arr = [], min = 1) {
     (parseInt(max) && max > 0) || (max = 0)
-    return num <= max ? num == parseInt(num.toString().split('').reverse().join('')) ? palindromos2(max, arr.concat(num), num + 1) : palindromos2(max, arr, num + 1) : arr
+    return min <= max ? min == parseInt(min.toString().split('').reverse().join('')) ? palindromos2(max, arr.concat(min), min + 1) : palindromos2(max, arr, min + 1) : arr
+}
+
+function main() {
+console.log(palindromos(3010, 3000))
 }
