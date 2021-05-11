f29b5b8bfea7813504e660891be424dc6f38337d
Junior Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2021-05-11 13:40:33 -0300

    Update palindromo.c

--- desafio-03/vieirajunior-90/c/palindromo.c
+++ desafio-03/vieirajunior-90/c/palindromo.c
@@ -1,38 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void palindromo_1e2digitos(void);
-void palindromo_3digitos(void);
-void palindromo_4digitos(void);
+int palindromo_1e2digitos(int x, int y);
+int palindromo_3digitos(int x, int y);
+int palindromo_4digitos(int x, int y);
 
 int main(void)
-{
-    printf("\n\t\t\tPALINDROMOS DE 1 E 2 DIGITOS\n\n");
-    palindromo_1e2digitos();
+{   
+    int inicio, final;
+    
+    /*O usuario vai digitar o numero de inicio e fim do intervalo para
+    detectar os palindromos.*/
+    
+    printf("\n\n\t==== D E T E C T O R   D E   P A L I N D R O M O S ====\n\n");
+    printf("Abaixo, digite o inicio e fim do intervalo.\n");
+    printf("OBS: o fim do intervalo deve ser de no maximo n <= 10000\n\n");
+    
+    /*Duas verificacoes com DO para que o usuario nao digite um valor fora do intervalo permitido*/
+    
+    do
+    {
+        printf("Inicio do intervalo: ");
+        scanf("%i",&inicio);
+        if(inicio >= 9999)
+        {
+            printf("\tEsse valor nao e permitido\n");
+        }
+
+    }
+    while(inicio >= 9999);
+    
+    do
+    {
+        printf("Fim do intervalo: ");
+        scanf("%i",&final);
+        if(final >= 10000)
+        {
+            printf("\tEsse valor nao e permitido\n");
+        }
+    }
+    while(final >= 10000);
+    
+    printf("\n\n\t==== P A L I N D R O M O S entre [%i] e [%i] ====\n\n",inicio, final);
+    
+    /*Funcoes para verificar os palindromos*/
+
+    palindromo_1e2digitos(inicio, final);
+    
+    palindromo_3digitos(inicio, final);
+    
+    palindromo_4digitos(inicio, final);
+    
     printf("\n\n");
-    printf("\t\t\tPALINDROMOS DE 3 DIGITOS\n\n");
-    palindromo_3digitos();
-    printf("\n");
-    printf("\t\t\tPALINDROMOS DE 4 DIGITOS\n\n");
-    palindromo_4digitos();
-    printf("\n\n");
+    
     return 0;
 }
 
-void palindromo_1e2digitos(void)
+int palindromo_1e2digitos(int x, int y)
 {
+    printf("\n");
+
     int array[100];
     int mod[100];
     int div[100];
-
+    
     for(int i = 0; i <= 100; i++)
     {
         array[i] = i;
-        if(array[i] < 10)
+        if(array[i] < 10 && array[i] >= x && array[i] <= y)
         {
             printf("[%i]",array[i]);
         }
-        else if (array[i] > 10 && array[i]% 11 == 0)
+        else if (array[i] > 10 && array[i]% 11 == 0 && array[i] >= x && array[i] <= y)
         {
             printf("[%i]",array[i]);
         }
@@ -40,8 +79,10 @@ void palindromo_1e2digitos(void)
 }
 
 
-void palindromo_3digitos(void)
+int palindromo_3digitos(int x, int y)
 {
+    printf("\n");
+
     int array[1000];
     int mod[1000];
     int div[1000];
@@ -51,7 +92,7 @@ void palindromo_3digitos(void)
         array[i] = i;
         mod[i] = array[i]%10;
         div[i] = array[i]/100;
-        if (mod[i] == div[i])
+        if (mod[i] == div[i] && array[i] >= x && array[i] <= y)
         {
             printf("[%i]",array[i]);
         }
@@ -60,8 +101,10 @@ void palindromo_3digitos(void)
 
 }
 
-void palindromo_4digitos(void)
+int palindromo_4digitos(int x, int y)
 {
+    printf("\n");
+    
     int array[10000];
     int mod,div,mod1,mod2,prod = 0;
 
@@ -76,21 +119,20 @@ void palindromo_4digitos(void)
             mod1 = mod%10;
             mod2 = mod/10;
             prod = mod1*10 + mod2;
-            if(prod == div)
+            if(prod == div && array[i] >= x && array[i] <= y)
             {
                 printf("[%i]",array[i]);
             }
         }
-        if (mod > div)
+        if (mod > div && array[i] >= x && array[i] <= y)
         {
             mod1 = mod%10;
             mod2 = mod/10;
             prod = mod1*10 + mod2;
-            if(prod == div)
+            if(prod == div && array[i] >= x && array[i] <= y)
             {
                 printf("[%i]",array[i]);
             }
         }
     }
 }
-
