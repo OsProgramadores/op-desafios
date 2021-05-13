b513f8412471fc0009c2b5709740a75cacb70ec8
Junior Vieira <80427803+vieirajunior-90@users.noreply.github.com>
2021-05-13 19:56:15 -0300

    Update palindromo.c

--- desafio-03/vieirajunior-90/c/palindromo.c
+++ desafio-03/vieirajunior-90/c/palindromo.c
@@ -5,33 +5,26 @@ int palindromo_1e2digitos(int x, int y);
 int palindromo_3digitos(int x, int y);
 int palindromo_4digitos(int x, int y);
 
-int main(void)
-{   
+int main(void){   
     int inicio, final;
-    
     /*O usuario vai digitar o numero de inicio e fim do intervalo para
     detectar os palindromos.*/
-    
     printf("\n\n\t==== D E T E C T O R   D E   P A L I N D R O M O S ====\n\n");
     printf("Abaixo, digite o inicio e fim do intervalo.\n");
     printf("OBS: o fim do intervalo deve ser de no maximo n <= 10000\n\n");
     
     /*Duas verificacoes com DO para que o usuario nao digite um valor fora do intervalo permitido*/
-    
-    do
-    {
+    do{
         printf("Inicio do intervalo: ");
         scanf("%i",&inicio);
         if(inicio >= 9999)
         {
             printf("\tEsse valor nao e permitido\n");
         }
-
     }
     while(inicio >= 9999);
     
-    do
-    {
+    do{
         printf("Fim do intervalo: ");
         scanf("%i",&final);
         if(final >= 10000)
@@ -42,30 +35,20 @@ int main(void)
     while(final >= 10000);
     
     printf("\n\n\t==== P A L I N D R O M O S entre [%i] e [%i] ====\n\n",inicio, final);
-    
     /*Funcoes para verificar os palindromos*/
-
     palindromo_1e2digitos(inicio, final);
-    
     palindromo_3digitos(inicio, final);
-    
     palindromo_4digitos(inicio, final);
-    
     printf("\n\n");
-    
     return 0;
 }
-
-int palindromo_1e2digitos(int x, int y)
-{
+int palindromo_1e2digitos(int x, int y){
     printf("\n");
-
     int array[100];
     int mod[100];
     int div[100];
     
-    for(int i = 0; i <= 100; i++)
-    {
+    for(int i = 0; i <= 100; i++){
         array[i] = i;
         if(array[i] < 10 && array[i] >= x && array[i] <= y)
         {
@@ -77,18 +60,13 @@ int palindromo_1e2digitos(int x, int y)
         }
     }
 }
-
-
-int palindromo_3digitos(int x, int y)
-{
+int palindromo_3digitos(int x, int y){
     printf("\n");
-
     int array[1000];
     int mod[1000];
     int div[1000];
 
-    for(int i = 100; i <= 1000; i++)
-    {
+    for(int i = 100; i <= 1000; i++){
         array[i] = i;
         mod[i] = array[i]%10;
         div[i] = array[i]/100;
@@ -98,24 +76,18 @@ int palindromo_3digitos(int x, int y)
         }
     }
     printf("\n");
-
 }
-
-int palindromo_4digitos(int x, int y)
-{
+int palindromo_4digitos(int x, int y){
     printf("\n");
-    
     int array[10000];
     int mod,div,mod1,mod2,prod = 0;
 
-    for(int i = 1000; i <= 10000; i++)
-    {
+    for(int i = 1000; i <= 10000; i++){
         array[i] = i;
         mod = array[i]%100;
         div = array[i]/100;
 
-        if (mod < div || (mod%11 == 0 && div%11 == 0))
-        {
+        if (mod < div || (mod%11 == 0 && div%11 == 0)){
             mod1 = mod%10;
             mod2 = mod/10;
             prod = mod1*10 + mod2;
@@ -124,8 +96,7 @@ int palindromo_4digitos(int x, int y)
                 printf("[%i]",array[i]);
             }
         }
-        if (mod > div && array[i] >= x && array[i] <= y)
-        {
+        if (mod > div && array[i] >= x && array[i] <= y){
             mod1 = mod%10;
             mod2 = mod/10;
             prod = mod1*10 + mod2;
