cf4fe9ac713ef8a8db757ac329ce2d89ef81a3e1
rochamrcs <mavinicius95@gmail.com>
2021-05-16 22:48:26 -0300

    correção do código

--- desafio-02/rochamrcs/python/desafio-2.py
+++ desafio-02/rochamrcs/python/desafio-2.py
@@ -1,4 +1,8 @@
+"""Minha solução para imprimir todos os primos de 1 a 10000"""
+
+
 def primos(n):
+    """Aqui é feito a validação se um numero é primo"""
     raiz = int(n ** 0.5)
     for d in range(2, raiz + 1):
         if n % d == 0:
