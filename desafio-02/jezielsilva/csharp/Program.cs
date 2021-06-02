79bc55cc893f4c0025287f8be9763402369e7090
jezielsilva <33553136+jezielsilva@users.noreply.github.com>
2021-06-02 14:03:29 -0300

    desafio 02 feito em csharp. (#594)
    * desafio 02 feito em csharp.
    
    * Revert "Add Desafio03 (#573)"
    
    This reverts commit a421f83d225758cf824e6c1049c4add09de2a276.
    
    * Adicionado somente os arquivos necessários como pedido.
    
    * Comentários removidos como pedido.

---
+++ desafio-02/jezielsilva/csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace numerosPrimos
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int max = 10000;
+            for(int i = 2; i<=max;i++){
+                if(i == 2){
+                    Console.WriteLine(i);
+                }else{
+                    if(i%2 == 0){
+                        continue;
+                    }else{
+                        bool isPrime = true;
+                        for(int j = 3; j<i/2;j++){
+                            if(i%j == 0){
+                                isPrime = false;
+                                break;
+                            }
+                        }
+                        if(isPrime){
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
