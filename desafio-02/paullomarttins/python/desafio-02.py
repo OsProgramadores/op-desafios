6b3d7ef37ab6198eaca5e0644a992963aecff370
Paulo Martins <p.o.marttins@gmail.com>
2021-06-02 15:30:08 -0300

    Adicionado metodo para otimizar o loop dos numeros - Desafio 02

--- desafio-02/paullomarttins/python/desafio-02.py
+++ desafio-02/paullomarttins/python/desafio-02.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 """
-Desafio 02, listando números primos de 1 até 10000!
+Desafio 02, listando números primos de 1 até 10000
+Para otimizar o loop e não percorrer todos os números do range utilizei um while
+Optimized School Method [6n + 1 or 6n – 1]
 """
+
 def num_primo(num):
-    for valor in range(2, num):
-        if num % valor == 0:
+    if num <= 1:
+        return False
+    if num == 2 or num == 2:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
             return False
+        i = i + 6
     return True
 
-for valor in range(2, 10000):
+    #for valor in range(2, num):
+    #    if num % valor == 0:
+    #        return False
+    #return True
+
+for valor in range(1, 10000):
     if(num_primo(valor)):
         print(valor)
