c464b716effb9e676bc9a8a0c9ae0e2abefa2b49
Anthony Louis <anthony.physis@gmail.com>
2021-06-21 23:18:20 -0300

    Commit consertando versao 4 do desafio

--- desafio-04/anthonylgf/java/chess.java
+++ desafio-04/anthonylgf/java/chess.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
 
-    Scanner in = new Scanner(System.in); //leitor
+    Scanner in = new Scanner(System.in); // leitor
 
-    int tabuleiro[][] = new int[8][8];// tabuleiro
+    int tabuleiro[][] = new int[8][8]; // tabuleiro
 
-    int i=0;
-    int j=0;
-
-        int numPeao = 0;
+    int i = 0;
+    int j = 0;
+    int numPeao = 0;
     int numRei = 0;
     int numDama = 0;
     int numBispo = 0;
@@ -18,21 +17,21 @@ class Main {
     int numCavalo = 0;
 
     /*Código para preencher tabuleiro*/
-    for(i=0; i<8; i++){
-      for(j=0; j<8; j++){
+    for (i = 0; i < 8; i++) {
+      for (j = 0; j < 8; j++) {
         tabuleiro[i][j] = in.nextInt();
       }
     }
 
     /*Somando num das peças usando operador ternário*/
-    for(i=0; i<8; i++){
-      for(j=0; j<8; j++){
-        numCavalo = tabuleiro[i][j] == 3 ? ++numCavalo:numCavalo;
-        numPeao = tabuleiro[i][j] == 1 ? ++numPeao:numPeao;
-        numBispo = tabuleiro[i][j] == 2 ? ++numBispo:numBispo;
-        numTorre = tabuleiro[i][j] == 4 ? ++numTorre:numTorre;
-        numDama = tabuleiro[i][j] == 5 ? ++numCavalo:numDama;
-        numRei = tabuleiro[i][j] == 6 ? ++numRei:numRei;
+    for (i = 0; i < 8; i++) {
+      for (j = 0; j < 8; j++) {
+        numCavalo = tabuleiro[i][j] == 3 ? ++numCavalo : numCavalo;
+        numPeao = tabuleiro[i][j] == 1 ? ++numPeao : numPeao;
+        numBispo = tabuleiro[i][j] == 2 ? ++numBispo : numBispo;
+        numTorre = tabuleiro[i][j] == 4 ? ++numTorre : numTorre;
+        numDama = tabuleiro[i][j] == 5 ? ++numCavalo : numDama;
+        numRei = tabuleiro[i][j] == 6 ? ++numRei : numRei;
       }
     }
 
