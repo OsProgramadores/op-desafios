9228b03446303d11117d998ae9282a382a984309
guiribeirodev <guiribeirodev@gmail.com>
2021-06-30 21:28:03 -0300

    Realização do exercício 02 - Números primos.

---
+++ desafio-02/guiribeirodev/javascript/desafio-02.js
@@ -0,0 +1,23 @@
+// Função "verificaPrimo()"" irá verificar se o número é primo, a verificação é feita com base na premissa que todo número primo irá ter apenas dois divisores, o número 1 e ele próprio, então caso o número de divisores sejam 2 será primo.
+
+//Função "listaPrimo" é responsável por criar a lista dos números primos do número inicial até o numero final.
+
+function listaPrimo(numeroInicio, numeroFim) {
+  for (numeroInicio = 1; numeroInicio <= numeroFim; numeroInicio++) {
+    verificaPrimo(numeroInicio)
+  }
+}
+
+function verificaPrimo(numeroInicio) {
+  let divisor = 0
+
+  for (let count = 1; count <= numeroInicio; count++) {
+    if (numeroInicio % count == 0) divisor++
+  }
+
+  if (divisor == 2) {
+    console.log(numeroInicio + ' é primo')
+  }
+}
+
+listaPrimo(1, 10000)
