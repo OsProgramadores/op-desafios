c208b179cbe69f2fa1e3a13cbddb9815c439b021
TH <82596085+ThiagoCostaBarbosa@users.noreply.github.com>
2021-07-10 19:36:54 -0300

    Code refactores: desafio-03

--- desafio-03/ThiagoCostaBarbosa/javascript/desafio03.js
+++ desafio-03/ThiagoCostaBarbosa/javascript/desafio03.js
@@ -1,8 +1,8 @@
-function palindromos(max, min = 1) {
-    let arr = []
-    for (min; min <= max; min++) {
-        if (min == [...(""+min)].reverse().join('')) arr.concat([min])
-    }
-    return arr
+function palindromos(max, min = 1, arr = []) {
+  return min <= max ?
+    min != [...(""+min)].reverse().join('') ?
+      palindromos(max, min + 1, arr) :
+      palindromos(max, min + 1, arr.concat([min])) :
+    arr
 }
 console.log(palindromos(3010, 3000))
