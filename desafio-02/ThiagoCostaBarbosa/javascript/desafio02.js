f40fdc74f575412490925fbae86083a5f40d9319
TH <82596085+ThiagoCostaBarbosa@users.noreply.github.com>
2021-07-10 20:02:16 -0300

    Code refactored: desafio-02

--- desafio-02/ThiagoCostaBarbosa/javascript/desafio02.js
+++ desafio-02/ThiagoCostaBarbosa/javascript/desafio02.js
@@ -1,26 +1,5 @@
-function prime(max) {
-  if (parseInt(max) && max > 1) {
-    parseInt(max)
-  } else {
-    arr = []
-    n = 1
-    max = 1
-  }
-  var arr = [2]
-  for (let n = 3; n <= max; n++) {
-    count = 0
-    arr.map(i => n % i ? '' : count++)
-    count === 0 ? arr.push(n) : ''
-  }
-  return arr
+function prime (max, arr = [2], n = 3, primo = true) {
+  arr.map( i => n % i ? '' : primo = false )
+  return n == max ? arr : prime(max, (primo ? arr.concat([n]) : arr), n + 1)
 }
-
-// Alternativa um pouco mais funcional
-function prime2(max, arr = [2], n = 3) {
-  (parseInt(max) && max > 1) || (arr = [], n = 1, max = 1)
-  return n == max ? arr : prime2(max, (arr.map(i => n % i ? true : false).includes(false) ? arr : arr.concat([n])), n + 1)
-}
-
-function main() {
-  console.log(prime(10000))
-}
+console.log(prime(10000))
