1324b8865350dc939c4deacb393eade36bf9607a
Alesandro Silva <silva_alesandro@outlook.com>
2021-07-11 01:04:35 -0400

    programa lista primos

---
+++ desafio-02/alleSilva/elixir/README.md
@@ -0,0 +1,18 @@
+# op-desafios/desafio-02
+
+### Como executar
+> Este programa lista os numero primos entre 1 e 10000.
+Para executar o programa digite o seguinte comando no terminal:
+   ```
+   $ elixirc list_primes.exs
+   ```
+   
+> Para executar a função privada```is_prime(number)``` :
+```
+   $ iex list_primes.exs
+   iex> ListPrimes.is_prime(5)
+    >true
+    
+    iex> ListPrimes.is_prime(12)
+    >false
+   ```
---
+++ desafio-02/alleSilva/elixir/list_primes.exs
@@ -0,0 +1,25 @@
+defmodule ListPrimes do
+  def call() do
+    1..10_000
+    |> Enum.filter(&is_prime/1)
+    |> Enum.each(&IO.inspect/1)
+  end
+
+  def is_prime(num) when num > 0,
+    do: verify_rem(num, 1)
+
+  defp verify_rem(n, n), do: true
+
+  defp verify_rem(n, acc) do
+    acc = acc + 1
+    root = :math.sqrt(n)
+
+    if acc <= root && rem(n, acc) == 0 do
+      false
+    else
+      verify_rem(n, acc)
+    end
+  end
+end
+
+ListPrimes.call()
