aa1295748dc5f2dea03c4f698a188d25b2ac49bb
vieirajunior-90 <junior.vieira.1990@gmail.com>
2021-07-14 16:39:17 -0300

    Solução em Python do Desafio 02

--- desafio-02/vieirajunior-90/python/primo.py
+++ desafio-02/vieirajunior-90/python/primo.py
@@ -1,14 +1,12 @@
-import sys
-
-def search_primes(n):
-    sys.setrecursionlimit(10500)
-    prime = 0
-    if n == 10000:
-        return 0
-    for c in range(1, n + 1):
-        if n % c == 0:
-            prime += 1
-    if prime == 2:
-        print(f'[{n}]', end='')
-    search_primes(n + 1)        
-search_primes(1)
+def prime(end=10000):
+    n = 1
+    while n <= end:
+        div = 0
+        for c in range(1, n + 1):
+            if n % c == 0:
+                div += 1
+        if div == 2:
+            print(f'[{n}]', end='')
+        n += 1
+    print()
+prime()
