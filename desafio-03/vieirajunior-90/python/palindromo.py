5783c666cfc3dfcc513485ea4af716c98316d9bd
vieirajunior-90 <junior.vieira.1990@gmail.com>
2021-07-14 17:34:38 -0300

    Solução em Python do Desafio 03

---
+++ desafio-03/vieirajunior-90/python/palindromo.py
@@ -0,0 +1,14 @@
+"""
+Solulão do Desafio 03 por Junior Vieira
+"""
+def palindromo(start=1, end=10000):
+    """
+    Função para procurar por algarismos palíndromos
+    """
+    while start <= end:
+        valor = str(start)
+        if valor == valor[::-1]:
+            print(f'[{valor}]', end='')
+        start += 1
+    print()
+palindromo()
