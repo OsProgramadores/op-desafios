2d6de2d865b0b8eea5aecf9c8f4c47eb664259de
ThiagoCostaBarbosa <82596085+ThiagoCostaBarbosa@users.noreply.github.com>
2021-07-31 22:05:53 -0300

    Solution in one line: Desafio 4

--- desafio-04/ThiagoCostaBarbosa/javascript/desafio04-alternative.js
+++ desafio-04/ThiagoCostaBarbosa/javascript/desafio04-alternative.js
@@ -27,30 +27,30 @@ Chess_count(tabuleiro)
 Chess_count(tabuleiro2)
 
 function Chess_count(array) {
-    //r[0] All pieces in Chess table
+        //r[0] All pieces in Chess table
 let r = [array,
-    //r[1]Quantity of Pieces
-    [0, 0, 0, 0, 0, 0],
-    //r[2] Types of pieces
-    ["Peão", "Bispo", "Cavalo", "Torre", "Rainha", "Rei"],
-    //r[3]Noun
-    ["peça", "peças"],
-    //r[4] Result
-    "Quantidade de peças:\n"],    
-    //Remove Subarrays
-v = [r[0].flat()
-        //Count quantity of every piece
-         .map(i => r[1][i - 1] += 1),
-    //Loop to assemble the quantity phrase in the result
-    r[1].map((v, i) => r[4] += ["",
-                                //Piece
-                                `${r[2][i]
-                                //Quantity
-                                }: ${v
-                                //Noun - is singular ou plural
-                                } ${r[3][(v != 1) * 1]
-                                //Condictional without a conditional function - only add to result if a type have piece
-                                }\n`][(v != 0) * 1]),
-    //Print result
-    console.log(r[4])]
+        //r[1]Quantity of Pieces
+        [0, 0, 0, 0, 0, 0],
+        //r[2] Types of pieces
+        ["Peão", "Bispo", "Cavalo", "Torre", "Rainha", "Rei"],
+        //r[3]Noun
+        ["peça", "peças"],
+        //r[4] Result
+        "Quantidade de peças:\n"],    
+        //Remove Subarrays
+    v = [r[0].flat()
+            //Count quantity of every piece
+            .map(i => r[1][i - 1] += 1),
+        //Loop to assemble the quantity phrase in the result
+        r[1].map((v, i) => r[4] += ["",
+                                    //Piece
+                                    `${r[2][i]
+                                    //Quantity
+                                    }: ${v
+                                    //Noun - is singular ou plural
+                                    } ${r[3][(v != 1) * 1]
+                                    //Condictional without a conditional function - only add to result if a type have piece
+                                    }\n`][(v != 0) * 1]),
+        //Print result
+        console.log(r[4])]
 }
