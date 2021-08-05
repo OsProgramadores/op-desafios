be09e12cefdae8fae3dfb2e5ff1e5705b9ef3c6b
Thiago Souza <thiagolsmail@gmail.com>
2021-08-05 18:05:36 -0300

    Solução do desafio 02 em Python

---
+++ desafio-02/thiagosouzalink/python/README.md
@@ -0,0 +1,26 @@
+#
+![PYTHON](https://img.shields.io/static/v1?label=python&message=v3.8.9&color=blue&logo=PYTHON)
+
+<br>
+
+# Desafio
+Escreva um programa para listar todos os números primos entre 1 e 10000.
+
+<br>
+
+# Tecnologias utilizadas: 
+- Python
+
+<br>
+
+# Execução do projeto:
+1 - Faça o clone ou download do projeto.<br>
+3 - Acesse o diretório desafio-02/thiagosouzalink/python <br>
+2 - Execute a aplicação através do comando: <br>
+```$ python desafio_02.py``` ou ```$ python3 desafio_02.py``` 
+
+<br>
+
+#
+Desafio #2, da lista de desafios criado por [OsProgramadores](https://osprogramadores.com/desafios/)
+
---
+++ desafio-02/thiagosouzalink/python/desafio_02.py
@@ -0,0 +1,31 @@
+"""
+Listando números primos.
+
+Escreva um programa para listar todos os números primos entre 1 e 10000, na
+linguagem de sua preferência.
+"""
+from math import sqrt
+
+
+NUMBER = 10000
+
+def verificar_numero_primo(num):
+    """ Função que verifica se um determinado número é primo.
+
+    Params:
+        num: Número a ser verificado.
+
+    Returns:
+        Valor booleano validando a condição de ser ou não ser número primo.
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+for n in range(1, NUMBER + 1):
+    if verificar_numero_primo(n):
+        print(n)
