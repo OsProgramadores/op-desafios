1a34f2dc59f4f26adedc806500f754f12ee1bcc3
R0drig1 <rodrigo-cientista@hotmail.com>
2021-08-29 22:39:14 -0300

    Ajuste modelo UTF-8 no código

--- desafio-02/R0drig/go/desafio2.go
+++ desafio-02/R0drig/go/desafio2.go
@@ -1,16 +1,15 @@
-
 package main
-
 import "fmt"
-
 func primos(numero int)bool{
 	for  divisor := 2 ;divisor <= numero/ 2; divisor++{
 		if  numero % divisor==0{
 			return false
-			}
-	
-	}
+}
+
+}
 	return numero != 1
+
+
 
 }
 
@@ -18,6 +17,7 @@ func main(){
 	for i := 1; i < 10000; i++{
 		if primos(i){
 			fmt.Println(i)
-		}
-	}
+
+}
+}
 }
