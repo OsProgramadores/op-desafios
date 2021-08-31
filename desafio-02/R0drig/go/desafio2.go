719f92b7a805a06fbd6471e3addfed68aa62044b
R0drig <rdrpo2014@gmail.com>
2021-08-31 00:17:26 -0300

    Formatção do arquivo pelo comando gofmt -s

--- desafio-02/R0drig/go/desafio2.go
+++ desafio-02/R0drig/go/desafio2.go
@@ -1,17 +1,20 @@
 package main
+
 import "fmt"
-func primos(numero int)bool{
-    for divisor := 2; divisor <= numero/2; divisor++{
-         if numero%divisor == 0{
-           return false
+
+func primos(numero int) bool {
+	for divisor := 2; divisor <= numero/2; divisor++ {
+		if numero%divisor == 0 {
+			return false
+		}
+	}
+	return numero != 1
 }
+func main() {
+	for i := 1; i < 10000; i++ {
+		if primos(i) {
+			fmt.Println(i)
+		}
+	}
 }
-      return numero != 1
-}
-func main(){
-      for i := 1; i < 10000; i++ {
-          if primos(i){
-              fmt.Println(i)
-}
-}
-}
+
