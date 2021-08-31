57e1cdd00cdb1763351b3b3b415f70935655a628
R0drig <rdrpo2014@gmail.com>
2021-08-30 21:38:04 -0300

    Remoção de tabs e espaços a mais no arquivo

--- desafio-02/R0drig/go/desafio2.go
+++ desafio-02/R0drig/go/desafio2.go
@@ -1,23 +1,17 @@
 package main
 import "fmt"
 func primos(numero int)bool{
-	for  divisor := 2 ;divisor <= numero/ 2; divisor++{
-		if  numero % divisor==0{
-			return false
+    for divisor := 2; divisor <= numero/2; divisor++{
+         if numero%divisor == 0{
+           return false
 }
-
 }
-	return numero != 1
-
-
-
+      return numero != 1
 }
-
 func main(){
-	for i := 1; i < 10000; i++{
-		if primos(i){
-			fmt.Println(i)
-
+      for i := 1; i < 10000; i++ {
+          if primos(i){
+              fmt.Println(i)
 }
 }
 }
