49fa1ab63f0002121d7df12a918c9fc68deec793
R0drig <rdrpo2014@gmail.com>
2021-08-31 23:02:52 -0300

    Formatação devido a erro na declaração da função

--- desafio-03/R0drig/go/desafio3.go
+++ desafio-03/R0drig/go/desafio3.go
@@ -1,8 +1,8 @@
 package main
 
 import "fmt"
-
-func Inverte(numero int) (numeroInvertido int) {
+//funcao que inverte o numero
+func inverte(numero int) (numeroInvertido int) {
 	for ; numero > 0; numero = numero / 10 {
 		digito := numero % 10
 		numeroInvertido = numeroInvertido*10 + digito
@@ -13,7 +13,7 @@ func Inverte(numero int) (numeroInvertido int) {
 
 func main() {
 	for i := 1; i <= 100000; i++ {
-		if i == Inverte(i) {
+		if i == inverte(i) {
 			fmt.Println(i)
 		}
 	}
