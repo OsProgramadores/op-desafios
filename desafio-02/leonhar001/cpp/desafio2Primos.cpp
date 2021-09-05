7049ddb03c11aeeee27b59f5c01d38873661fec9
leonhar001 <68454728+leonhar001@users.noreply.github.com>
2021-09-05 01:24:30 -0300

    Create desafio2Primos.cpp

---
+++ desafio-02/leonhar001/cpp/desafio2Primos.cpp
@@ -0,0 +1,27 @@
+	#include <stdio.h>
+	#define LIMITE 10000 //quantidade de números para testar
+	
+	int main(){
+		
+		int i=2;
+		int div=2;
+		int total=0;
+		bool isPrimo;
+		printf("Lista de números primos:\n");
+		while(i<=LIMITE){
+			isPrimo=true;
+			while(div<i){
+				if(i%div==0)
+					isPrimo=false;
+				div++;
+			}
+			if(isPrimo){	
+				printf("[%d]",i);
+				total++;
+			}
+			i++;
+			div=2;
+		}
+		printf("\nTotal de números primos: %d",total);
+		return 0;
+	}
