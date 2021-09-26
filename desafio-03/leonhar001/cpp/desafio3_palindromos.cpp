c93428c215610f0699e03c0a9c687b734039b782
leonhar001 <68454728+leonhar001@users.noreply.github.com>
2021-09-25 21:15:54 -0300

    Desafio 3 - Palíndromos

---
+++ desafio-03/leonhar001/cpp/desafio3_palindromos.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+bool isPalindrome(int number){
+	int pal=0;
+	int original=number;
+	while(number>0){
+		int LastDigit=number%10;
+		pal = (pal*10)+LastDigit;	
+		number= number/10;
+	}
+	return original==pal ? true:false;
+}
+int main(){
+	int init,final;
+
+	printf(".:Enter a range (positive integer):.\n");
+	printf("Start: ");
+	scanf("%d",&init);
+	printf("End: ");
+	scanf("%d",&fin);
+
+	while(init<fin){
+		if(isPalindrome(init))
+			printf("%d\n",init);
+		init++;
+	}
+}
