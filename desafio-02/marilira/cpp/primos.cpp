d256c23308bf8d59638b81c6e4c81bf721cc2ebc
Mariana Lira <marilira2006@gmail.com>
2021-09-28 14:06:12 -0300

    Remove espacos

--- desafio-02/marilira/cpp/primos.cpp
+++ desafio-02/marilira/cpp/primos.cpp
@@ -2,14 +2,10 @@
 using namespace std;
 int ePrimo(int n){
 	int divisores = 0;
-	for(int valor = 1; valor <= n; valor++){
-		if(n%valor == 0) divisores++;
-	}
-	if(divisores == 2) cout << n << endl;	
+	for(int valor = 1; valor <= n; valor++) if(n%valor == 0) divisores++;
+	if(divisores == 2) cout << n << '\n';	
 }
 int main(){
-	for(int i=1; i<=10000; i++){
-		ePrimo(i);
-	}
+	for(int i=1; i<=10000; i++) ePrimo(i);
 	return 0;
 }
