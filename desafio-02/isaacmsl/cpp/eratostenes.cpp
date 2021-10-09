be21ebb92b814d7b522452181249bc501966f92b
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2021-10-08 22:01:41 -0300

    Corrige spaces or tabs at end-of-line

--- desafio-02/isaacmsl/cpp/eratostenes.cpp
+++ desafio-02/isaacmsl/cpp/eratostenes.cpp
@@ -8,19 +8,20 @@ using namespace std;
 int main() {_
     // Inicialmente todos são
     vector<int> x(MAX+1, 1);
-    
     for (int i = 2; i*i <= MAX; i++) {
         // Se i é primo, então todos os múltiplos de i que são maiores do que i
         // não serão primos
         if (x[i]) {
-            for (int j = i*i; j <= MAX; j += i) x[j] = 0;
+            for (int j = i*i; j <= MAX; j += i) {
+                x[j] = 0;
+            }
         }
     }
-    
-    cout << 2;
+    cout << 2 << endl;
     for (int i = 3; i <= MAX; i++) {
-        if (x[i]) cout << ", " << i;
+        if (x[i]) {
+            cout << i << endl;
+        }
     }
-    
     return 0;
 }
