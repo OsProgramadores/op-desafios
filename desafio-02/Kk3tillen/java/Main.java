3d8d5ccba4bd4aabd1c8c3759545b2bd12364298
Kézia Lima <k.kezia@escolar.ifrn.edu.br>
2021-10-12 02:22:03 -0300

    Desafio 2 em java por: @Kk3tillen

---
+++ desafio-02/Kk3tillen/java/Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10000; i++) {
+            int counter = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) ++counter;
+            }
+            if (counter == 2 ) {
+                System.out.println(i);
+            }
+        }
+    }
+}
