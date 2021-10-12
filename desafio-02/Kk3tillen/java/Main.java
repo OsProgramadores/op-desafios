f7b5c81bcb2152449351d4977a8ad57f70415378
Isaac Lourenço <isaacmarlondasilvalourenco@gmail.com>
2021-10-12 15:28:57 -0300

    Corrige formatação do bloco condicional

--- desafio-02/Kk3tillen/java/Main.java
+++ desafio-02/Kk3tillen/java/Main.java
@@ -3,7 +3,9 @@ public class Main {
     for (int i = 1; i <= 10000; i++) {
       int counter = 0;
       for (int j = 1; j <= i; j++) {
-        if (i % j == 0) ++counter;
+        if (i % j == 0) {
+          ++counter;
+        }
       }
       if (counter == 2) {
         System.out.println(i);
