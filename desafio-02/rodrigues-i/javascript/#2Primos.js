731e100f61a26402eb28ec5ac82844bca7dac53b
rodrigues-i <lucas-f250@live.com>
2021-10-18 10:21:56 -0300

    Remove spaces between some lines to try passing the test.

--- desafio-02/rodrigues-i/javascript/#2Primos.js
+++ desafio-02/rodrigues-i/javascript/#2Primos.js
@@ -1,22 +1,18 @@
 // Imprime no console todos os números primos entre 1 e 10.000
-
 const numerosPrimos = () => {
-
+	
 	let nãoPrimos = [];
 	let primos = [];
 	let contagem = 0;
 
 	for(let i = 2; i <= 10000; i++) {
 		if(primos.length >= 1) {
-
 			for(item of primos) {
 				if(i % item === 0) {
 					continue;
 				}
 			}	
 		}
-		
-
 		for(let j = 1; j <= i; j++) {
 			if(i % j === 0) {
 				contagem++;
@@ -27,14 +23,13 @@ const numerosPrimos = () => {
 				}
 			if(i % j === 0 && j === i) {
 				primos.push(i);
+				console.log(i);
 				contagem = 0;
 				break;
 				}
 			}
 		}
-	}
-			
-	console.log(primos);
+	}			
 };
 
 numerosPrimos();
