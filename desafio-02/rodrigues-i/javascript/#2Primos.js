dc0f48d11a81b28310f83761c5dac82b262f083b
rodrigues-i <lucas-f250@live.com>
2021-10-18 01:12:38 -0300

    Inclusão do desafio 02 pelo usuário rodrigues-i.

---
+++ desafio-02/rodrigues-i/javascript/#2Primos.js
@@ -0,0 +1,40 @@
+// Imprime no console todos os números primos entre 1 e 10.000
+
+const numerosPrimos = () => {
+
+	let nãoPrimos = [];
+	let primos = [];
+	let contagem = 0;
+
+	for(let i = 2; i <= 10000; i++) {
+		if(primos.length >= 1) {
+
+			for(item of primos) {
+				if(i % item === 0) {
+					continue;
+				}
+			}	
+		}
+		
+
+		for(let j = 1; j <= i; j++) {
+			if(i % j === 0) {
+				contagem++;
+				if(contagem > 2){
+					nãoPrimos.push(i);
+					contagem = 0;
+					break;
+				}
+			if(i % j === 0 && j === i) {
+				primos.push(i);
+				contagem = 0;
+				break;
+				}
+			}
+		}
+	}
+			
+	console.log(primos);
+};
+
+numerosPrimos();
