0e561bf08cc68cf9ce804236953581115bb8a26d
Paulo Cezar Sheliga <paulosheliga@gmail.com>
2021-11-25 11:17:54 -0300

    Solução do desafio 02

---
+++ desafio-02/Sheliga/python/desafio_02.py
@@ -0,0 +1,34 @@
+"""
+Solução do desafio 02 do site os programadores
+"""
+def verifica_primo(numero):
+    """
+    metodo pra verificar se o numero é primo
+    """
+    numero_divisoes = 0
+    for inc in range(1, numero+1):
+        if numero%inc == 0:
+            numero_divisoes += 1
+    if numero_divisoes==2:
+        return True
+    return False
+
+def listar_primos(primos, numero_teto):
+    """
+    lista os numeros primos do inicio até o numero teto
+    """
+    for inc in range(1, numero_teto):
+        if verifica_primo(inc):
+            primos.append(inc)
+
+
+def main():
+    """
+    função main
+    """
+    primos = []
+    listar_primos(primos, 10000)
+    print(primos)
+
+if __name__ == "__main__":
+    main()
