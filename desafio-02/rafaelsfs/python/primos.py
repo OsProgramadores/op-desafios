5b60eca90b73fe41fefb690e1a2970fac26a8d69
Rafael Garcia <rafaelsfs@gmail.com>
2021-12-11 23:55:30 -0300

    Soluçao do desafio-02 @rafaelsfs

---
+++ desafio-02/rafaelsfs/python/primos.py
@@ -0,0 +1,27 @@
+"""
+Autor: Rafael Garcia
+#02 - Primos
+
+## Desafio
+Escrever um programa para listar todos os números primos entre 1 e 10000,
+na linguagem de sua preferência.
+
+## Objetivo
+Listando números primos entre 1 e 10000.
+"""
+
+i = 1
+while i <= 100:
+    if i == 2:
+        print(f"{i}")
+    if i == 3:
+        print(f"{i}")
+    else:
+        x = 3
+        while x < i:
+            if i % x == 0:
+                break
+            x += 2
+        if x == i:
+            print(f"{x}")
+    i += 1
