24386af0808676c0e7f8858a04d9a09193cd044b
henriqueibrahim <henriqueibrahim7@gmail.com>
2021-12-14 14:31:07 -0300

    Desafio 03, novamente trailing-newlines

--- desafio-03/henriqueibrahim/python/solucao.py
+++ desafio-03/henriqueibrahim/python/solucao.py
@@ -2,15 +2,9 @@
 input1 = int(input())
 input2 = int(input())
 
-numbers = list(range(input1, input2 + 1)) # +1 para poder incluir o último número
+numbersList = list(range(input1, input2 + 1)) # +1 para poder incluir o último número
 
-for n in numbers:
-    temp = n # Var temporária para salvar valor de n
-    reverseNum = 0
-    while n > 0:
-        lastDigit = n % 10 # Retirar último dígito
-        reverseNum = reverseNum * 10 + lastDigit # Add o último dígito para o lado esquerdo do n
-        n = n // 10 # Divide para deixar o último dígito
-    if temp == reverseNum:
-        print(reverseNum, end=" ")
-
+for n in numbersList:
+    number = str(n) # [::-1] Não inverte int, por isso transforma em str
+    if number == number[::-1]: # Confere se a string inicial é igual a string invertida.
+        print(number, end=" ")
