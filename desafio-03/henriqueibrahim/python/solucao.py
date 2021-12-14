002b9efe4f462d2222a02d8629e7b4e2f4ca6202
henriqueibrahim <henriqueibrahim7@gmail.com>
2021-12-14 13:29:35 -0300

    Soluções para o Desafio 03.

---
+++ desafio-03/henriqueibrahim/python/solucao.py
@@ -0,0 +1,15 @@
+input1 = int(input())
+input2 = int(input())
+
+numbers = list(range(input1, input2 + 1))           # +1 para poder incluir o último número, já que começa a contar do 0
+
+for n in numbers:
+    temp = n                                        # Var temporária para salvar valor de n
+    reverseNum = 0
+    while (n > 0):
+        lastDigit = n % 10                          # Retirar último dígito
+        reverseNum = reverseNum * 10 + lastDigit    # Adiciona o último dígito para o lado esquerdo do número (reverte)
+        n = n // 10                                 # Divide para deixar o último dígito
+    if (temp == reverseNum):
+        print(reverseNum)
+
