5d5af5d5e11e43e57dbbfe9a5ab9d9eaa7a28964
henriqueibrahim <henriqueibrahim7@gmail.com>
2021-12-14 14:08:38 -0300

    Novamente erro de long-line FIX

--- desafio-03/henriqueibrahim/python/solucao.py
+++ desafio-03/henriqueibrahim/python/solucao.py
@@ -2,14 +2,14 @@
 input1 = int(input())
 input2 = int(input())
 
-numbers = list(range(input1, input2 + 1)) # +1 para poder incluir o último número, já que começa a contar do 0
+numbers = list(range(input1, input2 + 1)) # +1 para poder incluir o último número
 
 for n in numbers:
     temp = n # Var temporária para salvar valor de n
     reverseNum = 0
     while n > 0:
         lastDigit = n % 10 # Retirar último dígito
-        reverseNum = reverseNum * 10 + lastDigit # Adiciona o último dígito para o lado esquerdo do número (reverte)
+        reverseNum = reverseNum * 10 + lastDigit # Add o último dígito para o lado esquerdo do n
         n = n // 10 # Divide para deixar o último dígito
     if temp == reverseNum:
         print(reverseNum)
