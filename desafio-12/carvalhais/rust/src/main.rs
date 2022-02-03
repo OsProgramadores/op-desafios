544f1c0e6b91427740b72d67679d8a6308a24aa9
Andre Carvalhais <carvalhais@live.com>
2022-02-03 11:52:15 -0300

    docs: docummented 'main.rs' source file

--- desafio-12/carvalhais/rust/src/main.rs
+++ desafio-12/carvalhais/rust/src/main.rs
@@ -1,3 +1,17 @@
+// A vanilla Rust solution for challenge #12 from OsProgramadores website
+//
+// (c) Andre Carvalhais <carvalhais@live.com>
+//
+// For the full copyright and license information, please view the LICENSE file
+// that was distributed with this source code.
+
+// use the num_bigint crate for the moment being, since the big integer helper
+// methods are still experimental in the rust API at the time of this writing
+// (2022-02-03)
+//
+// TODO: implement from scratch a nice popcount algoritm (possible candidates:
+// sideways sum, SWAR, etc), and write a nice article on the README.md file
+// explaining how it works
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
