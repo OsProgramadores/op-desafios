884595c87a210acf03095440bda3f6a1cc4d9a09
DanielRosa74 <58576743+DanielRosa74@users.noreply.github.com>
2022-02-23 23:46:25 -0300

    Changing code according to space separation diffs

--- desafio-02/DanielRosa74/java/PrimeNumbers.java
+++ desafio-02/DanielRosa74/java/PrimeNumbers.java
@@ -5,15 +5,15 @@ public class PrimeNumbers {
   public static void main(String[] args) {
     List<Integer> primes = new ArrayList<>();
     primes.add(2);
-    for(int i=3; i <= 10000; i++) {
+    for (int i = 3; i <= 10000; i++) {
       int count = 0;
-      for(int j=1; j < i; j++) {
-        if(i%j == 0) count++;
+      for (int j = 1; j < i; j++) {
+        if (i % j == 0) count++;
       }
-      if(count < 2) primes.add(i);
+      if (count < 2) primes.add(i);
     }
     System.out.print("Os números primos de 1 a 10000 são: ");
-    for(int prime: primes) {
+    for (int prime : primes) {
       System.out.print(prime + " ");
     }
   }
