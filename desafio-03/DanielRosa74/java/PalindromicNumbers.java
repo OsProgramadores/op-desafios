86e4931453def5b7b2f9d5577a2ffa7c99d463bf
DanielRosa74 <58576743+DanielRosa74@users.noreply.github.com>
2022-03-01 01:18:54 -0300

    Fixing formatting issues

--- desafio-03/DanielRosa74/java/PalindromicNumbers.java
+++ desafio-03/DanielRosa74/java/PalindromicNumbers.java
@@ -7,23 +7,24 @@ public class PalindromicNumbers {
       boolean checks = true;
       Integer inicio = 0;
       Integer termino = 0;
-      while(checks) {
+      while (checks) {
         System.out.print("Insira o valor inicial: ");
         inicio = sc.nextInt();
         System.out.print("Insira o valor final: ");
         termino = sc.nextInt();
-        if ( inicio > termino) {
-          System.out.println("O valor de término precisa ser maior que o de início. Tente novamente.");
+        if (inicio > termino) {
+          System.out.println(
+            "O valor de término precisa ser maior que o de início. Tente novamente.");
         } else if (inicio < 0 || termino < 0) {
-          System.out.println("Os valores de início e de término precisam ser positivos. Tente novamente.");
+          System.out.println(
+            "Os valores de início e de término precisam ser positivos. Tente novamente.");
         } else break;
       }
       for (Integer i = inicio; i <= termino; i++) {
         String str = i.toString();
         StringBuilder input = new StringBuilder();
         String reversedStr = input.append(str).reverse().toString();
-        if (str.equals(reversedStr))
-          System.out.println(str);
+        if (str.equals(reversedStr)) System.out.println(str);
       }
     } catch (InputMismatchException e) {
       System.out.println("Os limites de início e de término precisam ser números inteiros.");
