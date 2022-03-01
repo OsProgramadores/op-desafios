31901b2cbe9bab4dbd20756d8d6b0079c29478c6
DanielRosa74 <58576743+DanielRosa74@users.noreply.github.com>
2022-02-28 23:44:58 -0300

    Removing spaces

--- desafio-03/DanielRosa74/java/PalindromicNumbers.java
+++ desafio-03/DanielRosa74/java/PalindromicNumbers.java
@@ -1,30 +1,31 @@
 import java.util.*;
 
 public class PalindromicNumbers {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-            System.out.print("Insira o valor inicial: ");
-            Integer inicio = sc.nextInt();
-            System.out.print("Insira o valor final: ");
-            Integer termino = sc.nextInt();
-            if ( inicio > termino || inicio < 0 || termino < 0) {
-                System.out.println("O valor de término precisa ser maior que o de início e ambos devem ser positivos. Tente novamente.");
-                System.out.print("Insira o valor inicial: ");
-                inicio = sc.nextInt();
-                System.out.print("Insira o valor final: ");
-                termino = sc.nextInt();
-            }
-            for (Integer i = inicio; i <= termino; i++) {
-                String str = i.toString();
-                StringBuilder input = new StringBuilder();
-                String reversedStr = input.append(str).reverse().toString();
-                if (str.equals(reversedStr))
-                    System.out.println(str);
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Os limites de início e de término precisam ser números inteiros.");
-        }
-        sc.close();
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    try {
+      System.out.print("Insira o valor inicial: ");
+      Integer inicio = sc.nextInt();
+      System.out.print("Insira o valor final: ");
+      Integer termino = sc.nextInt();
+      if (inicio > termino || inicio < 0 || termino < 0) {
+        System.out.println(
+            "O valor de término precisa ser maior que o de início e ambos devem ser positivos. Tente novamente.");
+        System.out.print("Insira o valor inicial: ");
+        inicio = sc.nextInt();
+        System.out.print("Insira o valor final: ");
+        termino = sc.nextInt();
+      }
+      for (Integer i = inicio; i <= termino; i++) {
+        String str = i.toString();
+        StringBuilder input = new StringBuilder();
+        String reversedStr = input.append(str).reverse().toString();
+        if (str.equals(reversedStr))
+          System.out.println(str);
+           }
+      } catch (InputMismatchException e) {
+        System.out.println("Os limites de início e de término precisam ser números inteiros.");
+      }
+      sc.close();
+  }
 }
