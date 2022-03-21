093f9b333cd35d923c403dc69b5b248b0ba33ff7
MatMercer <contact@matbm.net>
2022-03-20 21:08:28 -0300

    Melhora usabilidade do desafio 4 [xadrez]

--- desafio-04/MatMercer/go/xadrez.go
+++ desafio-04/MatMercer/go/xadrez.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"os"
+	"time"
 )
 
 func main() {
@@ -10,41 +13,45 @@ func main() {
 		usoEDesliga()
 	}
 
-	tabuleiro := os.Args[1]
+	if "gerar-tabuleiro" == os.Args[1] {
+		tabuleiroExemplo()
+	} else if "gerar-tabuleiro-gigante" == os.Args[1] {
+		tabuleiroGigante()
+	}
 
-	validarTabuleiro(tabuleiro)
+	var buffer = abrirTabuleiro()
 
-	var pecas [7]int
-	var buffer = []byte(tabuleiro)
+	validarTabuleiro(buffer)
+	contarPecas(buffer)
+}
 
-	var idx = 0
-	for idx < len(buffer) {
-		for i := 0; i < 8; i++ {
-			var bytePeca = buffer[idx]
+func tabuleiroExemplo() {
+	fmt.Print("4 3 2 5 6 2 3 4\n1 1 1 1 1 1 1 1\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n0 0 0 0 0 0 0 0\n1 1 1 1 1 1 1 1\n4 3 2 5 6 2 3 4")
+	os.Exit(0)
+}
 
-			// ascii table
-			var peca = bytePeca - 48
-			pecas[peca]++
+func tabuleiroGigante() {
+	const tamanho = 32
+	rand.Seed(time.Now().UnixNano())
 
-			idx += 2
+	for i := 0; i < tamanho; i++ {
+		for j := 0; j < tamanho; j++ {
+			fmt.Print(rand.Intn(7))
+			if j < tamanho-1 {
+				fmt.Print(" ")
+			}
+		}
+		if i < tamanho-1 {
+			fmt.Print("\n")
 		}
 	}
 
-	fmt.Println("Peão:", pecas[1], "peça(s)")
-	fmt.Println("Bispo:", pecas[2], "peça(s)")
-	fmt.Println("Cavalo:", pecas[3], "peça(s)")
-	fmt.Println("Torre:", pecas[4], "peça(s)")
-	fmt.Println("Rainha:", pecas[5], "peça(s)")
-	fmt.Println("Rei:", pecas[6], "peça(s)")
+	os.Exit(0)
 }
 
-func validarTabuleiro(tabuleiro string) {
-	if len(tabuleiro) != 127 {
-		tabuleiroInvalido()
-	}
-
+func validarTabuleiro(tabuleiro []byte) {
 	for _, char := range tabuleiro {
-		if char != '\n' && char != ' ' && (char < 48 || char > 54) {
+		if char != '\n' && char != '\r' && char != ' ' && (char < 48 || char > 54) {
 			tabuleiroInvalido()
 		}
 	}
@@ -55,11 +62,46 @@ func tabuleiroInvalido() {
 	usoEDesliga()
 }
 
+func abrirTabuleiro() []byte {
+	arquivo := os.Args[1]
+	buffer, err := ioutil.ReadFile(arquivo)
+
+	if err != nil {
+		fmt.Printf("Falha ao abrir o arquivo \"%s\": %s\n", arquivo, err)
+		os.Exit(2)
+	}
+
+	return buffer
+}
+
+func contarPecas(buffer []byte) {
+	var pecas [7]int
+
+	var idx = 0
+	for idx < len(buffer) {
+		var bytePeca = buffer[idx]
+
+		// ascii table
+		// transforma um char na distância do número 0 que esse char está, excluindo ' ', '\r' e '\n'
+		var peca = bytePeca % '0' % ' ' % '\r' % '\n'
+		pecas[peca]++
+
+		idx += 1
+	}
+
+	fmt.Println("Peão:", pecas[1], "peça(s)")
+	fmt.Println("Bispo:", pecas[2], "peça(s)")
+	fmt.Println("Cavalo:", pecas[3], "peça(s)")
+	fmt.Println("Torre:", pecas[4], "peça(s)")
+	fmt.Println("Rainha:", pecas[5], "peça(s)")
+	fmt.Println("Rei:", pecas[6], "peça(s)")
+}
+
 func usoEDesliga() {
-	fmt.Println("Uso: xadrez [tabuleiro]")
-	fmt.Println("\nNo Linux Bash:")
-	fmt.Println("xadrez \"$(printf \"4 3 2 5 6 2 3 4\\n1 1 1 1 1 1 1 1\\n0 0 0 0 0 0 0 0\\n0 0 0 0 0 0 0 0\\n0 0 0 0 0 0 0 0\\n0 0 0 0 0 0 0 0\\n1 1 1 1 1 1 1 1\\n4 3 2 5 6 2 3 4\")\"")
-	fmt.Println("\nNo Windows Powershell:")
-	fmt.Println("xadrez.exe \"4 3 2 5 6 2 3 4`n1 1 1 1 1 1 1 1`n0 0 0 0 0 0 0 0`n0 0 0 0 0 0 0 0`n0 0 0 0 0 0 0 0`n0 0 0 0 0 0 0 0`n1 1 1 1 1 1 1 1`n4 3 2 5 6 2 3 4\"")
+	fmt.Println("Uso: xadrez [nome_arquivo]")
+	fmt.Println("\nVoce pode gerar o tabuleiro e salvar ele:")
+	fmt.Println("xadrez gerar-tabuleiro")
+	fmt.Println("\nO programa também aceita tabuleiros de qualquer tamanho:")
+	fmt.Println("xadrez gerar-tabuleiro-gigante")
 	os.Exit(1)
 }
