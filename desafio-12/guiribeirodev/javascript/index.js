3e2efd29dd25f376d7edfa48611b7d876bc9d5d5
guiribeirodev <guiribeirodev@gmail.com>
2022-07-14 16:57:16 -0300

    Resolução Exercício 12 - Potências de 2

---
+++ desafio-12/guiribeirodev/javascript/index.js
@@ -0,0 +1,42 @@
+const { readFileSync } = require('fs')
+const file = process.argv[2]
+const input = readFileSync(file, 'utf-8')
+
+
+let values = input.split('\n').filter((value) => {
+  return value
+})
+
+function isPotency(number) {
+  /* A função é feita usando BigInt, pois a divisão com  numeros maiores que o MAX_SAFE_INTEGER 
+  sempre resultam em infinity.*/
+  number = BigInt(number)
+  let exponent = 0
+
+  while (number >= 1n) {
+    if (number == 1n) {
+      return ['true', exponent]
+    }
+    else if (number % 2n != 0n) {
+      return ['false', exponent]
+    }
+    number /= 2n
+    exponent += 1
+  }
+
+  return ['false', exponent]
+}
+
+function main() {
+  for (value of values) {
+    const result = isPotency(value)
+    if (result[0] == "true") {
+      console.log(value, result[0], result[1])
+    }
+    else {
+      console.log(value, result[0])
+    }
+  }
+}
+
+main()
