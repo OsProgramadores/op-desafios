b2dec7a4b0f672f7239bc55f6531f26c0b30e786
Igor Rafael <guiboratto.2001@gmail.com>
2022-07-30 20:24:28 -0300

    Feito Desafio 02

---
+++ desafio-02/IgorRafaelSouza/csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NumPrimosDesafio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const int limiteDesafio = 10000;
+            List<int> primos = new();
+
+            for (int i = 3; i < limiteDesafio; i++)
+            {
+                if ((i % 2) == 0) continue;
+
+                bool ehPrimo = true;
+                foreach (int j in primos)
+                {
+                    if (i % j == 0)
+                    {
+                        ehPrimo = false;
+                        break;
+                    }
+                }
+                if (ehPrimo)
+                {
+                    primos.Add(i);
+                }
+            }
+            primos.Insert(0, 2);
+
+            foreach (int numPrimo in primos)
+            {
+                Console.WriteLine(numPrimo);
+            }
+        }
+    }
+}
