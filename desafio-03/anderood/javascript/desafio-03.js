e9a5e51e9836dd60499434ed67c50938c5063a33
Anderson Santos <anderson.santos@eunerd.com.br>
2022-08-06 23:18:00 -0300

    Realizando a impressão dos Numeros, ao inves do Vetor

--- desafio-03/anderood/javascript/desafio-03.js
+++ desafio-03/anderood/javascript/desafio-03.js
@@ -1,20 +1,16 @@
 function palindromeNumbers(number){
 
-    let numbers = [];
-
     for (let i = 0; i < 10; i++) {
-        numbers.push(i)
+        console.log(i)
     }
 
     for (let x = 10; x < number; x++) {
         let numberConvert = x.toString().split('').reverse().join('');
         let numberReverse = parseInt(numberConvert);
-        x === numberReverse ? numbers.push(x) : '';
+
+        x === numberReverse ? console.log(x) : '';
     }
 
-    return numbers;
 }
 
-const result = palindromeNumbers(100);
-
-console.log(result);
+palindromeNumbers(10000);
