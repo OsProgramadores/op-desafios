20cfa78553d0b07ba77069250a41913d3c4968cf
MantoDev97 <devmanto97@gmail.com>
2022-08-07 10:48:15 -0300

    Solução desafio 8

--- desafio-08/MantoDev97/c/desafio8.c
+++ desafio-08/MantoDev97/c/desafio8.c
@@ -2,7 +2,7 @@
 /*Escreva um programa que leia um arquivo texto contendo uma lista de frações em ASCII 
 (uma por linha) e produza na saída a versão simplificada de cada fração. 
 Números simples assumem denominador 1 (apenas imprima o número). 
-Em caso de erros na entrada (como divisão por zero), imprima “ERR” em maíusculas.*/
+Em caso de erros na entrada (como divisão por zero), imprima  “ERR”  em maíusculas.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
