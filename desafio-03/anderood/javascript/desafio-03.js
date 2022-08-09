57fcc793efb3b645c4b51c35d246a55e0b1afec9
Anderson Santos <anderson.santos@eunerd.com.br>
2022-08-09 01:02:56 -0300

    Correção de Logica

--- desafio-03/anderood/javascript/desafio-03.js
+++ desafio-03/anderood/javascript/desafio-03.js
@@ -1,16 +1,11 @@
 function palindromeNumbers(number){
 
-    for (let i = 0; i < 10; i++) {
-        console.log(i)
-    }
-
-    for (let x = 10; x < number; x++) {
+    for (let x = 1; x < number; x++) {
         let numberConvert = x.toString().split('').reverse().join('');
         let numberReverse = parseInt(numberConvert);
 
         x === numberReverse ? console.log(x) : '';
     }
-
 }
 
 palindromeNumbers(10000);
