8e5fde96ae6432b1a4d2b648894f310fa9404fca
Alexandre Oliver <Alexandreferreiradeoliveira40@gmail.com>
2022-09-04 13:53:35 -0300

    Resolução Desafio 02 por @AlexandreOliver

---
+++ desafio-02/AlexandreOliver/csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int countDivisores = 1;
+            
+            for (int n = 1; n < 10000; n++)
+            {
+                for (int div = n; div != 1; div--)
+                {
+                    if (n % div == 0)
+                        countDivisores++;
+                }
+
+                if (countDivisores == 2)
+                    Console.WriteLine(n);
+                
+                countDivisores = 1;
+                
+            } 
+        }
+    }
+}
