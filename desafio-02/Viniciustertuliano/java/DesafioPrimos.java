331266a93b17670bbe3e15e2732becf158895272
vinicius tertuliano <viniciustertulianodasilva@hotmail.com>
2022-09-10 21:55:11 -0300

    Primos de 0 a 10000 com java

---
+++ desafio-02/Viniciustertuliano/java/DesafioPrimos.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DesafioPrimos {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 10000; i++) {
+            boolean primo = true;
+            for (int a = 2; a < i; a++) {
+                if(i % a == 0){
+                    primo = false;
+                }
+            }
+            if (primo){
+                System.out.println(i);
+            }
+        }
+    }
+}
