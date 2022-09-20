6bc26adc7a54ab186bbe58ecaf07ff218ccdeeb6
Italo Santos <italo.santos02@outlook.com.br>
2022-09-20 01:09:07 +0000

    Concluindo o desafio 02

---
+++ desafio-02/KayleKylian/csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿int numDivisor;
+
+for (int num = 1; num <= 10000; num++)
+{
+    numDivisor = 0;
+    for (int i = num; i > 0; i--)
+    {
+        if (num % i == 0)
+            numDivisor++;
+    }
+
+    if (numDivisor == 2)
+        Console.WriteLine(num);
+}
+
+// 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,53, 59, 61, 67, 71, 73, 79, 83, 89 -- Números primos.
