b93858533d1c3ec959dea1a2aa4c8a6ec466d0c2
andretaveira90 <andretaveirati@gmail.com>
2022-09-24 21:01:38 -0300

    Resolução desafio-02 por @andretaveira90.

---
+++ desafio-02/andretaveira90/java/NumerosPrimos.java
@@ -0,0 +1,15 @@
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        for (int numero = 2; numero <= 10000; numero++) {
+            boolean ehPrimo = true;
+            for (int contador = 2; contador < numero; contador++) {
+                if (numero % contador == 0) {
+                    ehPrimo = false;
+                }
+            }
+            if (ehPrimo) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
