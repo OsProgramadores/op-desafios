a88813659799340267da31600543eb4f72a499cb
Anthony Louis <anthony@simbioseventures.com>
2022-10-01 08:58:55 -0300

    Fix file with errors using formatter

--- desafio-14/anthonylgf/java/src/main/java/ArquivoTeste.java
+++ desafio-14/anthonylgf/java/src/main/java/ArquivoTeste.java
@@ -15,53 +15,50 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class ArquivoTeste {
 
-public static void main(final String[] args) {
-if (args.length != 1) {
-out.println("You should run java Main <file-path>");
-exit(-1);
-}
+  public static void main(final String[] args) {
+    if (args.length != 1) {
+      out.println("You should run java Main <file-path>");
+      exit(-1);
+    }
 
-try (final var scanner = new Scanner(new File(args[0]))) {
-while (scanner.hasNextLine()) {
-final String expression = scanner
-.nextLine()
-.trim();
+    try (final var scanner = new Scanner(new File(args[0]))) {
+      while (scanner.hasNextLine()) {
+        final String expression = scanner.nextLine().trim();
 
-try {
-final int result = solveExpression(expression);
-out.println(result);
-} catch (final ArithmeticException arithmeticException) {
-out.println("ERR DIVBYZERO");
-} catch (final SyntaxException syntaxException) {
-out.println("ERR SYNTAX");
-}
-}
+        try {
+          final int result = solveExpression(expression);
+          out.println(result);
+        } catch (final ArithmeticException arithmeticException) {
+          out.println("ERR DIVBYZERO");
+        } catch (final SyntaxException syntaxException) {
+          out.println("ERR SYNTAX");
+        }
+      }
 
-} catch (FileNotFoundException e) {
-out.println("Defined a non-existent file: " + args[0]);
-exit(-1);
-}
-}
+    } catch (FileNotFoundException e) {
+      out.println("Defined a non-existent file: " + args[0]);
+      exit(-1);
+    }
+  }
 
-static int solveExpression(final String expression) {
-final var expressionAsStream = CharStreams.fromString(expression);
-final var listener = new ErrorListener();
-final var lexer = new CalculatorLexer(expressionAsStream);
+  static int solveExpression(final String expression) {
+    final var expressionAsStream = CharStreams.fromString(expression);
+    final var listener = new ErrorListener();
+    final var lexer = new CalculatorLexer(expressionAsStream);
 
-lexer.removeErrorListeners();
-lexer.addErrorListener(listener);
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(listener);
 
-final var token = new CommonTokenStream(lexer);
-final var parser = new CalculatorParser(token);
+    final var token = new CommonTokenStream(lexer);
+    final var parser = new CalculatorParser(token);
 
-parser.removeErrorListeners();
-parser.addErrorListener(listener);
+    parser.removeErrorListeners();
+    parser.addErrorListener(listener);
 
-final ParseTree rootNode = parser.plusOrMinus();
+    final ParseTree rootNode = parser.plusOrMinus();
 
-final Visitor visitor = new Visitor();
+    final Visitor visitor = new Visitor();
 
-return visitor.visit(rootNode);
-}
-
+    return visitor.visit(rootNode);
+  }
 }
