c375a96d7734c8572025ceb766ac3ce3cfc5daf3
Anthony Louis <anthony.physis@gmail.com>
2022-10-05 14:47:46 -0300

    Fix error in file

--- desafio-03/anthonylgf/javascript/palindromos-2.js
+++ desafio-03/anthonylgf/javascript/palindromos-2.js
@@ -4,7 +4,7 @@ for (var i = 1; i <= 100000; i++) {
   var string = String(i).split('')
 
   for (var j = 0; j < string.length / 2; j++) {
-    if (string[j] != string[string.length - (1 + j)]) {
+    if (string[j] !== string[string.length - (1 + j)]) {
       ehPalidromo = false
       break
     }
