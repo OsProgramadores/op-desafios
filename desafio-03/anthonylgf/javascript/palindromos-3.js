16793dae2d1cd095b80948e9e3ef6e09811f3348
Anthony Louis <anthony.physis@gmail.com>
2022-10-05 14:53:18 -0300

    Test with multiple files

---
+++ desafio-03/anthonylgf/javascript/palindromos-3.js
@@ -0,0 +1,15 @@
+for(var i=1; i<= 100000; i++)
+{
+  var eh_palidromo = true;
+  var string = String(i).split("");
+
+  for(var j=0; j<string.length/2; j++){
+      if(string[j] != string[string.length-(1+j)]){
+        eh_palidromo = false;
+        break;
+      }
+  }
+
+  if(eh_palidromo)
+    console.log(i);
+}
