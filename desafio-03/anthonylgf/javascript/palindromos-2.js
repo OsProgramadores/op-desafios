8c9c5f00a0aefddbf833150d8b35a540d49eed1f
Anthony Louis <anthony.physis@gmail.com>
2022-10-05 12:16:23 -0300

    Remove trailling spaces

--- desafio-03/anthonylgf/javascript/palindromos-2.js
+++ desafio-03/anthonylgf/javascript/palindromos-2.js
@@ -1,17 +1,16 @@
-for(var i=1; i<= 100000; i++)
-{
-  var eh_palidromo = true;
+for (var i = 1; i <= 100000; i++) {
+  var ehPalidromo = true
 
-  
-  var string = String(i).split("");
+  var string = String(i).split('')
 
-  for(var j=0; j<string.length/2; j++){
-      if(string[j] != string[string.length-(1+j)]){
-        eh_palidromo = false;
-        break;
-      }
+  for (var j = 0; j < string.length / 2; j++) {
+    if (string[j] != string[string.length - (1 + j)]) {
+      ehPalidromo = false
+      break
+    }
   }
 
-  if(eh_palidromo)
-    console.log(i);
+  if (ehPalidromo) {
+    console.log(i)
+  }
 }
