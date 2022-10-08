6cad4a0729bb36098300dab7a0986df32f75adc8
vinicius tertuliano <viniciustertulianodasilva@hotmail.com>
2022-10-08 06:09:50 -0300

    second formatting google

--- desafio-02/Viniciustertuliano/java/DesafioPrimos.java
+++ desafio-02/Viniciustertuliano/java/DesafioPrimos.java
@@ -1,11 +1,16 @@
 public class DesafioPrimos {
+
     public static void main(String[] args) {
         for (int i = 2; i <= 10000; i++) {
             boolean primo = true;
             for (int a = 2; a < i; a++) {
-                if (i % a == 0) primo = false;
+                if (i % a == 0) {
+                    primo = false;
+                }
             }
-            if (primo) System.out.println(i);
+            if (primo) {
+                System.out.println(i);
+            }
         }
     }
 }
