6843c2cb3405068bd9b86685f5fd95f5c84467ce
Luiiza0 <luuhcosta20@gmail.com>
2022-10-18 11:40:27 -0300

    desafio-02.

---
+++ desafio-02/luiza/Untitled-1.js
@@ -0,0 +1,29 @@
+desafio-02/
+luiiza0/
+Javascript/
+
+tarNumerosPrimos(10000);
+
+function listarNumerosPrimos(limite){
+    for(let numero = 1; numero <= limite; numero++){
+
+        if(NumeroPrimo(numero)) console.log(numero);
+
+}
+
+
+
+}
+
+function NumeroPrimo(numero) {
+
+    for (let divisor = 2; divisor < numero; divisor++){
+          if(numero % divisor === 0) {
+
+            return false;
+          }
+
+    }
+     return true
+
+}
