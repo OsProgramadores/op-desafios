f16c349059241ad661cf03599f26ab7dc44bfc22
Luiiza0 <luuhcosta20@gmail.com>
2022-10-21 16:55:13 -0300

    desafio-02

--- desafio-02/luiiza0/javascript/MeuPrograma.js
+++ desafio-02/luiiza0/javascript/MeuPrograma.js
@@ -1,14 +1,14 @@
-listarNumerosPrimos(10000)
-function listarNumerosPrimos (limite) {
-for (let numero = 1; numero <= limite; numero++) {
-if (NumeroPrimo (numero)) console.log (numero)
+listarNumerosPrimos(10000);
+function listarNumerosPrimos(limite) {
+    for (var numero = 1; numero <= limite; numero++) {
+        if (numeroPrimo (numero)) console.log (numero);
+    }
 }
-}
-function NumeroPrimo (numero) {
-for (let divisor = 2; divisor < numero; divisor++) {
-if (numero % divisor === 0) {
-return false;
-}
-}
-return true
+function numeroPrimo(numero) {
+   for (var divisor = 2; divisor < numero; divisor++) {
+       if (numero % divisor === 0) { 
+           return false;
+        }
+    }
+  return true;
 }
