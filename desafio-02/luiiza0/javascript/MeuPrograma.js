261d952977b87f986ee025ebee25be758fbfd39c
Luiiza0 <luuhcosta20@gmail.com>
2022-10-23 20:11:34 -0300

    não coloquei end no final do código.

--- desafio-02/luiiza0/javascript/MeuPrograma.js
+++ desafio-02/luiiza0/javascript/MeuPrograma.js
@@ -1,12 +1,12 @@
 listarNumerosPrimos(10000);
 function listarNumerosPrimos(limite) {
-    for (var numero = 1; numero <= limite; numero++) {
+    for (var numero = 2; numero <= limite; numero++) {
         if (numeroPrimo (numero)) console.log (numero);
     }
 }
 function numeroPrimo(numero) {
    for (var divisor = 2; divisor < numero; divisor++) {
-       if (numero % divisor === 0) { 
+       if (numero % divisor === 0){
            return false;
         }
     }
