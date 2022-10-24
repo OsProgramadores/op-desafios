981df180686fa66fd989bf16b522884a9e6da3b1
Bruna Oliveira <bruna.o.fonseca@hotmail.com>
2022-10-24 20:28:08 -0300

    add des02 JS

---
+++ desafio-02/bruna-luc/javascript/index.js
@@ -0,0 +1,26 @@
+// Escreva um programa para listar todos os números primos entre 1 e 10000, na linguagem de sua preferência.
+
+// 1passo :verrificar se o numero é primo
+
+// 2passo : Armazenar os numeros no Array
+
+// 3passo : Imprimir o array
+
+const final = 10000
+let count = 2
+let count2 = 2
+const primo = [1]
+
+for (count; count < final; count++) {
+  count2 = (count - 1)
+  primo.push([count])
+  for (count2; count2 > 1; count2--) {
+    if ((count % (count2)) === 0) {
+      count2 = 2
+      if (count2 === 2) {
+        primo.pop()
+      }
+    }
+  }
+}
+document.getElementById('resultado').innerHTML = primo
