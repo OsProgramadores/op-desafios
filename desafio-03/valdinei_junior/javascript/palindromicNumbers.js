327af56dcf938eca6a65b49bc90f341fa4f8530d
Valdinei de Paula Junior <43623234+ValdineiJunior@users.noreply.github.com>
2022-10-26 19:21:34 -0300

    Separate functions
    separate iteration function from palindrome function

--- desafio-03/valdinei_junior/javascript/palindromicNumbers.js
+++ desafio-03/valdinei_junior/javascript/palindromicNumbers.js
@@ -1,29 +1,39 @@
-function palindromicNumbers (firstNumber, lastNumber) {
-  if (typeof firstNumber !== 'number' || typeof lastNumber !== 'number' || firstNumber > lastNumber) {
-    console.log('Os parâmetros devem ser dois números inteiros sendo o primeiro menor que o segundo.')
+function iterationBetweenNumbers (firstNumber, lastNumber) {
+  if (
+    typeof firstNumber !== 'number' ||
+    typeof lastNumber !== 'number' ||
+    firstNumber > lastNumber
+  ) {
+    console.log(
+      'Os parâmetros devem ser dois números inteiros sendo o primeiro menor que o segundo.'
+    )
   } else {
     for (let number = firstNumber; number <= lastNumber; number++) {
-      let numberToCountDigit = number
-      let numberOfDigits = 0
-      while (numberToCountDigit >= 1) {
-        numberToCountDigit /= 10
-        numberOfDigits++
-      }
-      let expoente = 0
-      let reverseNumber = 0
-      while (numberOfDigits > 0) {
-        const digit = Math.floor(
-          (number % 10 ** numberOfDigits) / 10 ** (numberOfDigits - 1)
-        )
-        reverseNumber += digit * 10 ** expoente
-        numberOfDigits--
-        expoente++
-      }
-      if (number === reverseNumber) {
-        console.log(number)
-      }
+      checkIfTheNumberIsPalindrome(number)
     }
   }
 }
 
-palindromicNumbers(1, 10000)
+function checkIfTheNumberIsPalindrome (number) {
+  let numberToCountDigit = number
+  let numberOfDigits = 0
+  while (numberToCountDigit >= 1) {
+    numberToCountDigit /= 10
+    numberOfDigits++
+  }
+  let expoente = 0
+  let reverseNumber = 0
+  while (numberOfDigits > 0) {
+    const digit = Math.floor(
+      (number % 10 ** numberOfDigits) / 10 ** (numberOfDigits - 1)
+    )
+    reverseNumber += digit * 10 ** expoente
+    numberOfDigits--
+    expoente++
+  }
+  if (number === reverseNumber) {
+    console.log(number)
+  }
+}
+
+iterationBetweenNumbers(1, 10000)
