202c712420803c0289fbf7e9ef850ccbb4439ff4
Valdinei de Paula Junior <43623234+ValdineiJunior@users.noreply.github.com>
2022-10-26 18:36:06 -0300

    Change in Digit Count Calculation

--- desafio-03/valdinei_junior/javascript/palindromicNumbers.js
+++ desafio-03/valdinei_junior/javascript/palindromicNumbers.js
@@ -3,15 +3,20 @@ function palindromicNumbers (firstNumber, lastNumber) {
     console.log('Os parâmetros devem ser dois números inteiros sendo o primeiro menor que o segundo.')
   } else {
     for (let number = firstNumber; number <= lastNumber; number++) {
-      let lengthNumber = number.toString().length
+      let numberToCountDigit = number
+      let numberOfDigits = 0
+      while (numberToCountDigit >= 1) {
+        numberToCountDigit /= 10
+        numberOfDigits++
+      }
       let expoente = 0
       let reverseNumber = 0
-      while (lengthNumber > 0) {
+      while (numberOfDigits > 0) {
         const digit = Math.floor(
-          ((number % 10 ** lengthNumber) / 10 ** (lengthNumber - 1))
+          (number % 10 ** numberOfDigits) / 10 ** (numberOfDigits - 1)
         )
-        reverseNumber += (digit * 10 ** expoente)
-        lengthNumber--
+        reverseNumber += digit * 10 ** expoente
+        numberOfDigits--
         expoente++
       }
       if (number === reverseNumber) {
