430ceb596db0a86b8ff48dd4e4ea70373fcf85fe
Marcio Gusmao <marciogusmao@msn.com>
2022-10-27 01:40:49 -0300

    Nova revisão

--- desafio-02/sirmarcius/javascript/primo.js
+++ desafio-02/sirmarcius/javascript/primo.js
@@ -1,14 +1,22 @@
 /* Escreva um programa para listar todos os números primos
-entre 1 e 10000.*/
+entre 1 e 10000. */
 
-// eslint-disable-next-line require-jsdoc
+// eslint-disable-next-line require-jsdoc, consistent-return
 function numeroPrimo(num) {
-    for (let divisor = 2; divisor < num; divisor++) {
+  // eslint-disable-next-line no-unreachable-loop, no-plusplus
+  for (let divisor = 2; divisor < num; divisor++) {
     if (num % divisor === 0) {
-    return false;
+      return false;
     }
-  return true;
+    return true;
+  }
 }
 const numero = 10000;
-for (let i = 2; i < numero; i++)
- if (numeroPrimo(i)) console.log(i);
+// eslint-disable-next-line no-plusplus
+for (let i = 2; i < numero; i++) {
+  // eslint-disable-next-line no-console
+  if (numeroPrimo(i)) {
+    // eslint-disable-next-line no-console
+    console.log(i);
+  }
+}
