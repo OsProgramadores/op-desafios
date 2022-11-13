51c2f41a0512585813edcc626e2a6720037ca89b
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-11-13 14:45:06 -0800

    Desafio 2 em Java.

---
+++ desafio-02/leonelsanchesdasilva/java/README.md
@@ -0,0 +1,6 @@
+# Desafio 2 em Java
+
+```sh
+javac ./desafio02.java
+java Desafio02
+```
---
+++ desafio-02/leonelsanchesdasilva/java/desafio02.java
@@ -0,0 +1,17 @@
+class Desafio02 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10000; i++) {
+            Boolean primo = true;
+            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo) {
+                System.out.println(i);
+            }
+        }
+    }
+}
