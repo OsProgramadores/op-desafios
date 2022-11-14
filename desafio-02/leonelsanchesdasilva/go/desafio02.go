13c6bd50b0064041c6f1889cf3bf6236f2657640
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2022-11-13 16:26:56 -0800

    Desafio 2 em Go. (#868)
    * Desafio 2 em Go.
    
    * Lint de Go.

---
+++ desafio-02/leonelsanchesdasilva/go/desafio02.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	for i := 1; i <= 10000; i++ {
+		var primo bool = true
+		var raizQuadrada int = int(math.Floor(math.Sqrt(float64(i))))
+		for j := 2; j <= raizQuadrada; j++ {
+			if i%j == 0 {
+				primo = false
+				break
+			}
+		}
+		if primo {
+			fmt.Println(i)
+		}
+	}
+}
