f38ce4ea2d11386700d00883ebde89d9a7bbcd9b
Leonel Sanches da Silva <leonel@designliquido.com.br>
2022-11-13 17:09:22 -0800

    Linter.

--- desafio-03/leonelsanchesdasilva/javascript/desafio03.js
+++ desafio-03/leonelsanchesdasilva/javascript/desafio03.js
@@ -1,5 +1,5 @@
 for (let i = 0; i <= 3010; i++) {
-  if (String(i) === String(i).split("").reverse().join("")) {
-    console.log(i);
+  if (String(i) === String(i).split('').reverse().join('')) {
+    console.log(i)
   }
 }
