bf811413b4d76aab51cfe5558725529f8a64ad76
Hoklifter <biro1@outlook.com>
2022-11-13 21:42:41 -0300

    Correção DESAFIO3.

--- desafio-03/hoklifter/python/palindromos.py
+++ desafio-03/hoklifter/python/palindromos.py
@@ -2,9 +2,11 @@
 try:
     NUM1 = int(input('Digite o PRIMEIRO número:    '))
     NUM2 = int(input('Digite o SEGUNDO número:    '))
-    if NUM1 > 0 and NUM2 > 0 <= 18446744073709551615:
+    if NUM1 > NUM2:
         for x in range(NUM1, NUM2+1):
             if str(x)[::-1] == str(x):
                 print(x)
+    else:
+        raise ValueError
 except ValueError:
     print('Número invalido')
