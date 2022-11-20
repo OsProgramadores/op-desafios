5f42f205a6b693db699a01ad50f3f52f96539095
soualann <souzacordeiroalann@gmail.com>
2022-11-20 13:42:35 -0300

    Reenvio Desafio 02 Java

---
+++ desafio-02/soualann/Java/Desafio02.java
@@ -0,0 +1,17 @@
+public class Desafio02 {
+    public static void main(String[] args) {
+        int num = 10000;
+
+        for(int i = 2; i <= num; i++){
+            boolean primo = true;
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    primo = false;
+                }
+            }
+            if(primo){
+                System.out.println(i);
+            }
+        }
+    }
+}
