094480a28d1170a4f8abbaac8aacf415e4ecd692
liomar <liomarhora@gmail.com>
2022-11-20 07:50:37 -0300

    palinromicos corrigidos para operar com mais de 2 dígitos

--- desafio-03/liomarhora/cpp/palindromicos.cpp
+++ desafio-03/liomarhora/cpp/palindromicos.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 
 int main()
 {
 	std::string converter, numero_inverso;
 	
-	for(int i = 0; i <= 64;  i++)
+	for(int i = 9; i <= 100000;  i++)
 	{
-		if(i > 9)		
 		converter = std::to_string(i);
-		
+				
 		if(!converter.empty())
 		{
-			numero_inverso = converter[1];
-			numero_inverso += converter[0];
+			size_t convertSize = converter.length();
+			int j = static_cast<int>(convertSize);
 			
+			for(int x = j-1; x >= 0; x--)
+			{
+				if(numero_inverso.empty())
+				{
+					numero_inverso = converter[x];
+				} else {
+					numero_inverso += converter[x];
+				}				
+			}
+						
 			if(converter == numero_inverso)
 			{
 				std::cout << "Os numeros palindromicos sao: " << converter << '\n';
 			}
-	    }		
+		}
+		
+		numero_inverso.erase();
+		converter.erase();
 	}
 }
