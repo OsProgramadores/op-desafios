981928b640df5d8b0ae1581a87607cef70d2b108
Bruc3W <brucelopesgithub@gmail.com>
2022-11-21 22:08:36 -0300

    desafio 02 em js

---
+++ desafio-02/Bruc3W/javascript/script.js
@@ -0,0 +1,13 @@
+const limite = 100;
+
+for (let i = 2; i <= limite; i++) {
+  let ePrimo = true
+
+  for (let divisor = 2; divisor < i; divisor++) {
+    if (i % divisor === 0) {
+      ePrimo = false
+      break
+    }
+  }
+  if (ePrimo) console.log(i)
+}
