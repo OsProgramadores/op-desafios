88a83701182a773d97a920cc92ccb69d18bf59a5
soualann <souzacordeiroalann@gmail.com>
2022-12-07 20:54:29 -0300

    Desafio 03 - Java

--- desafio-03/soualann/java/DesafioPalindromico.java
+++ desafio-03/soualann/java/DesafioPalindromico.java
@@ -1,19 +1,17 @@
 public class DesafioPalindromico {
-    public static void main(String[] args) {
-        int ninicial = 1;
-        int nfinal = 2000;
+  public static void main(String[] args) {
+    int ninicial = 1;
+    int nfinal = 2000;
 
-        VerificarPalindromico(ninicial, nfinal);
+    VerificarPalindromico(ninicial, nfinal);
+  }
+
+  private static void VerificarPalindromico(int numberinicial, int b) {
+    for (int i = numberinicial; i <= b; i++) {
+      if ((Integer.toString(i))
+          .equals(new StringBuilder(Integer.toString(i)).reverse().toString())) {
+        System.out.println(i);
+      }
     }
-
-    private static void VerificarPalindromico(int numberinicial, int b) {
-        for(int i = numberinicial; i <= b; i++){
-           if((Integer.toString(i)).equals(new StringBuilder(Integer.toString(i)).reverse().toString())){
-            System.out.println(i);
-           }
-        
-        }
-    }
-
-
+  }
 }
