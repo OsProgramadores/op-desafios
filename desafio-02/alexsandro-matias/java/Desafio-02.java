324756b1e3e1ec8174d9e65c90850bf8cec64310
alexsandro-matias <matiasalexsandro@gmail.com>
2022-12-08 20:42:23 -0300

    Aprimorando em Java

--- desafio-02/alexsandro-matias/java/Desafio-02.java
+++ desafio-02/alexsandro-matias/java/Desafio-02.java
@@ -1,19 +1,13 @@
-public class Desafio02
-{
-	public static void main(String[] args)
-	{
-		for (int j = 2; j < 1000; j++)
-		{
+public class Desafio02 {
+	public static void main(String[] args) {
+		for (int j = 2; j < 1000; j++) {
 			int contador = 0;
-			for (int i = 1; i <= j; i++)
-			{
-				if ((j % i) == 0)
-				{
+			for (int i = 1; i <= j; i++) {
+				if ((j % i) == 0) {
 					contador++;
 				}
 			}
-			if (contador == 2)
-			{
+			if (contador == 2) {
 				System.out.println(j);
 				j++;
 			}
