3311803c50ea2e01203e4e2e5f953a8fe0fd6d67
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-12-11 07:18:40 -0300

    alterado para rodar no node

--- desafio-08/valdineijunior/javascript/desafio08.js
+++ desafio-08/valdineijunior/javascript/desafio08.js
@@ -1,54 +1,58 @@
-let fractions = []
-fetch('./frac.txt')
-  .then(response => response.text())
-  .then(text => {
-    fractions = text.split('\r\n')
-    fractions.pop()
-    for (let index = 0; index < fractions.length; index++) {
-      const element = fractions[index]
-      const fintDivisor = (/(\/)\w/)
-      const ElementIsAlreadyAninteger = !fintDivisor.test(element)
-      if (ElementIsAlreadyAninteger) {
-        console.log(element)
-      } else {
-        const ArrayWithNumeratorAndDenominator = element.split('/')
-        const numerator = parseInt(ArrayWithNumeratorAndDenominator[0])
-        const denominator = parseInt(ArrayWithNumeratorAndDenominator[1])
-        const denominatorIsZero = denominator === 0
-        if (denominatorIsZero) {
-          console.log('ERR')
+const fs = require('fs')
+
+function readFractions(path) {
+  fs.readFile(path, 'utf-8', function (error, data) {
+    if (error) {
+      console.log('erro de leitura: ' + error.message)
+    } else {
+      const fractions = data.split('\r\n')
+      fractions.pop()
+      for (let index = 0; index < fractions.length; index++) {
+        const element = fractions[index]
+        const fintDivisor = (/(\/)\w/)
+        const ElementIsAlreadyAninteger = !fintDivisor.test(element)
+        if (ElementIsAlreadyAninteger) {
+          console.log(element)
         } else {
-          const quotient = numerator / denominator
-          const resresultOfDivisionIsAninteger = Number.isInteger(quotient)
-          if (resresultOfDivisionIsAninteger) {
-            console.log(quotient.toString())
+          const ArrayWithNumeratorAndDenominator = element.split('/')
+          const numerator = parseInt(ArrayWithNumeratorAndDenominator[0])
+          const denominator = parseInt(ArrayWithNumeratorAndDenominator[1])
+          const denominatorIsZero = denominator === 0
+          if (denominatorIsZero) {
+            console.log('ERR')
           } else {
-            if (numerator > denominator) {
-              const remainder = numerator % denominator
-              const simplifiedNumerator = numerator - remainder
-              const integer = simplifiedNumerator / denominator
-              console.log(`${integer} ${remainder}/${denominator}`)
+            const quotient = numerator / denominator
+            const resresultOfDivisionIsAninteger = Number.isInteger(quotient)
+            if (resresultOfDivisionIsAninteger) {
+              console.log(quotient.toString())
             } else {
-              const proportion = denominator / numerator
-              const theTwoNumbersaremultiples = Number.isInteger(proportion)
-              if (theTwoNumbersaremultiples) {
-                const canBeSimplified = numerator > 1
-                if (canBeSimplified) {
-                  const simplifiedNumerator = numerator / proportion
-                  const simplifiedDenominator = denominator / proportion
-                  console.log(`${simplifiedNumerator}/${simplifiedDenominator}`)
-                } else {
-                  console.log(`${numerator}/${denominator}`)
-                }
+              if (numerator > denominator) {
+                const remainder = numerator % denominator
+                const simplifiedNumerator = numerator - remainder
+                const integer = simplifiedNumerator / denominator
+                console.log(`${integer} ${remainder}/${denominator}`)
               } else {
-                for (let index = numerator; index > 0; index--) {
-                  const checkNumerador = Number.isInteger(numerator / index)
-                  const checkDenominador = Number.isInteger(denominator / index)
-                  if (checkNumerador && checkDenominador) {
-                    const simplifiedNumerator = numerator / index
-                    const simplifiedDenominator = denominator / index
+                const proportion = denominator / numerator
+                const theTwoNumbersaremultiples = Number.isInteger(proportion)
+                if (theTwoNumbersaremultiples) {
+                  const canBeSimplified = numerator > 1
+                  if (canBeSimplified) {
+                    const simplifiedNumerator = numerator / proportion
+                    const simplifiedDenominator = denominator / proportion
                     console.log(`${simplifiedNumerator}/${simplifiedDenominator}`)
-                    break
+                  } else {
+                    console.log(`${numerator}/${denominator}`)
+                  }
+                } else {
+                  for (let index = numerator; index > 0; index--) {
+                    const checkNumerador = Number.isInteger(numerator / index)
+                    const checkDenominador = Number.isInteger(denominator / index)
+                    if (checkNumerador && checkDenominador) {
+                      const simplifiedNumerator = numerator / index
+                      const simplifiedDenominator = denominator / index
+                      console.log(`${simplifiedNumerator}/${simplifiedDenominator}`)
+                      break
+                    }
                   }
                 }
               }
@@ -57,5 +61,6 @@ fetch('./frac.txt')
         }
       }
     }
-  }
-  )
+  })
+}
+readFractions('./frac.txt')
