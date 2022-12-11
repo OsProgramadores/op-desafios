236d47610eb3a915f9ad4b31f1ad81058518adf3
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-12-11 08:21:11 -0300

    alterando arquivo pra rodar no node

--- desafio-12/valdineijunior/javascript/potenciaDeDois.js
+++ desafio-12/valdineijunior/javascript/potenciaDeDois.js
@@ -1,22 +1,28 @@
-let numbersArray = []
-fetch('./d12.txt')
-  .then(response => response.text())
-  .then(text => {
-    numbersArray = text.split('\n')
-    numbersArray.pop()
-    for (let i = 0; i < numbersArray.length; i++) {
-      const element = numbersArray[i]
-      let numberIsAPotentialOfTwo = false
-      let expoent = 0n
-      while ((2n ** expoent) <= element) {
-        numberIsAPotentialOfTwo = (2n ** expoent) === BigInt(element)
-        expoent++
-      }
-      if (numberIsAPotentialOfTwo) {
-        expoent = expoent - 1n
-        console.log(element, numberIsAPotentialOfTwo, parseInt(expoent))
-      } else {
-        console.log(element, numberIsAPotentialOfTwo)
+const fs = require('fs')
+
+function readFractions(path) {
+  fs.readFile(path, 'utf-8', function (error, data) {
+    if (error) {
+      console.log('erro de leitura: ' + error.message)
+    } else {
+      const numbersArray = data.split('\r\n')
+      numbersArray.pop()
+      for (let i = 0; i < numbersArray.length; i++) {
+        const element = numbersArray[i]
+        let numberIsAPotentialOfTwo = false
+        let expoent = 0n
+        while ((2n ** expoent) <= element) {
+          numberIsAPotentialOfTwo = (2n ** expoent) === BigInt(element)
+          expoent++
+        }
+        if (numberIsAPotentialOfTwo) {
+          expoent = expoent - 1n
+          console.log(element, numberIsAPotentialOfTwo, parseInt(expoent))
+        } else {
+          console.log(element, numberIsAPotentialOfTwo)
+        }
       }
     }
   })
+}
+readFractions('./d12.txt')
