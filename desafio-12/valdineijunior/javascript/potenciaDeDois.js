591d0274658925171f1833ceb90f45cf5a019489
Valdinei de Paula Junior <43623234+ValdineiJunior@users.noreply.github.com>
2022-12-14 22:04:44 -0300

    Solução desafio 12 Valdinei Junior (#905)
    * Solução do desafio 12
    
    * adcinando arquivo d12.txt
    
    * corrigindo token
    
    * change .valid
    
    * alterando arquivo pra rodar no node
    
    * mudança na forma em que faz a leitura do txt
    
    * corrigindo quebra de linha para atuar no windows e no linux
    
    * corrigindo caminho arquivo texto
    
    * corrigindo quebra de linha linux
    
    * adcionando ponto e virgula faltante

---
+++ desafio-12/valdineijunior/javascript/potenciaDeDois.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+function readFractions(path) {
+  fs.readFile(path, "utf-8", function (error, data) {
+    if (error) {
+      console.log("erro de leitura: " + error.message);
+    } else {
+      let numbersArray = [];
+      console.log(data);
+      if ((/(\r)/.test(data))) {
+        numbersArray = data.split("\r\n");
+      } else {
+        numbersArray = data.split("\n");
+      }
+      numbersArray.pop();
+      for (let i = 0; i < numbersArray.length; i++) {
+        const element = numbersArray[i];
+        let numberIsAPotentialOfTwo = false;
+        let expoent = 0n;
+        while ((2n ** expoent) <= element) {
+          numberIsAPotentialOfTwo = (2n ** expoent) === BigInt(element);
+          expoent++;
+        }
+        if (numberIsAPotentialOfTwo) {
+          expoent = expoent - 1n;
+          console.log(element, numberIsAPotentialOfTwo, parseInt(expoent));
+        } else {
+          console.log(element, numberIsAPotentialOfTwo);
+        }
+      }
+    }
+  });
+}
+readFractions("./d12.txt");
