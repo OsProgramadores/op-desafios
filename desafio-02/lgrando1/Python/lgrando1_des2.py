4854c3411f1d833732331b44bfec7c2f8ae7f893
lgrando1 <lgrando@gmail.com>
2022-12-16 15:00:35 -0300

    desafio02 - lgrando1

---
+++ desafio-02/lgrando1/Python/lgrando1_des2.py
@@ -0,0 +1,15 @@
+print('São primos os seguintes números:')
+for a in range(2,10000):
+    b = 2
+    contador = 0
+    while b < a:
+        if a % b == 0:
+            contador = 1
+            b += 1
+        else:
+            b += 1
+
+    if contador == 0:
+        print(a)
+
+    
