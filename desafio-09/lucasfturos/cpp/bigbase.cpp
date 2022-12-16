de4aa7577ffbfe05e4683c99449a6738eaf580b0
lucasfturos <lucasfturos@gmail.com>
2022-12-16 19:12:19 -0300

    BigBase em C++: Correção

--- desafio-09/lucasfturos/cpp/bigbase.cpp
+++ desafio-09/lucasfturos/cpp/bigbase.cpp
@@ -2,47 +2,19 @@
 #include <sstream>
 #include <iostream>
 
-class BigBase {
-public:
-    void manipulaArquivo(char const* argv);
-    bool test(std::string numInput, std::uint32_t baseInput);
-    std::string converteBase(std::uint32_t baseInput, std::uint32_t baseOutput, std::string numInput);
-};
-
-void BigBase::manipulaArquivo(char const* argv) {
-    std::string numInput;
-    std::string baseconv;
-    std::uint32_t baseInput{ 0 };
-    std::uint32_t baseOutput{ 0 };
-    std::ifstream baseconvArq(argv);
-
-    if (!baseconvArq.is_open()) {
-        std::cout << "Erro ao abrir o arquivo" << '\n';
-        std::exit(0);
-    }
-
-    while (std::getline(baseconvArq, baseconv)) {
-        std::stringstream separaBases{ baseconv };
-        while (separaBases >> baseInput >> baseOutput >> numInput) {
-            std::cout << converteBase(baseInput, baseOutput, numInput) << '\n';
-        }
-    }
-    baseconvArq.close();
-}
-
-std::string BigBase::converteBase(std::uint32_t baseInput, std::uint32_t baseOutput, std::string numInput) {
+std::string converteBase(uint64_t baseInput, uint64_t baseOutput, std::string numInput) {
     std::string bases{ "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" };
     std::string output;
     size_t numero{ 0 };
     size_t resto{ 0 };
-    std::uint32_t aux{ 0 };
+    uint64_t aux{ 0 };
     if (baseInput > bases.length() || baseOutput > bases.length() || baseOutput < 2 || baseInput < 2) {
         return "???";
     }
     if (numInput == "0") {
         return numInput;
     }
-    if (baseOutput < 62 && test(numInput, baseInput)) {
+    if (baseOutput < 62 && numInput == "???") {
         return "???";
     }
     output = "";
@@ -50,8 +22,9 @@ std::string BigBase::converteBase(std::uint32_t baseInput, std::uint32_t baseOut
         resto = 0;
         for (auto& i : numInput) {
             aux = bases.find(i);
-            if (aux == std::string::npos || aux >= baseInput)
+            if (aux == std::string::npos || aux >= baseInput) {
                 return "???";
+            }
             numero = (resto * baseInput) + aux;
             resto = numero % baseOutput;
             i = bases[(numero / baseOutput)];
@@ -69,17 +42,25 @@ std::string BigBase::converteBase(std::uint32_t baseInput, std::uint32_t baseOut
     return output;
 }
 
-bool BigBase::test(std::string numInput, std::uint32_t baseInput) {
-    numInput = converteBase(baseInput, 62, numInput);
-    if (numInput == "???") {
-        return true;
-    }
-    return false;
-}
-
 int main(int argc, char const* argv[]) {
-    BigBase bigbase;
-    bigbase.manipulaArquivo(argv[1]);
+    std::string numInput;
+    std::string baseconv;
+    uint64_t baseInput{ 0 };
+    uint64_t baseOutput{ 0 };
+    std::ifstream baseconvArq(argv[1]);
+
+    if (!baseconvArq.is_open()) {
+        std::cout << "Erro ao abrir o arquivo" << '\n';
+        return 1;
+    }
+
+    while (std::getline(baseconvArq, baseconv)) {
+        std::stringstream separaBases{ baseconv };
+        while (separaBases >> baseInput >> baseOutput >> numInput) {
+            std::cout << converteBase(baseInput, baseOutput, numInput) << '\n';
+        }
+    }
+    baseconvArq.close();
 
     return 0;
 }
