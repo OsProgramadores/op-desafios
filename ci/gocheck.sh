050db42337477f928496de052acecbd33ea3f17e
Marco Paganini <paganini@paganini.net>
2022-12-16 21:14:30 -0800

    Convert golint (deprecated) to golangci-lint.

--- ci/gocheck.sh
+++ ci/gocheck.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 # Test code formatting compliance (Go)
 
-# Run golint on all files in the current directory.
-function go_lint() {
-  golint -set_exit_status 2>&1
-}
-
-# Run govet on all files in the current directory.
-function go_vet() {
-  go vet 2>&1
+# Run golangci_lint on all files in the current directory.
+function golangci_lint() {
+  golangci-lint run -D errcheck 2>&1
 }
 
 # Run go_fmt in all go files in current dir. Report diffs.
 function go_fmt() {
   # Create a temp file just for this check.
-  local tmp=`mktemp`
-  trap "rm -f $tmp" EXIT
+  local tmp
+  tmp="$(mktemp)"
+  # shellcheck disable=SC2064
+  trap "rm -f ${tmp}" EXIT
 
-  find . -type f -name '*.go' -exec gofmt -s -l "{}" \; >"$tmp"
-  if grep -q . "$tmp"; then
+  find . -type f -name '*.go' -exec gofmt -s -l "{}" \; >"${tmp}"
+  if grep -q . "${tmp}"; then
     echo "ERROR: The following files need formatting with gofmt -s:"
-    cat "$tmp"
-    rm -f "$tmp"
+    cat "${tmp}"
+    rm -f "${tmp}"
     return 1
   fi
 }
@@ -43,22 +40,22 @@ echo "============="
 set -o nounset
 
 # List of all distinct directories having at least one modified with a ".go" extension
-go_dirs=$(grep '\.go$' "$HOME/changed_files.txt" | xargs -l dirname 2>/dev/null | sort -u)
+go_dirs=$(grep '\.go$' "${HOME}/changed_files.txt" | xargs -l dirname 2>/dev/null | sort -u)
 
-tmpfile=`mktemp`
-trap "rm -f $tmpfile" EXIT
+tmpfile="$(mktemp)"
+# shellcheck disable=SC2064
+trap "rm -f ${tmpfile}" EXIT
 
-
-# Save stdout/err in FD 6/7, redirect stdout and stderr to $tmpfile.
-exec 6>&1 >$tmpfile
+# Save stdout/err in FD 6/7, redirect stdout and stderr to ${tmpfile}.
+exec 6>&1 >"${tmpfile}"
 exec 7>&2 2>&1
 
 err=0
 
-for dir in $go_dirs; do
+for dir in ${go_dirs}; do
   echo
   echo "============================================================"
-  echo "Testing directory: $dir"
+  echo "Testing directory: ${dir}"
   echo "============================================================"
 
   # Copy current directory to a clean directory inside GOPATH.
@@ -66,19 +63,19 @@ for dir in $go_dirs; do
   export TESTDIR="${GOPATH}/src/test"
   clean_gopath "${GOPATH}"
 
-  pushd "${dir}" >/dev/null
-  cp -ar . "${TESTDIR}"
-  popd >/dev/null
+  cp -ar "${dir}" "${TESTDIR}"
 
-  # Fetch dependencies.
-  pushd "${TESTDIR}" >/dev/null
-  go get -t ./...
+  saved_pwd="${PWD}"
+  cd "${TESTDIR}" || exit 1
 
-  for test in go_lint go_vet go_fmt; do
-    echo -e "\n*** Running test $test"
-    $test && echo "No problems found" || err=1
+  go mod init main
+  go mod tidy
+
+  for test in golangci_lint go_fmt; do
+    echo -e "\n*** Running test ${test}"
+    ${test} && echo "No problems found" || err=1
   done
-  popd >/dev/null
+  cd "${saved_pwd}" || exit 1
 done
 
 # Restore FDs and close FDs 6/7.
@@ -86,7 +83,7 @@ exec 1>&6 6>&-
 exec 2>&7 7>&-
 
 # Display results of all tests.
-cat $tmpfile
+cat "${tmpfile}"
 
 if (( err == 1 )); then
   exit 1
