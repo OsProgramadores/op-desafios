21e451bff24d767513ba6d7343a79ea8fe804fb4
Leonardo Grando <36816722+lgrando1@users.noreply.github.com>
2022-12-18 01:04:09 -0300

    update (#914)
    * desafio03 - lgrando1
    
    * Revert "desafio03 - lgrando1"
    
    This reverts commit ec3b6b6d294a03d94a04dd5feecb48679d5327ca.
    
    * desafio02 - lgrando1
    
    * add shebang
    
    * removidos espacos extras
    
    * Update lgrando1_des2.py
    
    * Update lgrando1_des2.py
    
    * Alteracao nome pasta lgrando1
    
    * Passei o codigo no online linter
    
    * Passei o codigo no online linter 2
    
    * Update lgrando1_des2.py
    
    Co-authored-by: Marco Paganini <paganini@paganini.net>
    Co-authored-by: Washington Araújo Ramos <war.paz@hotmail.com>

---
+++ desafio-02/lgrando1/python/lgrando1_des2.py
@@ -0,0 +1,16 @@
+"""Module providingFunction printing python version."""
+#!/usr/bin/env python3
+
+print('Os números seguintes são primos:')
+for a in range(2,10000):
+    b = 2
+    contador = 0
+    while b < a:
+        if a % b == 0:
+            contador = 1
+            b += 1
+        else:
+            b += 1
+
+    if contador == 0:
+        print(a)
