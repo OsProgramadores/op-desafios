cb4639bed7edf73dd3bec898af22c1b1515ef53e
lgrando1 <lgrando@gmail.com>
2022-12-18 06:57:09 -0300

    removido trailing tabs

--- desafio-03/lgrando1/python/lgrando1_des3.py
+++ desafio-03/lgrando1/python/lgrando1_des3.py
@@ -1,6 +1,5 @@
 """Module providingFunction printing python version."""
 #!/usr/bin/env python3
-
 start = int(input('número inicial: '))
 end = int(input('número final: '))
 print('Os números palindrômicos entre este intervalo são:')
@@ -8,4 +7,5 @@ for i in range(start,end):
     ix = str(i)
     if ix == ix[::-1]:
         print(i)
+    
         
