f332de52b8f567193cb967713f74ea05cc7c1e5c
lgrando1 <lgrando@gmail.com>
2022-12-19 17:38:14 -0300

    Solucao_desafio_12_lgrando1

---
+++ desafio-12/lgrando1/python/desafio12_lgrando1.py
@@ -0,0 +1,23 @@
+"""Module providingFunction printing python version."""
+#!/usr/bin/env python3
+
+from math import log
+#para trabalhar com numeros grandes
+from decimal import Decimal, getcontext
+getcontext().prec = 6
+
+def potencia_de_2(numero):
+    numero = int(numero)
+    # criado um excessão, pois náo existe log de 0
+    if numero == 0:
+        print(str(numero) , "false")
+    else:
+        value = Decimal(log(numero)) / Decimal(log(2))
+        if float(value) == float(round(value)):
+            print(numero, "true" , int(value))
+        else: print(numero , "false")
+
+with open("d12.txt", 'r', encoding='UTF-8') as lin:
+    linhas = lin.readlines()
+    for linha in linhas:
+        potencia_de_2(linha)
