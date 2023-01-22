c6743b8b434a495590e51c1ac602ee8d20ae4549
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2023-01-22 11:38:15 -0300

    Update prime-numbers.js

--- desafio-02/valeriasouza28/prime-numbers.js
+++ desafio-02/valeriasouza28/prime-numbers.js
@@ -1,15 +1,13 @@
 // Código para verificar se um número é primo ou não
 
-const final = 10000
+const final = 10
 function generatePrime() {
   for (let i = 0; i <= final; i++) {
-    if (i === 1) {
-      console.log(i + ' Não é um número primo!')
-    } else if (i === 2) {
+    if (i === 1) {} 
+    else if (i === 2) {
       console.log(i + 'É um número primo!')
-    } else if (i % 2 === 0) {
-      console.log(i + 'Não é um número primo!')
-    } else {
+    } else if (i % 2 === 0) {} 
+    else {
       console.log(i + 'É um número primo!')
     }
   }
