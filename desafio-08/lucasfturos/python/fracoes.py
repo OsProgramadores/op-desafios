d0910d7d947b226b43fe51222680f40ed35c6da8
lucasfturos <lucasfturos@gmail.com>
2023-02-15 19:36:24 -0300

    Correção desafio 8

--- desafio-08/lucasfturos/python/fracoes.py
+++ desafio-08/lucasfturos/python/fracoes.py
@@ -1,26 +1,32 @@
 """ Author: lucasfturos | Description: Resolve frações simples contidas em arquivo *.txt """
 
 import sys
-from fractions import Fraction
+from math import gcd
 
 def fracao_simples(fracao: str) -> str:
     """ Função que resolve a fração e retorna o resultado """
     try:
-        frac = Fraction(fracao)
-        numerador = frac.numerator % frac.denominator
-        complemento = frac.numerator // frac.denominator
-        is_zero = '' if complemento == 0 else f'{complemento} '
+        if '/' in fracao:
+            numerador, denominador = list(map(int,fracao.split('/')))
+            result_div = numerador / denominador
+            div_int = numerador // denominador
+            mod_div = numerador % denominador
+            max_div_comum = gcd(numerador, denominador)
 
-        if frac.denominator == 1:
-            return frac.numerator
-        return f'{is_zero}{numerador}/{frac.denominator}'
+            if result_div == 1:
+                return str(1)
+            if div_int > 0 and denominador != 1:
+                return f'{div_int} {mod_div}/{denominador}'
+            if denominador == 1:
+                return numerador
+            return f'{numerador // max_div_comum}/{denominador // max_div_comum}'
+        return fracao
     except ZeroDivisionError:
         return 'ERR'
-
 try:
     with open(sys.argv[1], encoding='utf-8') as fracao_file:
         for value in fracao_file.readlines():
-            print(fracao_simples(value))
+            print(fracao_simples(value.replace('\n','')))
     fracao_file.close()
 except IOError:
     print('Erro ao abrir arquivo')
