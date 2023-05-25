ee3933486ec9bb5a2332aaa0ccb4dee351e66c52
guiribeirodev <guiribeirodev@gmail.com>
2023-05-25 19:41:14 -0300

    Complete exercise 07

---
+++ desafio-07/guiribeirodev/python/README.md
@@ -0,0 +1,13 @@
+# DESAFIO 07 - TAC
+
+## Versão Utilizada
+
+O desafio foi feito utilizando a versão 3.10.10, porém deve funcionar com qualquer versão 3.x .
+
+## Como executar?
+
+Para executar basta digitar:
+
+```
+   python main.py <arquivo.txt>
+   ```
---
+++ desafio-07/guiribeirodev/python/main.py
@@ -0,0 +1,43 @@
+import sys
+
+def python_tac(filename):
+    with open(filename, 'rb') as file:
+        file.seek(0, 2)
+        file_size = file.tell()
+
+        block_size = 400000
+        position = file_size - block_size
+
+        while position >= 0:
+            file.seek(position, 0)
+            data = file.read(block_size)
+
+            lines = data.splitlines()
+
+            remainder_line = len(lines[0])
+            lines = lines[1:]
+
+            reversed_lines = reversed(lines)
+            for line in reversed_lines:
+                print(line.decode('utf8'))
+
+            position -= block_size - remainder_line
+
+        position2 = position + block_size
+        file.seek(0)
+        data = file.read(position2)
+
+        lines = data.splitlines()
+
+        reversed_lines = reversed(lines)
+        for line in reversed_lines:
+            print(line.decode('utf8'))
+
+
+def main():
+    filename = sys.argv[1]
+
+    python_tac(filename)
+
+
+main()
