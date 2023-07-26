aaa51f630196b7ab7d2012107d88e7a3a7f0b832
Jonata <jholjonata@yahoo.com.br>
2023-07-25 23:26:33 -0300

    feito as alterações no nome da função

--- desafio-02/JonataMSilva/python/numeros_primos.py
+++ desafio-02/JonataMSilva/python/numeros_primos.py
@@ -1,4 +1,4 @@
-def encontrar_num_primo(numero):
+def verifica_se_numero_eh_primo(numero):
     divisor = 0
     for i in range(1, numero+1):
         if numero % i == 0:
@@ -7,6 +7,6 @@ def encontrar_num_primo(numero):
         return True
     return False
 
-for primos in range(1, 10000):
-    if encontrar_num_primo(primos):
-        print(primos)
+for numero in range(1, 10000):
+    if verifica_se_numero_eh_primo(numero):
+        print(numero)
