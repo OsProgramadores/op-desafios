42310515f2f5c86973cba1af1ef67ddce797afb9
LeoHenrique <leo-henrique23@hotmail.com>
2023-07-31 08:28:30 -0300

    correção-desafio02

--- desafio-02/leonardohenrique/python/desafio02.py
+++ desafio-02/leonardohenrique/python/desafio02.py
@@ -1,16 +1,16 @@
-#def cria a função onde irá verificar se a lista de numeros determinados no argumento da função são primos
+#função onde irá verificar se a lista de numeros determinados no argumento da função são primos
 def numsprimos(n):
-    #cria uma lista e define como verdadeira , também define os números 0 e 1 como falso já que não são primos
+#cria uma lista e define como verdadeira
     primos = [True] * (n + 1)
+#define os números 0 e 1 como falso já que não são primos
     primos[0] = primos[1] = False
 
-#utiliza o algorítimo "rivo de Eratóstenes" para verificar os números não primos 
+#utiliza o algorítimo "Crivo de Eratóstenes" para verificar números não primos
     for i in range(2, int(n**0.5) + 1):
         if primos[i]:
             for j in range(i * i, n + 1, i):
                 primos[j] = False
-    
-    #chama a lista de numeros restantes e imprime todos com a frase "é um numero primo!"
+#chama a lista de numeros restantes e imprime todos numeros primos
     for num in range(n + 1):
         if primos[num]:
             print(num ,"é um numero primo!")
