d9319b8a2d8381bc4f3198fecc743ab725c3c026
LeoHenrique <leo-henrique23@hotmail.com>
2023-08-01 16:50:29 -0300

    desafio03

---
+++ desafio-03/leonardohenrique/desafio03.py
@@ -0,0 +1,12 @@
+#variaveis definindo um range de numeros 
+numinicio = 100
+numfinal = 999
+
+for i in range (numinicio, numfinal):
+    #converte o numero em string
+    convstrg = str(i)
+    #inverte o numero para poder comparar se é palindromo
+    inverso = convstrg[::-1]
+    #testa e imprime o numero caso seja palindromo
+    if  convstrg == inverso:
+        print (i ,"é um palíndromo")
