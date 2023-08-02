37c43b64effdb573092debb77709d99293389e15
LeoHenrique <leo-henrique23@hotmail.com>
2023-08-02 11:50:44 -0300

    aplicação de melhorias sugeridas

--- desafio-03/leonardohenrique/python/desafio03.py
+++ desafio-03/leonardohenrique/python/desafio03.py
@@ -1,12 +1,12 @@
 #variaveis definindo um range de numeros
-numinicio = 100
-numfinal = 999
+num_inicio = int(input("Digite um numero inicial: "))
+num_final = int(input("digite o numero final: "))
 
-for i in range (numinicio, numfinal):
+for i in range (num_inicio, num_final):
     #converte o numero em string
-    convstrg = str(i)
+    convert_string = str(i)
     #inverte o numero para poder comparar se é palindromo
-    inverso = convstrg[::-1]
+    inverso = convert_string[::-1]
     #testa e imprime o numero caso seja palindromo
-    if  convstrg == inverso:
+    if  convert_string == inverso:
         print (i ,"é um palíndromo")
