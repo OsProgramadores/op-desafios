87c0cc1172095848ee9d6afd917c7545327a3c41
LeoHenrique <leo-henrique23@hotmail.com>
2023-08-07 08:53:33 -0300

    tratamento para valores inválidos adicionado

--- desafio-03/leonardohenrique/python/desafio03.py
+++ desafio-03/leonardohenrique/python/desafio03.py
@@ -1,12 +1,21 @@
-#variaveis definindo um range de numeros
-num_inicio = int(input("Digite um numero inicial: "))
-num_final = int(input("digite o numero final: "))
+try:
+    # Variáveis definindo um range de números
+    num_inicio = int(input("Digite um número inicial: "))
+    num_final = int(input("Digite o número final: "))
+    if num_inicio >= num_final:
+        print ("O número inicial deve ser menor que o final.")
 
-for i in range (num_inicio, num_final):
-    #converte o numero em string
-    convert_string = str(i)
-    #inverte o numero para poder comparar se é palindromo
-    inverso = convert_string[::-1]
-    #testa e imprime o numero caso seja palindromo
-    if  convert_string == inverso:
-        print (i ,"é um palíndromo")
+    if num_inicio < 0 or num_final < 0:
+        print("Os números precisam ser maior que 0.")    
+    else:
+
+        for i in range(num_inicio, num_final):
+            # Converte o número em string
+            convert_string = str(i)
+            # Inverte o número para poder comparar se é palíndromo
+            inverso = convert_string[::-1]
+            # Testa e imprime o número caso seja palíndromo
+            if convert_string == inverso:
+                print(i, "é um palíndromo")
+except ValueError:
+    print("Por favor, insira somente números válidos.")
