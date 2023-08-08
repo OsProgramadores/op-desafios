c3b967375eefcbf5e0feae23f5cf76ae7adeb4dc
Jonata <jholjonata@yahoo.com.br>
2023-08-07 21:52:07 -0300

    melhorando o codigo

--- desafio-03/JonataMSilva/python/numero_palindromicos.py
+++ desafio-03/JonataMSilva/python/numero_palindromicos.py
@@ -1,18 +1,20 @@
 """
-Números palindrômicos
+Números palindrômic2os
 
 Solução de Jonata M Silva
 Github: https://github.com/JonataMSilva
 """
-inicial = 1
-final = 65
+inicial = input("digite o numero inicial")
+final = input("digite o numero final")
 
 
 def numero_palindromico():
-    """função para buscar os numeros palindrômicos em um range"""
-    for numero in range(inicial, final):
+    """função para buscar os numeros palindrômicos em um range e armazenar em uma lista vazia"""
+    palindromico = []
+    for numero in range(int(inicial), int(final)):
         if str(numero)[-1] == str(numero)[0]:
-            print(numero)
+            palindromico.append(numero)
+    return palindromico
 
 
-numero_palindromico()
+print(numero_palindromico())
