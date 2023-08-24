95a33197cca0b32864b2d50b4811ed1da33a5778
Jonata Meneses <112676203+JonataMSilva@users.noreply.github.com>
2023-08-24 16:45:32 -0300

    corrigido numero final

--- desafio-03/JonataMSilva/python/numero_palindromicos.py
+++ desafio-03/JonataMSilva/python/numero_palindromicos.py
@@ -11,7 +11,7 @@ final = input("digite o numero final: ")
 def numero_palindromico():
     """função para buscar os numeros palindrômicos em um range e armazenar em uma lista vazia"""
     palindromico = []
-    for numero in range(int(inicial), int(final)):
+    for numero in range(int(inicial), int(final)+1):
         if str(numero) == str(numero)[::-1]:
             palindromico.append(numero)
     return palindromico
