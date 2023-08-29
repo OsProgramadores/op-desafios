d3bf85dc614226265b1e24f3f0af903e0876881e
valeriasouza28 <valleriasouza28@gmail.com>
2023-08-29 20:32:23 +0000

    update

--- desafio-03/valeriasouza28/javascript/palindromo.js
+++ desafio-03/valeriasouza28/javascript/palindromo.js
@@ -1,21 +1,22 @@
-const readline = require("readline-sync");
-
-const start = readline.question("Digite um número: ");
-const end = readline.question("Digite um número: ");
 const numbers = [];
 const numbersPalindrome = [];
 
-for (let i = start; i <= end; i++) {
-    numbers.push(i.toString());
-}
-
-for (let c = 0; c < numbers.length; c++) {
-    const reverse = numbers[c].split("").reverse().join("");
-    if (numbers[c] === reverse) {
-        numbersPalindrome.push(numbers[c]);
+function prime(start, end) {
+    for (let i = start; i <= end; i++) {
+        numbers.push(i.toString());
     }
+
+    for (let c = 0; c < numbers.length; c++) {
+        const reverse = numbers[c].split("").reverse().join("");
+        if (numbers[c] === reverse) {
+            numbersPalindrome.push(numbers[c]);
+        }
+    }
+    return numbersPalindrome;
 }
 
-numbersPalindrome.forEach(element => {
+const usingPrime = prime(1, 20);
+
+usingPrime.forEach(element => {
     console.log(element);
 });
