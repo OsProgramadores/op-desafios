2eda448a47eb4759c18279aef4b8b2a9247971a6
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-09-14 00:36:39 +0000

    "Resolução do desafio-02"

--- desafio-02/ParsivalT/primos.py
+++ desafio-02/ParsivalT/primos.py
@@ -3,11 +3,13 @@
     Propósito: Resolução do desafio-2
 """
 
-prime = []
+def primos(number: int):
 
-for num in range(2, 1000):
-    # Validando se o numero não é divisivel por 2 3 5 7
-    if not(num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0) or num in [2, 3, 5, 7]:
-        prime.append(num)
+    prime = []
+    for num in range(2, number):
+        # Validando se o numero não é divisivel por 2 3 5 7
+        if not(num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0) or num in [2, 3, 5, 7]:
+            prime.append(num)
+    return prime
 
-print(prime)
+print(primos(1000))
