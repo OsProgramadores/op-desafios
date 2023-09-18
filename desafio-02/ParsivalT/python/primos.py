6fc069b94a51214772ffb5bdb0b450894872459a
Thi <tthiagovinnicius@gmail.com>
2023-09-18 00:39:12 -0300

    Retirando os espaços

--- desafio-02/ParsivalT/python/primos.py
+++ desafio-02/ParsivalT/python/primos.py
@@ -5,13 +5,9 @@
 """
 def verificaPrimos(number: int) -> list:
     prime = []
-
     for num in range(2, number):
         if not(num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0) or num in [2, 3, 5, 7]:
             prime.append(num)
-
     return prime
 
-if __name__ == "__main__":
-    print(verificaPrimos(1000))
-    
+print(verificaPrimos(1000))
