2e6035d71f3e89f1c8573255c5285163e7825372
Thi <tthiagovinnicius@gmail.com>
2023-09-23 01:33:57 -0300

    Um novo começo

--- desafio-02/ParsivalT/python/primos.py
+++ desafio-02/ParsivalT/python/primos.py
@@ -2,9 +2,37 @@
     @Author: Thiago Felix.
     Propósito: Resolução do desafio-2.
 """
-def verificaPrimos(number: int):
-    for num in range(2, number + 1):
-        if all(num % i != 0 for i in range(2, int(num**0.5) + 1)):
-            print(num)
+def primo(num:int) -> bool:
+    """
+    Validador de numeros Primos.
 
-verificaPrimos(10000)
+    Função conta a quantidade de divisores um numero X possui,
+    se o numero possuir apenas 2 divisores é considerado primoa.
+
+    Args:
+        num (int): recebe um numero inteiro.
+
+    Returns:
+        tipo_de_retorno: retorna falso calso o numero de divisores ultrapasse 2.
+
+    Exemplos:
+        >>> primo(5)
+        True
+
+        >>> primo(6)
+        False
+    """
+    # Corpo da função aqui
+    numDivisores = int(0)
+    for apoio in range(1, num+1):
+        if numDivisores > 2:
+            return False
+        if num % apoio == 0:
+            numDivisores += 1
+    if numDivisores > 2:
+        return False
+    return True
+
+for numero in range(2,10001):
+    if primo(numero):
+        print(numero)
