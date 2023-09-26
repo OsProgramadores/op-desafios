6f20a63f946eb23e38b75889e8c92770c8609867
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-09-26 23:16:13 +0000

    Otmizando a validação

--- desafio-02/ParsivalT/python/primos.py
+++ desafio-02/ParsivalT/python/primos.py
@@ -1,19 +1,15 @@
-"""
-    @Author: Thiago Felix.
-    Propósito: Resolução do desafio-2.
-"""
-def primo(num:int) -> bool:
+def primo(num: int) -> bool:
     """
-    Validador de numeros Primos.
+    Verifica se um número é primo.
 
-    Função conta a quantidade de divisores um numero X possui,
-    se o numero possuir apenas 2 divisores é considerado primoa.
+    Esta função verifica se um número é primo ou não. Um número primo é aquele
+    que só é divisível por 1 e por ele mesmo.
 
     Args:
-        num (int): recebe um numero inteiro.
+        num (int): O número a ser verificado.
 
     Returns:
-        tipo_de_retorno: retorna falso calso o numero de divisores ultrapasse 2.
+        bool: True se o número for primo, False caso contrário.
 
     Exemplos:
         >>> primo(5)
@@ -22,17 +18,21 @@ def primo(num:int) -> bool:
         >>> primo(6)
         False
     """
-    # Corpo da função aqui
-    numDivisores = int(0)
-    for apoio in range(1, num+1):
-        if numDivisores > 2:
-            return False
-        if num % apoio == 0:
-            numDivisores += 1
-    if numDivisores > 2:
+    # Caso o número seja menor ou igual a 1, não é primo
+    if num <= 1:
         return False
+
+    # Pegando a raiz quadrada do número informado
+    raiz = int(num ** 0.5)
+
+    # Como um número primo é divisível apenas por 1 e por ele mesmo.
+    # vamos começar o laço a partir de 2 e verificar se há algum divisor.
+    # Se encontrarmos um divisor antes de chegar à raiz quadrada, o número não é primo.
+    for divisor in range(2, raiz + 1):
+        if num % divisor == 0:
+            return False
     return True
 
-for numero in range(2,10001):
+for numero in range(2, 10001):
     if primo(numero):
         print(numero)
