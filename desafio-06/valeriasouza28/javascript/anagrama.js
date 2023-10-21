571931b76cb0296a38cc5717e5a3a7948aaeffb7
valeriasouza28 <valleriasouza28@gmail.com>
2023-10-21 03:17:49 +0000

    alteração para retornar anagramas de frases não somente palavras. Tive que inserir uma frase teste no arquivo words.txt

--- desafio-06/valeriasouza28/javascript/anagrama.js
+++ desafio-06/valeriasouza28/javascript/anagrama.js
@@ -2,17 +2,17 @@ const fs = require("fs");
 const nomeDoArquivo = "./words.txt";
 const readline = require("readline-sync");
 const palavra = readline.question("Digite uma palavra : ");
-const verificaInputCaracteres = /^[a-zA-Z\s]*$/;
-const { Writable } = require("stream");
+const verificaInputCaracteres = /^[a-zA-Z\s]+$/;
 
 function normalizarPalavras(palavra) {
     const normaliza = palavra
         .normalize("NFD")
-        .replace(/[\u0300-\u036f\s]/g, "")
+        .replace(/[\u0300-\u036f]/g, "")
         .toUpperCase();
 
     return normaliza.toString();
 }
+
 function testaSeAnagrama(palavra1, palavra2) {
     if (Object.keys(palavra1).length !== Object.keys(palavra2).length) {
         return false;
@@ -31,19 +31,28 @@ function inspecionarPalavra(palavra) {
         const letter = palavra[i];
         count[letter] = (count[letter] || 0) + 1;
     }
-    return count;
+
+    const novoObjeto = {};
+
+    Object.keys(count).forEach(chave => {
+        if (chave !== " ") {
+            novoObjeto[chave] = count[chave];
+        }
+    });
+
+    return novoObjeto;
 }
+
 const linhas = fs.readFileSync(nomeDoArquivo, "utf8").split("\n");
+const palavra1 = inspecionarPalavra(normalizarPalavras(palavra));
 const palavrasEncontradas = [];
+
 try {
     if (!verificaInputCaracteres.test(palavra)) {
         throw new Error("A palavra digitada contém caracteres inválidos.");
     } else if (palavra.length > 16) {
         throw new Error("Digite uma palavra com menos de 16 caracteres.");
     } else {
-        const palavraNormalizada = normalizarPalavras(palavra);
-        const palavra1 = inspecionarPalavra(palavraNormalizada);
-
         for (const linha of linhas) {
             const palavra2 = inspecionarPalavra(normalizarPalavras(linha));
             const testaAnagrama = testaSeAnagrama(palavra1, palavra2);
@@ -53,22 +62,13 @@ try {
         }
     }
 
-    const writeSteam = new Writable({
-        write(chunk, encoding, callback) {
-            console.log(chunk.toString());
-            callback();
-        }
-    });
-
     palavrasEncontradas.forEach(element => {
-        writeSteam.write(element + "\n");
+        console.log(element);
     });
 } catch (error) {
     if (error.message === "A palavra digitada contém caracteres inválidos.") {
         console.error(`Erro : ${error.message}`);
-    } else if (
-        error.message === "Digite uma palavra com menos de 16 caracteres."
-    ) {
+    } else if (error.message === "Digite uma palavra com menos de 16 caracteres.") {
         console.error(`Erro : ${error.message}`);
     } else {
         console.error(error);
