b1e3ffb3d766150b108d3f8b0397dd094eaba05f
Erick Fernandes <80919235+erickofs@users.noreply.github.com>
2023-12-26 15:26:51 -0300

    Create prime10001.py (#1015)
    * Create prime10001.py
    
    * lint fit
    
    changes to match linter
    
    * Update prime_calc
    
    changed unction to work with set, custo limit and prime quantity range added, printing one result by line added
    
    * Readme creation
    
    Readme created on oot directory
    
    * Update prime_calc
    
    Updates on the function preserving only calculations. Functions variables are now global. Print logic changed to avoid errors.
    
    * Update on Readme
    
    Readme updated to match new file's name
    
    * Update primos.py
    
    Update on error handling for constants to include a greater range of user's input scenarios
    
    * Sieve of Eratosthenes module creation
    
    Sieve of Eratosthenes module created to allow escalability. Primos.py now only contains the function call within the loop.
    
    * Module changes
    
    Calculation module change to be inline with Python PEEP-8
    
    * Main module changes
    
    Main module name changed from primos to __main__.py to be inline with Python best practices
    
    * Readme changes
    
    Changes made on Redme to better clarify file functionality based on last changes
    
    * .gitignore creation
    
    gitignore created to prevent cache files to be commited
    
    * main update
    
    Update on main script
    
    * Classs & Main update
    
    Class PrimeCalc's methods updated to avoid overwriting on main's execution. Main update for proper execution
    
    * Atualizar o README.md
    
    * Atualizar o soecalc.py
    
    remove trail space from line 83.

---
+++ desafio-02/erickofs/python/soecalc.py
@@ -0,0 +1,82 @@
+"""
+Prime calculation based on the Sieve of Eratosthenes algorithm.
+"""
+class PrimeCalc():
+    """
+    Unique class to store the calculation logic
+    """
+    def get_primes(self):
+        """
+        Implements the Sieve of Eratosthenes to find all prime numbers up to the given limit.
+        Args:
+        - primelimit (float): The calculation limit.
+        - primerange (float): The number of prime numbers to be displayed.
+        Returns:
+        - primelist (list): List of prime numbers up to the user's nth given range.
+        """
+        primelimit = self.get_valid_limit()
+        primerange = self.get_valid_range()
+
+        primelist = set(range(2, primelimit))
+
+        for i in range(2, int(primelimit**0.5) + 1):
+            for j in range(i*2, primelimit, i):
+                if j in primelist:
+                    primelist.remove(j)
+
+        return list(primelist)[:primerange] if primerange != 0 else list(primelist)
+
+    def get_valid_limit(self):
+        """
+        Validates and returns the calculation limit.
+        """
+        while True:
+            primelimit = input("Enter the calculation limit: ")
+            try:
+                primelimit = float(primelimit)
+                if primelimit < 0:
+                    print("Please enter a positive number.")
+                elif primelimit > 1000000:
+                    print("The limit is too high. Please enter a number less than 1000000.")
+                else:
+                    break
+            except ValueError:
+                primelimit = primelimit.strip()
+                if primelimit == "":
+                    primelimit = 100.0
+                    print("No given limit. It has been automatically set to 100.")
+                    break
+                print("Invalid input. Please enter a valid number.")
+        if round(primelimit) != primelimit:
+            print(f"The limit has been rounded to the nearest integer: {round(primelimit)}")
+        primelimit = int(round(primelimit))
+        if primelimit == 0:
+            print("The limit has been automatically set to 100.")
+            primelimit = 100
+        return primelimit
+
+    def get_valid_range(self):
+        """
+        Validates and returns the number of prime numbers to be displayed.
+        """
+        while True:
+            primerange = input("Enter the number of prime numbers to be displayed (0 = all): ")
+            try:
+                primerange = float(primerange)
+                if primerange < 0:
+                    print("Please enter a positive number.")
+                elif primerange > 1000000:
+                    print("The limit is too high. Please enter a number less than 1000000.")
+                else:
+                    break
+            except ValueError:
+                primerange = primerange.strip()
+                if primerange == "":
+                    primerange = 0
+                    print("No given limit. All primes will be printed.")
+                    break
+                print("Invalid input. Please enter a valid number.")
+        if round(primerange) != primerange:
+            print(f"The limit has been rounded to the nearest integer: {round(primerange)}")
+        primerange = int(round(primerange))
+        return primerange
