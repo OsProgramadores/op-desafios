ec08aefe1fcadbd5ff53787e3af97add869ba9db
Lucas Alves <lucas.a.freitas16@gmail.com>
2024-01-03 12:33:21 -0300

    Correção do erro trailing spaces no pylint

--- desafio-12/Lkzeu/python/d12_potencia.py
+++ desafio-12/Lkzeu/python/d12_potencia.py
@@ -3,19 +3,18 @@ def main():
         numbers = f.readlines()
         for n in numbers:
             number = int(n)
-            e_par  = not(number & 1)
-   
+            e_par  = not number & 1
+
             if e_par or number == 1:
                 expoentes = contar_exp(number)
                 if (1 << expoentes) == number: # ou if (2 ** expoentes) == number:
                     print(f"{number} true {expoentes}")
                     continue
             print(f"{number} false")
-    
 
 def contar_exp(number: int) -> int:
     counter = 0
-    while(number > 1):
+    while number > 1:
         counter += 1
         number >>= 1
     return counter
