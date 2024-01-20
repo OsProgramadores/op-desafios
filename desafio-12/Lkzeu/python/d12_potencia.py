1e2d8a444bb29264eea49674f1832e23fb568acd
Lucas Alves <lucas.a.freitas16@gmail.com>
2024-01-20 12:26:56 -0300

    lida com exceções caso o arquivo d12.txt não exista ou ocorra algum erro durante a leitura do arquivo.

--- desafio-12/Lkzeu/python/d12_potencia.py
+++ desafio-12/Lkzeu/python/d12_potencia.py
@@ -1,16 +1,25 @@
 def main():
-    with open("d12.txt", "r") as f:
-        numbers = f.readlines()
-        for n in numbers:
-            number = int(n)
-            e_par  = not number & 1
+    try:
+        with open("d12.txt", "r") as f:
+            numbers = f.readlines()
+            for n in numbers:
+                number = int(n)
+                e_par  = not number & 1
 
-            if e_par or number == 1:
-                expoentes = contar_exp(number)
-                if (1 << expoentes) == number: # ou if (2 ** expoentes) == number:
-                    print(f"{number} true {expoentes}")
-                    continue
-            print(f"{number} false")
+                if e_par or number == 1:
+                    expoentes = contar_exp(number)
+                    if (1 << expoentes) == number: # ou if (2 ** expoentes) == number:
+                        print(f"{number} true {expoentes}")
+                        continue
+                print(f"{number} false")
+
+    except FileNotFoundError:
+        print("d12.txt não encontrado!\n" +
+              "Verifique se o arquivo d12.txt está na mesma pasta do programa.\n" +
+              "você pode baixa-lo em: https://osprogramadores.com/desafios/d12/")
+
+    except OSError as e:
+        print(f"{type(e).__name__}: {e}")
 
 def contar_exp(number: int) -> int:
     counter = 0
