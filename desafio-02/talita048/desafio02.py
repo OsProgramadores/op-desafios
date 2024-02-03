c9387b28c3c4f76dd7bf35718ef357738bd3176b
Talita Replit <talitacostatj@gmail.com>
2024-02-03 13:50:28 +0000

    Primeiro envio do Desafio 02 por Talita Ribeiro

---
+++ desafio-02/talita048/desafio02.py
@@ -0,0 +1,14 @@
+# Por Talita Ribeiro
+# talita048 in GitHub
+
+numeros = list(range(2, 10001))
+primos = []
+
+for numero in numeros:
+    if numero in numeros:
+        primos.append(numero)
+        for multiplo in range(numero * 2, 10001, numero):
+            if multiplo in numeros:
+                numeros.remove(multiplo)
+
+print (primos)
