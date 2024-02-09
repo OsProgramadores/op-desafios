cbad074b335b033b2bfda283254627a8b3ffbf18
talita048 <talitacostatj@gmail.com>
2024-02-09 15:46:22 -0300

    Remoção de lista primos, por ser redundante

--- desafio-02/talita048/python/desafio02.py
+++ desafio-02/talita048/python/desafio02.py
@@ -1,10 +1,9 @@
-numeros = list(range(2, 10001))
-primos = []
+numeros = list(range(3, 10001, 2))
 
+print (2)
 for numero in numeros:
     if numero in numeros:
         print (numero)
-        primos.append(numero)
         for multiplo in range(numero * 2, 10001, numero):
             if multiplo in numeros:
                 numeros.remove(multiplo)
