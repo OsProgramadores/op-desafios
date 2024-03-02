e1fea6fae6d60a4cb4bca3324cfcd26f2a5240c0
Dickson Souza <Dickson Souza 36424026+disouzam@users.noreply.github.com>
2024-03-02 00:02:25 -0300

    Solução para o desafio 2 - Imprimir números primos entre 1 e 10000, inclusive.
    - .gitignore adicionado para não comitar acidentalmente arquivos de configuração do ambiente Python

---
+++ desafio-02/disouzam/python/numeros_primos.py
@@ -0,0 +1,28 @@
+"""
+    Código que implementa a solução do desafio #02 do site Os Programadores
+"""
+
+
+def exibir_numeros_primos():
+    """
+        Exibe todos os números primos de 1 a 10000 no console
+    """
+    for numero in range(1, 10001):
+        if e_primo(numero):
+            print(numero)
+
+
+def e_primo(numero):
+    """
+        Determina se o número passado como argumento é primo ou não
+    """
+    divisor = 2
+    while divisor <= numero / 2:
+        if numero % divisor == 0:
+            return False
+        divisor += 1
+    return True
+
+
+if __name__ == "__main__":
+    exibir_numeros_primos()
