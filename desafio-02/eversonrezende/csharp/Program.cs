580e68ae2b4b3c9c3d4d355cf614dbb3b28fd0b7
Everson Alves de Rezende <61418106+eversonrezende@users.noreply.github.com>
2024-03-05 10:35:35 -0300

    Desafio 02: Numeros Primos (#1026)
    * Desafio 02: Numeros Primos
    
    * Desafio 03: Palindromos por @eversonrezende
    
    * Deletando desafio-03 do PR desafio-02.
    
    * Adicionando arquivo README.md
    
    * Ajustando algoritmo e README.md
    
    * Ajustando algoritmo
    
    * Removendo caracteres inválidos.
    
    ---------
    
    Co-authored-by: Everson Alves de Rezende <erezende@osg.com.br>

---
+++ desafio-02/eversonrezende/csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿//Algoritmo - Crivo de Erastotenes
+
+//Valor Limite
+var valorLimite = 10000;
+
+//Maior numero a ser verificado, ele corresponde a raiz quadrada do valor limite, arredondando pra baixo.
+var maiorNumeroVerificado = Math.Floor(Math.Sqrt(valorLimite));
+
+//Criar uma lista de todos os numeros inteiros de 2 ate o valor limite.
+List<int> listaPrimos = new();
+
+for (int i = 2; i <= valorLimite; i++)
+{
+  listaPrimos.Add(i);
+}
+
+//Comecando em 2 (i), percorra a lista e remova todos os multiplos de i ate o valor limite. 
+for (int i = 2; i <= maiorNumeroVerificado; i++)
+{
+  for (int j = 2; j <= valorLimite; j++)
+  {
+    if (j % i == 0 && j != i && listaPrimos.Contains(j))
+    {
+      listaPrimos.Remove(j);
+    }
+  }
+}
+
+//Apos a verificacao acima, sobram apenas os numeros primos
+foreach (var numero in listaPrimos)
+{
+  Console.WriteLine(numero);
+}
