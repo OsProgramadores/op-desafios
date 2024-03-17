741b87274e14d1608151b7521c74332a6e13218b
Dickson Souza <Dickson Souza 36424026+disouzam@users.noreply.github.com>
2024-03-17 10:04:06 -0300

    Docstring corrigida e ponto de entrada do módulo inserido

--- desafio-03/disouzam/python/palindromos.py
+++ desafio-03/disouzam/python/palindromos.py
@@ -1,3 +1,7 @@
 """
-    Verifica quais os números palíndromos entre dois números fornecidos, extremos inclusos na pesquisa
+    Verifica quais os números palíndromos entre dois números fornecidos, 
+    extremos inclusos na pesquisa
 """
+
+if __name__ == "__main__":
+    pass
