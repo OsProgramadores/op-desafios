aab372bf797a87c564eeeffba9d868bccf5e4df0
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-03-30 22:53:30 -0300

    Implementada função que obtém o número da última linha do arquivo
    - Referências:
        - https://stackoverflow.com/questions/4999340/python-random-access-file
        - https://stackoverflow.com/questions/4999340/python-random-access-file/24809292#24809292
        - https://docs.python.org/3/library/linecache.html

--- desafio-07/disouzam/python/tac.py
+++ desafio-07/disouzam/python/tac.py
@@ -4,6 +4,7 @@
 from ctypes import ArgumentError
 import os
 import sys
+import linecache
 
 
 def main(args):
@@ -30,7 +31,8 @@ def main(args):
             "Número excessivo de argumentos. \
             Apenas um argumento com o caminho do arquivo é aceito.")
 
-    imprimir_arquivo_em_ordem_normal(caminho_do_arquivo)
+    numero_linhas_arquivo = obter_a_ultima_linha_do_arquivo(caminho_do_arquivo)
+    print(numero_linhas_arquivo)
 
 
 def imprimir_arquivo_em_ordem_normal(caminho_do_arquivo):
@@ -43,6 +45,60 @@ def imprimir_arquivo_em_ordem_normal(caminho_do_arquivo):
     with open(caminho_do_arquivo, "r", encoding="utf-8") as arquivo:
         for linha in arquivo:
             print(linha, end='')
+
+
+def obter_a_ultima_linha_do_arquivo(caminho_do_arquivo):
+    """obter_a_ultima_linha_do_arquivo(caminho_do_arquivo):
+    Obtém através de uma busca pelo método da bisseção o número da última linha do arquivo
+
+    Parâmetros:
+    caminho_do_arquivo: Caminho do arquivo no disco
+    """
+    result = 0
+    linha_inexistente = ''
+
+    # Verifica primeira linha do arquivo
+    conteudo_linha = linecache.getline(caminho_do_arquivo, 1)
+
+    # Arquivo vazio
+    if conteudo_linha == linha_inexistente:
+        result = 0
+
+    ultima_linha_potencial = 1
+    conteudo_linha = linecache.getline(
+        caminho_do_arquivo, ultima_linha_potencial)
+
+    while conteudo_linha != linha_inexistente:
+        ultima_linha_potencial *= 2
+        conteudo_linha = linecache.getline(
+            caminho_do_arquivo, ultima_linha_potencial)
+
+    ultimo = ultima_linha_potencial
+    meio = int(ultimo/2)
+
+    while meio < ultimo:
+        conteudo_linha_meio = linecache.getline(
+            caminho_do_arquivo, meio)
+
+        conteudo_linha_seguinte = linecache.getline(
+            caminho_do_arquivo, meio + 1)
+
+        # Caso 1: linha do meio e a seguinte estão no meio do arquivo de fato
+        if conteudo_linha_meio != linha_inexistente and conteudo_linha_seguinte != linha_inexistente:
+            meio = meio + int((ultimo - meio)/2)
+
+        # Caso 2: linha do meio e a seguinte estão após o final do arquivo
+        if conteudo_linha_meio == linha_inexistente and conteudo_linha_seguinte == linha_inexistente:
+            ultimo = meio + 1
+            meio = int(ultimo/2)
+
+        # Caso 3: linha do meio é a última linha do arquivo
+        if conteudo_linha_meio != linha_inexistente and conteudo_linha_seguinte == linha_inexistente:
+            break
+
+    result = meio
+
+    return result
 
 
 def debugger_is_active() -> bool:
