31d4fa7c5795d4e4f43cb029771a9c0229306cd8
maurilioSo <maurilio.sousil@gmail.com>
2024-04-08 12:51:39 -0300

    Update push Desafio2

--- desafio-02/maurilioSo/java/numerosPrimos.java
+++ desafio-02/maurilioSo/java/numerosPrimos.java
@@ -1,30 +1,24 @@
 
-public class numerosPrimos {
+public class NumerosPrimos {
 
 	public static void main(String[] args) {
-
-		/*
-		 * Escreva um programa para listar todos os números primos entre 1 e 10000, na
-		 * linguagem de sua preferência.
-		 */
-
 		int i = 2;
-
 		while (i < 10000) {
 			int teste = 0;
-
 			for (int j = 2; j <= i / 2; j++) {
-
 				if (i % j == 0) {
 					teste++;
 					break;
+
 				}
+
 			}
 
 			if (teste == 0) {
 				System.out.println(i);
 
 			}
+
 			i++;
 
 		}
