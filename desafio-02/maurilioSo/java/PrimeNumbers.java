f747442f6e22f9b6a9a567d0c2311797aeec3011
maurilioSo <maurilio.sousil@gmail.com>
2024-04-09 02:23:01 -0300

    Upadate com comentario

--- desafio-02/maurilioSo/java/PrimeNumbers.java
+++ desafio-02/maurilioSo/java/PrimeNumbers.java
@@ -1,23 +1,28 @@
 
 public class PrimeNumbers {
 
-    public static void main(String[] args) {
-        int num = 2, max = 10000;
+	public static void main(String[] args) {
+		//O Programa gera uma lista com os numeros primos entre 1 e 10000.
 
-        while (num < max) {
-            int test = 0;
-            for (int i = 2; i <= num / 2; i++) {
-                if (num % i == 0) {
-                    test++;
-                    break;
-                }
-            }
+		int num = 2, max = 10000;
 
-            if (test == 0) {
-                System.out.println(num);
-            }
+		while(num < max) {
+			int test = 0;
+			for(int i = 2; i <= num / 2; i++) {
+				if(num % i == 0) {
+					test++;
+					break;
+				}
 
-            num++;
-        }
-    }
+			}
+
+			if(test == 0) {
+				System.out.println(num);
+			}
+
+			num++;
+		}
+
+	}
+
 }
