c2075ccc43fdbec410a9f8c02e5ea40ebf080f08
Magno Lomardo <magnussolidus@gmail.com>
2024-04-09 18:23:22 -0300

    fix: fixed prime method for edge cases. fixed initial number to be 1 instead of 2.

--- desafio-02/maurilioSo/java/PrimeNumbers.java
+++ desafio-02/maurilioSo/java/PrimeNumbers.java
@@ -1,27 +1,36 @@
+import java.text.MessageFormat;
 
-public class PrimeNumbers {
+public class PrimeNumbers
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// For para iterar entre 2 e 10000
-		for (int i = 2; i <= 10000; i++) {
-			if(isPrime(i)) {
+		for (int i = 1; i <= 10000; i++)
+		{
+			if(isPrime(i))
+			{
 				System.out.println(i);
 			}
-
 		}
-
 	}
 
 	//Função para verificar se o numero é primo
-	public static boolean isPrime (int num) {
-		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) {
+	public static boolean isPrime (int num)
+	{
+		if(num < 2)
+		{
+			return false;
+		}
+
+		for (int i = 2; i <= num / 2; i++)
+		{
+			if (num % i == 0)
+			{
 				return false;
 			}
 
 		}
-
-	return true;
+		return true;
 	}
-
 }
