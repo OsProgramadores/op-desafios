cdf8e8f0e10897310db7bdf82763dfb792d9d7c6
maurilioSo <maurilio.sousil@gmail.com>
2024-04-08 22:03:56 -0300

    Removendo os espaço colocado automatico pela IDE usada

--- desafio-02/maurilioSo/java/PrimeNumbers.java
+++ desafio-02/maurilioSo/java/PrimeNumbers.java
@@ -3,20 +3,21 @@ public class PrimeNumbers {
 
 	public static void main(String[] args) {
 		int num = 2, max = 10000;
-		
-		while (num < max) {
+
+		while(num < max) {
 			int test = 0;
-			for(int i = 2; i <= num /2; i++) {
+			for(int i = 2; i <= num / 2; i++) {
 				if(num % i == 0) {
 					test++;
 					break;
 				}
+
 			}
-			
+
 			if(test == 0) {
 				System.out.println(num);
 			}
-			
+
 			num++;
 		}
 
