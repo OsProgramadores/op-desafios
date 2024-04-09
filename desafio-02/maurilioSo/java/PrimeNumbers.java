629473a818d20bbd7654f181b806fcbbe0950e2d
maurilioSo <maurilio.sousil@gmail.com>
2024-04-08 22:52:25 -0300

    Update

--- desafio-02/maurilioSo/java/PrimeNumbers.java
+++ desafio-02/maurilioSo/java/PrimeNumbers.java
@@ -1,23 +1,23 @@
 
 public class PrimeNumbers {
 
-	public static void main(String[] args) {
-		int num = 2, max = 10000;
+    public static void main(String[] args) {
+        int num = 2, max = 10000;
 
-		while (num < max) {
-			int test = 0;
-			for (int i = 2; i <= num / 2; i++) {
-				if (num % i == 0) {
-					test++;
-					break;
-				}
-			}
+        while (num < max) {
+            int test = 0;
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    test++;
+                    break;
+                }
+            }
 
-			if (test == 0) {
-				System.out.println(num);
-			}
+            if (test == 0) {
+                System.out.println(num);
+            }
 
-			num++;
-		}
-	}
+            num++;
+        }
+    }
 }
