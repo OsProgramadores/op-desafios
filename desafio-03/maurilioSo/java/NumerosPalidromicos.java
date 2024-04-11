f1fbd50866a5bb6c7f96bf59403c1f9a8dca6782
maurilioSo <maurilio.sousil@gmail.com>
2024-04-11 12:57:39 -0300

    Update formatação

--- desafio-03/maurilioSo/java/NumerosPalidromicos.java
+++ desafio-03/maurilioSo/java/NumerosPalidromicos.java
@@ -1,36 +1,36 @@
 
 public class NumerosPalidromicos {
 
-	public static void main(String[] args) {
-		for(int i = 1; i <= 100000; i++) {
-			if(ehPalidromico(toString(i), i)) {
-				System.out.println(i);
-			}
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100000; i++) {
+            if (ehPalidromico(toString(i), i)) {
+                System.out.println(i);
+            }
 
-		}
+        }
 
-	}
+    }
 
-	//Metodo para transformar o numero em String inverter a ordem do número
-	public static String toString(int num_s) {
-		String b = (num_s + "");
-		String d = "";
+    // Metodo para transformar o numero em String inverter a ordem do número
+    public static String toString(int num_s) {
+        String b = (num_s + "");
+        String d = "";
 
-		for(int i = b.length() - 1; i >= 0; i--) {
-			d += b.charAt(i);
-		}
+        for (int i = b.length() - 1; i >= 0; i--) {
+            d += b.charAt(i);
+        }
 
-		return d;
-	}
+        return d;
+    }
 
-	//Metodo para verificar se é Palidromico
-	public static boolean ehPalidromico(String a, int num) {
-		String b = (num + "");
-		if(a.contentEquals(b)) {
-			return true;
-		}
+    // Metodo para verificar se é Palidromico
+    public static boolean ehPalidromico(String a, int num) {
+        String b = (num + "");
+        if (a.contentEquals(b)) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
 }
