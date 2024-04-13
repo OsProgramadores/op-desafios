e72411d67a0ee0a53a3462d8022ee5e4c11d2495
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-04-13 12:26:32 -0300

    Setup inicial para o desafio 10 - Turing machine

---
+++ desafio-10/disouzam/python/README.md
@@ -0,0 +1,33 @@
+# Introdução
+
+
+
+# Verificação do código com Pylint
+
+O comando a seguir altera o diretório para a pasta do desafio:
+
+```shell
+cd desafio-10/disouzam/python
+```
+
+Executando dentro da pasta do desafio (desafio-10/disouzam/python), o comando para verificar o código através do Pylint é:
+
+```python
+pylint --rcfile=../../../ci/pylint3.rc turing_machine.py
+```
+
+# Como executar o script
+
+Esse código foi testado com a versão 3.11.2 do Python e pode apresentar alguma instabilidade com o Python 3.11.8 (não foi checado contra essa versão).
+
+Executando dentro da pasta do desafio (desafio-10/disouzam/python), o comando é:
+
+```python
+python -m turing_machine
+```
+
+# Testes para desenvolvimento
+
+```python
+python -m turing_machine
+```
---
+++ desafio-10/disouzam/python/turing_machine.py
@@ -0,0 +1,53 @@
+""""""
+from ctypes import ArgumentError
+import os
+import sys
+
+
+def main(args):
+    """main(args):
+
+    Parâmetros:
+    args: Lista de argumentos recebido da linha de comando e
+          pré-processado na chamada da função main. Deve conter 1 argumento apenas indicando
+          o caminho relativo ou absoluto do arquivo contendo linhas com bases de entrada e saída e
+          números para converter na base de saída.
+    """
+    # Análise dos argumentos recebidos em args
+    nargs = len(args)
+    if nargs == 0:
+        raise ArgumentError("Nenhum argumento foi fornecido.")
+
+    elementos_para_conversao = []
+    # Validação dos argumentos
+    if nargs >= 1:
+        caminho_do_arquivo_conversao_base = args[0]
+
+        if not os.path.isfile(caminho_do_arquivo_conversao_base):
+            mensagem1 = "Arquivo não encontrado. Caminho fornecido ou nome do arquivo incorreto."
+            print(mensagem1)
+            return
+
+        with open(caminho_do_arquivo_conversao_base, "r", encoding='utf-8') as arquivo:
+            for linha in arquivo:
+                linha_processada = linha.split("\n")
+                elementos_entrada = linha_processada[0].split(" ")
+                elementos_para_conversao.append(elementos_entrada)
+
+    if nargs >= 2:
+        mensagem1 = f"Você informou um número excessivo de argumentos ({nargs}). "
+        mensagem1 += "Apenas um argumento que aponte o caminho (relativo ou absoluto) "
+        mensagem1 += "do arquivo contendo as bases e o número a ser convertido é necessário."
+        print(mensagem1)
+
+        mensagem2 = "Deseja prosseguir ignorando os demais argumentos? (S para Sim e N para não)"
+        escolha_do_usuario = input(mensagem2)
+
+        if escolha_do_usuario.lower() != "s":
+            print("Programa abortado.")
+            return
+
+
+if __name__ == "__main__":
+    filtered_args = sys.argv[1:]
+    main(filtered_args)
