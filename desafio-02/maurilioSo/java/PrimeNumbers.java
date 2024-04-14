969b092792ca6c1c83f71144495729fcda9f493f
maurilioSo <maurilio.sousil@gmail.com>
2024-04-14 00:21:27 -0300

    Formatação Tabulação

--- desafio-02/maurilioSo/java/PrimeNumbers.java
+++ desafio-02/maurilioSo/java/PrimeNumbers.java
@@ -1,32 +1,32 @@
 
 public class PrimeNumbers {
 
-	public static void main(String[] args) {
-		// For para iterar entre 1 e 10000
-		for (int i = 1; i <= 10000; i++) {
-			if(isPrime(i)) {
-				System.out.println(i);
-			}
+  public static void main(String[] args) {
+    // For para iterar entre 1 e 10000
+    for (int i = 1; i <= 10000; i++) {
+      if(isPrime(i)) {
+        System.out.println(i);
+      }
 
-		}
+    }
 
-	}
+  }
 
-	//Função para verificar se o numero é primo
-	public static boolean isPrime (int num) {
-		//Se o numero for menor ou igual a 1 retornar false
-		if (num <= 1) {
-			return false;
-		}
+  //Função para verificar se o numero é primo
+  public static boolean isPrime (int num) {
+    //Se o numero for menor ou igual a 1 retornar false
+    if (num <= 1) {
+      return false;
+    }
 
-		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) {
-				return false;
-			}
+    for (int i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+        return false;
+      }
 
-		}
+    }
 
-	return true;
-	}
+  return true;
+  }
 
 }
