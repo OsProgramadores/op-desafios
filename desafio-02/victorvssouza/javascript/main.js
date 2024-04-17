a01452a16dafa51b929caabea0491e982c4d5766
victorvssouza <vs.developer.vs310@gmail.com>
2024-04-17 14:38:55 -0300

    Script construido em JS para listar no console os numeros primos de 1 até 1000

--- desafio-02/victorvssouza/javascript/main.js
+++ desafio-02/victorvssouza/javascript/main.js
@@ -1,14 +1,14 @@
 function numerosPrimos(numero) {
     const listaNumerosPrimos = [];
-    for (let numeroAtual = 1; numeroAtual <= numero; numeroAtual += 1){
+    for (let numeroAtual = 1; numeroAtual <= numero; numeroAtual += 1) {
         let numeroPrimo = true;
-        for (let numeroDivisor = 2; numeroDivisor <= numeroAtual; numeroDivisor += 1){
-            if (numeroAtual % numeroDivisor === 0 && numeroAtual !== numeroDivisor){
+        for (let numeroDivisor = 2; numeroDivisor <= numeroAtual; numeroDivisor += 1) {
+            if (numeroAtual % numeroDivisor === 0 && numeroAtual !== numeroDivisor) {
                 numeroPrimo = false;
                 break;
             }
         }
-        if (numeroPrimo === true && numeroAtual !== 1){
+        if (numeroPrimo === true && numeroAtual !== 1) {
             listaNumerosPrimos.push(numeroAtual);
         }
     }
