3544476675d10301c0dacd57883fe7deecafc29d
Maurilio Souza <mauriliosouza@Maurilios-Mac-Pro.local>
2024-04-27 10:58:00 -0300

    Alterar o codigo

--- desafio-03/maurilioSo/java/NumerosPalidromicos.java
+++ desafio-03/maurilioSo/java/NumerosPalidromicos.java
@@ -1,15 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class NumerosPalidromicos {
   public static void main(String[] args) {
-    for (int i = 1; i <= 1000; i++) {
-      verificarPalidromico(i);
+    Scanner sc = new Scanner(System.in);
+
+
+    try {
+      System.out.print("Digite o primeiro numero: ");
+      long numeroInicial = sc.nextLong();
+      System.out.print("Digite o ultimo numero: ");
+      long numeroFinal = sc.nextLong();
+
+      if(numeroInicial < 0 || numeroFinal < 0) {
+        System.out.println("Você deve informar 2 números inteiros positivos!");
+      } else if (numeroInicial > numeroFinal) {
+        System.out.println("O primeiro número deve ser menor que o segundo número!");
+      } else {
+        System.out.println("Os polindrômicos do intervalo entre "+numeroInicial+" e "+numeroFinal+ " : ");
+        for (long i = numeroInicial; i <= numeroFinal; i++) {
+          verificarPalidromico(i);
+          }
+        }
+    }catch (InputMismatchException e) {
+      System.out.println("Os números devem ser inteiros, com no maximo 64 bits!");
     }
-  }
-  // Verificar se o numero é palidromico
-  public static void verificarPalidromico(int numero) {
-    int numeroInvertido = 0;
-    int resto;
-    int numeroOriginal = numero;
+      sc.close();
+    }
+  public static void verificarPalidromico(long numero) {
+    long numeroInvertido = 0;
+    long resto;
+    long numeroOriginal = numero;
     while (numero != 0) {
       resto = numero % 10;
       numeroInvertido = numeroInvertido * 10 + resto;
