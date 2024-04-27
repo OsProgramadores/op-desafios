6951692052fd10e70ad2c4c75798b50f05078555
Maurilio Souza <mauriliosouza@Maurilios-Mac-Pro.local>
2024-04-27 09:22:27 -0300

    Modificação no código

--- desafio-03/maurilioSo/java/NumerosPalidromicos.java
+++ desafio-03/maurilioSo/java/NumerosPalidromicos.java
@@ -1,33 +1,22 @@
 
 public class NumerosPalidromicos {
-
   public static void main(String[] args) {
-    for (int i = 1; i <= 100000; i++) {
-      if (ehPalidromico(toString(i), i)) {
-        System.out.println(i);
-      }
+    for (int i = 1; i <= 1000; i++) {
+      verificarPalidromico(i);
     }
   }
 
-  // Metodo para transformar o numero em String inverter a ordem do número
-  public static String toString(int num_s) {
-    String b = (num_s + "");
-    String d = "";
-
-    for (int i = b.length() - 1; i >= 0; i--) {
-      d += b.charAt(i);
+  public static void verificarPalidromico(int numero) {
+    int numeroInvertido = 0;
+    int resto;
+    int numeroOriginal = numero;
+    while (numero != 0) {
+      resto = numero % 10;
+      numeroInvertido = numeroInvertido * 10 + resto;
+      numero /= 10;
     }
-
-    return d;
-  }
-
-  // Metodo para verificar se é Palidromico
-  public static boolean ehPalidromico(String a, int num) {
-    String b = (num + "");
-    if (a.contentEquals(b)) {
-      return true;
+    if (numeroOriginal == numeroInvertido) {
+      System.out.println(numeroInvertido);
     }
-
-    return false;
   }
 }
