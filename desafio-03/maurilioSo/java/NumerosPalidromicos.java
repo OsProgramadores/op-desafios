1e985188bb05950e6c24d66062529ee9e365be35
Maurilio Souza <mauriliosouza@Maurilios-Mac-Pro.local>
2024-04-28 21:13:51 -0300

    Corrigir o codigo

--- desafio-03/maurilioSo/java/NumerosPalidromicos.java
+++ desafio-03/maurilioSo/java/NumerosPalidromicos.java
@@ -5,39 +5,43 @@ public class NumerosPalidromicos {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-
     try {
       System.out.print("Digite o primeiro numero: ");
       long numeroInicial = sc.nextLong();
       System.out.print("Digite o ultimo numero: ");
       long numeroFinal = sc.nextLong();
 
-      if(numeroInicial < 0 || numeroFinal < 0) {
+      if (numeroInicial < 0 || numeroFinal < 0) {
         System.out.println("Você deve informar 2 números inteiros positivos!");
       } else if (numeroInicial > numeroFinal) {
         System.out.println("O primeiro número deve ser menor que o segundo número!");
       } else {
-        System.out.println("Os polindrômicos do intervalo entre "+numeroInicial+" e "+numeroFinal+ " : ");
+        System.out.println("Os polindrômicos do intervalo entre " + numeroInicial + " e " + numeroFinal + " : ");
         for (long i = numeroInicial; i <= numeroFinal; i++) {
-          verificarPalidromico(i);
+          if (i == verificarPalidromico(i)) {
+            System.out.println(i);
           }
+
         }
-    }catch (InputMismatchException e) {
+
+      }
+    } catch (InputMismatchException e) {
       System.out.println("Os números devem ser inteiros, com no maximo 64 bits!");
     }
-      sc.close();
-    }
-  public static void verificarPalidromico(long numero) {
+
+    sc.close();
+  }
+
+  public static long verificarPalidromico(long numero) {
     long numeroInvertido = 0;
     long resto;
-    long numeroOriginal = numero;
     while (numero != 0) {
       resto = numero % 10;
       numeroInvertido = numeroInvertido * 10 + resto;
       numero /= 10;
     }
-    if (numeroOriginal == numeroInvertido) {
-      System.out.println(numeroInvertido);
-    }
+
+    return numeroInvertido;
   }
+
 }
