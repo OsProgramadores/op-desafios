d6486b39e52e0e8a2009631a9ba30e7a5655cee8
Leonardo Rühmann <leonardoruhmannwork@gmail.com>
2024-05-30 23:59:22 -0300

    desafio-02

---
+++ desafio-02/LeonardRuhmann/javascript/primos.js
@@ -0,0 +1,20 @@
+let primos = [];
+let naoPrimos = [];
+
+for (let numero = 2; numero <= 1000; numero++) {
+  let ehPrimo = true;
+  for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+      ehPrimo = false;
+      break;
+    }
+  }
+  if (ehPrimo) {
+    primos.push(numero);
+  } else {
+    naoPrimos.push(numero);
+  }
+}
+
+console.log(primos);
+console.log(naoPrimos);
