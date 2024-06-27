61b5bfd46e8590846e3c66e1ad422a99b42cbe34
Ronaldo Silva <ronaldofas@gmail.com>
2024-06-27 01:07:53 -0300

    Proposta de solução do exercício 03 em csharp.

---
+++ desafio-03/ronaldofas/csharp/palindromos/Palindromo.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace palindromos;
+
+public class Palindromo
+{
+    public Palindromo() { }
+
+    public bool EhPalindromo(ulong numero){
+        // Converte o número para uma string.
+        string numeroStr = numero.ToString();
+
+        // Inverte a string.
+        char[] charArray = numeroStr.ToCharArray();
+        Array.Reverse(charArray);
+        string numeroInvertidoStr = new string(charArray);
+
+        // Compara a string original com a string invertida.
+        return numeroStr.Equals(numeroInvertidoStr);
+    }
+
+    public List<ulong> PalindromosEntre(string inicio, string fim)
+    {
+        ValidarParametros(inicio);
+        ValidarParametros(fim);
+        
+        List<ulong> resultado = new List<ulong>();
+
+        for (ulong i = UInt64.Parse(inicio); i <= UInt64.Parse(fim); i++)
+        {
+            if (EhPalindromo(i))
+            {
+                resultado.Add(i);
+            }
+        }
+
+        return resultado;
+    }
+
+    private static void ValidarParametros(string numero)
+    {
+        try
+        {
+            var conversao = UInt64.Parse(numero);
+        } catch (OverflowException)
+        {
+            throw new OverflowException("Valor maior ou menor que o permitido");
+        } catch (ArgumentNullException)
+        {
+            throw new ArgumentNullException("Valor não pode ser nulo");
+        } catch (FormatException)
+        {
+            throw new FormatException("Valor inválido");
+        }
+    }
+}
---
+++ desafio-03/ronaldofas/csharp/palindromos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace palindromos;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Palindromo palindromo = new Palindromo();
+        
+        Console.WriteLine("Palindromos entre 1 e 20: ");
+        foreach (ulong item in palindromo.PalindromosEntre("1", "20"))   
+        {
+            Console.WriteLine(item);
+        }
+        
+        Console.WriteLine("Palindromos entre 3000 e 3010: ");
+        foreach (ulong item in palindromo.PalindromosEntre("3000", "3010"))   
+        {
+            Console.WriteLine(item);
+        }
+        
+        Console.WriteLine("Palindromos entre 101 e 121: ");
+        foreach (ulong item in palindromo.PalindromosEntre("101", "121"))   
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
