8a5ddb6ea1628cb75634e360dea839043abaffe6
Leandro Barbosa <leandro.barbosa.705@ufrn.edu.br>
2024-06-28 20:39:20 -0300

    Desafio 02

---
+++ desafio-02/leandro208/cpp/primos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    
+    for(int i = 1; i<=11; i++){
+        int contadorDivisor = 0;
+
+        for(int j = 1; j<=i; j++){
+            if(i%j == 0){
+                contadorDivisor++;
+            }
+        }
+        if(contadorDivisor == 2){
+            std::cout<<i<<"\n";
+        }
+    }
+
+    return 0;
+}
