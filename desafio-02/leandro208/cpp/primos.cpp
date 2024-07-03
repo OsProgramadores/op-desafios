293e5f995602142ba4c2174899c0440e94f0e69d
Leandro Barbosa <leandro.barbosa.705@ufrn.edu.br>
2024-07-03 20:24:47 -0300

    fix: removendo espaços acusados no pipeline

--- desafio-02/leandro208/cpp/primos.cpp
+++ desafio-02/leandro208/cpp/primos.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-
 int main()
 {
-    
     for(int i = 1; i<=10000; i++){
         int contadorDivisor = 0;
-
         for(int j = 1; j<=i; j++){
             if(i%j == 0){
                 contadorDivisor++;
             }
         }
         if(contadorDivisor == 2){
-            std::cout<<i<<"\n";
+            std::cout<<i<<std::endl;
         }
     }
-
     return 0;
 }
