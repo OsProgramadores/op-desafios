17de5116d5490ffe003c12b15f965e69ec23369f
Ronaldo Silva <ronaldofas@gmail.com>
2024-08-03 22:10:01 -0300

    Desafio03 (#1062)
    * Proposta de Solução desafio-02.
    
    * Efetuadas correções sugeridas
    
    * Removendo op-desafios.sln
    
    * Proposta de solução do exercício 03 em csharp.
    
    * Corrigidos erros de Trailing Spaces
    
    * Adicionado instruções de como executar o teste.
    
    * Correção do nome do arquivo
    
    * Correção do desafio 03
    
    * Alterado entrada da aplicação para que o usuário possa efetuar testes variados por input de parâmetros.

---
+++ desafio-03/ronaldofas/csharp/palindromos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace palindromos;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Palindromo palindromo = new Palindromo();
+        
+        Console.WriteLine("Informe o número inicial para validação dos palíndromos: ");
+        string inicio = Console.ReadLine();
+        Console.WriteLine("Informe o número final para validação dos palíndromos: ");
+        string fim = Console.ReadLine();
+        
+        Console.WriteLine("\nNumeros palíndromos entre " + inicio + " e " + fim + ": ");
+        foreach (ulong item in palindromo.PalindromosEntre(inicio, fim))   
+        {
+            Console.WriteLine(item);
+        }
+        
+    }
+}
