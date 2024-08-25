e7aa8cef6fabaf1671e74ec68d2acca93227bd33
Yan Ferreira <yanfvc@gmail.com>
2024-08-24 23:05:53 -0300

    Desafio 02 concluído utilizando a linguagem java por @yanvferreira.

---
+++ desafio-02/yanvferreira/java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static boolean verificaPrimos(int numero){
+        if (numero == 1){ //número 1 não é primo
+            return false;
+        }
+
+        for (int divisor = 2; divisor < numero; divisor++){
+            if (numero % divisor == 0){ //todo número é divisivel por ele mesmo, portanto, não precisa fazer essa operaçã. Caso a divisão pelo divisor seja zero, então não é
+                return false;
+            }
+        }
+        return true;
+    }
+
+ public static void main(String[] args) {
+
+    
+    int tamanhoDaLista = 10000;
+    int numero = 1;
+
+    while (numero <= tamanhoDaLista) {
+        if (verificaPrimos(numero)){
+            System.out.println(numero);
+        }
+        numero++;
+    }
+ }
+}
