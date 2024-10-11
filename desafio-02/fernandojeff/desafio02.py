97024c3ca88aed6002740a59bb2ff580ae0963f0
fernandojeff <fernandojefferson321@gmail.com>
2024-10-11 15:05:11 -0300

    Resolução desafio 02

---
+++ desafio-02/fernandojeff/desafio02.py
@@ -0,0 +1,15 @@
+def eh_primo(n):
+    
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primos = []
+for num in range(1, 10001):
+    if eh_primo(num):
+        primos.append(num)
+
+print(primos)
