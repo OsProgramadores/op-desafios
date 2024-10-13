4739fab89c99d37ccf0747cbb1282e4e86d13591
brenosantos <brenosantosgomes@gmail.com>
2024-10-13 02:12:29 -0300

    Desafio 02

---
+++ desafio-02/brenosantos/php/primos.php
@@ -0,0 +1,15 @@
+<?php
+
+function ehPrimo($n) {
+	if ($n < 2) return false;
+
+	for ($i = 2; $i <= sqrt($n); $i++) { 
+        if ($n % $i == 0) return false;
+    }
+
+    return true;
+}
+
+for ($i = 1; $i <= 1000; $i++) {
+	if(ehPrimo($i)) echo $i . PHP_EOL;
+}
