1754dbc2801892bb4905f92de0f98516f59f75eb
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-10-13 11:52:34 -0300

    feat: Conclusao do desafio 2

--- desafio-02/JamesStewart-314/python/desafio02.py
+++ desafio-02/JamesStewart-314/python/desafio02.py
@@ -9,7 +9,7 @@ def check_primality(number: int, /) -> bool:
     for i in range(3, int(number ** (1 / 2)) + 1, 2):
         if not number % i:
             return False
-        
+
     return True
 
 [ print("• Prime Number Detected:\033[32m", number, "\033[0m") for number in range(10001) if check_primality(number) ]
