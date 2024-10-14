701586dc6182c3cf8748edd07c203bc63f93e027
Adriano Jr <adrianojunr@gmail.com>
2024-10-14 19:01:37 -0300

    Correção indentação

--- desafio-02/devjunr/java/DesafioNumPrimo.java
+++ desafio-02/devjunr/java/DesafioNumPrimo.java
@@ -1,24 +1,19 @@
 public class DesafioNumPrimo {
     public static void main(String[] args) {
         int quantMax = 10000;
-        System.out.println("\n".repeat(2) + "Números primos de 1 a " + quantMax + "\n");
-        while(true){
-            for(int i=2; i<=quantMax; i++){
-                int contador = 2;
-				boolean isPrimo = true;
-                while(isPrimo && contador < i){
-                    if (i % contador == 0){
-                        isPrimo = false;
-                    }
-                    else{
-                        contador++;
-                    }
-                }
-                if (isPrimo){
-                    System.out.println(i);
+        for (int i = 2; i <= quantMax; i++) {
+            int contador = 2;
+            boolean isPrimo = true;
+            while (isPrimo && contador < i) {
+                if (i % contador == 0) {
+                    isPrimo = false;
+                } else {
+                    contador++;
                 }
             }
-            break;
+            if (isPrimo) {
+                System.out.println(i);
+            }
         }
     }
 }
