d7c175527bbd40f1f6bdf89b9ba5f19ee49cd459
OsmarWilliam <osmarwilliam4@gmail.com>
2024-10-14 20:00:52 -0300

    Adicionando desafio 02 em C: @osmarwilliam

--- desafio-02/osmarwilliam/c/numPrimos.c
+++ desafio-02/osmarwilliam/c/numPrimos.c
@@ -6,16 +6,17 @@ int main()
     {
         // todo número primo é divido por ele mesmo e por 1, logo, só temos duas possibilidades (1 e o próprio número)
         int count = 0;
-        for (int y = 1; y <= i; y++)
+        for (int j = 2; j < i / 2; j++) // entretando, não é necessário iterar por todos números, para otimizar iremos análisar sempre até a metade do determinado número
+        // exemplo, dentre os divisores de 64, o 32 é o último número, uma vez que a partir do 32 nenhum número terá resto = 0, 
+        // assim como 15, a partir do 7,5 não é necessário procurar algum divisor
         {
-            if (i % y == 0)
+            if (i % j == 0)
             {
                 count++;
                 // iremos calcular quantas vezes um determinado número tem resto = 0.
             }
         }
-        if (count == 2)
-        // se count for > 2, significa que ele é divisível por 1, ele mesmo, e por outros números. Portanto, não é primo
+        if (count == 0)
         {
             printf("%i\n", i);
         }
