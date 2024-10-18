28a3f2008831345e18e213fccd0d617069ba7791
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-10-18 18:18:18 -0300

    docs: Adição da docstring correspondente à função sieveofEratosthenesPrimeGenerator

--- desafio-02/JamesStewart-314/python/desafio02.py
+++ desafio-02/JamesStewart-314/python/desafio02.py
@@ -1,6 +1,25 @@
 from typing import Generator
 
 def sieveofEratosthenesPrimeGenerator() -> Generator[int, None, None]:
+
+    """
+     Generates an infinite sequence of prime numbers 
+    using the Sieve of Eratosthenes algorithm.
+     
+     This generator function uses a dictionary to keep track of 
+    non-prime numbers and their smallest prime factors.
+    It starts from the number 2 and iteratively finds the next
+    prime number by checking if the current number is
+    in the dictionary. If it is not, the number is prime and is
+    yielded. If it is, the function updates the dictionary
+    to mark the next multiple of the corresponding prime factor
+    as non-prime. The dictionary is also optimized for
+    memory usage by deleting old values.
+
+    :return: A generator to produce prime numbers
+    :rtype: Generator[int, None, None]
+    """
+
     # Dictionary to allocate the numbers that isn't prime:
     sieveofEratosthenesDict: dict[int, int] = {}
     # Number currently analyzed:
