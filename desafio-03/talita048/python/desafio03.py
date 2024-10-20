8a316e000b33cbae10de8e931432e59557f25c60
Talita Ribeiro <90120664+talita048@users.noreply.github.com>
2024-10-20 17:59:39 -0300

    Adicionando checagem dos números
    Adicionadas funções que conferem se as entradas do usuário são números válidos e se o primeiro número é menor que o segundo.

--- desafio-03/talita048/python/desafio03.py
+++ desafio-03/talita048/python/desafio03.py
@@ -1,13 +1,40 @@
+def eh_numero(val):
+    if val.isdigit():
+        return True
+    else:
+        return False
+
+def eh_positivo(val):
+    if int(val) >= 0:
+        return True
+    else:
+        return False
+
+def check_numero(mensagem):
+    while True:
+        num = input(mensagem)
+        if not eh_numero(num):
+            print("Por favor, insira um número válido.")
+        elif not eh_positivo(num):
+            print("Por favor, insira um número positivo.")
+        else:
+            return int(num)
+
 print("\nBem-vindo(a) ao detector de números palíndromos!")
 print("Um número palíndromo é aquele que permanece igual quando lido de trás para frente.")
 print("Insira dois números e vou te informar todos os palíndromos que existem entre eles :D")
 
-limite_a = int(input("Digite o primeiro número: "))
-limite_b = int(input("Digite o segundo número: "))
+while True:
+    limite_a = check_numero("Digite o primeiro número: ")
+    limite_b = check_numero("Digite o segundo número: ")
+    if limite_a >= limite_b:
+        print("O primeiro número deve ser menor que o segundo. Tente novamente.")
+    else:
+        break
 
 lista_pal = []
 
-while limite_a < limite_b+1 :
+while limite_a <= limite_b :
     reverso = int(str(limite_a)[::-1])
     if limite_a == reverso :
         lista_pal.append(limite_a)
