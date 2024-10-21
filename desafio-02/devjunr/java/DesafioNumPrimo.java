7a8ac19c08d0b0387c5c9b760b00006c36a35593
Adriano J. <70298185+devJunr@users.noreply.github.com>
2024-10-20 22:44:57 -0300

    @devjunr - Resolução Desafio 2 (#1093)

---
+++ desafio-02/devjunr/java/DesafioNumPrimo.java
@@ -0,0 +1,19 @@
+public class DesafioNumPrimo {
+  public static void main(String[] args) {
+    int quantMax = 10000;
+    for (int i = 2; i <= quantMax; i++) {
+      int contador = 2;
+      boolean isPrimo = true;
+      while (isPrimo && contador < i) {
+        if (i % contador == 0) {
+          isPrimo = false;
+        } else {
+          contador++;
+        }
+      }
+      if (isPrimo) {
+        System.out.println(i);
+      }
+    }
+  }
+}
