ca71f505ddf79ba6a4aa9ad8f2edbf6583961a60
Adriano Jr <adrianojunr@gmail.com>
2024-10-23 13:52:49 -0300

    Adição de Tratamento

--- desafio-03/devjunr/java/palindromicos.java
+++ desafio-03/devjunr/java/palindromicos.java
@@ -1,34 +1,28 @@
 public class palindromicos {
-  public static void main(String[] args) {
-    int valorInicial = Integer.parseInt(args[0]);
-    int valorFinal = Integer.parseInt(args[1]);
-    if (args.length < 2) {
-      System.out.println("Forneça dois valores positivos como argumentos no momento da execução");
-    } else if (valorInicial < 0 || valorFinal < 0) {
-      System.out.println("Os valores dos argumentos devem ser positivos");
-    } else {
-      System.out.println(
-          "_".repeat(15)
-              + "\n"
-              + "Valor Inicial: "
-              + valorInicial
-              + "\n"
-              + "Valor Final: "
-              + valorFinal
-              + "\n"
-              + "_".repeat(10));
-      tratamentoDosValores(valorInicial, valorFinal);
+   public static void main(String[] args) {
+        try {
+            int valorInicial = Integer.parseInt(args[0]);
+            int valorFinal = Integer.parseInt(args[1]);
+            if (valorInicial > 0 & valorFinal > 0) {
+                System.out.println("_".repeat(15)+"\nValor Inicial: " + valorInicial+"\nValor Final: " + valorFinal+"\n"+"_".repeat(15));
+                tratamentoDosValores(valorInicial,valorFinal);
+            }else if (valorInicial <= 0 || valorFinal <= 0) {
+                System.out.println("São aceitos apenas valores positivos");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Apenas números inteiros são aceitos");
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("É necessário pelo menos dois valores inteiros como argumento");
+        }
     }
-  }
 
-  private static void tratamentoDosValores(int valorInicial, int valorFinal) {
-    System.out.println("Resultado: ");
-    for (int i = valorInicial; i <= valorFinal; i++) {
-      String numero = Integer.toString(i);
-      String reversed = new StringBuilder(numero).reverse().toString();
-      if (reversed.equals(numero)) {
-        System.out.println(numero);
-      }
+   public static void tratamentoDosValores(int valorInicial, int valorFinal){
+        for(int i = valorInicial;i<=valorFinal;i++){
+            String numero = Integer.toString(i);
+            String reversed = new StringBuilder(numero).reverse().toString();
+            if (reversed.equals(numero)) {
+                System.out.println(numero);
+            }
+        }
     }
-  }
 }
