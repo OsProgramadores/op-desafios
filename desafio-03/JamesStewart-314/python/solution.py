bec68e834feaaef188942ffca57c28a5040f93aa
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-10-22 23:28:43 -0300

    Adição de docstrings e refatoração do código

--- desafio-03/JamesStewart-314/python/solution.py
+++ desafio-03/JamesStewart-314/python/solution.py
@@ -7,33 +7,111 @@ r"""
 
 import os
 import time
+import inspect
 from typing import Callable, TypeAlias, Final
 
+
+# ------------------------------------ // Type Alias \\ ----------------------------------- ||
 # NumberConstraint: Tuple containing the lower
 # and upper bound of the new input number, respectivaly:
 NumberConstraint: TypeAlias = tuple[int | None, int | None] | None
-MAXINTSIZE: Final[int] = (1 << 64) - 1
+# ----------------------------------------------------------------------------------------- ||
 
+# ------------------------------------ // Constants \\ ------------------------------------ ||
+MAXINTSIZE: Final[int] = (1 << 64) - 1
+# ----------------------------------------------------------------------------------------- ||
+
+# --------------------------------- // Lambda Functions \\ -------------------------------- ||
 number_is_palindromic: Callable[[int], bool] = lambda x: \
                (strNumber := str(x)) == strNumber[::-1]
-clear_terminal: Callable[[], None] = lambda: os.system("cls" if os.name == "nt" else "clear")
+
+clear_terminal: Callable[[], int] = lambda: os.system("cls" if os.name == "nt" else "clear")
+
+skip_terminal_lines: Callable[[int], None] = lambda qty: print('\n' * qty, end="")
+# ----------------------------------------------------------------------------------------- ||
+
+def constraints_assertion(constraints: NumberConstraint, functionName: str, /) -> None:
+
+    """
+     Specific function to define the validation routine
+    for a variable of the "NumberConstraint" type.
+
+    :param constraints: A variable corresponding to the
+     "NumberConstraint" type to be validated.
+    :type constraints: NumberConstraint
+
+    :param functionName: The name of the original function
+     that invoked the validation of the "constraints" variable.
+    :type functionName: str
+
+    :return: No return
+    :rtype: None
+    """
+
+    assert isinstance(functionName, str), \
+           "Assertion Error: \'functionName\' parameter in \'constraints_assertion\'"\
+           " function must be of type \'str\'."
+
+    assert isinstance(constraints, tuple | None), \
+           f"Assertion Error: \'constraints\' parameter in \'{functionName}\'"\
+           " function must be of type \'NumberConstraint\'."
+
+    if constraints is not None:
+        assert len(constraints) == 2, "Assertion Error: \'constraints\'"\
+              f"parameter in \'{functionName}\' function must have lenght equal to two."
+
+        assert all((isinstance((exc := element), int | None)) for element in constraints),\
+               "Assertion Error: \'constraints\'"\
+              f"parameter in \'{functionName}\' function must"\
+              f"contain only elements of type \'int\' or \'None\'. (Exception Caught: {exc})"
 
 
 def clear_upper_lines(quantity: int, /) -> None:
+
+    """
+     Deletes the contents of the upper lines of the terminal,
+    including the current line on which the cursor is located
+    when this function is called.
+
+    :param quantity: Number of terminal predecessor lines to
+     be cleared, including the current one.
+    :type quantity: int
+
+    :return: No return
+    :rtype: None
+    """
+
+    # Type Assertions:
+    assert isinstance(quantity, int), "Assertion Error: \'quantity\'"\
+           "parameter in \'clear_upper_lines\' function must be of type \'int\'."
+
     print("\r", end="")
     for _ in range(quantity + 1):
         print("\033[K\033[1A", end="")
-    print()
+    skip_terminal_lines(1)
 
 
 def validade_number_constraints(number: int,
-                              constraints: NumberConstraint, /) -> None:
+                                constraints: NumberConstraint, /) -> None:
+    """
+     Checks whether a given integer value is obeying the
+    constraints specified in the "constraints" parameter.
+
+    :param number: The parsed integer value.
+    :type number: int
+
+    :param constraints: A tuple containing the range in
+     which the number should be contained.
+    :type constraints: NumberConstraint
+
+    :return: No return
+    :rtype: None
+    """
+
     # Type Assertions:
     assert isinstance(number, int), "Assertion Error: \'number\' parameter in"\
-        " \'validade_number_constraints\' function must be of type \'int\'"
-    assert isinstance(constraints, tuple | None), \
-    "Assertion Error: \'constraints\' parameter in \'validade_number_constraints\'"\
-        " function must be of type \'NumberConstraint\'"
+           " \'validade_number_constraints\' function must be of type \'int\'."
+    constraints_assertion(constraints, inspect.stack()[0][3])
 
     if constraints is None:
         return
@@ -50,14 +128,34 @@ def validade_number_constraints(number: int,
 
 def get_valid_number(*, message: str | None = None,
                    constraints: NumberConstraint = None) -> int:
-    # Function to get an integer valid number
+    """
+     Function to obtain an integer from the terminal, limited or not
+    by a closed interval specified by two other integer values.
+
+    :param message: The optional message that will be displayed in the
+     terminal when prompted to input a new integer.
+    :type message: str | None
+
+    :param constraints: Defines a tuple with two optional values that represent
+     the lower and upper bounds for inputting a value. If there is no lower bound,
+     the first element of the tuple is None. If there is no upper bound, the 
+     second element is None.
+
+      E.g.: (10, None) means that the value must be greater than or equal to 10,
+      while (None, 20) means that the value must be less than or equal to 20.
+      The tuple (None, None) indicates that there are no bound restrictions.
+
+    :type constraints: NumberConstraint
+
+    :return: An integer value provided by the user through the terminal,
+     to which the value will be limited if a range is specified during the function call.
+    :rtype: int
+    """
 
     # Type Assertions:
     assert isinstance(message, str | None), "Assertion Error: \'message\' parameter"\
-                                            " in \'get_valid_number\'"\
-                                            " function must be of type \'str\'"
-    assert isinstance(constraints, tuple | None), "Assertion Error: \'constraints\'"\
-           "parameter in \'get_valid_number\' function must be of type \'NumberConstraint\'"
+           " in \'get_valid_number\'function must be of type \'str\'."
+    constraints_assertion(constraints, inspect.stack()[0][3])
 
     while True:
         try:
@@ -78,8 +176,31 @@ def get_valid_number(*, message: str | None = None,
 
         return userInput
 
-def impress_palindromic_numbers_in_range(start: int, end: int, /) -> None:
-    numbersFoundFlag: bool = False
+def get_pal_numbers(start: int, end: int, /) -> None:
+
+    """
+     Function to display on the terminal a set
+    of palindromic numeric values identified in
+    a range specified by the 'start' and 'end'
+    parameters.
+
+    :param start: The inclusive lower limit
+     of the analyzed range.
+    :type start: int
+
+    :param end: The inclusive upper limit
+     of the analyzed range.
+    :type end: int
+
+    :return: No return
+    :rtype: None
+    """
+
+    # Type Assertions:
+    assert isinstance(start, int), "Assertion Error: \'start\'"\
+           "parameter in \'get_pal_numbers\' function must be of type \'int\'."
+    assert isinstance(end, int), "Assertion Error: \'end\'"\
+           "parameter in \'get_pal_numbers\' function must be of type \'int\'."
 
     palindromicNumbersFound: list[int] = []
 
@@ -95,30 +216,37 @@ def impress_palindromic_numbers_in_range(start: int, end: int, /) -> None:
         print("[ sorry, no palindromic numbers found in the given interval ]")
 
 
-clear_terminal()
+if __name__ == '__main__':
 
-print("\033[4m", f"{"<<< Welcome to the numeric palindrome detector! >>>":^60}",\
-      "\033[0m\b ", sep="")
-print("|||", " " * 51, "|||")
-print("|||", f"{"Please, insert two numbers greater or equal to":^51}", \
-      "|||\n||| each other to find palindromic numbers between them |||")
-print("\\\\\\", "_" * 53, "///\n", sep="")
+    # Clear the terminal before
+    # the program starts:
+    clear_terminal()
 
-firstNumber: int = get_valid_number(message = "• Enter the "\
-                                  "\033[33;1mFirst\033[0m Number: ",
-                                  constraints = (0, MAXINTSIZE))
-secondNumber: int = get_valid_number(message = "• Enter the "\
-                                  "\033[34;1mSecond\033[0m Number: ",
-                                  constraints = (firstNumber, MAXINTSIZE))
+    print("\033[4m", f"{"<<< Welcome to the numeric palindrome detector! >>>":^60}",\
+        "\033[0m\b ", sep="")
+    print("|||", " " * 51, "|||")
+    print("|||", f"{"Please, insert two numbers greater or equal to":^51}", \
+        "|||\n||| each other to find palindromic numbers between them |||")
+    print("\\\\\\", "_" * 53, "///\n", sep="")
 
-print()
-impress_palindromic_numbers_in_range(firstNumber, secondNumber)
-print()
+    firstNumber: int = get_valid_number(message = "• Enter the "\
+                                    "\033[33;1mFirst\033[0m Number: ",
+                                    constraints = (0, MAXINTSIZE))
+    secondNumber: int = get_valid_number(message = "• Enter the "\
+                                    "\033[34;1mSecond\033[0m Number: ",
+                                    constraints = (firstNumber, MAXINTSIZE))
 
-input("Press Enter to End the Program!")
-for i in range(5, -1, -1):
-    print("Thank you for using!!!! Closing in", i, '...', end="")
-    time.sleep(1)
-    print("\r", end="")
+    skip_terminal_lines(1)
+    get_pal_numbers(firstNumber, secondNumber)
+    skip_terminal_lines(1)
 
-clear_terminal()
+    input("Press Enter to Exit!")
+
+    # Countdown to end of program:
+    print("Thank you for using this program!!!! Closing in  ...", end='\b' * 4)
+    for counter in range(5, -1, -1):
+        print(counter, end="\033[3C")
+        time.sleep(1)
+        print('\b' * 4, end="")
+
+    clear_terminal()
