ed25376aa3dfd037784c5981349cca9005ba95db
Adriano Jr <adrianojunr@gmail.com>
2024-10-23 22:40:45 -0300

    Correção sintaxe

--- desafio-03/devjunr/java/palindromicos.java
+++ desafio-03/devjunr/java/palindromicos.java
@@ -4,19 +4,19 @@ public class palindromicos {
       int valorInicial = Integer.parseInt(args[0]);
       int valorFinal = Integer.parseInt(args[1]);
       if (valorInicial > 0 & valorFinal > 0) {
-         if (valorFinal < valorInicial){
-            System.out.println("O valor final não pode ser menor que o valor inicial");
-         }else{
-            System.out.println(
-            "_".repeat(15)
-                + "\nValor Inicial: "
-                + valorInicial
-                + "\nValor Final: "
-                + valorFinal
-                + "\n"
-                + "_".repeat(15));
-        tratamentoDosValores(valorInicial, valorFinal);
-         }
+        if (valorFinal < valorInicial) {
+          System.out.println("O valor final não pode ser menor que o valor inicial");
+        } else {
+          System.out.println(
+              "_".repeat(15)
+                  + "\nValor Inicial: "
+                  + valorInicial
+                  + "\nValor Final: "
+                  + valorFinal
+                  + "\n"
+                  + "_".repeat(15));
+          tratamentoDosValores(valorInicial, valorFinal);
+        }
       } else if (valorInicial <= 0 || valorFinal <= 0) {
         System.out.println("São aceitos apenas valores positivos");
       }
