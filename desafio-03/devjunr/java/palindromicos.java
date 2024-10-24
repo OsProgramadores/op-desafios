9fc381f3988358710df739478ec777eef23bd9b1
Adriano Jr <adrianojunr@gmail.com>
2024-10-23 22:36:30 -0300

    Add regra de valores

--- desafio-03/devjunr/java/palindromicos.java
+++ desafio-03/devjunr/java/palindromicos.java
@@ -4,7 +4,10 @@ public class palindromicos {
       int valorInicial = Integer.parseInt(args[0]);
       int valorFinal = Integer.parseInt(args[1]);
       if (valorInicial > 0 & valorFinal > 0) {
-        System.out.println(
+         if (valorFinal < valorInicial){
+            System.out.println("O valor final não pode ser menor que o valor inicial");
+         }else{
+            System.out.println(
             "_".repeat(15)
                 + "\nValor Inicial: "
                 + valorInicial
@@ -13,6 +16,7 @@ public class palindromicos {
                 + "\n"
                 + "_".repeat(15));
         tratamentoDosValores(valorInicial, valorFinal);
+         }
       } else if (valorInicial <= 0 || valorFinal <= 0) {
         System.out.println("São aceitos apenas valores positivos");
       }
