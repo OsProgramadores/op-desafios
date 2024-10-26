9d489f819c2fae1840bdc346d4016ca926ab5a3f
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-10-26 12:44:38 -0300

    perf: Aperfeiçoamento do código

--- desafio-06/JamesStewart-314/python/solution.py
+++ desafio-06/JamesStewart-314/python/solution.py
@@ -1,40 +1,49 @@
+#!usr/bin/python
 import sys
 import os
 import re
+import functools
 from collections import Counter as ctr
-from typing import List, Set, Tuple, Final
+from typing import Final, Generator
 
 
-all_anagrams: Set[Tuple[str, ...]] = set()
-all_valid_words: List[str] = []
+all_anagrams: set[tuple[str, ...]] = set()
+all_valid_words: list[str] = []
 default_word: Final[str] = "VERMELHO"
 
 
+@functools.cache
 def is_sub_anagram(super_string: str, sub_string: str) -> bool:
-    return all((ctr(super_string).get(kew, 0) >= value) for\
-                kew, value in ctr(sub_string).items())
+    super_string_count = ctr(super_string)
+    sub_string_count = ctr(sub_string)
+
+    return all(super_string_count[k] >= count for k, count in sub_string_count.items())
+
+
+@functools.cache
+def check_word(string_pattern: str) -> Generator[tuple[str | None, str | None], None, None]:
+    for current_word in all_valid_words:
+        if len(string_pattern) >= len(current_word) and is_sub_anagram(string_pattern, current_word):
+            yield (''.join(sorted((ctr(string_pattern) - ctr(current_word)).elements())), current_word)
 
 
 def search_palindromes(string_pattern: str,
-                       palindrome_set: set[str] | None = None) -> None:
+                       palindrome_set: list[str] | None = None) -> None:
 
     if palindrome_set is None:
-        palindrome_set = set()
+        palindrome_set = []
 
     if not string_pattern:
         all_anagrams.add(tuple(sorted(palindrome_set)))
         return
 
-    for current_word in all_valid_words:
-        if is_sub_anagram(string_pattern, current_word):
-            new_string_pattern_letters = ''.join((ctr(string_pattern) - \
-                                        ctr(current_word)).elements())
-
-            search_palindromes(new_string_pattern_letters,
-                              palindrome_set.union({current_word}))
+    result = check_word(string_pattern)
+    for word in result:
+        if word is not None:
+            search_palindromes(word[0], palindrome_set + [word[1]])
 
 try:
-    input_word: str = tmp_word if \
+    input_word: str = ''.join(sorted(tmp_word)) if \
     (tmp_word := (''.join(sys.argv[1:])).upper().replace(" ", "")) else \
     default_word
 
@@ -47,7 +56,6 @@ except IndexError as error:
 except ValueError as error:
     print("Error:", error)
 
-
 try:
     with open(os.path.join(os.path.dirname(__file__), "anagramWords.txt"), "r") as txt_file:
         while (current_file_word := txt_file.readline().strip()):
@@ -59,5 +67,5 @@ except (FileNotFoundError, PermissionError, IOError) as error:
 
 search_palindromes(input_word)
 
-for anagramTuple in sorted(all_anagrams, key=lambda x: (-len(x), x)):
+for anagramTuple in all_anagrams:
     print(*anagramTuple)
