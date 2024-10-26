4796890d4a6ad41d3edd5332b7bb06303681dfd2
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-10-26 19:25:44 -0300

    perf: Aprimoramento da solução

--- desafio-06/JamesStewart-314/python/solution.py
+++ desafio-06/JamesStewart-314/python/solution.py
@@ -1,8 +1,6 @@
-#!usr/bin/python
 import sys
 import os
 import re
-import functools
 from collections import Counter as ctr
 from typing import Final, Generator
 
@@ -12,35 +10,66 @@ all_valid_words: list[str] = []
 default_word: Final[str] = "VERMELHO"
 
 
-@functools.cache
+def remove_characters(original_string: str, char_to_remove: str) -> str:
+    return ''.join(sorted((ctr(original_string) - ctr(char_to_remove)).elements()))
+
+
 def is_sub_anagram(super_string: str, sub_string: str) -> bool:
+    if len(sub_string) > len(super_string):
+        return False
+
     super_string_count = ctr(super_string)
     sub_string_count = ctr(sub_string)
 
-    return all(super_string_count[k] >= count for k, count in sub_string_count.items())
+    return all(super_string_count[key] >= value for key, value in sub_string_count.items())
 
 
-@functools.cache
-def check_word(string_pattern: str) -> Generator[tuple[str | None, str | None], None, None]:
+def check_word(string_pattern: str) -> Generator[tuple[str, str], None, None]:
     for current_word in all_valid_words:
-        if len(string_pattern) >= len(current_word) and is_sub_anagram(string_pattern, current_word):
-            yield (''.join(sorted((ctr(string_pattern) - ctr(current_word)).elements())), current_word)
+        if is_sub_anagram(string_pattern, current_word):
+            yield (remove_characters(string_pattern, current_word), current_word)
 
+
+search_palindromes_memoization: dict[str, list[str]] = {}
 
 def search_palindromes(string_pattern: str,
-                       palindrome_set: list[str] | None = None) -> None:
-
-    if palindrome_set is None:
-        palindrome_set = []
+                       palindrome_list: list[str] | None = None) -> None:
+    if palindrome_list is None:
+        palindrome_list = []
 
     if not string_pattern:
-        all_anagrams.add(tuple(sorted(palindrome_set)))
+        all_anagrams.add(tuple(sorted(palindrome_list)))
+
+        try:
+            all_valid_words.remove(tuple(sorted(palindrome_list))[0])
+        except ValueError:
+            pass
+
         return
 
-    result = check_word(string_pattern)
-    for word in result:
-        if word is not None:
-            search_palindromes(word[0], palindrome_set + [word[1]])
+    if string_pattern in search_palindromes_memoization:
+        all_check_words = search_palindromes_memoization[string_pattern]
+
+        for word in all_check_words:
+            search_palindromes(word[0], palindrome_list + [word[1]])
+
+    else:
+        result = check_word(string_pattern)
+        search_palindromes_memoization[string_pattern] = []
+
+        try:
+            word: str = next(result)
+        except StopIteration:
+            return
+
+        while True:
+            try:
+                search_palindromes_memoization[string_pattern].append(word)
+                search_palindromes(word[0], palindrome_list + [word[1]])
+                word = next(result)
+
+            except StopIteration:
+                break
 
 try:
     input_word: str = ''.join(sorted(tmp_word)) if \
@@ -57,15 +86,17 @@ except ValueError as error:
     print("Error:", error)
 
 try:
-    with open(os.path.join(os.path.dirname(__file__), "anagramWords.txt"), "r") as txt_file:
+    with open(os.path.join(os.path.dirname(__file__), "anagramWords.txt"), "r") \
+        as txt_file:
+
         while (current_file_word := txt_file.readline().strip()):
             if is_sub_anagram(input_word, current_file_word):
                 all_valid_words.append(current_file_word)
 
 except (FileNotFoundError, PermissionError, IOError) as error:
-    print("An Exception While Opening File Occured:", error)
+    print("An Exception While Opening the File Occured:", error)
 
 search_palindromes(input_word)
 
-for anagramTuple in all_anagrams:
+for anagramTuple in sorted(all_anagrams):
     print(*anagramTuple)
