76c34c961ba41c13862c2f42f6f7cd054ac4018b
fernandojeff <fernandojefferson321@gmail.com>
2024-11-06 12:01:29 -0300

    Código com o tratamento da entrada de dados, como solicitado.

--- desafio-03/fernandojeff/python/desafio03.py
+++ desafio-03/fernandojeff/python/desafio03.py
@@ -6,10 +6,48 @@ def encontrar_palindromos(inicio, fim):
         if eh_palindromo(num):
             print(num)
 
-# Numero inicial
-start = int(input("Digite o numero INICIAL: "))
+def verfi(entrada):
+    max = (1 << 64) - 1
 
-# Numero final
-limit = int(input("Digite o numero FINAL: "))
+    try:
+        numero = int(entrada)
+    except ValueError:
+        print("O valor digitado não é um número válido.")
+        return False
+
+    if numero <= 0:
+        print("Digite um INTEIRO POSITIVO!")
+        return False
+    
+    if numero > max:
+        print("O numero execede o valor limite! Digite outro numero menor.")
+        return False
+    
+    return numero
+
+while True:
+    primeiro_numero = input("Digite o numero INICIAL: ")
+    
+    if verfi(primeiro_numero) is False:
+        continue
+    
+    start = verfi(primeiro_numero) 
+    
+    break
+
+while True:
+
+    ultimo_numero = input("Digite o numero FINAL: ")
+
+    if verfi(ultimo_numero) is False:
+        continue
+
+    limit = verfi(ultimo_numero)
+    
+    if limit < start:
+        print("O ultimo numero deve ser menor que o primeiro.")
+        continue
+    
+    break
 
 encontrar_palindromos(start, limit)
