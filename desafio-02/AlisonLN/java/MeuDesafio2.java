44def711d70449ee8e864045b03fb66bf25db39d
AlisonLN <alison.nascimento@hotmail.com>
2024-11-07 18:18:20 -0300

    Corrigindo linha vazia no final do arquivo

--- desafio-02/AlisonLN/java/MeuDesafio2.java
+++ desafio-02/AlisonLN/java/MeuDesafio2.java
@@ -1,27 +1,25 @@
-package desafioOsProgramadores;
-
 import java.util.Arrays;
 
 public class MeuDesafio2 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int max = 10000;
-        boolean[] numPrimos = new boolean[max + 1];
+    int max = 10000;
+    boolean[] numPrimos = new boolean[max + 1];
 
-        Arrays.fill(numPrimos, true);
+    Arrays.fill(numPrimos, true);
 
-        for (int i = 2; i * i <= max; i++) {
-            if (numPrimos[i]) {
-                for (int j = i * i; j <= max; j += i) {
-                    numPrimos[j] = false;
-                }
-            }
+    for (int i = 2; i * i <= max; i++) {
+      if (numPrimos[i]) {
+        for (int j = i * i; j <= max; j += i) {
+          numPrimos[j] = false;
         }
-
-        for (int i = 2; i <= max; i++) {
-            if (numPrimos[i]) {
-                System.out.print(i + " ");
-            }
-        }
+      }
     }
+
+    for (int i = 2; i <= max; i++) {
+      if (numPrimos[i]) {
+        System.out.print(i + " ");
+      }
+    }
+  }
 }
