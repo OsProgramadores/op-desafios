345a50b01f1d3e879f0e6cdf524dbd95abeb36e7
Maurilio Souza <maurilio.sousil@gmail.com>
2024-11-09 00:22:27 -0300

    Resolução do desafio 3 (#1138)

---
+++ desafio-03/maurilioSo/java/NumerosPalidromicos.java
@@ -0,0 +1,28 @@
+public class NumerosPalidromicos {
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 100000; ++i) {
+      if (ehPalindromico(i)) {
+        System.out.println(i);
+      }
+    }
+  }
+
+  public static boolean ehPalindromico(int num) {
+
+    if (num < 0) {
+      return false;
+    } else {
+      int numeroInvertido = 0;
+      int numeroOriginal = num;
+      while (num != 0) {
+        int resto = num % 10;
+        numeroInvertido = numeroInvertido * 10 + resto;
+        num /= 10;
+      }
+
+      return numeroOriginal == numeroInvertido;
+    }
+  }
+}
