456131ff7a17be63c39ce31a494ffb11147fb676
AlisonLN <alison.nascimento@hotmail.com>
2024-11-13 00:24:59 -0300

    Enviando imports

--- desafio-03/AlisonLN/java/MeuDesafio3.java
+++ desafio-03/AlisonLN/java/MeuDesafio3.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MeuDesafio3 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
