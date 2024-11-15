54f3e01a4b838254d9249439b472310acd8e78af
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-11-15 15:13:30 -0300

    feat: Conclusão do desafio 7 - @JamesStewart-314

---
+++ desafio-07/JamesStewart-314/python/solution.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import io
+from typing import Final, Generator
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+
+MEGABYTE_IN_BYTES: Final[int] = 1_048_576
+CHUNK_SIZE: int = 100
+CHUNK_SIZE_IN_BYTES: int = CHUNK_SIZE * MEGABYTE_IN_BYTES
+
+
+def process_buffer(bin_string_buffer: str) -> Generator[str | bool, None, str]:
+    bin_string_buffer_list: list[str] = bin_string_buffer.split(b"\n")
+    while len(bin_string_buffer_list) > 1:
+        yield bin_string_buffer_list.pop()
+
+    # Throws false to signal the sending of the remaining buffer contents.
+    yield False
+    yield bin_string_buffer_list[0]
+
+
+def tac_python(file_path: str) -> None:
+    global MEGABYTE_IN_BYTES, CHUNK_SIZE, CHUNK_SIZE_IN_BYTES
+
+    try:
+        with open(file_path, "rb") as bin_file:
+            bin_file.seek(0, io.SEEK_END)
+            bin_file.seek(bin_file.tell() - 1)
+            file_cursor_position: int = bin_file.tell()
+            string_buffer: str = b""
+
+            while file_cursor_position != 0:
+                old_file_cursor_position: int = bin_file.tell()
+                file_cursor_position = max(0, file_cursor_position - CHUNK_SIZE)
+
+                bin_file.seek(file_cursor_position)
+                string_buffer = bin_file.read(old_file_cursor_position - file_cursor_position) + string_buffer
+                bin_file.seek(file_cursor_position)
+
+                for word in (str_gen := process_buffer(string_buffer)):
+                    if word is not False:
+                        print(word.decode("utf-8"))
+                        continue
+                    break
+                string_buffer = next(str_gen)
+            print(string_buffer.decode("utf-8"))
+
+    except FileNotFoundError as error:
+        print("Error: File path is invalid or doesn\'t exist.")
+
+
+if __name__ == '__main__':
+    tac_python(os.path.join(os.path.dirname(__file__), "1GB.txt"))
