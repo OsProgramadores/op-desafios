3c2957aa0f4ff91dfedbaa9531a361e4fd1fa567
joseildoandrade12 <joseildoandrade1405@gmail.com>
2024-11-18 15:16:08 -0300

    correção de indentação

--- desafio-02/joseildoandrade12/javascript/primos.js
+++ desafio-02/joseildoandrade12/javascript/primos.js
@@ -1,17 +1,17 @@
 function numerosPrimos(max) {
-  const arrayNumerosPrimos = [];
-  for (let num = 2; num <= max; num++) {
-    let numeroPrimo = true;
-    for (let divisor = 2; divisor < num; divisor++) {
-      if (num % divisor === 0) {
-        numeroPrimo = false;
-      }
+    const arrayNumerosPrimos = [];
+    for (let num = 2; num <= max; num++) {
+        let numeroPrimo = true;
+        for (let divisor = 2; divisor < num; divisor++) {
+            if (num % divisor === 0) {
+                numeroPrimo = false;
+            }
+        }
+        if (numeroPrimo === true) {
+            arrayNumerosPrimos.push(num);
+        }
     }
-    if (numeroPrimo === true) {
-      arrayNumerosPrimos.push(num);
-    }
-  }
-  return arrayNumerosPrimos;
+    return arrayNumerosPrimos;
 }
 
 console.log(numerosPrimos(10000));
