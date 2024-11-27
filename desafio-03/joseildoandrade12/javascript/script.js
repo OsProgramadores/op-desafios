3140c630cd172afaf511865a5ce451bea1bc5ce0
joseildoandrade12 <joseildoandrade1405@gmail.com>
2024-11-27 02:07:34 -0300

    Correção de código com implementação de validação junto ao aumento do README

--- desafio-03/joseildoandrade12/javascript/script.js
+++ desafio-03/joseildoandrade12/javascript/script.js
@@ -1,11 +1,21 @@
-let numerosPalidromos = [];
+const numerosPalidromos = [];
 function palindromos(min, max) {
-    for (; min <= max; min++) {
-        const numInverso = min.toString().split("").reverse().join("");
-        if (min == numInverso) {
-            numerosPalidromos.push(min);
+    if (min < 1 || max < 1) {
+        console.log("Adicione apenas valores positivos ou maior que 0");
+    } else if (min >= max) {
+        console.log(
+            "O valor mínimo não pode ser maior ou igual ao valor máximo"
+        );
+    } else if (!(Number.isInteger(min) && Number.isInteger(max))) {
+        console.log("Adicione apenas números inteiros");
+    } else {
+        for (; min <= max; min++) {
+            const numInverso = +min.toString().split("").reverse().join("");
+            if (min === numInverso) {
+                numerosPalidromos.push(min);
+            }
         }
     }
 }
-palindromos(1, 2000);
+palindromos(1, 100);
 console.log(numerosPalidromos);
