ccaa719bc5d1429c768dbe252d8efb1bcda8a2c3
joseildoandrade12 <joseildoandrade1405@gmail.com>
2024-11-29 16:08:14 -0300

    implementação de código para pegar dados do usuário e correção de validação

--- desafio-03/joseildoandrade12/javascript/script.js
+++ desafio-03/joseildoandrade12/javascript/script.js
@@ -1,28 +1,53 @@
-const numerosPalidromos = [];
 function palindromos(min, max) {
+    let valueMin = min;
+    let valueMax = max;
+    const numerosPalidromos = [];
     for (; min <= max; min++) {
         const numInverso = +min.toString().split("").reverse().join("");
         if (min === numInverso) {
             numerosPalidromos.push(min);
         }
     }
+    console.log(
+        `os números palindromos entre ${valueMin} e ${valueMax} são : ${numerosPalidromos}`
+    );
     return numerosPalidromos;
 }
 
-function verificacao(min, max) {
-    if (typeof min !== "number" || typeof max !== "number") {
+function verificacao(value) {
+    if (isNaN(value)) {
         console.log("Coloque apenas valores númericos");
-    } else if (min < 1 || max < 1) {
+    } else if (value < 1) {
         console.log("Adicione apenas valores positivos ou maior que 0");
-    } else if (min >= max) {
-        console.log(
-            "O valor mínimo não pode ser maior ou igual ao valor máximo"
-        );
-    } else if (!(Number.isInteger(min) && Number.isInteger(max))) {
+    } else if (!Number.isInteger(value)) {
         console.log("Adicione apenas números inteiros");
     } else {
-        palindromos(min, max);
+        return true;
     }
 }
-console.log(verificacao(1, 50));
-console.log(numerosPalidromos);
+
+const readline = require("node:readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Informe o valor minimo: ", (min) => {
+    if (!verificacao(+min)) {
+        rl.close();
+    } else {
+        rl.question("Informe o valor máximo: ", (max) => {
+            if (!verificacao(+max)) {
+                rl.close();
+            } else if (min >= max) {
+                console.log(
+                    "O valor mínimo não pode ser maior ou igual ao valor máximo"
+                );
+                
+            } else {
+                palindromos(+min, +max);
+            }
+            rl.close();
+        });
+    }
+});
