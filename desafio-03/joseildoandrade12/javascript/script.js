65a30c6e0f056da67aa866b7f21698347c917346
joseildoandrade12 <joseildoandrade1405@gmail.com>
2024-11-28 21:17:07 -0300

    correção do código

--- desafio-03/joseildoandrade12/javascript/script.js
+++ desafio-03/joseildoandrade12/javascript/script.js
@@ -12,15 +12,12 @@ function palindromos(min, max) {
 function verificacao(min, max) {
     if (typeof min !== "number" || typeof max !== "number") {
         console.log("Coloque apenas valores númericos");
-        return;
     } else if (min < 1 || max < 1) {
         console.log("Adicione apenas valores positivos ou maior que 0");
-        return;
     } else if (min >= max) {
         console.log(
             "O valor mínimo não pode ser maior ou igual ao valor máximo"
         );
-        return;
     } else if (!(Number.isInteger(min) && Number.isInteger(max))) {
         console.log("Adicione apenas números inteiros");
         return;
@@ -28,6 +25,5 @@ function verificacao(min, max) {
         palindromos(min, max);
     }
 }
-
-verificacao(1, 200); //valores apenas para exemplo
+console.log(verificacao(1, 50));
 console.log(numerosPalidromos);
