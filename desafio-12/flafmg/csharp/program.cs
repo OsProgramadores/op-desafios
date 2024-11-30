003a82adc121374eed6ec810fc8c88261f783a3f
flaffy <flaafmg@gmail.com>
2024-11-30 02:17:25 +0000

    Desafio 12 por @flafmg.

---
+++ desafio-12/flafmg/csharp/program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Numerics;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("uso correto: dotnet run <arquivo>");
+            return;
+        }
+
+        var fileName = args[0];
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine($"arquivo não encontrado: {fileName}");
+            return;
+        }
+
+        var lines = File.ReadAllLines(fileName);
+        foreach (var line in lines)
+        {
+            if (!BigInteger.TryParse(line, out var number))
+            {
+                Console.WriteLine($"{line} false");
+                continue;
+            }
+
+            if (number <= 0)
+            {
+                Console.WriteLine($"{number} false");
+                continue;
+            }
+
+            var (isPowerOfTwo, exponent) = IsPowerOfTwo(number);
+            Console.WriteLine(isPowerOfTwo ? $"{number} true {exponent}" : $"{number} false");
+        }
+    }
+    private static (bool, int) IsPowerOfTwo(BigInteger number)
+    {
+        var exponent = 0;
+
+        while (number > 1)
+        {
+            if (number % 2 != 0)
+                return (false, -1);
+
+            number /= 2;
+            exponent++;
+        }
+
+        return (true, exponent);
+    }
+}
