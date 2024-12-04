15563446f5f79f9278ca894ff380f70fa676d43a
flaffy <flaafmg@gmail.com>
2024-12-04 16:06:53 -0300

    Aplica revisões

--- desafio-12/flafmg/csharp/program.cs
+++ desafio-12/flafmg/csharp/program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 using System.Numerics;
-
+using System.Security;
 public class Program
 {
     private static void Main(string[] args)
@@ -19,13 +19,25 @@ public class Program
             return;
         }
 
-        try
+        try//Adiciona os outros tratamentos de erro, exceto os que já são tratados pelo !File.Exists
         {
             ProcessFile(fileName);
         }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"erro: {ex.Message}");
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("erro: acesso negado ao arquivo");
+        }
+        catch (SecurityException)
+        {
+            Console.WriteLine("erro: sem permissões para acessar o arquivo");
+        }
         catch (IOException ex)
         {
-            Console.WriteLine($"erro ao processar o arquivo: {ex.Message}");
+            Console.WriteLine($"erro: {ex.Message}");
         }
     }
 
@@ -46,24 +58,24 @@ public class Program
                 continue;
             }
 
-            var (isPowerOfTwo, exponent) = IsPowerOfTwo(number);
-            Console.WriteLine(isPowerOfTwo ? $"{number} true {exponent}" : $"{number} false");
+            var result = IsPowerOfTwo(number);
+            Console.WriteLine(result.IsPowerOfTwo ? $"{number} true {result.Exponent}" : $"{number} false");
         }
     }
-
-    private static (bool, int) IsPowerOfTwo(BigInteger number)
+    private static PowerOfTwoResult IsPowerOfTwo(BigInteger number)//Nodificado para usar record
     {
         var exponent = 0;
 
         while (number > 1)
         {
             if (number % 2 != 0)
-                return (false, -1);
+                return new PowerOfTwoResult(false, -1);
 
             number /= 2;
             exponent++;
         }
 
-        return (true, exponent);
+        return new PowerOfTwoResult(true, exponent);
     }
 }
+public record PowerOfTwoResult(bool IsPowerOfTwo, int Exponent);
