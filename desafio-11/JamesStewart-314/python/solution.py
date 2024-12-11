cad40fc34f28567fe2d7233ab1b4324503fe9f4d
Allber Fellype <133912146+JamesStewart314@users.noreply.github.com>
2024-12-11 20:32:28 -0300

    Correções sugeridas

--- desafio-11/JamesStewart-314/python/solution.py
+++ desafio-11/JamesStewart-314/python/solution.py
@@ -1,16 +1,22 @@
 import os
+import math
 from itertools import count
 from collections import defaultdict
 from typing import Generator
 
-def sieve_of_eratosthenes_prime_generator() -> Generator[int, None, None]:
-    yield 2
+def get_set_primes(num_digits: int, upper_limit: int) -> set[str]:
+    primes_set: set[str] = set()
+    current_num: str = '2'
+    primes_set.update(current_num.zfill(z_qty) for z_qty in range(1, num_digits))
     sieve_dict: dict[int, int] = {}
     num_counter: count = count(start=3, step=2)
     for current_num in num_counter:
+        if current_num >= upper_limit:
+            return primes_set
         if current_num not in sieve_dict:
-            yield current_num
             sieve_dict[current_num * current_num] = current_num
+            current_num: str = str(current_num)
+            primes_set.update(current_num.zfill(z_qty) for z_qty in range(1, num_digits))
         else:
             current_num_key: int = current_num + (sieve_dict[current_num] << 1)
             while sieve_dict.get(current_num_key):
@@ -19,16 +25,21 @@ def sieve_of_eratosthenes_prime_generator() -> Generator[int, None, None]:
             del sieve_dict[current_num]
 
 
-def find_biggest_seq(pi_digits: str, upper_limit: int | float = 1e4) -> str:
-    prime_numbers: set[str] = set()
-    for prime_number in sieve_of_eratosthenes_prime_generator():
-        if prime_number >= upper_limit:
-            break
-        str_num: str = str(prime_number)
-        prime_numbers.update(str_num.zfill(z_qty) for z_qty in range(1, 5))
+def print_biggest_seq(file_path: str, upper_limit: int = 10 ** 4) -> str:
+    try:
+        pi_digits: str = open(file_path, "r").readline().rstrip()
+    except FileNotFoundError as error:
+        raise Exception(f"Error: Could open file \'{file_path}\'.") from error
+    
+    try:
+        pi_digits: str = pi_digits[pi_digits.index('.') + 1:]
+    except ValueError:
+        pass
+
+    prime_numbers: set[str] = get_set_primes(int(math.log10(upper_limit)) + 1, upper_limit)
 
     prim_seq: defaultdict[int, str] = defaultdict(str)
-    for idx in range(2, len(pi_digits)):
+    for idx in range(len(pi_digits)):
         current_num: str = ""
         for qty in range(4):
             try:
@@ -40,11 +51,8 @@ def find_biggest_seq(pi_digits: str, upper_limit: int | float = 1e4) -> str:
             if len(prim_seq[(t := s + 1)]) < len(prim_seq[idx]) + len(current_num):
                 prim_seq[t] = prim_seq[idx] + current_num
 
-    return max(prim_seq.values(), key=len, default='')
+    print(max(prim_seq.values(), key=len, default=''))
 
 if __name__ == '__main__':
     file_path: str = os.path.join(os.path.dirname(__file__), "pi-1M.txt")
-    try:
-        print(find_biggest_seq(open(file_path, "r").readline().rstrip()))
-    except FileNotFoundError as error:
-        raise Exception(f"Error: Could open file \'{file_path}\'.") from error
+    print_biggest_seq(file_path)
