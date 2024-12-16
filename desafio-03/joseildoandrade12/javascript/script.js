dac4da272b5918fe44f9ea3bd8dcb347ace093f0
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2024-12-15 23:17:07 -0300

    conclusão desafio 03 por @joseildoandrade12 em javascript (#1163)
    * Desafio 03 feito em javascript por @joseildoandrade12
    
    * Correção de código com implementação de validação junto ao aumento do README
    
    * correção de indentação
    
    * Correção do código e README
    
    * correção do código
    
    * retirada dos return
    
    * implementação de código para pegar dados do usuário e correção de validação
    
    * Correção de indentação
    
    * correção de erros mostrados no pipeline
    
    * Correção de README
    
    * Correção de README
    
    * Ajustes no README
    
    * Correção README
    
    * Update README.md
    
    * correção de código e readme
    
    * correção de virgula
    
    * Correção de frase que chama os valores

---
+++ desafio-03/joseildoandrade12/javascript/script.js
@@ -0,0 +1,63 @@
+function palindromos(min, max) {
+    const valueMin = min;
+    const valueMax = max;
+    const numerosPalidromos = [];
+    for (; min <= max; min++) {
+        const numInverso = +min.toString().split("").reverse().join("");
+        if (min === numInverso) {
+            numerosPalidromos.push(min);
+        }
+    }
+    console.log(
+        `os números palindromos entre ${valueMin} e ${valueMax} são : ${numerosPalidromos}`
+    );
+    return numerosPalidromos;
+}
+
+function verificacao(value) {
+    if (isNaN(value)) {
+        console.log("Digite apenas valores númericos");
+        return false;
+    } else if (value < 1) {
+        console.log("Digite apenas valores maiores que 0");
+        return false;
+    } else if (!Number.isInteger(value)) {
+        console.log("Digite apenas números inteiros");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const readline = require("node:readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function valorMin() {
+    rl.question("Informe o valor minimo: ", (min) => {
+        if (!verificacao(+min)) {
+            valorMin();
+        } else {
+            valorMax(min);
+        }
+    });
+}
+
+function valorMax(min) {
+    rl.question("Informe o valor máximo: ", (max) => {
+        if (!verificacao(+max)) {
+            valorMax(min);
+        } else if (min >= max) {
+            console.log(
+                "O valor mínimo não pode ser maior ou igual ao valor máximo"
+            );
+            valorMax(min);
+        } else {
+            palindromos(+min, +max);
+            rl.close();
+        }
+    });
+}
+valorMin();
