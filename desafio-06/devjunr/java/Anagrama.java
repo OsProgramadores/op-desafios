5967f428d3475bc2c1dff395597eed66487a9494
Adriano Jr <adrianojunr@gmail.com>
2024-12-30 18:34:45 -0300

    Move a leitura do arquivo para o main

--- desafio-06/devjunr/java/Anagrama.java
+++ desafio-06/devjunr/java/Anagrama.java
@@ -12,24 +12,18 @@ public class Anagrama{
         }
     }
 
-    public static void lerPalavrasValidas(){
-        try {
-            Path caminho = Paths.get("words.txt");
-            List<String> linhas = Files.readAllLines(caminho);
-            System.out.println(linhas);
-        } catch (java.nio.file.NoSuchFileException e) {
-            System.out.println("Erro: Não foi possivel encontrar o arquivo words.txt");
-        } catch (Exception e){
-            System.out.println("Erro: " + e);
-        }
-    }
-
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite a palavra ou expressão: ");
         String expressao = sc.nextLine();
         expressao = expressao.toUpperCase();
-        lerPalavrasValidas();
         validaEntrada(expressao);
+        try{
+            Path caminho = Paths.get("words.txt");
+            List<String> palavras = Files.readAllLines(caminho);
+            System.out.println(palavras);
+        }catch(Exception e){
+            System.out.println("> Erro ao ler o arquivo. " + e);
+        }
     }
 }
