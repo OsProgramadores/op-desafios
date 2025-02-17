275c771df8628eda0942213bda51f5dc6cc49aff
Sankha20 <victor.pcs@outlook.com>
2025-02-16 23:18:15 -0300

    Resolução do desafio-02 de @Sankha20

---
+++ desafio-02/sankha20/python/README.md
@@ -0,0 +1,2 @@
+# Desafio 02 - Primos
+Lista todos os números primos até 10.000
---
+++ desafio-02/sankha20/python/desafio-02.py
@@ -0,0 +1,23 @@
+primes = [2]
+def is_prime(n: int) -> bool:    
+    if n in primes: return True
+    if n < 2: return False
+    if n % 2 == 0: return False
+    
+    for prime in primes:
+        if prime > n ** 0.5:
+            break
+
+        if n % prime == 0:
+            return False
+    return True
+
+
+def generate_primes_list(n: int) -> list:
+    for i in range(3, n):
+        if is_prime(i):
+            primes.append(i)
+
+
+generate_primes_list(10000)
+print("\n".join(map(str, primes)))
