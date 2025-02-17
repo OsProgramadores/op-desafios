7ab06794e39cee1c18ad2f286f278eff955e874b
Sankha20 <victor.pcs@outlook.com>
2025-02-17 02:02:38 -0300

    Removendo todas as linhas e espaços

--- desafio-02/sankha20/python/desafio-02.py
+++ desafio-02/sankha20/python/desafio-02.py
@@ -4,21 +4,15 @@ def is_prime(n: int) -> bool:
         return True
     if n < 2 or n % 2 == 0:
         return False
-
     for prime in primes:
         if prime > n ** 0.5:
             break
-
         if n % prime == 0:
             return False
     return True
-
-
 def generate_primes_list(n: int) -> list:
     for i in range(3, n):
         if is_prime(i):
             primes.append(i)
-
-
 generate_primes_list(10000)
 print("\n".join(map(str, primes)))
