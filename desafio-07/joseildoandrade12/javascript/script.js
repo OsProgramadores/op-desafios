e906e8cae67791950575a54c0a2ef85a538543c4
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2025-02-19 01:47:58 -0300

    Conclusão do desafio-07 (#1182)
    * Conclusão do desafio-07
    
    * correção eslint
    
    * correção do desafio 07
    
    * correção loop
    
    * Correção impressão de linhas e retirada da validação tac
    
    * correção de md5sum
    
    * exclusão de linha indiferente da solução
    
    * adição de ultima linha no readme

---
+++ desafio-07/joseildoandrade12/javascript/script.js
@@ -0,0 +1,82 @@
+const fs = require("fs");
+const readline = require("readline");
+
+async function commandInline() {
+    const command = process.argv.slice(2);
+    if (command[0]) {
+        try {
+            const quantityLines = await countingLines(command[0]);
+            await pushLines(command[0], quantityLines);
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+        console.log("Use: node script.js <caminho_do_arquivo>");
+    }
+}
+commandInline();
+
+async function pushLines(path, quantityLines) {
+    const size = 512000;
+    for (let finallyLine = quantityLines; finallyLine > 0; finallyLine -= size) {
+        try {
+            const initLines = Math.max(finallyLine - size + 1, 1);
+            const arrayLines = await pullData(path, initLines, finallyLine);
+            arrayLines.reverse();
+            console.log(arrayLines.join("\n"));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function pullData(path, initLines, finallyLine) {
+    return new Promise((resolve, reject) => {
+        const arrayItens = [];
+        let indexLine = 0;
+        const fileStream = fs.createReadStream(path);
+        const reader = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+
+        reader.on("error", (err) => {
+            console.log("Arquivo não encontrado!");
+            reject(err);
+        });
+
+        reader.on("line", (line) => {
+            indexLine++;
+            if (indexLine >= initLines && indexLine <= finallyLine) {
+                arrayItens.push(line);
+            }
+        });
+
+        reader.on("close", () => {
+            resolve(arrayItens);
+        });
+    });
+}
+
+async function countingLines(path) {
+    return new Promise((resolve, reject) => {
+        let quantityLine = 0;
+        const fileStream = fs.createReadStream(path);
+        const reader = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+        reader.on("error", (err) => {
+            console.log("Arquivo não encontrado!");
+            reject(err);
+        });
+
+        reader.on("line", () => {
+            quantityLine++;
+        });
+
+        reader.on("close", () => {
+            resolve(quantityLine);
+        });
+    });
+}
