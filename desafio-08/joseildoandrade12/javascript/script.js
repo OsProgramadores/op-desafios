720d7dcaa6e09526330ee57e624620257e71b687
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-04-02 19:59:47 -0300

    correção de identação

--- desafio-08/joseildoandrade12/javascript/script.js
+++ desafio-08/joseildoandrade12/javascript/script.js
@@ -2,30 +2,30 @@ const readline = require("readline");
 const fs = require("fs");
 
 async function commandInline() {
-	const command = process.argv.slice(2);
-	if (command[0]) {
-		try {
-		readFileByLine(command[0]);
-		} catch (erro) {
-		console.log(erro);
-		}
-	} else {
-		console.log("Use: node script.js <caminho_do_arquivo>");
-	}
+    const command = process.argv.slice(2);
+    if (command[0]) {
+        try {
+            readFileByLine(command[0]);
+        } catch (erro) {
+            console.log(erro);
+        }
+    } else {
+        console.log("Use: node script.js <caminho_do_arquivo>");
+    }
 }
 commandInline();
 
 async function readFileByLine(file) {
-	const fileStream = fs.createReadStream(file);
-	const rl = readline.createInterface({
-		input: fileStream,
-		crlfDelay: Infinity,
-	});
+    const fileStream = fs.createReadStream(file);
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
 
-	rl.on("line", (line) => {
-		const arrayValues = valueReset(line);
-		console.log(validateValues(arrayValues));
-	});
+    rl.on("line", (line) => {
+        const arrayValues = valueReset(line);
+        console.log(validateValues(arrayValues));
+    });
 }
 
 function valueReset(value) {
@@ -33,31 +33,31 @@ function valueReset(value) {
 }
 
 function validateValues(value) {
-	if (value.length === 1) return value[0];
-	if (+value[0] === 0 || +value[1] === 0) return "ERR";
-	return divisionValues(value);
+    if (value.length === 1) return value[0];
+    if (+value[0] === 0 || +value[1] === 0) return "ERR";
+    return divisionValues(value);
 }
 
 function divisionValues(value) {
-	const remainderDivision = value[0] % value[1] === 0;
-	if (remainderDivision) {
-		return value[0] / value[1];
-	}
-	const arraySort = [...value].sort();
-	const maximumDivisor = maximumDivisorCommon(arraySort[0], arraySort[1]);
-	const simplifyFraction = `${value[0] / maximumDivisor}/${value[1] / maximumDivisor}`;
-	return maximumDivisor !== 1 ? simplifyFraction : calculateMixedFraction(value);
+    const remainderDivision = value[0] % value[1] === 0;
+    if (remainderDivision) {
+        return value[0] / value[1];
+    }
+    const arraySort = [...value].sort();
+    const maximumDivisor = maximumDivisorCommon(arraySort[0], arraySort[1]);
+    const simplifyFraction = `${value[0] / maximumDivisor}/${value[1] / maximumDivisor}`;
+    return maximumDivisor !== 1 ? simplifyFraction : calculateMixedFraction(value);
 }
 
 function maximumDivisorCommon(value1, value2) {
-	const remainderDivision = value1 % value2;
-	if (remainderDivision === 0) return value2;
-	return maximumDivisorCommon(value2, remainderDivision);
+    const remainderDivision = value1 % value2;
+    if (remainderDivision === 0) return value2;
+    return maximumDivisorCommon(value2, remainderDivision);
 }
 
 function calculateMixedFraction(value) {
-	const division = Math.floor(value[0] / value[1]);
-	const remainderDivision = value[0] % value[1];
-	if (division === 0) return `${value[0]}/${value[1]}`;
-	return `${division} ${remainderDivision}/${value[1]}`;
+    const division = Math.floor(value[0] / value[1]);
+    const remainderDivision = value[0] % value[1];
+    if (division === 0) return `${value[0]}/${value[1]}`;
+    return `${division} ${remainderDivision}/${value[1]}`;
 }
