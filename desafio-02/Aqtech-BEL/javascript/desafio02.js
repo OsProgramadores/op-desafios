75fbff9f19efa22baaaeed1ffbed6f6e61d33446
Aqtech-BEL <aqtech.pe@gmail.com>
2025-04-13 10:24:22 -0300

    Adiciona novamente a pasta javascript como diretório comum

---
+++ desafio-02/Aqtech-BEL/javascript/desafio02.js
@@ -0,0 +1,18 @@
+const numbers = [];
+for (let i = 1; i <= 10000; i++) {
+    numbers.push(i);
+};
+
+const cousinNumber = numbers.filter((num) => {
+    if (num < 2) {
+        return false;
+    }       
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }    
+    return true;
+});
+
+console.log(cousinNumber); 
