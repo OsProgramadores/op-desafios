520428524187b3b1ba51eb5909b076e30800e664
Aqtech-BEL <aqtech.pe@gmail.com>
2025-04-13 11:33:42 -0300

    Removi os espaços e adicionei uma no linha ao final do código

--- desafio-02/Aqtech-BEL/javascript/desafio02.js
+++ desafio-02/Aqtech-BEL/javascript/desafio02.js
@@ -6,13 +6,12 @@ for (let i = 1; i <= 10000; i++) {
 const cousinNumber = numbers.filter((num) => {
     if (num < 2) {
         return false;
-    }       
+    }
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
             return false;
         }
-    }    
+    }
     return true;
 });
-
-console.log(cousinNumber); 
+console.log(cousinNumber);
