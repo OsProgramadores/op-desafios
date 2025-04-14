347f85d5d5487192fc67f3aea4d884aecadb4597
Albéres Queiroz <aqtech.pe@gmail.com>
2025-04-14 11:11:22 -0300

    Desafio 03 - Javascript (#1206)

---
+++ desafio-03/Aqtech-BEL/javascript/desafio03.js
@@ -0,0 +1,18 @@
+const numbers = [];
+for (let i = 1; i <= 100; i++) {
+    numbers.push(i);
+}
+
+const palindromicos = numbers.filter((num) => {
+    const original = num;
+    let reversed = 0;
+
+    while (num > 0) {
+        const digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+
+    return original === reversed;
+});
+console.log(palindromicos);
