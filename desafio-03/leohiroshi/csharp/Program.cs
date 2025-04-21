78303acceebcc6db73ecd8d43fcf00643ca162d6
leohiroshi <leonardohiroshisilva@gmail.com>
2025-04-21 18:44:42 -0300

    Desafio 03 concluido por @leohiroshi

---
+++ desafio-03/leohiroshi/csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Palindromos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] palindromo;
+            Console.WriteLine("Escreva o primeiro número: ");
+            int num01 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva o segundo número: ");
+            int num02 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Os números {num01} e {num02} são legais!");
+            Console.ReadLine();
+
+        }
+    }
+}
