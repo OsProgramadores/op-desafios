ae02848a5f234a52da871684fcf76f2d3e519898
Karine Ferreira de Brito <karineferreiradebrito@gmail.com>
2025-05-03 12:56:24 -0300

    Formatação do arquivo

--- desafio-02/KarineFBrito/java/Primo.java
+++ desafio-02/KarineFBrito/java/Primo.java
@@ -1,14 +1,16 @@
-public class Primo{
-    public static void main(String[]args){
-        for(int num = 2; num<=10000; num++){
+public class Primo {
+
+    public static void main(String[] args) {
+        for (int num = 2; num <= 10000; num++) {
             boolean ehPrimo = true;
             for (int i = 2; i < num; i++) {
                 if (num % i == 0) {
                     ehPrimo = false;
                     break;
-                } 
-            } if(ehPrimo) {
-              System.out.println(num);
+                }
+            }
+            if (ehPrimo) {
+                System.out.println(num);
             }
         }
     }
