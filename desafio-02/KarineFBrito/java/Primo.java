0dd71e91938d1ead728c641973bd9ebcc40c6767
Karine Ferreira de Brito <karineferreiradebrito@gmail.com>
2025-05-06 19:01:34 -0300

    Melhorado arquivo Primos

--- desafio-02/KarineFBrito/java/Primo.java
+++ desafio-02/KarineFBrito/java/Primo.java
@@ -3,7 +3,7 @@ public class Primo {
     public static void main(String[] args) {
         for (int num = 2; num <= 10000; num++) {
             boolean ehPrimo = true;
-            for (int i = 2; i < num; i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
                     ehPrimo = false;
                     break;
