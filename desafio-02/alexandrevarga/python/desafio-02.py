9f35e15eece4862c60bcdc118719eae54d0e093b
Alexandre <alexandrevarga1@gmail.com>
2025-05-25 20:24:38 -0300

    desafio-02_alexandrevarga

---
+++ desafio-02/alexandrevarga/python/desafio-02.py
@@ -0,0 +1,16 @@
+def encontrar_primos(limite_inferior, limite_superior):
+    primos = []
+    for num in range(limite_inferior, limite_superior + 1):
+        if num > 1:  # Números primos
+            primo = True
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    primo = False
+                    break
+            if primo: 
+                primos.append(num)
+    return primos
+
+# Encontrar números primos entre 1 e 10000
+primos = encontrar_primos(1, 10000)
+print(primos)
