f19f8ebf8c4110603258ecf8ac20c1cce93d9872
Karine Ferreira de Brito <karineferreiradebrito@gmail.com>
2025-06-06 18:49:22 -0300

    Código do desafio 7

--- desafio-07/KarineFBrito/java/tac.java
+++ desafio-07/KarineFBrito/java/tac.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class tac {
   public static void main(String[] args) {
     if (args.length == 0) {
@@ -15,22 +18,38 @@ public class tac {
     }
     try (RandomAccessFile aq = new RandomAccessFile(caminho, "r")) {
       long tamanho = aq.length();
-      StringBuilder linha = new StringBuilder();
+      List <Byte> linha = new ArrayList<>();
       for (long i = tamanho - 1; i >= 0; i--) {
         aq.seek(i);
         int c = aq.read();
         if (c == '\n') {
-          if(linha.length() != 0){
-            System.out.println(linha.reverse().toString());
-            linha.setLength(0);
-          }  
-        } else if (c != '\r') {
-          linha.append((char) c);
+          if(linha.size() != 0){
+            Collections.reverse(linha);
+            byte[] array = new byte[linha.size()];
+
+            for (int j = 0; j < linha.size(); j++) {
+              array[j] = linha.get(j);
+            }
+
+            System.out.print(new String(array, StandardCharsets.UTF_8));
+            linha.clear();
+          }
+                     linha.add((byte) c);
+ 
+          
+        } else{
+          linha.add((byte) c);
+        } 
+      } 
+      if (linha.size() > 0) {
+        byte[] array = new byte[linha.size()];
+        Collections.reverse(linha);
+        for (int j = 0; j < linha.size(); j++) {
+              array[j] = linha.get(j);
         }
+        System.out.print(new String(array, StandardCharsets.UTF_8));
       }
-      if (linha.length() > 0) {
-        System.out.println(linha.reverse().toString());
-      }
+
     } catch (IOException e) {
       e.printStackTrace();
     }
