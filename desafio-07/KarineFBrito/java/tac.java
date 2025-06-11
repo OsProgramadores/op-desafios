b7d3a26592890b66ce59e5b714550171aa8934df
Karine Ferreira de Brito <karineferreiradebrito@gmail.com>
2025-06-11 17:59:53 -0300

    Código do desafio 7

--- desafio-07/KarineFBrito/java/tac.java
+++ desafio-07/KarineFBrito/java/tac.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class tac {
+public class Tac {
   public static void main(String[] args) {
     if (args.length == 0) {
       System.out.println("Nenhum caminho foi fornecido");
@@ -17,12 +17,18 @@ public class tac {
       System.out.println("Arquivo não encontrado.");
       return;
     }
+    int tam = 4096;
     try (RandomAccessFile aq = new RandomAccessFile(caminho, "r")) {
       long tamanho = aq.length();
-      byte[] buffer = new byte[(int) tamanho];
-      aq.readFully(buffer);
+      byte[] buffer = new byte[tam];
       List<Byte> linha = new ArrayList<>();
-      for (int i = buffer.length - 1; i >= 0; i--) {
+       while (tamanho > 0) {
+        int aLer = (int) Math.min(tam, tamanho);
+        tamanho -= aLer;
+        aq.seek(tamanho);
+        aq.readFully(buffer, 0, aLer);
+
+      for (int i = aLer - 1; i >= 0; i--) {
         byte c = buffer[i];
         if (c == '\n') {
           if (linha.size() != 0) {
@@ -32,12 +38,14 @@ public class tac {
             for (int j = 0; j < linha.size(); j++) {
               array[j] = linha.get(j);
             }
-            System.out.println(new String(array, StandardCharsets.UTF_8));
+            System.out.print(new String(array, StandardCharsets.UTF_8));
             linha.clear();
           }
+          linha.add(c);
         } else {
           linha.add((byte) c);
         }
+      }
       }
       if (!linha.isEmpty()) {
         Collections.reverse(linha);
@@ -45,7 +53,7 @@ public class tac {
         for (int j = 0; j < linha.size(); j++) {
           array[j] = linha.get(j);
         }
-        System.out.println(new String(array, StandardCharsets.UTF_8));
+        System.out.print(new String(array, StandardCharsets.UTF_8));
       }
     } catch (IOException e) {
       e.printStackTrace();
