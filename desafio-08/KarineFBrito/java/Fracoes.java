7560c4050cc0bdbb1352025a3393b187514fbdda
Karine Ferreira de Brito <karineferreiradebrito@gmail.com>
2025-06-13 21:03:44 -0300

    Código do desafio 8

--- desafio-08/KarineFBrito/java/Fracoes.java
+++ desafio-08/KarineFBrito/java/Fracoes.java
@@ -20,46 +20,47 @@ public class Fracoes {
       String linha;
       while ((linha = aq.readLine()) != null) {
         if (linha.isEmpty()) {
-            continue;
+          continue;
         }
         String[] partes = linha.split("/");
         int numerador;
         int denominador;
         if (partes.length == 1) {
-            numerador = Integer.parseInt(partes[0]);
-            denominador = 1;
+          numerador = Integer.parseInt(partes[0]);
+          denominador = 1;
         } else {
-            numerador = Integer.parseInt(partes[0]);
-            denominador = Integer.parseInt(partes[1]);
+          numerador = Integer.parseInt(partes[0]);
+          denominador = Integer.parseInt(partes[1]);
         }
-        if (denominador == 0){
-            System.out.println("ERR");
-            continue;
+        if (denominador == 0) {
+          System.out.println("ERR");
+          continue;
         }
         int divisor = mdc(numerador, denominador);
         numerador /= divisor;
         denominador /= divisor;
         if (denominador == 1) {
           System.out.println(numerador);
-        } else if (numerador >= denominador){
-            int inteiro = numerador/denominador;
-            int resto = numerador%denominador;
-            if (resto == 0){
-                System.out.println(inteiro);
-            }else{
-                System.out.println(inteiro + " " + resto + "/" + denominador);
-            }
-        } else{
-            System.out.println(numerador + "/" + denominador);
+        } else if (numerador >= denominador) {
+          int inteiro = numerador / denominador;
+          int resto = numerador % denominador;
+          if (resto == 0) {
+            System.out.println(inteiro);
+          } else {
+            System.out.println(inteiro + " " + resto + "/" + denominador);
+          }
+        } else {
+          System.out.println(numerador + "/" + denominador);
         }
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
+
   public static int mdc(int a, int b) {
-    if (b == 0){
-        return a;
+    if (b == 0) {
+      return a;
     }
     return mdc(b, a % b);
   }
