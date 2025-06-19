8e6afbc3c2663bc5e5f2ab4899dec38aa4a4954c
Alexandre <alexandrevarga1@gmail.com>
2025-06-19 02:45:21 -0300

    Arquivo de teste atualizado

--- desafio-03/alexandrevarga/python/teste_palindromos.py
+++ desafio-03/alexandrevarga/python/teste_palindromos.py
@@ -5,13 +5,13 @@ Utiliza pytest para validar as funções de identificação de números palíndr
 
 import numeros_palindromos
 
-def test_is_palindromo():
-    """Testa a função is_palindromo com diferentes casos."""
-    assert numeros_palindromos.is_palindromo(121) is True
-    assert numeros_palindromos.is_palindromo(123) is False
-    assert numeros_palindromos.is_palindromo(1) is True
-    assert numeros_palindromos.is_palindromo(22) is True
-    assert numeros_palindromos.is_palindromo(10) is False
+def test_eh_palindromo():
+    """Testa a função eh_palindromo com diferentes casos."""
+    assert numeros_palindromos.eh_palindromo(121) is True
+    assert numeros_palindromos.eh_palindromo(123) is False
+    assert numeros_palindromos.eh_palindromo(1) is True
+    assert numeros_palindromos.eh_palindromo(22) is True
+    assert numeros_palindromos.eh_palindromo(10) is False
 
 def test_palindromos_entre():
     """Testa a função palindromos_entre para diferentes intervalos."""
