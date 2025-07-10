dba49d0feb4b2afea32edb6a20d7ccdf108d8bfd
KarineFBrito <karineferreiradebrito@gmail.com>
2025-07-10 00:02:15 +0000

    Ajuste do código e README do desafio 9

--- desafio-09/KarineFBrito/java/BigBase.java
+++ desafio-09/KarineFBrito/java/BigBase.java
@@ -12,7 +12,7 @@ public class BigBase {
         if (args.length != 1) { // Verifico se possui um caminho dado pelo o usuario
             System.out.println("Nenhum caminho foi fornecido,  execute o programa usando 'java bigbase <caminho-absoluto>'");
             return;
-        } 
+        }
         File caminho = new File(args[0]);
         if (!caminho.exists()) { //Verifico se o caminho passado existe
             System.out.println("Arquivo não encontrado.");
@@ -22,20 +22,20 @@ public class BigBase {
             String linha;
             BigInteger limiteMax = converter(Limite, 62); // Transforma a string limite em um numero decimal
 
-            while ((linha = aq.readLine()) != null) { // Verifico se a linha não está vazia, se não estiver ele continua rodando; 
+            while ((linha = aq.readLine()) != null) { // Verifico se a linha não está vazia, se não estiver ele continua rodando;
                 String[] partes = linha.split(" ");
                 if (partes.length != 3) { // Divido a linha em três partes Base_Entrada, Base_Saída e Numero
                     System.out.println("???");
                     continue;
                 }
-                int Base_Entrada = Integer.parseInt(partes[0]); 
+                int Base_Entrada = Integer.parseInt(partes[0]);
                 int Base_Saida = Integer.parseInt(partes[1]);
-                String numero = partes[2];        
-                if ( negativo (numero) || Base_Entrada < Base_Min || Base_Entrada > Base_Max || Base_Saida < Base_Min || Base_Saida > Base_Max) { //Verifico se o numero e negativo e se as bases se estão maior ou menor do permitido 
+                String numero = partes[2];
+                if ( negativo (numero) || Base_Entrada < Base_Min || Base_Entrada > Base_Max || Base_Saida < Base_Min || Base_Saida > Base_Max) { //Verifico se o numero e negativo e se as bases se estão maior ou menor do permitido
                     System.out.println("???");
                     continue;
                 }
-                BigInteger decimal = converter(numero, Base_Entrada); //Converto para a base 10      
+                BigInteger decimal = converter(numero, Base_Entrada); //Converto para a base 10
                 if(decimal == null ||  decimal.compareTo(limiteMax) > 0 ){ // Verifico se o numero decimal não e nulo e verifico se está no limite
                     System.out.println("???");
                     continue;
@@ -47,17 +47,17 @@ public class BigBase {
              e.printStackTrace();
         }
     }
-    
+
     static boolean negativo(String numero) { // Uso esse método para verificar se o numero e negativo
         return numero.startsWith("-"); //verifica se o numero inicia com o sinal negativo (-)
     }
 
     public static BigInteger converter(String numero, int Base_Entrada) { // Uso esse método para converter os numeros com a Base_Entrada para base decimal(10)
-        BigInteger resultado = BigInteger.ZERO; 
+        BigInteger resultado = BigInteger.ZERO;
         BigInteger base = BigInteger.valueOf(Base_Entrada); ///base recebe o valor de Base_Entrada
         for (int i = 0; i <  numero.length(); i++) { // enquanto i for menor que o tamanho do numero, o laço vai rodar
             char c = numero.charAt(i); // Pega um caracter por vez do numero
-            int valor = Valor_Digito(c); 
+            int valor = Valor_Digito(c);
             if (valor < 0 || valor >= Base_Entrada) { // verifica se o Digito e valido
                 return null;
             }
@@ -70,15 +70,15 @@ public class BigBase {
              return "0";
         }
         StringBuilder resultado = new StringBuilder();
-        BigInteger baseBig = BigInteger.valueOf(base); 
+        BigInteger baseBig = BigInteger.valueOf(base);
         while (numero.compareTo(BigInteger.ZERO) > 0) { // Verifico se o numero e maior que zero(0)
             BigInteger[] divmod = numero.divideAndRemainder(baseBig); // Aqui estou fazendo o calculo da divisao completa e retornando o quociente e o resto
             int resto = divmod[1].intValue(); //Aqui estou pegando o resto da divisão e salvando no resto do tipo inteiro
-            resultado.append(digitos.charAt(resto)); //Estou convertendo o resultado(resto da divisao) para uma base textual  
+            resultado.append(digitos.charAt(resto)); //Estou convertendo o resultado(resto da divisao) para uma base textual
             numero = divmod[0]; //Atualiza o valor do numero para o quociente da divisão
         }
         return resultado.reverse().toString();
-    } 
+    }
 
     public static int Valor_Digito(char c) { //converte um caracter em valor um valor numerico
         if (c >= '0' && c <= '9') {
--- desafio-09/KarineFBrito/java/README.md
+++ desafio-09/KarineFBrito/java/README.md
@@ -1,21 +1,21 @@
-## Lógica do Código 
+## Lógica do Código
 - Ele determina valores fixos na classe como os dígitos, base máxima, base mínima e o limite
-- O código recebe o arquivo por linha de comando. Verifica se foi passado um caminho e se ele existe. 
-- Depois, em cada linha, ele faz o processo de: 
-    - separar a linha em partes para determinar base entrada, base saída e o número; 
-    - Faz as verificações(se as bases estão no limite, não possuir números negativos e nem acima do limite determinado) se uma dessas condições for verdadeira imprime a mensagem "???"; 
-    - Logo após realiza a conversão da base entrada para a base decimal (Dessa maneira fica mais fácil converter para outras bases); 
+- O código recebe o arquivo por linha de comando. Verifica se foi passado um caminho e se ele existe.
+- Depois, em cada linha, ele faz o processo de:
+    - separar a linha em partes para determinar base entrada, base saída e o número;
+    - Faz as verificações(se as bases estão no limite, não possuir números negativos e nem acima do limite determinado) se uma dessas condições for verdadeira imprime a mensagem "???";
+    - Logo após realiza a conversão da base entrada para a base decimal (Dessa maneira fica mais fácil converter para outras bases);
     - Depois desses passos o código converte a base decimal para a base saída e em seguida imprime o resultado.
 
 ## Versão que usei:
 - Java 21 (JDK 21).
 
-## Como executar o código 
+## Como executar o código
   ```
-   javac BigBase.java 
+   javac BigBase.java
   ```
 - Para compilar o código.
 ```
-java BigBase <caminho-absoluto> 
+java BigBase <caminho-absoluto>
 ```
 - Para executar o código.
