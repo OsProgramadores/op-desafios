11e7d8516eda089eaaf0401c063c89de1e47bac5
LestterX <lgs.alves@hotmail.com>
2025-07-30 21:17:13 -0300

    melhoria na formatação do código

--- desafio-02/lestterx/javascript/main.js
+++ desafio-02/lestterx/javascript/main.js
@@ -3,14 +3,19 @@ const primos = [];
 for (let i = 2; i <= 10000; i++) {
     let isPrimo = true;
     if (i % 2 !== 0) {
-        for (let st = 2; st <= Math.floor(Math.sqrt(i)); st++) {
+        const raizQuadrada = Math.floor(Math.sqrt(i))
+        for (let st = 2; st <= raizQuadrada; st++) {
             if (i % st === 0) {
                 isPrimo = false;
                 break;
             }
         }
-        if (isPrimo) { primos.push(i); };
+        if (isPrimo) {
+            primos.push(i);
+        };
     }
 }
 
-for (let i = 0; i < primos.length; i++) { console.log(`Primo Nº${i + 1}: ${primos.at(i)}`); }
+for (let i = 0; i < primos.length; i++) {
+    console.log(`Primo Nº${i + 1}: ${primos.at(i)}`);
+}
