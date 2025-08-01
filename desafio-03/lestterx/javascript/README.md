0b0a09f2738a0ae37f95758f93c1ed3e09929b26
LestterX <lgs.alves@hotmail.com>
2025-08-01 12:31:15 -0300

    obtenção de inputs do usuario e tratamento de erros; melhor descrição no readme

--- desafio-03/lestterx/javascript/README.md
+++ desafio-03/lestterx/javascript/README.md
@@ -1,15 +1,31 @@
-## Desafio 03 - Palíndromos
+## DESAFIO 03 - PALÍNDROMOS
 
-Minha ideia principal é obter uma cópia invertida o número da vez em um array, transformar
-de volta em uma string e comparar os dois para ver se são iguais invertido e original.
-
-Se for igual, ele adiciona no array e após o loop exibe na tela
+### Funcionamento
+Obtenho os argumentos de início e fim passados na execução do código e os valido antes de executar de fato a lógica. 
+Após isso, eu sigo com a lógica, passando por cada valor e criando uma cópia intertida para comparação. Se a cópia
+for igual ao original, o número é adicionado ao array Palíndromos, que os exibo na tela através de um outro FOR.
 
 ### Requisitos
-Node v20.17.0
+- NodeJS a partir da versão 20.17.0
 
 ### Execução
-Execute o código com:
+Não é preciso nenhuma dependência. Execute com:
 ```shell
-node main.js
+node main.js <arg1> <arg2>
+```
+>Se for passado nenhum ou mais que *2 valores* ou que não sejam *numéricos* um erro será informado e o script não será executado
+
+- Exemplo VÁLIDO
+```shell
+node main.js 100 1000
+```
+
+
+- Exemplos INVÁLIDOS
+```shell
+node main.js asd 1000
+```
+ou
+```shell
+node main.js 100 1000 2000
 ```
--- desafio-03/lestterx/javascript/main.js
+++ desafio-03/lestterx/javascript/main.js
@@ -1,19 +1,40 @@
-const INICIO = 10100;
-const FINAL = 12111;
-
 const palindromos = [];
 
-for (let i = INICIO; i <= FINAL; i++) {
-    let inverted = "";
-    const invertedChars = i.toString().split("").reverse();
-    for (let i = 0; i < invertedChars.length; i++) {
-        inverted += invertedChars.at(i);
-    }
-    if (String(i) === inverted) {
-        palindromos.push(i);
-    }
-}
+let INICIO;
+let FINAL;
 
-for (const p of palindromos) {
-    console.log(p);
+try {
+    const args = process.argv.slice(2);
+    if (args.length === 0 || args.length > 2) {
+        throw new Error("Forneça no máximo 2 argumentos numéricos ao executar o código");
+    }
+
+    INICIO = Number(args[0]);
+    FINAL = Number(args[1]);
+    if (isNaN(INICIO + FINAL)) {
+        throw new Error("Forneça apenas números");
+    }
+
+    for (let i = INICIO; i <= FINAL; i++) {
+        let inverted = "";
+        const chars = i.toString().split("");
+        const invertedChars = [];
+        for (let i = 0; i < chars.length; i++) {
+            invertedChars.push(chars.slice(0, chars.length - i).pop());
+        }
+        for (let i = 0; i < invertedChars.length; i++) {
+            inverted += invertedChars.at(i);
+        }
+        if (String(i) === inverted) {
+            palindromos.push(i);
+        }
+    }
+
+    for (const p of palindromos) {
+        console.log(p);
+    }
+
+    console.log(`\n${palindromos.length} palindromos encontrados entre ${INICIO} e ${FINAL}`);
+} catch (error) {
+    console.error("Um erro ocorreu: ", error.message);
 }
