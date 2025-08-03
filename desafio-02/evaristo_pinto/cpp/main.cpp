94f0315069678abbed29e51cbb37f9ca4de2babb
Evaristo Calucango <110438215+Evaristopinto16@users.noreply.github.com>
2025-08-03 18:30:04 +0300

    Update main.cpp

--- desafio-02/evaristo_pinto/cpp/main.cpp
+++ desafio-02/evaristo_pinto/cpp/main.cpp
@@ -1,36 +1,20 @@
 #include "iostream"
 #include "vector"
-
 using namespace std;
-
-
 int VerificandoEprimo(int n) {
-    
     if (n <= 1) return 0;     
     if (n <= 3) return 1;    
     if (n % 2 == 0 || n % 3 == 0) return 0;  
- 
-    for (int i = 5; i * i <= n; i += 6) {
+ for (int i = 5; i * i <= n; i += 6) {
         if (n % i == 0 || n % (i + 2) == 0) {
-            return 0;   
-        }
-    }
-    
-    return 1; 
-}
+            return 0;  }}
+    return 1; }
  void ListarPrimos(int inicio, int fim){
-
-    for (int i = inicio; i <= fim; i++)
+for (int i = inicio; i <= fim; i++)
     {
-        
-            if(VerificandoEprimo(i) == 1){
-                cout<<i;
-            }
-        
-    }
-    
-
- }
+if(VerificandoEprimo(i) == 1){
+                cout<<i;}
+    }}
 
 int main(){
     int iniciaNumero, finalNumero;
