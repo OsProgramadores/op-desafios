cb5f9fcdce9f4aeeb89a93a2dce74324fd636e32
Evaristo Calucango <110438215+Evaristopinto16@users.noreply.github.com>
2025-08-13 14:28:56 +0300

    tornando o loop mais elegível  main.cpp

--- desafio-02/Evaristopinto16/cpp/main.cpp
+++ desafio-02/Evaristopinto16/cpp/main.cpp
@@ -10,11 +10,13 @@ int VerificandoEprimo(int n) {
     if (n % 2 == 0 || n % 3 == 0) {
         return 0;
     }
-    for (int i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) {
+    
+   for (int i = 5; i * i <= n; i += 2) {
+        if (n % i == 0) {
             return 0;
         }
     }
+    
  return 1;
 }
 void ListarPrimos(int inicio, int fim) {
