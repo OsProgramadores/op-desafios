9a62014e7bb5b421f0d16e4f7474b742e05f188a
KarineFBrito <karineferreiradebrito@gmail.com>
2025-08-25 10:31:42 -0300

    Formatação do código

--- desafio-10/KarineFBrito/java/MaquinaTuring.java
+++ desafio-10/KarineFBrito/java/MaquinaTuring.java
@@ -155,3 +155,83 @@ public class MaquinaTuring {
     return fita.toString();
   }
 }
+
+class Regra {
+    int posicao;
+    String estadoAtual;
+    String simboloLido;
+    String novoSimbolo;
+    String direcao;
+    String estadoNovo;
+
+    public Regra(
+            int posicao,
+            String estadoAtual,
+            String simboloLido,
+            String novoSimbolo,
+            String direcao,
+            String estadoNovo) {
+        this.posicao = posicao;
+        this.estadoAtual = estadoAtual;
+        this.simboloLido = simboloLido;
+        this.novoSimbolo = novoSimbolo;
+        this.direcao = direcao;
+        this.estadoNovo = estadoNovo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "(%s,%s, %s -> %s, %s, %s)",
+                posicao, estadoAtual, simboloLido, novoSimbolo, direcao, estadoNovo);
+    }
+}
+
+
+class Fita {
+    List<Character> fita;
+    int posicaoCabeca = 0;
+
+    public Fita(String entrada) {
+        fita = new ArrayList<>();
+        for (char c : entrada.toCharArray()) {
+            fita.add(c);
+        }
+    }
+
+    public char ler() {
+        return fita.get(posicaoCabeca);
+    }
+
+    public void escrever(char simbolo) {
+        if (posicaoCabeca >= fita.size()) {
+            fita.add(simbolo);
+        } else {
+            fita.set(posicaoCabeca, simbolo);
+        }
+    }
+
+    public void mover(String direcao) {
+        if (direcao.equals("r")) {
+            posicaoCabeca++;
+            if (posicaoCabeca >= fita.size()) {
+                fita.add('_');
+            }
+        } else if (direcao.equals("l")) {
+            posicaoCabeca--;
+            if (posicaoCabeca < 0) {
+                fita.add(0, '_');
+                posicaoCabeca = 0;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (char c : fita) {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
