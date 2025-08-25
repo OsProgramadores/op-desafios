5aa398fd18fb24787fed18a949eb78e6e4d78be8
KarineFBrito <karineferreiradebrito@gmail.com>
2025-08-25 11:45:54 -0300

    Formatação do código

--- desafio-10/KarineFBrito/java/Fita.java
+++ desafio-10/KarineFBrito/java/Fita.java
@@ -2,49 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Fita {
-    List<Character> fita;
-    int posicaoCabeca = 0;
+  List<Character> fita;
+  int posicaoCabeca = 0;
 
-    public Fita(String entrada) {
-        fita = new ArrayList<>();
-        for (char c : entrada.toCharArray()) {
-            fita.add(c);
-        }
+  public Fita(String entrada) {
+    fita = new ArrayList<>();
+    for (char c : entrada.toCharArray()) {
+      fita.add(c);
     }
+  }
 
-    public char ler() {
-        return fita.get(posicaoCabeca);
-    }
+  public char ler() {
+    return fita.get(posicaoCabeca);
+  }
 
-    public void escrever(char simbolo) {
-        if (posicaoCabeca >= fita.size()) {
-            fita.add(simbolo);
-        } else {
-            fita.set(posicaoCabeca, simbolo);
-        }
+  public void escrever(char simbolo) {
+    if (posicaoCabeca >= fita.size()) {
+      fita.add(simbolo);
+    } else {
+      fita.set(posicaoCabeca, simbolo);
     }
+  }
 
-    public void mover(String direcao) {
-        if (direcao.equals("r")) {
-            posicaoCabeca++;
-            if (posicaoCabeca >= fita.size()) {
-                fita.add('_');
-            }
-        } else if (direcao.equals("l")) {
-            posicaoCabeca--;
-            if (posicaoCabeca < 0) {
-                fita.add(0, '_');
-                posicaoCabeca = 0;
-            }
-        }
+  public void mover(String direcao) {
+    if (direcao.equals("r")) {
+      posicaoCabeca++;
+      if (posicaoCabeca >= fita.size()) {
+        fita.add('_');
+      }
+    } else if (direcao.equals("l")) {
+      posicaoCabeca--;
+      if (posicaoCabeca < 0) {
+        fita.add(0, '_');
+        posicaoCabeca = 0;
+      }
     }
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        for (char c : fita) {
-            sb.append(c);
-        }
-        return sb.toString();
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (char c : fita) {
+      sb.append(c);
     }
+    return sb.toString();
+  }
 }
--- desafio-10/KarineFBrito/java/Regra.java
+++ desafio-10/KarineFBrito/java/Regra.java
@@ -1,30 +1,30 @@
 public class Regra {
-    int posicao;
-    String estadoAtual;
-    String simboloLido;
-    String novoSimbolo;
-    String direcao;
-    String estadoNovo;
+  int posicao;
+  String estadoAtual;
+  String simboloLido;
+  String novoSimbolo;
+  String direcao;
+  String estadoNovo;
 
-    public Regra(
-            int posicao,
-            String estadoAtual,
-            String simboloLido,
-            String novoSimbolo,
-            String direcao,
-            String estadoNovo) {
-        this.posicao = posicao;
-        this.estadoAtual = estadoAtual;
-        this.simboloLido = simboloLido;
-        this.novoSimbolo = novoSimbolo;
-        this.direcao = direcao;
-        this.estadoNovo = estadoNovo;
-    }
+  public Regra(
+      int posicao,
+      String estadoAtual,
+      String simboloLido,
+      String novoSimbolo,
+      String direcao,
+      String estadoNovo) {
+    this.posicao = posicao;
+    this.estadoAtual = estadoAtual;
+    this.simboloLido = simboloLido;
+    this.novoSimbolo = novoSimbolo;
+    this.direcao = direcao;
+    this.estadoNovo = estadoNovo;
+  }
 
-    @Override
-    public String toString() {
-        return String.format(
-                "(%s,%s, %s -> %s, %s, %s)",
-                posicao, estadoAtual, simboloLido, novoSimbolo, direcao, estadoNovo);
-    }
+  @Override
+  public String toString() {
+    return String.format(
+        "(%s,%s, %s -> %s, %s, %s)",
+        posicao, estadoAtual, simboloLido, novoSimbolo, direcao, estadoNovo);
+  }
 }
