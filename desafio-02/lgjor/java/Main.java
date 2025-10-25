4fe0ef417987831be487659877427a4071964154
Lucas Godoy <86794138+lgjor@users.noreply.github.com>
2025-10-25 12:20:20 -0300

    Refactor prime number output to use BufferedWriter
    Obrigado, eu desconhecia essa diferença entre os formatos de saída, sempre usei o print, println. Agora, pelo que pesquisei, descobri que ao usar o BufferedWriter é possível reduzir a quantidade de envios ao sistema operacional em um único envio (mais eficiente).

--- desafio-02/lgjor/java/Main.java
+++ desafio-02/lgjor/java/Main.java
@@ -1,30 +1,37 @@
-public class Main{
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-    enum LIMITS{
-        LOWER(2),
-        UPPER(10000);
-        int number;
+public class Main {
+  enum LIMITS {
+    LOWER(2),
+    UPPER(10000);
+    int number;
 
-        private LIMITS(int number){
-            this.number = number;
-        }
+    private LIMITS(int number) {
+      this.number = number;
     }
+  }
 
-    private static boolean isPrimeNumber(int number){
-        for (int i = LIMITS.LOWER.number; i * i <= number; i++) {
-            if (number%i==0){
-                return false;
-            }
-        }
-        return true;
+  private static boolean isPrimeNumber(int number) {
+    for (int i = LIMITS.LOWER.number; i * i <= number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    public static void main(String[] args) {
-        for (int i = LIMITS.LOWER.number; i <= LIMITS.UPPER.number; i++) {
-            if (isPrimeNumber(i)){
-                System.out.println(i);
-            }
+  public static void main(String[] args) {
+    try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+      for (int i = LIMITS.LOWER.number; i <= LIMITS.UPPER.number; i++) {
+        if (isPrimeNumber(i)) {
+          writer.write(String.valueOf(i));
+          writer.newLine();
         }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
-
