b6d8adb728a78703850ccee56febf9e12913b528
Natan Costa <natanscosta7@gmail.com>
2025-11-06 00:26:17 -0300

    remove unless code

--- desafio-03/NatanTheMan/php/tests/PalindromeTest.php
+++ desafio-03/NatanTheMan/php/tests/PalindromeTest.php
@@ -10,14 +10,14 @@ final class PalindromeTest extends TestCase
 {
     public function testArgsAreInts()
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Parâmetros devem ser números inteiros");
         new Palindrome("bar", "foo");
     }
 
     public function testArgsAreBiggerThan0()
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Parâmetros devem ser maiores que 0");
         new Palindrome("0", "-1");
     }
