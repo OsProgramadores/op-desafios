ea45f99e519b9f5cb899a2001f315bcc4ddb67fc
Lucas Godoy <86794138+lgjor@users.noreply.github.com>
2025-11-07 14:21:35 -0300

    Classe Palindromos refatorada conforme orientação
    Olá, obrigado pelo retorno. Alterações efetuadas:
    
    - Agora imprime apenas o resultado (Prints desnecessários removidos).
    - Lógica de validação da entrada foi movida do construtor para o método isValidInput.
    - Lógica de validação foi simplificada em uma única condição para o intervalo inicial e final.
    - Anotação @java.lang.Override foi simplificada apenas para @Override que já vem inclusa no Java.
    - Entrada de dados agora é feita pelos argumentos passados ao programa (antes havia sido implementada por método de leitura do console).

--- desafio-03/lgjor/java/Palindromos.java
+++ desafio-03/lgjor/java/Palindromos.java
@@ -1,124 +1,88 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Palindromos {
 
-    int intervaloInicial;
-    int intervaloFinal;
-    List<Integer> palindromos;
+  int intervaloInicial;
+  int intervaloFinal;
+  List<Integer> palindromos;
 
-    public Palindromos(int intervaloInicial, int intervaloFinal) {
-        if (intervaloInicial <= 0) {
-            throw new IllegalArgumentException(
-                    "O valor inicial deve ser um número inteiro positivo (maior que 0).");
-        }
+  public Palindromos(int intervaloInicial, int intervaloFinal) {
+    this.intervaloInicial = intervaloInicial;
+    this.intervaloFinal = intervaloFinal;
+    this.palindromos = encontrarPalindromos(intervaloInicial, intervaloFinal);
+  }
 
-        if (intervaloFinal <= 0) {
-            throw new IllegalArgumentException(
-                    "O valor final deve ser um número inteiro positivo (maior que 0).");
-        }
-
-        if (intervaloInicial > intervaloFinal) {
-            throw new IllegalArgumentException(
-                    "O valor inicial ("
-                            + intervaloInicial
-                            + ") não pode ser maior que o valor final ("
-                            + intervaloFinal
-                            + ").");
-        }
-
-        this.intervaloInicial = intervaloInicial;
-        this.intervaloFinal = intervaloFinal;
-        this.palindromos = encontrarPalindromos(intervaloInicial, intervaloFinal);
+  /**
+   * Metodo para validar se a entrada é válida. Caso não seja, lança exceção {@code
+   * IllegalArgumentException}
+   *
+   * @param intervaloInicial
+   * @param intervaloFinal
+   */
+  public void isValidInput(int intervaloInicial, int intervaloFinal) {
+    if (intervaloInicial <= 0 || intervaloFinal <= 0) {
+      throw new IllegalArgumentException(
+          "Os valores iniciais e finais devem ser um número inteiro positivo (maior que 0).");
     }
 
-    public static int lerInteiroPositivoMaiorQueOMinimo(String mensagem, int valorMinimo) {
-        Scanner scanner = new Scanner(System.in);
-        int numeroLido = 0;
-        boolean entradaValida = false;
+    if (intervaloInicial > intervaloFinal) {
+      throw new IllegalArgumentException(
+          "O valor inicial ("
+              + intervaloInicial
+              + ") não pode ser maior que o valor final ("
+              + intervaloFinal
+              + ").");
+    }
+  }
 
-        while (!entradaValida) {
-            System.out.print(mensagem);
-            try {
+  private boolean isPalindromo(int numeroAReverter) {
+    int original = numeroAReverter;
+    int numeroRevertido = 0;
+    while (numeroAReverter != 0) {
+      int ultimoDigito = numeroAReverter % 10; // Pega o último dígito
+      numeroRevertido = numeroRevertido * 10 + ultimoDigito; // Adiciona ao número reverso
+      numeroAReverter /= 10; // Remove o último dígito
+    }
+    return original == numeroRevertido;
+  }
 
-                if (scanner.hasNextInt()) {
-                    numeroLido = scanner.nextInt();
-
-                    if (numeroLido > 0) {
-                        if (numeroLido >= valorMinimo) {
-                            entradaValida = true;
-                        } else {
-                            System.err.println(
-                                    "Erro: O número deve ser um maior que o intervalo inicial. Tente novamente.");
-                        }
-                    } else {
-                        System.err.println(
-                                "Erro: O número deve ser um inteiro positivo (maior que zero). Tente novamente.");
-                    }
-                } else {
-                    System.err.println("Erro: Entrada inválida. Por favor, digite um número inteiro.");
-                    scanner.next();
-                }
-
-            } catch (InputMismatchException e) {
-                System.err.println("Erro inesperado durante a leitura. Tente novamente.");
-                scanner.nextLine(); // Limpa o buffer
-            }
-        }
-        scanner.nextLine();
-        return numeroLido;
+  List<Integer> encontrarPalindromos(int intervaloInicial, int intervaloFinal) {
+    isValidInput(intervaloInicial, intervaloFinal);
+    palindromos = new ArrayList<>();
+    for (int i = intervaloInicial; i <= intervaloFinal; i++) {
+      if (i < 10) {
+        palindromos.add(i);
+      } else if (isPalindromo(i)) {
+        palindromos.add(i);
+      }
     }
 
-    private boolean isPalindromo(int numeroAReverter) {
-        int original = numeroAReverter;
-        int numeroRevertido = 0;
-        while (numeroAReverter != 0) {
-            int ultimoDigito = numeroAReverter % 10; // Pega o último dígito
-            numeroRevertido = numeroRevertido * 10 + ultimoDigito; // Adiciona ao número reverso
-            numeroAReverter /= 10; // Remove o último dígito
-        }
-        return original == numeroRevertido;
+    return palindromos;
+  }
+
+  @Override
+  public java.lang.String toString() {
+    String resultadoFormatado =
+        String.join(", ", palindromos.stream().map(Object::toString).toList());
+    return resultadoFormatado;
+  }
+
+  public static void main(String[] args) {
+    int intervaloInicial = 0;
+    int intervaloFinal = 0;
+
+    if (args != null && args.length >= 1) {
+      try {
+        intervaloInicial = Integer.parseInt(args[0]);
+        intervaloFinal = Integer.parseInt(args[1]);
+      } catch (NumberFormatException e) {
+        System.err.println("Argumentos devem ser números inteiros válidos.");
+        System.err.println("Detalhe: " + e.getMessage());
+      }
     }
 
-    List<Integer> encontrarPalindromos(int intervaloInicial, int intervaloFinal) {
-        palindromos = new ArrayList<>();
-        for (int i = intervaloInicial; i <= intervaloFinal; i++) {
-            if (i < 10) {
-                palindromos.add(i);
-            } else if (isPalindromo(i)) {
-                palindromos.add(i);
-            }
-        }
-
-        return palindromos;
-    }
-
-    @java.lang.Override
-    public java.lang.String toString() {
-
-        String resultadoFormatado =
-                String.join(", ", palindromos.stream().map(Object::toString).toList());
-
-        return resultadoFormatado;
-    }
-
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("-------------- Desafio 03 - Os programadores --------------");
-        System.out.println("Encontrar números palindrômicos entre dois outros números");
-        System.out.println("-----------------------------------------------------------\n");
-        int intervaloInicial =
-                lerInteiroPositivoMaiorQueOMinimo(
-                        "Informe um número inteiro positivo como intervalo inicial: ", 1);
-        int intervaloFinal =
-                lerInteiroPositivoMaiorQueOMinimo(
-                        "Informe um número inteiro positivo maior ou igual a "
-                                + intervaloInicial
-                                + " como intervalo final: ",
-                        intervaloInicial);
-        Palindromos palindromos = new Palindromos(intervaloInicial, intervaloFinal);
-        System.out.println(palindromos);
-    }
+    Palindromos palindromos = new Palindromos(intervaloInicial, intervaloFinal);
+    System.out.println(palindromos);
+  }
 }
