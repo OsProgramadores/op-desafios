b5c00479c925c400e514a00fe9711db1c8eec1d4
KarineFBrito <karineferreiradebrito@gmail.com>
2026-01-19 10:51:34 -0300

    Código do desafio 12 completo

--- desafio-12/KarineFBrito/java/Potencia.java
+++ desafio-12/KarineFBrito/java/Potencia.java
@@ -1,12 +1,14 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Potencia{
     public static void main(String[] args){
-        
+
         if (args.length != 1) {
-          System.out.println(
-              "Nenhum caminho foi fornecido,  execute o programa usando 'java Fracoes"
-              + " <caminho-absoluto>'");
+            System.out.println(
+                    "Nenhum caminho foi fornecido,  execute o programa usando 'java Potencia"
+                            + " <caminho-absoluto>'");
             return;
         }
 
@@ -15,15 +17,21 @@ public class Potencia{
             System.out.println("Arquivo não encontrado.");
             return;
         }
-        try (BufferedReader br = new BufferedReader(new FileReader(caminho))){
-            String linha; 
-            while(linha = br.readLine != null){
+        try (Scanner sc = new Scanner(caminho)){
+            while(sc.hasNext()){
+                String linha = sc.next();
                 if(linha.trim().isEmpty()){
                     continue;
                 }
+                BigInteger n = new BigInteger(linha.trim());
+                boolean ehPotencia = n.signum() > 0 && n.and(n.subtract(BigInteger.ONE)).equals(BigInteger.ZERO);
+                if(ehPotencia){
+                    int expoente = n.bitLength() - 1;
+                    System.out.println(n + " " + ehPotencia+ " " +expoente);
+                }else {
+                    System.out.println(n + " " + ehPotencia);
+                }
             }
-
-
         } catch (IOException e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         }
