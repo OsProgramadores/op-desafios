08841c3e45a8f0712ada4e8817b372203ee1b397
KarineFBrito <karineferreiradebrito@gmail.com>
2026-01-20 14:42:52 -0300

    Atualização do código

--- desafio-12/KarineFBrito/java/Potencia.java
+++ desafio-12/KarineFBrito/java/Potencia.java
@@ -3,38 +3,35 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Potencia {
-  public static void main(String[] args) {
-
-    if (args.length != 1) {
-      System.out.println(
-          "Nenhum caminho foi fornecido,  execute o programa usando 'java Potencia"
-              + " <caminho-absoluto>'");
-      return;
-    }
-
-    File caminho = new File(args[0]);
-    if (!caminho.exists()) {
-      System.out.println("Arquivo não encontrado.");
-      return;
-    }
-    try (Scanner sc = new Scanner(caminho)) {
-      while (sc.hasNext()) {
-        String linha = sc.next().trim();
-        if (linha.isEmpty()) {
-          continue;
-        }
-        BigInteger n = new BigInteger(linha);
-        boolean ehPotencia =
-            n.signum() > 0 && n.and(n.subtract(BigInteger.ONE)).equals(BigInteger.ZERO);
-        if (ehPotencia) {
-          int expoente = n.bitLength() - 1;
-          System.out.println(n + " " + ehPotencia + " " + expoente);
-        } else {
-          System.out.println(n + " " + ehPotencia);
-        }
+    public static void main(String[] args) throws IOException{
+      if (args.length != 1) {
+          System.out.println(
+                  "Nenhum caminho foi fornecido,  execute o programa usando 'java Potencia"
+                          + " <caminho-absoluto>'");
+          return;
       }
-    } catch (IOException e) {
 
-    }
+      File caminho = new File(args[0]);
+      if (!caminho.exists()) {
+          System.out.println("Arquivo não encontrado.");
+          return;
+      }
+      try (Scanner sc = new Scanner(caminho)) {
+          while (sc.hasNext()) {
+              String linha = sc.next().trim();
+              if (linha.isEmpty()) {
+                  continue;
+              }
+              BigInteger numero  = new BigInteger(linha);
+              boolean ehPotencia =
+                      numero.signum() > 0 && numero.and(numero.subtract(BigInteger.ONE)).equals(BigInteger.ZERO);
+              if (ehPotencia) {
+                  int expoente = numero.bitLength() - 1;
+                  System.out.println(numero + " " + ehPotencia + " " + expoente);
+              } else {
+                  System.out.println(numero + " " + ehPotencia);
+              }
+          }
+      }
   }
 }
