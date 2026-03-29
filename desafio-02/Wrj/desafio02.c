7fec003d8c3e4cf6ce685b20b9398646b4c0b418
WRJ <wallace.joviano@hotmail.com>
2026-03-29 20:19:54 -0300

    Enviando o algoritmo do desafio 2 para revisão

--- desafio-02/Wrj/desafio02.c
+++ desafio-02/Wrj/desafio02.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 
-int main(){
-
-    for(int i = 2; i <= 10000; i++){
-
+int main() {
+    for (int i = 2; i <= 10000; i++) {
         int cont = 1;
 
-        for(int j = 2; j <= i; j++){
-            if((i % j) == 0){
+        for (int j = 2; j <= i; j++) {
+            if ((i % j) == 0) {
                 cont++;
             }
-            if(cont > 2){
+            if (cont > 2) {
                 break;
             }
         }
-        if(cont == 2){
+        if (cont == 2) {
             printf("%d\n", i);
         }
-
     }
-
 }
