58cf8dd17cdf9ec08328f10af2d9028843a91dab
MarcosVinciusCM <marcosviniciuscm16@gmail.com>
2026-04-27 16:06:09 -0300

    correção

--- desafio-03/MarcosViniciusCM/python/main.py
+++ desafio-03/MarcosViniciusCM/python/main.py
@@ -9,9 +9,10 @@ def num_palindromico(n, m):
                 palindromos.append(num)
     return palindromos
 
-# ex:
-n = 101
-m = 121
-resultado = num_palindromico(n, m)
-for num in resultado:
-    print(num)
+if __name__ == "__main__":
+    n_inicial = 101
+    m_final = 121
+    resultado = num_palindromico(n_inicial, m_final)
+
+    for numero in resultado:
+        print(numero)
