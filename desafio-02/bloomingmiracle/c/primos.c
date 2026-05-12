fe16f69b1c9fa39023c00a65b01b340cdbc574fa
Miracle Temitope Hazzan <bloomingMi01@gmail.com>
2026-05-12 08:42:16 -0300

    Melhora algoritmo e README do desafio 02.

--- desafio-02/bloomingmiracle/c/primos.c
+++ desafio-02/bloomingmiracle/c/primos.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
 int main() {
+
     int n = 10000;
+
+    int primo[10001];
+
+    for (int i = 0; i <= n; i++) {
+        primo[i] = 1;
+    }
+
+    primo[0] = 0;
+    primo[1] = 0;
+
+    for (int i = 2; i * i <= n; i++) {
+
+        if (primo[i] == 1) {
+
+            for (int j = i * i; j <= n; j += i) {
+                primo[j] = 0;
+            }
+        }
+    }
 
     for (int i = 2; i <= n; i++) {
 
-        int prime = 1;
-
-        for (int j = 2; j < i; j++) {
-
-            if (i % j == 0) {
-                prime = 0;
-                break;
-            }
-        }
-
-        if (prime == 1) {
+        if (primo[i] == 1) {
             printf("%d\n", i);
         }
     }
