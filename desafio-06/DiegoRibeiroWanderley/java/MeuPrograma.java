221258faee9ed35310d50fe3d0ba892ccf51af05
Diego Ribeiro Wanderley <eliwandejp@gmail.com>
2026-05-15 15:45:56 -0300

    Desafio 6 -Java

---
+++ desafio-06/DiegoRibeiroWanderley/java/MeuPrograma.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MeuPrograma {
+    public static void main(String[] args) {
+
+        if (args.length == 0) return;
+
+        String palavra = args[0].toUpperCase().replace(" ", "");
+        if (!palavra.matches("[A-Z]+")) {
+            System.out.println("ERRO: Caracteres inválidos.");
+            System.exit(1);
+        }
+
+        int[] freqPalavra = gerarFrequencia(palavra);
+        List<String> dicionarioFiltrado = gerarDicionario("words.txt", freqPalavra);
+        procurarAnagramas(freqPalavra, dicionarioFiltrado, 0, new ArrayList<>(), palavra.length());
+    }
+
+    public static int[] gerarFrequencia(String palavra) {
+        char[] palavraArray = palavra.toCharArray();
+        int[] freqPalavra = new int[26];
+
+        for (char c : palavraArray) {
+            if (c >= 'A' && c <= 'Z') {
+                freqPalavra[c - 'A']++;
+            }
+        }
+
+        return freqPalavra;
+    }
+
+    public static List<String> gerarDicionario(String caminho, int[] freqPalavra) {
+        List<String> dicionario = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String linha = br.readLine();
+
+            while (linha != null) {
+                int[] freqLinha = gerarFrequencia(linha);
+                boolean cabeNoEstoque = cabeNoEstoque(freqPalavra, freqLinha);
+                if (cabeNoEstoque) dicionario.add(linha);
+
+                linha = br.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("ERRO: O arquivo não existe ou seu caminho está errado");
+            System.exit(1);
+        }
+
+        return dicionario;
+    }
+
+    public static boolean cabeNoEstoque(int[] freqPalavra, int[] freqLinha) {
+        for (int i = 0; i < 26; i++) {
+            if (freqPalavra[i] - freqLinha[i] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void procurarAnagramas(
+            int[] freqPalavra,
+            List<String> dicionario,
+            int comeco,
+            List<String> atual,
+            int letrasRestantes) {
+        if (letrasRestantes == 0) {
+            List<String> resultado = new ArrayList<>(atual);
+            Collections.sort(resultado);
+            System.out.println(String.join(" ", resultado));
+            return;
+        }
+
+        for (int i = comeco; i < dicionario.size(); i++) {
+            String palavra = dicionario.get(i);
+            int[] freqP = gerarFrequencia(palavra);
+
+            if (cabeNoEstoque(freqPalavra, freqP)) {
+                for (int j = 0; j < 26; j++) freqPalavra[j] -= freqP[j];
+                atual.add(palavra);
+
+                procurarAnagramas(freqPalavra, dicionario, i, atual, letrasRestantes - palavra.length());
+
+                atual.remove(atual.size() - 1);
+                for (int j = 0; j < 26; j++) freqPalavra[j] += freqP[j];
+            }
+        }
+    }
+}
---
+++ desafio-06/DiegoRibeiroWanderley/java/README.md
@@ -0,0 +1,58 @@
+# Anagramas
+
+Esse programa recebe do usuário uma palavra qualquer e, dentre uma lista de
+palavras, faz todos os anagramas possíveis da palavra fornecida
+
+Anagrama é um rearranjo de palavras, tal palavra é anagrama de outra se possuir
+as mesmas letras, porém em ordem diferentes.
+
+Importante ressaltar que esse algoritmo não considera espaços como impedimento
+para formar anagramas
+
+# Como funciona
+
+1. Com a palavra fornecida pelo usuário, é criado um "estoque" de letras contidas na String, ou seja, a quantidade de cada letra na palavra
+2. Após isso, é percorrido todo o arquivo de palavras e são selecionadas aquelas que cabem no estoque da palavra escolhida, essas entram no dicionário
+3. Depois, usando recursividade, os anagramas são procurados
+4. Para cada palavra do dicionário é verificado se ela cabe no estoque da palavra, se sim adiciona ela em uma lista
+5. Depois parte para a próxima do dicionário, adiciona na lista e verifica novamente o estoque
+6. Se não tiver estoque, a última palavra da lista é removida e repete o processo
+7. Se o estoque chegar em 0, significa que o anagrama foi encontrado
+8. Printa o anagrama na tela
+
+# Tecnologias utilizadas
+
+Linguagem: Java 11+
+
+# Execução
+
+Para rodar o programa, basta seguir os seguintes passos
+
+1. Esteja no diretório onde a classe se encontra
+2. É importante que o arquivo com as palavras esteja no mesmo diretório, se chame `words.txt` e que possua uma palavra por linha
+4. Rode no terminal:
+```bash
+$ java MeuPrograma.java palavra
+```
+# Saída
+
+Ao executar o programa você verá uma lista dos possíveis anagramas
+
+Ex.:
+```
+$ java MeuPrograma.java anagrama
+```
+```java
+A A AM RANG
+A A ARM NAG
+A A MA RANG
+A A MAN RAG
+A A MAR NAG
+A A NAG RAM
+A AM AN RAG
+A AMRA NAG
+A AN MA RAG
+A ANAGRAM
+A MANA RAG
+AM ANA RAG
+ANA MA RAG
