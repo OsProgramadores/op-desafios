d4055bb9668150815d33f7e399bc2360dfb2f168
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2026-05-15 09:07:35 -0300

    feat: Implementado a terceira atividade de números palidrômicos

---
+++ desafio-03/joaoVitor-amaro/Palindromo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Palindromo {
+    public static boolean isPalidromo(long num) {
+        String numStr = String.valueOf(num);
+        StringBuilder strInverso = new StringBuilder(numStr).reverse();
+        return numStr.equals(strInverso.toString());
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long inicial, fim;
+        System.out.print("Escolha o numero inicial: ");
+        inicial = scan.nextLong();
+        System.out.print("Escolha o numero final: ");
+        fim = scan.nextLong();
+        if (inicial < 0 || fim < 0) {
+            System.out.println("Não podem numeros negativos");
+            scan.close();
+            return;
+        }
+
+        if(inicial > fim) {
+            System.out.println("O inicio não pode ser maior");
+            scan.close();
+            return;
+        }
+
+        for(long i = inicial; i <= fim; i++) {
+            if(Palindromo.isPalidromo(i)) {
+                System.out.println(i);
+            }
+        }
+        scan.close();
+    }
+}
---
+++ desafio-03/joaoVitor-amaro/README.md
@@ -0,0 +1,17 @@
+# Desafio 3 - Números Palidrômicos
+
+## Solução
+
+Foi criado uma função que converte os numeros em string
+para fazer a comparação entre o número original com o invertido 
+para verificar se é palidrômico e será imprimido
+esse número palidrômico.
+
+## executar
+
+```bash
+javac Palindromo.java
+java Palindromo
+```
+
+Foi utilizado o Java 21.
