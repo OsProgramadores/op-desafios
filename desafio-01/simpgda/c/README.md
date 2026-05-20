21b6b60f1930c351a57b86d4bcd5731ab9593857
Gabriel <simplachb@gmail.com>
2026-05-20 19:57:23 -0300

    Solução do desafio 01 em C

---
+++ desafio-01/simpgda/c/README.md
@@ -0,0 +1,10 @@
+# Desafio 01 - Números Primos
+
+Solução do Desafio 01 escrita em C para listar todos os números primos entre 1 e 10000.
+
+## Como compilar e executar
+
+No terminal, execute o comando abaixo para compilar o código e gerar o executável:
+
+```bash
+gcc primos.c -o primos
---
+++ desafio-01/simpgda/c/primos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, primo;
+    
+    printf("Numeros primos entre 1 e 10000:\n");
+    printf("2\n"); // 2 é o único primo par
+    
+    // Começa do 3 e pula números pares
+    for (i = 3; i <= 10000; i += 2) {
+        primo = 1;
+        for (j = 3; j * j <= i; j += 2) { // só testa divisores ímpares
+            if (i % j == 0) {
+                primo = 0;
+                break;
+            }
+        }
+        if (primo) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
