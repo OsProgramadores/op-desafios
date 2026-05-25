5a3fbb4f5716e5e7b37216a4701b10653ab0e509
Lucas Nunes <lucasnunes78434@gmail.com>
2026-05-24 21:41:44 -0300

    Corrige estrutura do desafio

---
+++ desafio-02/LucasNunes/python/python.py
@@ -0,0 +1,15 @@
+limite = 10000
+
+numeros_primos = [True] * (limite + 1)
+
+numeros_primos[0] = False
+numeros_primos[1] = False
+
+for numero in range(2, limite + 1):
+    if numeros_primos[numero] == True:
+        for multiplo in range(numero * 2, limite + 1, numero):
+            numeros_primos[multiplo] = False
+
+for numero in range(2, limite + 1):
+    if numeros_primos[numero] == True:
+        print(numero)
