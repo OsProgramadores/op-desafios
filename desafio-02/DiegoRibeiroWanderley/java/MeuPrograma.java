cfcef8e90799631f3062aa99f8f4f3f16b1b0a1d
Diego Ribeiro Wanderley <eliwandejp@gmail.com>
2026-06-06 16:00:40 -0300

    Otimizado implementação do Crivo de Eratóstenes

--- desafio-02/DiegoRibeiroWanderley/java/MeuPrograma.java
+++ desafio-02/DiegoRibeiroWanderley/java/MeuPrograma.java
@@ -1,21 +1,21 @@
+import java.util.Arrays;
+
 public class MeuPrograma {
   public static void main(String[] args) {
-    int[] primos = new int[10000 + 1];
-    for (int i = 2; i <= 10000; i++) {
-      primos[i] = i;
-    }
+    boolean[] primos = new boolean[10000 + 1];
+    Arrays.fill(primos, true);
 
     for (int i = 2; i * i <= 10000; i++) {
-      if (primos[i] != -1) {
+      if (primos[i]) {
         for (int j = i * i; j <= 10000; j += i) {
-          primos[j] = -1;
+          primos[j] = false;
         }
       }
     }
 
-    for (int i = 2; i < 10000 - 2; i++) {
-      if (primos[i] != -1) {
-        System.out.println(primos[i]);
+    for (int i = 2; i <= 10000; i++) {
+      if (primos[i]) {
+        System.out.println(i);
       }
     }
   }
