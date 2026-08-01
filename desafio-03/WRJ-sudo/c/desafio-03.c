30093b7cb26fee90cc4127b63e19617758ee3a19
WRJ <wallace.joviano@hotmail.com>
2026-08-01 20:04:39 -0300

    adiciona uma verifição de execução e verifica se o caractere é válido

--- desafio-03/WRJ-sudo/c/desafio-03.c
+++ desafio-03/WRJ-sudo/c/desafio-03.c
@@ -3,17 +3,64 @@
 
 #define TAMANHO 20 //tamanho máximo de caracteres mais o '\0'
 
+long long int inicial;
+long long int final;
+
+int programa();
+int primeiro_num();
+int segundo_num();
+
 int main() {
-   const long long int num_inicial, limite;
+
+    int excecao;
+
+
+        printf("Para continuar com a busca de palindromos: Digite 1\n");
+        printf("\n");
+        printf("Para encerrar o programa: Digite 0\n");
+        printf("\n");
+        scanf("%d",&excecao);
+
+            switch(excecao){
+            case 0: return 0;
+            case 1: programa();
+                    break;
+            default: return 0;
+
+            }
+
+        return 0;
+
+}
+
+int programa(){
+    int  temp_exe=1;
     long long int contador;
     char str_lida[TAMANHO] = {0};
     char str_a_acomparar[TAMANHO];
     int aux;
 
-    // pega os valores da entrada
-    scanf("%lld", &num_inicial);
-    scanf("%lld", &limite);
+    while(temp_exe){
 
+        if(primeiro_num()){
+            temp_exe = 0;
+        }else{
+            return 0;
+        }
+
+    }
+    const long long int num_inicial = inicial ;
+    temp_exe = 1;
+    while(temp_exe){
+
+        if(segundo_num()){
+            temp_exe = 0;
+        }else{
+            return 0;
+        }
+
+    }
+    const long long int limite = final;
     contador = num_inicial;
 
     for (contador; contador <= limite; contador++) {
@@ -51,4 +98,35 @@ int main() {
             printf("%s\n", str_lida);
         }
     }
+}
+
+int primeiro_num(){
+
+    long long numero;
+    printf("Digite um numero inicial para a busca: ");
+
+    if (scanf("%lld", &numero) == 1) {
+        inicial = numero;
+        return 1;
+    }else {
+        printf("Entrada invalida!\n");
+        return 0;
+    }
+
+}
+
+int segundo_num(){
+
+    long long numero;
+
+    printf("Digite um numero para o limite da busca: ");
+
+    if (scanf("%lld", &numero) == 1) {
+        final = numero;
+        return 1;
+    }else {
+        printf("Entrada invalida!\n");
+        return 0;
+    }
+
 }
