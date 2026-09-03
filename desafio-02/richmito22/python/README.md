7d5e014e0c4a263a5848eb10fb2c528117f79742
Richardson <richardsonmachado2017@gmail.com>
2026-09-03 18:12:19 -0300

    Corrige solicitação do review

--- desafio-02/richmito22/python/README.md
+++ desafio-02/richmito22/python/README.md
@@ -1,4 +1,4 @@
-# Números primos
+## Descrição
 
 Programa em Python que lista todos os números primos entre 1 e 10000.
 
@@ -6,7 +6,12 @@ Programa em Python que lista todos os números primos entre 1 e 10000.
 
 O programa verifica os números de 2 a 10000 e identifica quais são primos.
 
-Ele testa possíveis divisores somente até a raiz quadrada de cada número.
+Ele testa possíveis divisores somente até a raiz quadrada de cada número. Apesar de exisitr algoritmo mais eficiente, essa foi a forma mais próxima do  meu conhecimento prévio de programação.
+
+## Versão
+
+Versão utilizada: Python 3.14.7
+
 
 ## Como executar
 
--- desafio-02/richmito22/python/prime_numbers.py
+++ desafio-02/richmito22/python/prime_numbers.py
@@ -8,7 +8,8 @@ for i in range(2,10001):    #Percorre números de 2 até 10.000(Número 1 não �
     for x in range(2, raiz_quadrada +1):#percorre raiz_quadrada do loop. Início: 2, +1 no final.
 
         if i % x == 0:    #verifico se o resto da divisão entre i e x é igual a 0.
-            total_divisores += 1    #Incremento 1 no contador, caso o primeiro if seja verdadeiro.
-
-    if total_divisores == 0:    #Condição para saber se o contador está zerado.
-        print(i)    #Mostrar númmeros do primeiro for, caso sejam primos.
+            total_divisores += 1    #Incremento 1 no contador, caso o if seja verdadeiro.
+            break #Não preciso continuar no loop, se já encontrei o divisor.
+        
+    else:
+        print(i)    #Imprimir números que não foram encontrados divisores.
