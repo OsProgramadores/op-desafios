60c061c30c1e950b35e59b49fc9a72c70e9c01a9
Richardson <richardsonmachado2017@gmail.com>
2026-09-05 18:29:11 -0300

    Corrigi o espaço do Linter

--- desafio-02/richmito22/python/prime_numbers.py
+++ desafio-02/richmito22/python/prime_numbers.py
@@ -8,6 +8,6 @@ for i in range(2,10001):    #Percorre números de 2 até 10.000(Número 1 não �
 
         if i % x == 0:    #verifico se o resto da divisão entre i e x é igual a 0.
             break #Não preciso continuar no loop, se já encontrei o divisor.
-        
+
     else:
         print(i)    #Imprimir números que não foram encontrados divisores.
